1978b9ca82be71d534b45170147cf27fdda85b64
gwt.team.knorton <gwt.team.knorton@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2006-12-07 19:31:15 +0000

    ordercheck passes.
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@32 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- dev/core/src/com/google/gwt/dev/jjs/ast/js/JsniMethod.java
+++ dev/core/src/com/google/gwt/dev/jjs/ast/js/JsniMethod.java
@@ -1,4 +1,18 @@
-// Copyright 2006 Google Inc. All Rights Reserved.
+/*
+ * Copyright 2006 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.gwt.dev.jjs.ast.js;
 
 import com.google.gwt.dev.jjs.ast.JMethod;
@@ -13,28 +27,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A Java native method that is implemented in JSNI code.  
+ * A Java native method that is implemented in JSNI code.
  */
 public class JsniMethod extends JMethod {
 
+  public final List/* <JsniFieldRef> */jsniFieldRefs = new ArrayList/* <JsniFieldRef> */();
+  public final List/* <JsniMethodRef> */jsniMethodRefs = new ArrayList/* <JsniMethodRef> */();
   private JsFunction jsFunction = null;
-  public final List/*<JsniFieldRef>*/ jsniFieldRefs = new ArrayList/*<JsniFieldRef>*/();
-  public final List/*<JsniMethodRef>*/ jsniMethodRefs = new ArrayList/*<JsniMethodRef>*/();
 
   public JsniMethod(JProgram program, String name,
       JReferenceType enclosingType, JType returnType, boolean isStatic,
       boolean isFinal, boolean isPrivate) {
     super(program, name, enclosingType, returnType, false, isStatic, isFinal,
-      isPrivate);
-  }
-
-  public boolean isNative() {
-    return true;
+        isPrivate);
   }
 
   public JsFunction getFunc() {
     assert (this.jsFunction != null);
     return jsFunction;
+  }
+
+  public boolean isNative() {
+    return true;
   }
 
   public void setFunc(JsFunction jsFunction) {
