c0ff76aab6cba028154f7258caf211126d87d9ab
gwt.team.mmendez <gwt.team.mmendez@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2007-04-18 16:09:25 +0000

    Fixed assertion to allow capacityIncrement to be zero.  Growth logic will double size if capacityIncrement is zero.
    Review by: scottb (desk check)
    
    
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@901 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/server/rpc/impl/CharVector.java
+++ user/src/com/google/gwt/user/server/rpc/impl/CharVector.java
@@ -25,36 +25,38 @@ class CharVector {
 
   public CharVector(int initialCapacity, int capacityIncrement) {
     assert (initialCapacity >= 0);
-    assert (capacityIncrement > 0);
-    
+    assert (capacityIncrement >= 0);
+
     this.capacityIncrement = capacityIncrement;
     chars = new char[initialCapacity];
   }
 
   public void add(char ch) {
-    if (size + 1 > chars.length) {
-      char newChars[] = new char[chars.length + capacityIncrement];
+    if (size >= chars.length) {
+      int growBy = (capacityIncrement == 0) ? chars.length * 2
+          : capacityIncrement;
+      char newChars[] = new char[(chars.length + growBy)];
       System.arraycopy(chars, 0, newChars, 0, size);
       chars = newChars;
     }
-    
+
     chars[size++] = ch;
   }
 
   public char[] asArray() {
     return chars;
   }
-  
+
   public char get(int index) {
     assert (index < size);
-    
+
     return chars[index];
   }
 
   public int getSize() {
     return size;
   }
-  
+
   public void set(int index, char ch) {
     assert (index >= 0 && index < size);
 
