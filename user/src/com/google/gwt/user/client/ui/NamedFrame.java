66a499dc8ee9c4b537105cbf4a2549248e9f25ec
gwt.team.bruce <gwt.team.bruce@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2007-04-19 19:22:07 +0000

    Increases the strictness of the 'name' argument for the NamedFrame to disallow HTML escape chars in the name.
    Review by: jgw
    Change by: bruce
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@926 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/client/ui/NamedFrame.java
+++ user/src/com/google/gwt/user/client/ui/NamedFrame.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 Google Inc.
+ * Copyright 2007 Google Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.user.client.ui;
 
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
 
@@ -25,12 +26,43 @@ import com.google.gwt.user.client.Element;
  */
 public class NamedFrame extends Frame {
 
+  // Used inside JSNI, so please don't delete this field just because
+  // your compiler or IDE says it's unused.
+  private static JavaScriptObject PATTERN_NAME;
+
+  static {
+    initStatics();
+  }
+
+  private static native void initStatics() /*-{
+    @com.google.gwt.user.client.ui.NamedFrame::PATTERN_NAME = /^[^<>&\'\"]+$/;
+  }-*/;
+
+  /**
+   * @param name the specified frame name to be checked
+   * @return <code>true</code> if the name is valid, <code>false</code> if
+   *         not
+   */
+  private static native boolean isValidName(String name) /*-{
+    return @com.google.gwt.user.client.ui.NamedFrame::PATTERN_NAME.test(name);
+  }-*/;
+
   /**
    * Constructs a frame with the given name.
    * 
-   * @param name the name of the frame
+   * @param name the name of the frame, which must contain at least one
+   *          non-whitespace character and must not contain reserved HTML markup
+   *          characters such as '<code>&lt;</code>', '<code>&gt;</code>',
+   *          or '<code>&amp;</code>'
+   *          
+   * @throws IllegalArgumentException if the supplied name is not allowed 
    */
   public NamedFrame(String name) {
+    if (name == null || !isValidName(name.trim())) {
+      throw new IllegalArgumentException(
+          "expecting one or more non-whitespace chars with no '<', '>', or '&'");
+    }
+
     // Use innerHTML to implicitly create the <iframe>. This is necessary
     // because most browsers will not respect a dynamically-set iframe name.
     Element div = DOM.createDiv();
