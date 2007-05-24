ee53e2e631b6d34643f596075dff5f0c9e26dba5
gwt.team.scottb <gwt.team.scottb@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2007-05-24 22:08:06 +0000

    Fixed imports.
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@1136 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/test/com/google/gwt/user/server/ui/FormPanelTestServlet.java
+++ user/test/com/google/gwt/user/server/ui/FormPanelTestServlet.java
@@ -23,8 +23,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gwt.user.client.ui.FormPanelTest;
-
 /**
  * Servlet for use with {@link FormPanelTest}.
  */
