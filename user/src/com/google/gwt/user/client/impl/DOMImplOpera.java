b42442ac6f642631397ce21c28093622f0eff134
gwt.team.knorton <gwt.team.knorton@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2007-05-25 02:24:25 +0000

    Fixes Issue #852. Adds an Opera-specific implementations of getAbsoluteTop & getAbsoluteLeft which provides more reliable results by accounting for Opera's inaccurate scrollTop & scrollLeft on TR's and TBODY's.
    Patch by: bobv
    Review by: knorton
    
    
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@1140 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/client/impl/DOMImplOpera.java
+++ user/src/com/google/gwt/user/client/impl/DOMImplOpera.java
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.user.client.impl;
 
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Event;
 
 /**
@@ -37,4 +38,45 @@ public class DOMImplOpera extends DOMImplStandard {
     return evt.detail * 4;
   }-*/;
 
+  public native int getAbsoluteLeft(Element elem) /*-{
+    var left = 0;
+    var curr = elem.parentNode;
+    // This intentionally excludes body
+    while (curr != $doc.body) {
+
+      // see https://bugs.opera.com/show_bug.cgi?id=249965
+      // The net effect is that TR and TBODY elemnts report the scroll offsets
+      // of the BODY and HTML elements instead of 0.
+      if (curr.tagName != 'TR' && curr.tagName != 'TBODY') {
+        left -= curr.scrollLeft;
+      }
+      curr = curr.parentNode;
+    }
+
+    while (elem) {
+      left += elem.offsetLeft;
+      elem = elem.offsetParent;
+    }
+    return left;
+  }-*/;
+
+  public native int getAbsoluteTop(Element elem) /*-{
+    var top = 0;
+
+    // This intentionally excludes body
+    var curr = elem.parentNode;
+    while (curr != $doc.body) {
+      // see getAbsoluteLeft()
+      if (curr.tagName != 'TR' && curr.tagName != 'TBODY') {
+        top -= curr.scrollTop;
+      }
+      curr = curr.parentNode;
+    }
+
+    while (elem) {
+      top += elem.offsetTop;
+      elem = elem.offsetParent;
+    }
+    return top;
+  }-*/;
 }
