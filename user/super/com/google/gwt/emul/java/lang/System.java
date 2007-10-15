73e345b02249fd984fd5c2c0639828f68caba129
gwt.team.jat <gwt.team.jat@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2007-10-15 18:52:26 +0000

    Implement System.arraycopy and its associated test.
    Issue: 1012
    Patch by: jat
    Review by: scottb
    
    
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@1473 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/super/com/google/gwt/emul/java/lang/System.java
+++ user/super/com/google/gwt/emul/java/lang/System.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 Google Inc.
+ * Copyright 2007 Google Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -14,6 +14,8 @@
  * the License.
  */
 package java.lang;
+
+import com.google.gwt.core.client.GWT;
 
 import java.io.PrintStream;
 
@@ -36,6 +38,51 @@ public final class System {
    */
   public static final PrintStream out = new PrintStream(null);
 
+  public static void arraycopy(Object src, int srcOfs, Object dest, int destOfs, int len) {
+    if (src == null || dest == null) {
+      throw new NullPointerException();
+    }
+    String srcTypeName = GWT.getTypeName(src); 
+    String destTypeName = GWT.getTypeName(dest);
+    if (srcTypeName.charAt(0) != '[' || destTypeName.charAt(0) != '[') {
+      throw new ArrayStoreException("Must be array types");
+    }
+    if (srcTypeName.charAt(1) != destTypeName.charAt(1)) {
+      throw new ArrayStoreException("Array types must match");
+    }
+    int srclen = getArrayLength(src);
+    int destlen = getArrayLength(dest);
+    if (srcOfs < 0 || destOfs < 0 || len < 0 || srcOfs + len > srclen || destOfs + len > destlen) {
+      throw new IndexOutOfBoundsException();
+    }
+    /*
+     * If the arrays are not references or if they are exactly the same type,
+     * we can copy them in native code for speed.  Otherwise, we have to copy
+     * them in Java so we get appropriate errors.
+     */ 
+    if ((srcTypeName.charAt(1) == 'L' || srcTypeName.charAt(1) == '[') &&
+        !srcTypeName.equals(destTypeName)) {
+      // copy in Java to make sure we get ArrayStoreExceptions if the values
+      // aren't compatible
+      Object[] srcArray = (Object[]) src;
+      Object[] destArray = (Object[]) dest;
+      if (src == dest && srcOfs < destOfs) {
+        // TODO(jat): how does backward copies handle failures in the middle?
+        // copy backwards to avoid destructive copies
+        srcOfs += len;
+        for (int destEnd = destOfs + len; destEnd-- > destOfs; ) {
+          destArray[destEnd] = srcArray[--srcOfs];
+        }
+      } else {
+        for (int destEnd = destOfs + len; destOfs < destEnd; ) {
+          destArray[destOfs++] = srcArray[srcOfs++];
+        }
+      }
+    } else {
+      nativeArraycopy(src, srcOfs, dest, destOfs, len);
+    }
+  }
+  
   public static native long currentTimeMillis() /*-{
     return (new Date()).getTime();
   }-*/;
@@ -58,6 +105,29 @@ public final class System {
 
   public static native void setOut(PrintStream out) /*-{
     @java.lang.System::out = out;
+  }-*/;
+
+  /**
+   * Returns the length of an array via Javascript.
+   */
+  private static native int getArrayLength(Object array) /*-{
+    return array.length;
+  }-*/;
+
+  /**
+   * Copy an array using native Javascript. The destination array must be a real
+   * Java array (ie, already has the GWT type info on it).  No error checking is
+   * performed -- the caller is expected to have verified everything first.
+   * 
+   * @param src source array for copy
+   * @param srcOfs offset into source array
+   * @param dest destination array for copy
+   * @param destOfs offset into destination array
+   * @param len number of elements to copy
+   */
+  private static native void nativeArraycopy(Object src, int srcOfs, Object dest, int destOfs,
+      int len) /*-{
+    Array.prototype.splice.apply(dest, [destOfs, len].concat(src.slice(srcOfs, srcOfs + len)));
   }-*/;
 
 }
