d71ef826187925c731842cb211f6d1d86b804c13
gwt.team.ecc <gwt.team.ecc@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2007-11-13 15:23:24 +0000

    Applying GWT formatter
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@1527 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/client/impl/DOMImplMozilla.java
+++ user/src/com/google/gwt/user/client/impl/DOMImplMozilla.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -75,7 +75,7 @@ class DOMImplMozilla extends DOMImplStandard {
     return $doc.getBoxObjectFor(elem).screenY
         - $doc.getBoxObjectFor($doc.documentElement).screenY;
   }-*/;
-  
+
   @Override
   public native int getChildIndex(Element parent, Element toFind) /*-{
     var count = 0, child = parent.firstChild;
