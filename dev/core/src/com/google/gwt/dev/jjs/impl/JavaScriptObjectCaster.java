rstudio/rstudio

commit 0cfb529c639668b22e2064e752f7254840c06171

Removes unnecessary wrapJSO calls when the JavaScriptObject is being passed into JavaScript.
Review by: jgw (desk check)


git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@1555 8db76d5a-ed1c-0410-87a9-c151d255dfc7

dev/core/src/com/google/gwt/dev/jjs/impl/JavaScriptObjectCaster.java
@@ -32,6 +32,7 @@ import com.google.gwt.dev.jjs.ast.JReferenceType;
 import com.google.gwt.dev.jjs.ast.JReturnStatement;
 import com.google.gwt.dev.jjs.ast.JType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -98,20 +99,35 @@ public class JavaScriptObjectCaster {
 
     @Override
     public void endVisit(JMethodCall x, Context ctx) {
-      for (int i = 0; i < x.getTarget().params.size(); ++i) {
-        JParameter param = x.getTarget().params.get(i);
-        JExpression newArg = checkAndReplaceJso(x.getArgs().get(i),
-            param.getType());
-        x.getArgs().set(i, newArg);
+      // Check implicit assignments from argument and instance passing.
+
+      ArrayList<JExpression> args = x.getArgs();
+      JMethod target = x.getTarget();
+      /*
+       * Check arguments for calls to non-native methods. Do not check native
+       * invocations because we're losing all static type information anyway.
+       */
+      if (!target.isNative()) {
+        for (int i = 0; i < target.params.size(); ++i) {
+          JParameter param = target.params.get(i);
+          JExpression arg = args.get(i);
+          JExpression newArg = checkAndReplaceJso(arg, param.getType());
+          this.didChange |= (newArg != arg);
+          args.set(i, newArg);
+        }
       }
-      if (!x.getTarget().isStatic()) {
-        // for polymorphic calls, force wrapping
+
+      /*
+       * Virtual calls *require* wrapping to dispatch correctly. This should be
+       * a rare case since most call should get statically resolved already.
+       */
+      if (!target.isStatic()) {
         JExpression newInst = checkAndReplaceJso(x.getInstance(),
             program.getTypeJavaLangObject());
         if (newInst != x.getInstance()) {
           JMethodCall newCall = new JMethodCall(program, x.getSourceInfo(),
-              newInst, x.getTarget(), x.isStaticDispatchOnly());
-          newCall.getArgs().addAll(x.getArgs());
+              newInst, target, x.isStaticDispatchOnly());
+          newCall.getArgs().addAll(args);
           ctx.replaceMe(newCall);
         }
       }

commit 1b5bd9284c46de639274d7fb62a2b9fe7402c7a2

Fixed for problems caused by r1555.  The problem was that doing an arrayList.add(Element) would not wrap the element going into JSNI, and the object would fail a dynamic type check upon retrieval.
The solution is to actually do the wrapping exactly when the declared type of the parameter passing into JSNI is not JavaScriptObject or a subclass.  We comply by making it a valid Java Object.  If the declared type is JSO or a subclass, then there's no reason to wrap.

git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@1571 8db76d5a-ed1c-0410-87a9-c151d255dfc7

dev/core/src/com/google/gwt/dev/jjs/impl/JavaScriptObjectCaster.java
@@ -103,18 +103,14 @@ public class JavaScriptObjectCaster {
 
       ArrayList<JExpression> args = x.getArgs();
       JMethod target = x.getTarget();
-      /*
-       * Check arguments for calls to non-native methods. Do not check native
-       * invocations because we're losing all static type information anyway.
-       */
-      if (!target.isNative()) {
-        for (int i = 0; i < target.params.size(); ++i) {
-          JParameter param = target.params.get(i);
-          JExpression arg = args.get(i);
-          JExpression newArg = checkAndReplaceJso(arg, param.getType());
-          this.didChange |= (newArg != arg);
-          args.set(i, newArg);
-        }
+
+      for (int i = 0; i < target.params.size(); ++i) {
+        JParameter param = target.params.get(i);
+        JExpression arg = args.get(i);
+        JExpression newArg = checkAndReplaceJso(arg, param.getType(),
+            target.isNative());
+        this.didChange |= (newArg != arg);
+        args.set(i, newArg);
       }
 
       /*
@@ -168,10 +164,15 @@ public class JavaScriptObjectCaster {
       return true;
     }
 
+    private JExpression checkAndReplaceJso(JExpression arg, JType targetType) {
+      return checkAndReplaceJso(arg, targetType, false);
+    }
+
     /**
      * Wraps a JSO-typed argument if the target type is a different type.
      */
-    private JExpression checkAndReplaceJso(JExpression arg, JType targetType) {
+    private JExpression checkAndReplaceJso(JExpression arg, JType targetType,
+        boolean nowrapJso) {
       JType argType = arg.getType();
       if (argType == targetType) {
         return arg;
@@ -180,6 +181,15 @@ public class JavaScriptObjectCaster {
         return arg;
       }
       if (!program.isJavaScriptObject(argType)) {
+        return arg;
+      }
+      /*
+       * Special case: when calling a native method, only force a wrapping if
+       * the type is explicitly Object. As long as the target type is a JSO
+       * subclass, don't bother wrapping since we're losing type information
+       * anyway.
+       */
+      if (nowrapJso && program.isJavaScriptObject(targetType)) {
         return arg;
       }
       // Synthesize a cast to the arg type to force a wrap