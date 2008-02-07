48ee4c04dca90619afe02cebde21e95260cf58f6
scottb@google.com <scottb@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2008-02-07 17:52:39 +0000

    Removed unused function from r1793.
    Suggested by: bobv
    
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@1799 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- dev/core/src/com/google/gwt/dev/jjs/impl/Pruner.java
+++ dev/core/src/com/google/gwt/dev/jjs/impl/Pruner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 Google Inc.
+ * Copyright 2008 Google Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,7 +15,6 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
-import com.google.gwt.dev.jjs.ast.CanBeStatic;
 import com.google.gwt.dev.jjs.ast.Context;
 import com.google.gwt.dev.jjs.ast.JAbsentArrayDimension;
 import com.google.gwt.dev.jjs.ast.JArrayType;
@@ -366,11 +365,6 @@ public class Pruner {
         }
       }
       return false;
-    }
-
-    private boolean pruneViaNoninstantiability(boolean isInstantiated,
-        CanBeStatic it) {
-      return (!isInstantiated && !it.isStatic());
     }
   }
 
