6ef7147ef5dd71824bb21ff51b10848e938c6393
gwt.team.jat <gwt.team.jat@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2008-03-17 19:05:22 +0000

    Correct misleading documentation.
    Patch by: jat
    Review by: knorton
    
    
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@2109 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/client/ui/SimplePanel.java
+++ user/src/com/google/gwt/user/client/ui/SimplePanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 Google Inc.
+ * Copyright 2008 Google Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -22,7 +22,7 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
- * Abstract base class for panels that contain only one widget.
+ * Base class for panels that contain only one widget.
  */
 public class SimplePanel extends Panel {
 
