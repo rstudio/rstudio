27d808b3c4cfc73d6f99253b0668a939e7b73901
gwt.team.ecc <gwt.team.ecc@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2008-04-22 22:07:36 +0000

    Modified bug museum to allow bugs to be sliced and diced more easily by    a) maintaining  separate bug lists and customizable Museum entry points    b) adding a SuggestBox search ability    c) mandating bug summaries.    d) fixed css loading problem    e) added ability to check DOM after widgets are created Desk check by:jlabanca
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@2518 8db76d5a-ed1c-0410-87a9-c151d255dfc7

---
+++ reference/code-museum/src/com/google/gwt/museum/client/defaultmuseum/DefaultMuseum.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.gwt.museum.client.defaultmuseum;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.museum.client.viewer.Museum;
+
+/**
+ * Default bug museum. Contains a list of all GWT issues reported in the system
+ * to date.
+ */
+public class DefaultMuseum extends Museum implements EntryPoint {
+  public DefaultMuseum() {
+    addIssue(new Issue2290());
+    addIssue(new Issue2307());
+    addIssue(new Issue2321());
+  }
+}
