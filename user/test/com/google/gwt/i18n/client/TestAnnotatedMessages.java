25811b458dca1f08b11c4f6f1cc92250b3d6944e
jat@google.com <jat@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2008-10-27 17:38:53 +0000

    Fix missing Javadoc and whitespace.
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/releases/1.6@3867 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/test/com/google/gwt/i18n/client/TestAnnotatedMessages.java
+++ user/test/com/google/gwt/i18n/client/TestAnnotatedMessages.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2006 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -31,6 +31,12 @@ import java.util.Date;
 @Generate(format = "com.google.gwt.i18n.rebind.format.PropertiesFormat")
 public interface TestAnnotatedMessages extends Messages {
 
+  /**
+   * Test of property file lookup on nested classes.
+   *
+   * nestedDollar() is redefined in a property file with a $ in it.
+   * nestedUnderscore() is redefined in a property file with a _ in it.
+   */
   public interface Nested extends Messages {
 
     @DefaultMessage("nested dollar")
@@ -38,39 +44,39 @@ public interface TestAnnotatedMessages extends Messages {
 
     @DefaultMessage("nested underscore")
     String nestedUnderscore();
-}
+  }
 
   @DefaultMessage("Test me")
   String basicText();
-  
+
   @DefaultMessage("Once more, with meaning")
   @Meaning("Mangled quote")
   String withMeaning();
-  
+
   @DefaultMessage("One argument: {0}")
   String oneArgument(String value);
-  
+
   @DefaultMessage("One argument, which is optional")
   String optionalArgument(@Optional String value);
-  
+
   @DefaultMessage("Two arguments, {1} and {0}, inverted")
   String invertedArguments(String one, String two);
-  
+
   @DefaultMessage("Don''t tell me I can''t '{'quote things in braces'}'")
   String quotedText();
-  
+
   @DefaultMessage("This '{0}' would be an argument if not quoted")
   String quotedArg();
-  
+
   @DefaultMessage("Total is {0,number,currency}")
   String currencyFormat(double value);
-  
+
   @DefaultMessage("Default number format is {0,number}")
   String defaultNumberFormat(double value);
-  
+
   @DefaultMessage("It is {0,time,short} on {0,date,full}")
   String getTimeDate(Date value);
-  
+
   @DefaultMessage("{0} widgets")
   @PluralText({"one", "A widget"})
   String pluralWidgetsOther(@PluralCount int count);
