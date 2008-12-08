2762932992b4407a7f837feacbfa22dd40d5a59b
fabbott@google.com <fabbott@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2008-12-08 20:56:35 +0000

    typo correction; the ListenerWrapper for Trees should fire a StateChange rather than Selection for node open.  (Node close was correct.)
    Review by: ecc
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@4266 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/client/ui/ListenerWrapper.java
+++ user/src/com/google/gwt/user/client/ui/ListenerWrapper.java
@@ -460,7 +460,7 @@ abstract class ListenerWrapper<T> implements EventHandler {
     }
 
     public void onOpen(OpenEvent<TreeItem> event) {
-      listener.onTreeItemSelected(event.getTarget());
+      listener.onTreeItemStateChanged(event.getTarget());
     }
 
     public void onSelection(SelectionEvent<TreeItem> event) {
