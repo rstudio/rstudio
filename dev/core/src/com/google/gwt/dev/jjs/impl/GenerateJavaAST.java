a12cbc096d19d497daaf99249f8c690c2cbdb99c
scottb@google.com <scottb@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2009-03-07 07:55:34 +0000

    Removed unused imports.
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@4956 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- dev/core/src/com/google/gwt/dev/jjs/impl/GenerateJavaAST.java
+++ dev/core/src/com/google/gwt/dev/jjs/impl/GenerateJavaAST.java
@@ -185,17 +185,14 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 import java.util.TreeSet;
 
 /**
