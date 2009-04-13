d5e167dfde53b0a30840473806dcf58c88c13d3a
scottb@google.com <scottb@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2009-04-13 22:26:28 +0000

    Removing System.out.println hack we used to track down the flakey RPC test failure.
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@5222 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/server/rpc/impl/ServerSerializationStreamReader.java
+++ user/src/com/google/gwt/user/server/rpc/impl/ServerSerializationStreamReader.java
@@ -526,30 +526,13 @@ public final class ServerSerializationStreamReader extends
 
     } catch (ClassNotFoundException e) {
       throw new SerializationException(e);
-
     } catch (InstantiationException e) {
       throw new SerializationException(e);
-
     } catch (IllegalAccessException e) {
       throw new SerializationException(e);
-
     } catch (IllegalArgumentException e) {
       throw new SerializationException(e);
-
     } catch (InvocationTargetException e) {
-      /*
-       * HACK(scottb): temporary hack to print internal exceptions to the
-       * console while we try to pin down a flaky RPC test that fails very
-       * intermittently. The stack trace gets lost when we send this server
-       * exception back to the client, because we currently don't serialize
-       * cause and stacktrace across the wire. We end up with a useless
-       * client-side stacktrace that has no cause, and that's what JUnit sees.
-       * We can remove this spam if we serialize cause/stacktrace back to the
-       * client even if we haven't yet solve the flaky RPC test issue.
-       */
-      e.getTargetException().printStackTrace(System.out);
-      e.getTargetException().printStackTrace(System.err);
-
       throw new SerializationException(e.getTargetException());
     } catch (NoSuchMethodException e) {
       throw new SerializationException(e);
