cf057030d2ae271484f4c8ee896c79a9c7f206d6
amitmanjhi@google.com <amitmanjhi@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2009-06-01 23:12:37 +0000

    Adding diagnostic information when a MissingResourceException is thrown.
    Patch by: amitmanjhi
    Review by: jat
    
    
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@5487 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/i18n/client/Dictionary.java
+++ user/src/com/google/gwt/i18n/client/Dictionary.java
@@ -102,6 +102,7 @@ public final class Dictionary {
         null, name);
   }
 
+  private JavaScriptObject accessedKeys;
   private JavaScriptObject dict;
 
   private String label;
@@ -123,7 +124,12 @@ public final class Dictionary {
           "Cannot find JavaScript object with the name '" + name + "'", name,
           null);
     }
+    createAccessedKeysArray();
   }
+
+  private native void createAccessedKeysArray() /*-{
+    this.@com.google.gwt.i18n.client.Dictionary::accessedKeys = new Array();
+  }-*/;
 
   /**
    * Get the value associated with the given Dictionary key.
@@ -138,6 +144,12 @@ public final class Dictionary {
    */
   public native String get(String key) /*-{
    var value = this.@com.google.gwt.i18n.client.Dictionary::dict[key];
+   var keys = this.@com.google.gwt.i18n.client.Dictionary::accessedKeys;
+   keys.unshift(key);
+   // only keep the last 30 elements. Shrink it when array exceeds 60
+   if (keys.length > 60) {
+     keys.splice(30);
+   }
    if (value == null || !Object.prototype.hasOwnProperty.call(
        this.@com.google.gwt.i18n.client.Dictionary::dict, key))
    {
@@ -179,6 +191,7 @@ public final class Dictionary {
     if (s.size() < MAX_KEYS_TO_SHOW) {
       error += "\n keys found: " + s;
     }
+    error += "\n accessed keys: " + accessedKeys;
     throw new MissingResourceException(error, this.toString(), key);
   }
 
