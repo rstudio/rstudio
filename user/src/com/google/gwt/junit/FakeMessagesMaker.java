ad7b0e1e9102dae55baaa43e425398d6a382b60d
rjrjr@google.com <rjrjr@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2009-09-01 22:18:26 +0000

    Internal contribution that extends FakeMessagesMaker to work with Constants as well.
    review by: rjrjr
    
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@6063 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/junit/FakeMessagesMaker.java
+++ user/src/com/google/gwt/junit/FakeMessagesMaker.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.junit;
 
+import com.google.gwt.i18n.client.Constants;
 import com.google.gwt.i18n.client.Messages;
 
 import java.lang.reflect.InvocationHandler;
@@ -23,10 +24,10 @@ import java.lang.reflect.Proxy;
 import java.util.Arrays;
 
 /**
- * Helper to make a fake implementation of any {@link Messages} interface via
- * reflection, for use in JUnit tests. (This will not work in GWTTestCase.) All
- * calls to the returned object return the method name followed by the passed
- * parameters as a list surrounded by [].
+ * Helper to make a fake implementation of any {@link Messages} or
+ * {@link Constants} interface via reflection, for use in JUnit tests. (This
+ * will not work in GWTTestCase.) All calls to the returned object return the
+ * method name followed by the passed parameters as a list surrounded by [].
  * <p>
  * Note that the default message text is very consciously not made available
  * through the fake, to help tests ensure that specific translations of
@@ -50,6 +51,12 @@ public class FakeMessagesMaker implements InvocationHandler {
   public static <T extends Messages> T create(Class<T> messagesClass) {
     return messagesClass.cast(Proxy.newProxyInstance(
         FakeMessagesMaker.class.getClassLoader(), new Class[] {messagesClass},
+        new FakeMessagesMaker()));
+  }
+
+  public static <T extends Constants> T create(Class<T> constantsClass) {
+    return constantsClass.cast(Proxy.newProxyInstance(
+        FakeMessagesMaker.class.getClassLoader(), new Class[] {constantsClass},
         new FakeMessagesMaker()));
   }
 
