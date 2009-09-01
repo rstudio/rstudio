0e85ca6057d1d964a64d4c9a1cd22febc59cb6cb
rjrjr@google.com <rjrjr@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2009-09-01 22:46:20 +0000

    Revert r6063, "Internal contribution that extends FakeMessagesMaker to work with Constants as well." It doesn't compile. And yet I would swear that I tested it.
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@6067 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/test/com/google/gwt/junit/FakeMessagesMakerTest.java
+++ user/test/com/google/gwt/junit/FakeMessagesMakerTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,7 +15,6 @@
  */
 package com.google.gwt.junit;
 
-import com.google.gwt.i18n.client.Constants;
 import com.google.gwt.i18n.client.Messages;
 
 import junit.framework.TestCase;
@@ -28,41 +27,21 @@ public class FakeMessagesMakerTest extends TestCase {
     @DefaultMessage("Isn''t this the fakiest?")
     @Description("A sample message to be tested.")
     String myMessage();
-
+    
     @DefaultMessage("Isn''t this the fakiest? Pick one: {1} or {2}?")
     @Description("A sample message with parameters.")
-    String myArgumentedMessage(@Example("yes") String yes,
+    String myArgumentedMessage(@Example("yes") String yes, 
         @Example("no") String no);
   }
-
-  interface MyConstants extends Constants {
-    @DefaultStringValue("This is a very simple message")
-    String myFixedMessage();
-
-    @DefaultStringValue("This message is so complicated, it requires a description")
-    @Description("42")
-    String messageWithDescription();
-  }
-
-  public void testSimpleWithMessages() {
+  
+  public void testSimple() {
     MyMessages messages = FakeMessagesMaker.create(MyMessages.class);
     assertEquals("myMessage", messages.myMessage());
   }
-
-  public void testArgsWithMessages() {
+  
+  public void testArgs() {
     MyMessages messages = FakeMessagesMaker.create(MyMessages.class);
-    assertEquals("myArgumentedMessage[oui, non]",
+    assertEquals("myArgumentedMessage[oui, non]", 
         messages.myArgumentedMessage("oui", "non"));
   }
-
-  public void testSimpleWithConstants() {
-    MyConstants constants = FakeMessagesMaker.create(MyConstants.class);
-    assertEquals("myFixedMessage", constants.myFixedMessage());
-  }
-
-  public void testConstantWithDescription() {
-    MyConstants constants = FakeMessagesMaker.create(MyConstants.class);
-    assertEquals("messageWithDescription", constants.messageWithDescription());
-  }
-
 }
