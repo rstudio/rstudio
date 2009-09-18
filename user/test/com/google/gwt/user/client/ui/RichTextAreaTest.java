63fa1b1d96f6895401c792d8c70a05bd4163e05d
rjrjr@google.com <rjrjr@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2009-09-18 22:24:57 +0000

    Disable RTF tests that are clobbered by HTMLUnit bug
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/branches/farewellSwt@6165 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/test/com/google/gwt/user/client/ui/RichTextAreaTest.java
+++ user/test/com/google/gwt/user/client/ui/RichTextAreaTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,6 +16,8 @@
 package com.google.gwt.user.client.ui;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.DoNotRunWith;
+import com.google.gwt.junit.Platform;
 import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.RichTextArea.BasicFormatter;
@@ -87,6 +89,7 @@ public class RichTextAreaTest extends GWTTestCase {
     }
   }
 
+  @DoNotRunWith(Platform.Htmlunit)
   public void testFormatAfterInitialize() {
     final RichTextArea area = new RichTextArea();
     RootPanel.get().add(area);
@@ -126,6 +129,7 @@ public class RichTextAreaTest extends GWTTestCase {
     }
   }
 
+  @DoNotRunWith(Platform.Htmlunit)
   public void testFormatWhenHidden() {
     final RichTextArea area = new RichTextArea();
     RootPanel.get().add(area);
