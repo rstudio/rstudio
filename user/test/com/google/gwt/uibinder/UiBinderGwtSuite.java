b01f2321c27b0db2c330e2a63b698b48ac2cd361
rjrjr@google.com <rjrjr@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2009-10-07 20:03:24 +0000

    Adds UiBinderUtilTest.
    Created this while failing to reproduce a bug. Might as well hold on to it.
    
    Reviewed by: jgw
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@6316 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/test/com/google/gwt/uibinder/UiBinderGwtSuite.java
+++ user/test/com/google/gwt/uibinder/UiBinderGwtSuite.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,6 +16,7 @@
 package com.google.gwt.uibinder;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
+import com.google.gwt.uibinder.client.UiBinderUtilTest;
 import com.google.gwt.uibinder.sample.client.UiBinderTest;
 
 import junit.framework.Test;
@@ -26,12 +27,13 @@ import junit.framework.Test;
 public class UiBinderGwtSuite {
   public static Test suite() {
     GWTTestSuite suite = new GWTTestSuite("Test suite for UiBinder GWTTestCases");
-    
+
+    suite.addTestSuite(UiBinderUtilTest.class);
     suite.addTestSuite(UiBinderTest.class);
-    
+
     return suite;
   }
-  
+
   private UiBinderGwtSuite() {
   }
 }
