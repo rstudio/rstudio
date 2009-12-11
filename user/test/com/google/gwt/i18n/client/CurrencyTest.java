6d6afe3f8c79b6f8d6726ca015b36e35b6b95d81
jat@google.com <jat@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2009-12-11 06:21:43 +0000

    Remove unnecessary imports.
    Patch by: jat
    Unreviewed
    
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@7297 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/test/com/google/gwt/i18n/client/CurrencyTest.java
+++ user/test/com/google/gwt/i18n/client/CurrencyTest.java
@@ -15,8 +15,6 @@
  */
 package com.google.gwt.i18n.client;
 
-import com.google.gwt.i18n.client.CurrencyData;
-import com.google.gwt.i18n.client.CurrencyList;
 import com.google.gwt.junit.client.GWTTestCase;
 
 import java.util.Iterator;
