ed93e5eeaa7d7bf41e433ef7a5a73a2a4f500bf0
rice@google.com <rice@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2010-02-18 15:30:03 +0000

    Add misisng files.
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@7596 8db76d5a-ed1c-0410-87a9-c151d255dfc7

---
+++ bikeshed/src/com/google/gwt/cells/client/FieldUpdater.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.cells.client;
+
+/**
+ * A {@link FieldUpdater} may be added to a Column to update a particular field
+ * of a data item.
+ * 
+ * @param <T> the data type that will be modified
+ * @param <C> the data type of the modified field
+ */
+public interface FieldUpdater<T, C> {
+  
+  /**
+   * Announces a new value for a field within a base object.
+   * 
+   * @param object the base object to be updated
+   * @param value the new value of the field being updated.
+   */
+  void update(T object, C value);
+}
