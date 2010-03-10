2f45c104b5a3afcd903dc3d31efcc7121f8a76ef
jgw@google.com <jgw@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2010-03-10 17:49:59 +0000

    Adds missing header/text-header classes from previous commit.
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@7697 8db76d5a-ed1c-0410-87a9-c151d255dfc7

---
+++ bikeshed/src/com/google/gwt/bikeshed/list/client/Header.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2010 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.bikeshed.list.client;
+
+import com.google.gwt.bikeshed.cells.client.Cell;
+import com.google.gwt.bikeshed.cells.client.ValueUpdater;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
+
+public class Header<H> {
+  private final Cell<H> cell;
+  private ValueUpdater<H> updater;
+  private H value;
+
+  public H getValue() {
+    return value;
+  }
+
+  public void setValue(H value) {
+    this.value = value;
+  }
+
+  public Header(Cell<H> cell) {
+    this.cell = cell;
+  }
+
+  public void setUpdater(ValueUpdater<H> updater) {
+    this.updater = updater;
+  }
+
+  public void onBrowserEvent(Element elem, NativeEvent event) {
+    cell.onBrowserEvent(elem, value, event, updater);
+  }
+
+  public void render(StringBuilder sb) {
+    cell.render(value, sb);
+  }
+}
