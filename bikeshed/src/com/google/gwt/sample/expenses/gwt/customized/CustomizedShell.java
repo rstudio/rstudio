26b38f2300bb2c344746a6a4ebff11ff0db59617
rjrjr@google.com <rjrjr@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2010-04-15 01:32:33 +0000

    Fix ExpensesCustomized after ListView.Delegate refactor.
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@7922 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- bikeshed/src/com/google/gwt/sample/expenses/gwt/customized/CustomizedShell.java
+++ bikeshed/src/com/google/gwt/sample/expenses/gwt/customized/CustomizedShell.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -42,7 +42,8 @@ import java.util.List;
  * UI shell for expenses sample app. A horrible clump of stuff that should be
  * refactored into proper MVP pieces.
  */
-public class CustomizedShell extends Composite implements TakesValueList<Values<ReportKey>>, ReportChanged.Handler {
+public class CustomizedShell extends Composite implements
+    TakesValueList<Values<ReportKey>>, ReportChanged.Handler {
   interface Listener {
     void setPurpose(Values<ReportKey> report, String purpose);
   }
@@ -120,7 +121,10 @@ public class CustomizedShell extends Composite implements TakesValueList<Values<
 
   @UiFactory
   PagingTableListView<Values<ReportKey>> createListView() {
-    return new PagingTableListView<Values<ReportKey>>(adapter, 10);
+    PagingTableListView<Values<ReportKey>> table = new PagingTableListView<Values<ReportKey>>(
+        adapter, 10);
+    adapter.addView(table);
+    return table;
   }
 
   private void refresh() {
