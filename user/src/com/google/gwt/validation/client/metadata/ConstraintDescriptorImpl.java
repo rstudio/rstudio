5a1b83d83b1d0a5b59af2b0912d842cd92651e3f
nchalko@google.com <nchalko@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2010-08-17 17:07:06 +0000

    Public: Move the validation metadata classes to client/metadata.
    Review at http://gwt-code-reviews.appspot.com/773801
    
    Review by: robertvawter@google.com
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@8552 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/validation/client/metadata/ConstraintDescriptorImpl.java
+++ user/src/com/google/gwt/validation/client/metadata/ConstraintDescriptorImpl.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.gwt.validation.metadata;
+package com.google.gwt.validation.client.metadata;
 
 import java.lang.annotation.Annotation;
 import java.util.List;
