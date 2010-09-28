8d8280e7e976759cc9bf7b68b68dedc8b23097b5
jlabanca@google.com <jlabanca@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2010-09-28 20:43:03 +0000

    Removing isChanged() because requestProxyGenerator doesn't handle "is".
    Review at http://gwt-code-reviews.appspot.com/907802
    
    Review by: rjrjr@google.com
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@8889 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- samples/expenses/src/main/java/com/google/gwt/sample/expenses/client/request/ReportProxy.java
+++ samples/expenses/src/main/java/com/google/gwt/sample/expenses/client/request/ReportProxy.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -36,14 +36,12 @@ public interface ReportProxy extends EntityProxy {
   String getDepartment();
 
   Long getId();
-  
+
   String getNotes();
 
   String getPurpose();
-  
-  EmployeeProxy getReporter();
 
-  boolean isChanged();
+  EmployeeProxy getReporter();
 
   void setApprovedSupervisor(EmployeeProxy approvedSupervisor);
 
@@ -56,6 +54,6 @@ public interface ReportProxy extends EntityProxy {
   void setPurpose(String purpose);
 
   void setReporter(EmployeeProxy reporter);
-  
+
   EntityProxyId<ReportProxy> stableId();
 }
