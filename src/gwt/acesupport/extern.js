14ea89b528814246ceff8d6b26cb4e62c04e0774
Joe Cheng <joe@rstudio.org>
2011-02-11 17:16:53 -0800

    AGPL copyright notices on ace support files

--- src/gwt/acesupport/extern.js
+++ src/gwt/acesupport/extern.js
@@ -1,2 +1,14 @@
+/*
+ * extern.js
+ *
+ * Copyright (C) 2009-11 by RStudio, Inc.
+ *
+ * This program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
 function define(name, func) {}
 function require(name) {}
