2920e37b692a3903057ebd77903144c3eb23867b
Joe Cheng <joe@rstudio.org>
2011-02-16 16:42:43 -0500

    Update pull scripts to sync sub-submodule paths

--- pull.cmd
+++ pull.cmd
@@ -1,3 +1,7 @@
 call git pull
 call git submodule sync
-call git submodule update --init --recursive
+call git submodule update --init
+cd src/gwt/tools/ace
+call git submodule sync
+cd ../../../..
+call git submodule update --recursive
--- pull.sh
+++ pull.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+
+set -e
+
 git pull
 git submodule sync
-git submodule update --init --recursive
+git submodule update --init
+cd src/gwt/tools/ace
+git submodule sync
+cd ../../../..
+git submodule update --recursive
+
