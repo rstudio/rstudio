4b089463548426051bf3c44309b908a3dcfa1093
jlabanca@google.com <jlabanca@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2011-02-18 19:21:07 +0000

    Updating API checker for GWT 2.3. In CellBasedWidgetImplStandard, I fixed the package declaration per an Api Checker warning.
    Review at http://gwt-code-reviews.appspot.com/1356805
    
    Review by: rjrjr@google.com
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@9752 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/cellview/client/CellBasedWidgetImplStandard.java
+++ user/src/com/google/gwt/user/cellview/client/CellBasedWidgetImplStandard.java
@@ -12,7 +12,8 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- */package com.google.gwt.user.cellview.client;
+ */
+package com.google.gwt.user.cellview.client;
 
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Element;
