2aec548d32d2ebbc8df817235b3a93259bd8db6f
Joe Cheng <joe@rstudio.org>
2011-02-24 11:38:48 -0800

    pull.sh should be quiet when called from a script

--- pull.sh
+++ pull.sh
@@ -2,11 +2,21 @@
 
 set -e
 
-git pull
-git submodule sync
-git submodule update --init
-cd src/gwt/tools/ace
-git submodule sync
-cd ../../../..
-git submodule update --recursive
+# We want this to be chatty if you use it at the command line
+# but not if you call it from build automation. We could pass
+# in an explicit argument if we need more flexibility in the
+# future
+if [ "$SHLVL" -eq 2 ]
+then
+    GIT_OPTS=
+else
+    GIT_OPTS=-q
+fi
 
+git pull $GIT_OPTS
+git submodule $GIT_OPTS sync
+git submodule $GIT_OPTS update --init
+cd src/gwt/tools/ace
+git submodule $GIT_OPTS sync
+cd ../../../..
+git submodule $GIT_OPTS update --recursive
