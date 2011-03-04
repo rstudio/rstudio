bbcfef9fa25dd3c11dd3ed52cd91bec12a0e9667
JJ Allaire <jj@rstudio.org>
2011-03-04 11:35:54 -0500

    fix for network proxy factory

--- src/cpp/desktop/DesktopMain.cpp
+++ src/cpp/desktop/DesktopMain.cpp
@@ -214,7 +214,7 @@ int main(int argc, char* argv[])
          return EXIT_FAILURE;
       }
 
-      NetworkProxyFactory* pProxyFactory;
+      NetworkProxyFactory* pProxyFactory = new NetworkProxyFactory();
       QNetworkProxyFactory::setApplicationProxyFactory(pProxyFactory);
 
       MainWindow* browser = new MainWindow(url);
