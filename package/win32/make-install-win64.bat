265de122df73335446da31fd2839b445ed20363b
JJ Allaire <jj@rstudio.org>
2011-03-10 13:55:56 -0800

    improve build dependency management: distinguish better between install from source and binary packaging cases

--- package/win32/make-install-win64.bat
+++ package/win32/make-install-win64.bat
@@ -15,7 +15,10 @@ cmake -G"MinGW Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:String=%INSTALL_PATH% ^
       -DRSTUDIO_TARGET=SessionWin64 ^
       -DCMAKE_BUILD_TYPE=Release ^
+      -DRSTUDIO_PACKAGE_BUILD=1 ^
       ..\..\..
 mingw32-make install
 cd ..
 endlocal
+
+
