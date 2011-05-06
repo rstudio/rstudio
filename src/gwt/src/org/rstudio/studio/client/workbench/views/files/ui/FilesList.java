d8d507a7aaef5e6a179f5416966ac5e9f8720cdf
JJ Allaire <jj@rstudio.org>
2011-05-06 05:42:02 -0400

    make files pane name compares case insensitive

--- src/gwt/src/org/rstudio/studio/client/workbench/views/files/ui/FilesList.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/files/ui/FilesList.java
@@ -184,7 +184,7 @@ public class FilesList extends Composite
          @Override
          public int doCompare(FileSystemItem arg0, FileSystemItem arg1)
          {
-            return arg0.getName().compareTo(arg1.getName());
+            return arg0.getName().compareToIgnoreCase(arg1.getName());
          }
       });
       
