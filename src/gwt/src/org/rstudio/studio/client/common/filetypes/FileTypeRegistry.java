3b8b65011ef6ec215d9e46947cdf30c07d639b37
Joe Cheng <joe@rstudio.org>
2011-05-31 22:36:02 -0700

    1604: Add .Rout.save as a plaintext file format

--- src/gwt/src/org/rstudio/studio/client/common/filetypes/FileTypeRegistry.java
+++ src/gwt/src/org/rstudio/studio/client/common/filetypes/FileTypeRegistry.java
@@ -89,6 +89,7 @@ public class FileTypeRegistry
       register("*.cpp", TEXT, icons.iconText());
       register("*.h", TEXT, icons.iconText());
       register("*.hpp", TEXT, icons.iconText());
+      register("*.Rout.save", TEXT, icons.iconText());
       register("*.rd", RD, icons.iconRd());
       register("*.rdata", RDATA, icons.iconRdata());
       register("*.rda", RDATA, icons.iconRdata());
