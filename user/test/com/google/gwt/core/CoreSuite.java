68b07664a6db17cdd80849afc897be63b767fd85
zundel@google.com <zundel@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2011-06-21 18:21:52 +0000

    A general purpose script injection class for injecting a script directly or adding a reference to a script by URL to the DOM
    Review at http://gwt-code-reviews.appspot.com/1451818
    
    Review by: unnurg@google.com
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@10362 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/test/com/google/gwt/core/CoreSuite.java
+++ user/test/com/google/gwt/core/CoreSuite.java
@@ -21,6 +21,7 @@ import com.google.gwt.core.client.JavaScriptExceptionTest;
 import com.google.gwt.core.client.JsArrayMixedTest;
 import com.google.gwt.core.client.JsArrayTest;
 import com.google.gwt.core.client.SchedulerTest;
+import com.google.gwt.core.client.ScriptInjectorTest;
 import com.google.gwt.core.client.impl.AsyncFragmentLoaderTest;
 import com.google.gwt.core.client.impl.SchedulerImplTest;
 import com.google.gwt.core.client.impl.StackTraceCreatorTest;
@@ -47,6 +48,7 @@ public class CoreSuite {
     suite.addTestSuite(JsArrayMixedTest.class);
     suite.addTestSuite(SchedulerImplTest.class);
     suite.addTestSuite(SchedulerTest.class);
+    suite.addTestSuite(ScriptInjectorTest.class);
     suite.addTestSuite(StackTraceCreatorTest.class);
     suite.addTestSuite(StrictModeTest.class);
     suite.addTestSuite(XhrLoadingStrategyTest.class);
