f21bf07e500a1a276c618263b6617af91f64bbad
JJ Allaire <jj@rstudio.org>
2011-06-23 17:01:26 -0400

    dcf parser

---
+++ src/cpp/core/text/DcfParser.cpp
@@ -0,0 +1,125 @@
+/*
+ * DcfParser.cpp
+ *
+ * Copyright (C) 2009-11 by RStudio, Inc.
+ *
+ * This program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#include <core/text/DcfParser.hpp>
+
+#include <iostream>
+
+#include <string>
+#include <vector>
+
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/algorithm/string/split.hpp>
+
+#include <boost/regex.hpp>
+
+#include <core/Error.hpp>
+#include <core/FilePath.hpp>
+#include <core/FileSerializer.hpp>
+
+namespace core {
+namespace text {
+
+
+
+Error parseDcfFile(const FilePath& dcfFilePath,
+                   bool preserveKeyCase,
+                   std::map<std::string,std::string>* pFields)
+{
+   // read the file
+   std::string dcfFileContents;
+   Error error = readStringFromFile(dcfFilePath,
+                                    &dcfFileContents);
+   if (error)
+      return error;
+
+   // split into lines
+   std::vector<std::string> dcfLines;
+   boost::algorithm::split(dcfLines,
+                           dcfFileContents,
+                           boost::algorithm::is_any_of("\r\n"));
+
+   // iterate over lines
+   std::string currentKey;
+   std::string currentValue;
+   for(std::vector<std::string>::const_iterator it = dcfLines.begin();
+       it != dcfLines.end();
+       ++it)
+   {
+      // get a trimmed copy to check for blank and comment lines
+      std::string trimmedLine =  boost::algorithm::trim_copy(*it);
+
+      // skip fully blank lines
+      if (trimmedLine.empty())
+         continue;
+
+      // skip comment lines
+      if (trimmedLine[0] == '#')
+         continue;
+
+      // define regexes
+      boost::regex keyValueRegx("(.*?)\\s*\\:\\s*(.*)$");
+      boost::regex continuationRegex("[\t\\s](.*)");
+
+       // look for a key-value pair line
+      boost::smatch keyValueMatch, continuationMatch;
+      if (regex_match(*it, keyValueMatch, keyValueRegx))
+      {
+         // if we have a pending key & value then resolve it
+         if (!currentKey.empty())
+         {
+            pFields->insert(std::make_pair(currentKey,currentValue));
+            currentKey.clear();
+            currentValue.clear();
+         }
+
+         // update the current key and value
+         currentKey = preserveKeyCase ?
+                                 keyValueMatch[1] :
+                                 string_utils::toLower(keyValueMatch[1]);
+         currentValue = keyValueMatch[2];
+      }
+
+      // look for a continuation
+      else if (!currentKey.empty() &&
+               regex_match(*it, continuationMatch, continuationRegex))
+      {
+         currentValue.append("\n");
+         currentValue.append(continuationMatch[1]);
+      }
+
+      // invalid line
+      else
+      {
+         Error error = systemError(boost::system::errc::protocol_error,
+                                   ERROR_LOCATION);
+         error.addProperty("invalid-line", *it);
+         return error;
+      }
+   }
+
+   // resolve any pending key and value
+   if (!currentKey.empty())
+      pFields->insert(std::make_pair(currentKey,currentValue));
+
+   return Success();
+}
+
+std::string dcfMultilineAsFolded(const std::string& line)
+{
+   return boost::algorithm::trim_copy(
+       boost::regex_replace(line, boost::regex("\\s*\r?\n\\s*"), " "));
+}
+
+} // namespace dcf
+} // namespace core
