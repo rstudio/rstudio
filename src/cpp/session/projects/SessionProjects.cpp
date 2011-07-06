babc7888206017cef5a4ffdb4336c1f83f55dafb
JJ Allaire <jj@rstudio.org>
2011-07-06 09:28:45 -0400

    update project todos

--- src/cpp/session/projects/SessionProjects.cpp
+++ src/cpp/session/projects/SessionProjects.cpp
@@ -14,6 +14,10 @@
 // TODO: when switching from hidden source to project with source docs
 //       need to show the source view
 
+// TODO: project file changed event
+
+// TODO: switch to file based semantics
+
 // TODO: detecting copy/move/network:
 /*
     - read INDEX
