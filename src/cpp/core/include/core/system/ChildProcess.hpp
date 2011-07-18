842bef286fc665ced4f9f1b547d751a46fd76da0
JJ Allaire <jj@rstudio.org>
2011-07-18 17:11:10 -0400

    child process declarations

--- src/cpp/core/include/core/system/ChildProcess.hpp
+++ src/cpp/core/include/core/system/ChildProcess.hpp
@@ -15,12 +15,62 @@
 #ifndef CORE_SYSTEM_CHILD_PROCESS_HPP
 #define CORE_SYSTEM_CHILD_PROCESS_HPP
 
+#include <vector>
 
+#include <boost/utility.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
 
 namespace core {
+
+class Error;
+
 namespace system {
 
+class ChildProcess : boost::noncopyable
+{
+public:
+   ChildProcess(const std::string& cmd,
+                const std::vector<std::string>& args);
+   virtual ~ChildProcess() ;
 
+
+
+   Error writeToStdin(const std::string& input);
+
+   Error terminate();
+
+   bool exited();
+
+   void pollForEvents();
+
+protected:
+   virtual void onStdout(const std::string& output);
+   virtual void onStderr(const std::string& err);
+   virtual void onExited(int statusCode);
+
+private:
+   struct Impl;
+   boost::scoped_ptr<Impl> pImpl_;
+};
+
+
+class ChildProcessSupervisor : boost::noncopyable
+{
+public:
+
+   template<typename T>
+   void add(boost::shared_ptr<T> pChild)
+   {
+      children_.push_back(boost::shared_static_cast<ChildProcess>(pChild));
+   }
+
+
+   void pollForEvents();
+
+private:
+   std::vector<boost::shared_ptr<ChildProcess> > children_;
+};
 
 
 } // namespace system
