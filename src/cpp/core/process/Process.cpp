de7402bb7f207cc5e609da9e5d6422f3573b1aab
JJ Allaire <jj@rstudio.org>
2011-07-18 15:41:14 -0400

    Revert "just include BoostThread as preface to boost/process rather than entire core/Thread"
    This reverts commit 4f29dfe1f9d68cef5695f55d47af1f29f3c694b1.

--- src/cpp/core/process/Process.cpp
+++ src/cpp/core/process/Process.cpp
@@ -13,6 +13,7 @@
 
 #include <core/process/Process.hpp>
 
+#include <core/Thread.hpp>
 
 // NOTE: Boost.Process is a proposed boost library for process management
 //
@@ -32,7 +33,6 @@
 //
 //   http://thread.gmane.org/gmane.comp.lib.boost.devel/207594/
 //
-#include <core/BoostThread.hpp>
 #include <boost/process/all.hpp>
 
 
