af1f3f1956a887f61ef02db677b97cee4070058b
unknown <jj@rstudio.org>
2011-07-21 11:06:08 -0400

    stubs for win32 child process implementation

--- src/cpp/core/system/Win32ChildProcess.cpp
+++ src/cpp/core/system/Win32ChildProcess.cpp
@@ -11,12 +11,74 @@
  *
  */
 
-#include <core/system/Process.hpp>
+#include "ChildProcess.hpp"
 
 
 namespace core {
 namespace system {
 
+struct ChildProcess::Impl
+{
+};
+
+
+ChildProcess::ChildProcess(const std::string& cmd,
+                           const std::vector<std::string>& args)
+  : pImpl_(new Impl()), cmd_(cmd), args_(args)
+{
+}
+
+ChildProcess::~ChildProcess()
+{
+}
+
+Error ChildProcess::writeToStdin(const std::string& input, bool eof)
+{
+   return Success();
+}
+
+
+Error ChildProcess::terminate()
+{
+   return Success();
+}
+
+
+Error ChildProcess::run()
+{
+   return Success();
+}
+
+
+Error SyncChildProcess::run(const std::string& input, ProcessResult* pResult)
+{
+   return Success();
+}
+
+
+struct AsyncChildProcess::AsyncImpl
+{
+};
+
+AsyncChildProcess::AsyncChildProcess(const std::string& cmd,
+                                     const std::vector<std::string>& args)
+   : ChildProcess(cmd, args), pAsyncImpl_(new AsyncImpl())
+{
+}
+
+AsyncChildProcess::~AsyncChildProcess()
+{
+}
+
+void AsyncChildProcess::poll()
+{
+
+}
+
+bool AsyncChildProcess::exited()
+{
+   return true;
+}
 
 } // namespace system
 } // namespace core
