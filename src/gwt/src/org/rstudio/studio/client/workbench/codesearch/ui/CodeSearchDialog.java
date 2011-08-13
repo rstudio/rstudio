04d1836d82ef0edd430e5ee312f63ed1e26b1aee
JJ Allaire <jj@rstudio.org>
2011-08-13 14:20:10 -0400

    delay closing search dialog on ESC (so keystroke doesn't end up in the editor)

--- src/gwt/src/org/rstudio/studio/client/workbench/codesearch/ui/CodeSearchDialog.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/codesearch/ui/CodeSearchDialog.java
@@ -4,6 +4,8 @@ import org.rstudio.core.client.widget.CanFocus;
 import org.rstudio.core.client.widget.ModalDialogBase;
 import org.rstudio.studio.client.workbench.codesearch.CodeSearch;
 
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -62,7 +64,16 @@ public class CodeSearchDialog extends ModalDialogBase
    @Override
    protected void onEscapeKeyDown(Event.NativePreviewEvent event)
    {
-      closeDialog();
+      // close dialog on ESC -- delay so that the ESC key doesn't 
+      // end up in the editor
+      Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+         @Override
+         public void execute()
+         {
+            closeDialog();
+         }
+      });
+     
    }
    
    @Override
