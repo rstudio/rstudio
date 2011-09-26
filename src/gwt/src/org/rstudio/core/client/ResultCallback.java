d98e01596b1253f759a89311bb8142c0c21f6580
JJ Allaire <jj@rstudio.org>
2011-09-26 13:51:58 -0400

    Revert "Revert "Revert "don't pass ServerError to RequestCallback.onFailure"""
    This reverts commit 48ce0bf31149bc2932660fb3c79d33bac3b5ec72.

--- src/gwt/src/org/rstudio/core/client/ResultCallback.java
+++ src/gwt/src/org/rstudio/core/client/ResultCallback.java
@@ -16,15 +16,15 @@ package org.rstudio.core.client;
  * Provides a generic interface for handling success and/or failure of an
  * operation, especially an asynchronous one.
  */
-public abstract class ResultCallback<TSuccess>
+public abstract class ResultCallback<TSuccess, TFailure>
 {
    public void onSuccess(TSuccess result) {}
-   public void onFailure() {}
+   public void onFailure(TFailure info) {}
 
-   public static <TSuccess>
-   ResultCallback<TSuccess> create(final CommandWithArg<TSuccess> cmd)
+   public static <TSuccess, TFailure>
+   ResultCallback<TSuccess, TFailure> create(final CommandWithArg<TSuccess> cmd)
    {
-      return new ResultCallback<TSuccess>()
+      return new ResultCallback<TSuccess, TFailure>()
       {
          @Override
          public void onSuccess(TSuccess result)
