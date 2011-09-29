c46b7f0e82f8406c50365a9421c85808ab6dd965
JJ Allaire <jj@rstudio.org>
2011-09-29 11:54:37 -0400

    eliminate eclipse warnings

--- src/gwt/src/org/rstudio/studio/client/workbench/views/vcs/dialog/ReviewPresenter.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/vcs/dialog/ReviewPresenter.java
@@ -170,12 +170,10 @@ public class ReviewPresenter implements IsWidget
                           final EventBus events,
                           final VcsState vcsState,
                           final Commands commands,
-                          final Session session,
-                          final GlobalDisplay globalDisplay)
+                          final Session session)
    {
       server_ = server;
       view_ = view;
-      globalDisplay_ = globalDisplay;
 
       vcsState.addVcsRefreshHandler(new VcsRefreshHandler()
       {
@@ -530,7 +528,6 @@ public class ReviewPresenter implements IsWidget
    private final Invalidation diffInvalidation_ = new Invalidation();
    private final VCSServerOperations server_;
    private final Display view_;
-   private final GlobalDisplay globalDisplay_;
    private ArrayList<DiffChunk> activeChunks_ = new ArrayList<DiffChunk>();
    private static final String MODULE_VCS = "vcs";
    private static final String KEY_CONTEXT_LINES = "context_lines";
