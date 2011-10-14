b91bfe235cfbcc1bd673d92b6dffb8ba65c94ea9
Joe Cheng <joe@rstudio.org>
2011-10-14 10:44:48 -0700

    Fix up copyright headers

--- src/gwt/src/org/rstudio/studio/client/workbench/model/WorkbenchLists.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/model/WorkbenchLists.java
@@ -1,5 +1,5 @@
 /*
- * WarningBarMessage.java
+ * WorkbenchLists.java
  *
  * Copyright (C) 2009-11 by RStudio, Inc.
  *
--- src/gwt/src/org/rstudio/studio/client/workbench/model/WorkbenchListsServerOperations.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/model/WorkbenchListsServerOperations.java
@@ -1,5 +1,5 @@
 /*
- * WorkbenchServerOperations.java
+ * WorkbenchListsServerOperations.java
  *
  * Copyright (C) 2009-11 by RStudio, Inc.
  *
