b1c324c6a238e029b70e612cbcc4490ae74944ec
Joe Cheng <joe@rstudio.org>
2011-10-18 11:12:00 -0700

    Upgrade Windows dependency script to GWT 2.4

--- dependencies/windows/install-dependencies.cmd
+++ dependencies/windows/install-dependencies.cmd
@@ -11,7 +11,7 @@ set BASEURL=https://s3.amazonaws.com/rstudio-buildtools/
 set BOOST_FILE=boost-1.44-win.zip
 set MINGW_FILE=mingw64-2010-10-03.zip
 set GIN_FILE=gin-1.5.zip
-set GWT_FILE=gwt-2.3.0.zip
+set GWT_FILE=gwt-2.4.0.zip
 set JUNIT_FILE=junit-4.9b3.jar
 
 if not exist boost-win (
@@ -39,12 +39,12 @@ if not exist gin\1.5 (
   del "%GIN_FILE%"
 )
 
-if not exist gwt\2.3.0 (
+if not exist gwt\2.4.0 (
   wget %WGET_ARGS% "%BASEURL%%GWT_FILE%"
   echo Unzipping %GWT_FILE%
   unzip %UNZIP_ARGS% "%GWT_FILE%"
   mkdir gwt
-  move gwt-2.3.0 gwt\2.3.0
+  move gwt-2.4.0 gwt\2.4.0
   del "%GWT_FILE%"
 )
 
