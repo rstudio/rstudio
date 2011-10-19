9a103481201697a7b0f0160220f01145d73ca32b
Joe Cheng <joe@rstudio.org>
2011-10-19 00:22:26 -0700

    Introduce default DataGrid style

---
+++ src/gwt/src/org/rstudio/core/client/theme/RStudioDataGridStyle.java
@@ -0,0 +1,20 @@
+/*
+ * RStudioDataGridStyle.java
+ *
+ * Copyright (C) 2009-11 by RStudio, Inc.
+ *
+ * This program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+package org.rstudio.core.client.theme;
+
+import com.google.gwt.user.cellview.client.DataGrid;
+
+public interface RStudioDataGridStyle extends DataGrid.Style
+{
+   String RSTUDIO_DEFAULT_CSS = "org/rstudio/core/client/theme/RStudioCellTableStyle.css";
+}
