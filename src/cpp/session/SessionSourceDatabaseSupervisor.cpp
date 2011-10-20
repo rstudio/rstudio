d64ff2cb0b9bbcc347100c48679bb7f4bb7f31fe
JJ Allaire <jj@rstudio.org>
2011-10-20 13:26:28 -0400

    introduce source_database supervisor module

---
+++ src/cpp/session/SessionSourceDatabaseSupervisor.cpp
@@ -0,0 +1,32 @@
+
+/*
+ * SessionSourceDatabaseSupervisor.cpp
+ *
+ * Copyright (C) 2009-11 by RStudio, Inc.
+ *
+ * This program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#include "SessionSourceDatabaseSupervisor.hpp"
+
+namespace session {
+namespace source_database {
+namespace supervisor {
+
+namespace {
+
+} // anonymous namespace
+
+
+
+} // namespace supervisor
+} // namespace source_database
+} // namespace session
+
+
+
