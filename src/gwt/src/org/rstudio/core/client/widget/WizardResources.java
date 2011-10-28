8aed7209d107255ddddb0c65f09ef5372298506a
JJ Allaire <jj@rstudio.org>
2011-10-28 07:53:46 -0400

    eliminate eclipse warnings

--- src/gwt/src/org/rstudio/core/client/widget/WizardResources.java
+++ src/gwt/src/org/rstudio/core/client/widget/WizardResources.java
@@ -4,7 +4,6 @@ package org.rstudio.core.client.widget;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
-import com.google.gwt.resources.client.DataResource;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.resources.client.ImageResource.ImageOptions;
 import com.google.gwt.resources.client.ImageResource.RepeatStyle;
