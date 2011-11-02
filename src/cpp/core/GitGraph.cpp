530ee60d62b39105bc449dc5c00d9ab6fcc5a20c
Joe Cheng <joe@rstudio.org>
2011-11-02 15:15:49 -0700

    Git branch graph support code
    These are just the data structures and algorithms we will need on the back end. Still need to generate the rendering logic on the client side, then hook it all together.

---
+++ src/cpp/core/GitGraph.cpp
@@ -0,0 +1,143 @@
+/*
+ * GitGraph.cpp
+ *
+ * Copyright (C) 2009-11 by RStudio, Inc.
+ *
+ * This program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#include <core/GitGraph.hpp>
+
+#include <boost/bind.hpp>
+#include <boost/foreach.hpp>
+
+#include <core/SafeConvert.hpp>
+
+namespace core {
+namespace gitgraph {
+
+namespace {
+
+// If true, then this column terminates in this row's nexus.
+bool isColumnTerminating(const Column& column)
+{
+   return column.postCommit.empty();
+}
+
+// If true, then this column either starts in this row's nexus,
+// ends there, or passes through it. If false, then the column
+// has nothing going on in this row (well, it's possibly changing
+// positions as a result of other columns being added/removed).
+bool isColumnDynamic(const Column& column)
+{
+   return column.preCommit != column.postCommit;
+}
+
+} // namespace
+
+size_t Line::nexus() const
+{
+   Line::const_iterator it = std::find_if(begin(), end(), &isColumnDynamic);
+   if (it == end())
+      return SIZE_MAX;
+   else
+      return it - begin();
+}
+
+std::string Line::string() const
+{
+   std::string output;
+   bool sawNexus = false;
+   for (size_t i = 0; i < size(); i++)
+   {
+      const Column& c = at(i);
+      if (!sawNexus && isColumnDynamic(c))
+      {
+         sawNexus = true;
+         output.append("*");
+      }
+      else if (isColumnTerminating(c))
+      {
+         continue;
+      }
+      output.append(boost::lexical_cast<std::string>(c.id));
+      output.append(" ");
+   }
+   return output;
+}
+
+Line GitGraph::addCommit(const std::string& commit,
+                         const std::vector<std::string>& parents)
+{
+   Line::iterator insertNewColumnsAt = pendingLine_.end();
+   size_t parentsUsed = 0;
+
+   for (Line::iterator it = pendingLine_.begin();
+        it != pendingLine_.end();
+        it++)
+   {
+      if (it->preCommit == commit)
+      {
+         if (insertNewColumnsAt == pendingLine_.end())
+         {
+            insertNewColumnsAt = it + 1;
+            if (parentsUsed == parents.size())
+               it->postCommit = "";
+            else
+               it->postCommit = parents[parentsUsed++];
+         }
+         else
+         {
+            it->postCommit = "";
+         }
+      }
+   }
+
+   // Make new columns for any parents we haven't already used.
+   while (parentsUsed != parents.size())
+   {
+      insertNewColumnsAt = 1 + pendingLine_.insert(
+            insertNewColumnsAt,
+            Column(nextColumnId_++, "", parents[parentsUsed++]));
+   }
+
+   // Update end positions.
+   size_t endPos = 0;
+   for (Line::iterator it = pendingLine_.begin();
+        it != pendingLine_.end();
+        it++)
+   {
+      if (it->postCommit.empty())
+         it->endPosition = SIZE_MAX;
+      else
+         it->endPosition = endPos++;
+   }
+
+   // This line is ready. Make a copy of it.
+   Line result = pendingLine_;
+
+   /*
+    * Now fix up pendingLine_ to get ready for the next call to addCommit.
+    */
+
+   // First remove all columns that have empty postCommit--these terminated.
+   Line::iterator newEnd = std::remove_if(
+         pendingLine_.begin(), pendingLine_.end(), &isColumnTerminating);
+   pendingLine_.erase(newEnd, pendingLine_.end());
+
+   // Now copy all of the postCommits to preCommit.
+   BOOST_FOREACH(Column& column, pendingLine_)
+   {
+      column.preCommit = column.postCommit;
+   }
+
+   return result;
+}
+
+} // namespace gitgraph
+} // namespace core
---
+++ src/cpp/core/include/core/GitGraph.hpp
@@ -0,0 +1,88 @@
+/*
+ * GitGraph.hpp
+ *
+ * Copyright (C) 2009-11 by RStudio, Inc.
+ *
+ * This program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+#ifndef CORE_GITGRAPH_HPP
+#define CORE_GITGRAPH_HPP
+
+#include <string>
+#include <vector>
+
+#include <boost/noncopyable.hpp>
+
+namespace core {
+namespace gitgraph {
+
+struct Column
+{
+   Column(int id, const std::string& preCommit, const std::string& postCommit)
+      : id(id), preCommit(preCommit), postCommit(postCommit)
+   {
+   }
+
+   // The ID of the column--useful for coloring.
+   int id;
+
+   // The commit that the column points to at the start. If the column
+   // starts here the preCommit will be empty
+   std::string preCommit;
+   // The commit that the column points to at the end. If the column
+   // terminates here the postCommit will be empty
+   std::string postCommit;
+
+   size_t endPosition;
+};
+
+// Represents one line of the graph.
+class Line : public std::vector<Column>
+{
+public:
+   // The nexus is the column that represents this row's commit.
+   // There are potentially multiple columns that could be used
+   // to represent the commit; we pick the leftmost one as this
+   // results in the most orderly graphs.
+   size_t nexus() const;
+
+   // Prints out a string representation of this row for debug
+   // and testing purposes.
+   std::string string() const;
+};
+
+typedef std::vector<Line> Lines;
+
+// Encapsulates the state and logic used to build up a graph,
+// based on repeated calls with commit-and-parent info.
+// This class doesn't hold all of the result lines; the caller
+// must decide what to do with the lines as they are created
+// during calls to addCommit.
+class GitGraph : boost::noncopyable
+{
+public:
+   GitGraph() : nextColumnId_(0)
+   {}
+
+   // Call addCommit to yield the next line of the graph.
+   // Note that GitGraph is stateful; each call to addCommit
+   // builds on the state of previous calls to addCommit.
+   // So it's important to call in reverse chronological or
+   // topographic order.
+   Line addCommit(const std::string& commit,
+                  const std::vector<std::string>& parents);
+
+private:
+   int nextColumnId_;
+   Line pendingLine_;
+};
+
+} // namespace gitgraph
+} // namespace core
+
+#endif // CORE_GITGRAPH_HPP
