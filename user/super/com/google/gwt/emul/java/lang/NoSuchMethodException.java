3d7ead722f531fd381d44ef297a2e460703dc83c
jat@google.com <jat@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2011-11-23 17:50:18 +0000

    Add NoSuchMethodException for compatibility.
    Public review at: http://gwt-code-reviews.appspot.com/1529807/
    Patch by: stephen.haberman
    Review by: scottb
    
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@10762 8db76d5a-ed1c-0410-87a9-c151d255dfc7

---
+++ user/super/com/google/gwt/emul/java/lang/NoSuchMethodException.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package java.lang;
+
+/**
+ * See <a href="http://java.sun.com/j2se/1.5.0/docs/api/java/lang/NoSuchMethodException.html">the
+ * official Java API doc</a> for details.
+ * 
+ * This exception is never thrown by GWT or GWT's libraries, as GWT does not support reflection. It
+ * is provided in GWT only for compatibility with user code that explicitly throws or catches it for
+ * non-reflection purposes.
+ */
+public class NoSuchMethodException extends Exception {
+
+  public NoSuchMethodException() {
+  }
+
+  public NoSuchMethodException(String message) {
+    super(message);
+  }
+}
