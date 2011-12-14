a890aa246fd973b5445d5ee5b45048111421d31b
Joe Cheng <joe@rstudio.org>
2011-12-13 21:30:20 -0800

    Use proper status icons for SVN

--- src/gwt/src/org/rstudio/studio/client/workbench/views/vcs/svn/SVNStatusRenderer.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/vcs/svn/SVNStatusRenderer.java
@@ -12,24 +12,120 @@
  */
 package org.rstudio.studio.client.workbench.views.vcs.svn;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.text.shared.SafeHtmlRenderer;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
+import org.rstudio.studio.client.workbench.views.vcs.common.ChangelistTable.ChangelistTableCellTableResources;
 
 public class SVNStatusRenderer implements SafeHtmlRenderer<String>
 {
-   @Override
-   public SafeHtml render(String object)
+   interface StatusResources extends ClientBundle
    {
-      return SafeHtmlUtils.fromString(object);
+      @Source("images/statusAdded.png")
+      ImageResource statusAdded();
+      @Source("images/statusConflicted.png")
+      ImageResource statusConflicted();
+      @Source("images/statusDeleted.png")
+      ImageResource statusDeleted();
+      @Source("images/statusExternal.png")
+      ImageResource statusExternal();
+      @Source("images/statusIgnored.png")
+      ImageResource statusIgnored();
+      @Source("images/statusMissing.png")
+      ImageResource statusMissing();
+//      @Source("images/statusMerged")
+//      ImageResource statusMerged();
+      @Source("images/statusModified.png")
+      ImageResource statusModified();
+      @Source("images/statusNone.png")
+      ImageResource statusNone();
+      @Source("images/statusObstructed.png")
+      ImageResource statusObstructed();
+      @Source("images/statusUnversioned.png")
+      ImageResource statusUnversioned();
+   }
+
+   public SVNStatusRenderer()
+   {
    }
 
    @Override
-   public void render(String object, SafeHtmlBuilder builder)
+   public SafeHtml render(String str)
    {
-      SafeHtml html = render(object);
-      if (html != null)
-         builder.append(html);
+      if (str.length() != 1)
+         return SafeHtmlUtils.fromString(str);
+
+      ImageResource img = imgForStatus(str.charAt(0));
+
+      if (img == null)
+         return SafeHtmlUtils.fromString(str);
+
+      SafeHtmlBuilder builder = new SafeHtmlBuilder();
+      builder.append(SafeHtmlUtils.fromTrustedString(
+            "<span " +
+            "class=\"" + ctRes_.cellTableStyle().status() + "\" " +
+            "title=\"" +
+            SafeHtmlUtils.htmlEscape(descForStatus(str)) +
+            "\">"));
+
+      builder.append(SafeHtmlUtils.fromTrustedString(AbstractImagePrototype.create(
+            img).getHTML()));
+
+      builder.appendHtmlConstant("</span>");
+
+      return builder.toSafeHtml();
    }
+
+   private String descForStatus(String str)
+   {
+      // TODO: Provide a suitable tooltip value for status
+      return "";
+   }
+
+   private ImageResource imgForStatus(char c)
+   {
+      switch (c)
+      {
+         case 'A':
+            return resources_.statusAdded();
+         case 'C':
+            return resources_.statusConflicted();
+         case 'D':
+            return resources_.statusDeleted();
+         case 'X':
+            return resources_.statusExternal();
+         case 'I':
+            return resources_.statusIgnored();
+         case '!':
+            return resources_.statusMissing();
+//         case 'G':
+//            return resources_.statusMerged();
+         case 'M':
+            return resources_.statusModified();
+         case ' ':
+            return resources_.statusNone();
+         case '~':
+            return resources_.statusObstructed();
+         case '?':
+            return resources_.statusUnversioned();
+         default:
+            return null;
+      }
+   }
+
+   @Override
+   public void render(String str, SafeHtmlBuilder builder)
+   {
+      SafeHtml safeHtml = render(str);
+      if (safeHtml != null)
+         builder.append(safeHtml);
+   }
+
+   private static final StatusResources resources_ = GWT.create(StatusResources.class);
+   private static final ChangelistTableCellTableResources ctRes_ = GWT.create(ChangelistTableCellTableResources.class);
 }
