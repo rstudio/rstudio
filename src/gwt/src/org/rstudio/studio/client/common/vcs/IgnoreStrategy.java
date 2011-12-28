9d9a643022906ac01a6d12f95fbf2c89531dc313
Joe Cheng <joe@rstudio.org>
2011-12-28 10:34:02 -0800

    Fixup copyright headers

--- src/gwt/src/org/rstudio/studio/client/common/vcs/IgnoreStrategy.java
+++ src/gwt/src/org/rstudio/studio/client/common/vcs/IgnoreStrategy.java
@@ -8,7 +8,7 @@
  * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
  * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
  * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
- * 
+ *
  */
 package org.rstudio.studio.client.common.vcs;
 
