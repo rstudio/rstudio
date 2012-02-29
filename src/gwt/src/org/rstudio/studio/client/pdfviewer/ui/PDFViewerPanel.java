b943821792e3915cb5e4b1b4af08cff540e17d53
Joe Cheng <joe@rstudio.org>
2012-02-29 11:17:01 -0800

    Add custom toolbar to PDF viewer; UI polish

--- src/gwt/src/org/rstudio/studio/client/pdfviewer/ui/PDFViewerPanel.java
+++ src/gwt/src/org/rstudio/studio/client/pdfviewer/ui/PDFViewerPanel.java
@@ -15,9 +15,13 @@ package org.rstudio.studio.client.pdfviewer.ui;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import org.rstudio.core.client.theme.res.ThemeStyles;
+import org.rstudio.core.client.widget.Toolbar;
 import org.rstudio.studio.client.pdfviewer.PDFViewerPresenter;
 import org.rstudio.studio.client.pdfviewer.events.InitCompleteEvent;
 import org.rstudio.studio.client.pdfviewer.pdfjs.PdfJs;
@@ -28,9 +32,12 @@ public class PDFViewerPanel extends Composite
    interface Binder extends UiBinder<Widget, PDFViewerPanel>
    {}
 
+   @Inject
    public PDFViewerPanel()
    {
       initWidget(GWT.<Binder>create(Binder.class).createAndBindUi(this));
+
+      toolbar_.addStyleName(ThemeStyles.INSTANCE.toolbar());
    }
 
    @Override
@@ -79,4 +86,6 @@ public class PDFViewerPanel extends Composite
    private boolean loaded_;
    private String initialUrl_;
    private boolean once_;
+   @UiField
+   Toolbar toolbar_;
 }
