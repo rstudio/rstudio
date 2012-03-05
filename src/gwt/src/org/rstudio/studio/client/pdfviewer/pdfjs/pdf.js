408d240ed1218f6033287370a46cd498d34c0480
Joe Cheng <joe@rstudio.org>
2012-03-05 15:34:13 -0800

    Update colors in PDF viewer

--- src/gwt/src/org/rstudio/studio/client/pdfviewer/pdfjs/pdf.js
+++ src/gwt/src/org/rstudio/studio/client/pdfviewer/pdfjs/pdf.js
@@ -7,7 +7,7 @@ var PDFJS = {};
   // Use strict in our context only - users might not want it
   'use strict';
 
-  PDFJS.build = '9e0c3dc';
+  PDFJS.build = 'f219107';
 
   // Files are inserted below - see Makefile
   /* PDFJSSCRIPT_INCLUDE_ALL */
