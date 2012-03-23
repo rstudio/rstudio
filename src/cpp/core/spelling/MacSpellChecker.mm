802e769ca1e0977941c9d20418b5d14ea2c63428
JJ Allaire <jj@rstudio.org>
2012-03-23 12:55:48 -0400

    implement checkSpelling and suggestionList on the mac

--- src/cpp/core/spelling/MacSpellChecker.mm
+++ src/cpp/core/spelling/MacSpellChecker.mm
@@ -21,14 +21,31 @@
 #include <core/Error.hpp>
 
 #import <Foundation/NSString.h>
+#import <Foundation/NSArray.h>
 #import <Foundation/NSAutoreleasePool.h>
+#import <Foundation/NSException.h>
 #import <AppKit/NSSpellChecker.h>
+
+// convenience macro to catch objective-c exceptions and convert them
+// into standard Error returns
+#define CATCH_NS_EXCEPTION \
+   @catch (NSException* ex) \
+   { \
+      std::string name = [[ex name] UTF8String]; \
+      std::string reason = [[ex reason] UTF8String]; \
+      return systemError(boost::system::errc::state_not_recoverable, \
+                         name + " (" + reason + ")", \
+                         ERROR_LOCATION); \
+   }
 
 namespace core {
 namespace spelling {
 
 namespace {
 
+// utility class to allocate and drain an autorelease pool within a
+// scope. this allows the caller to not have a global / event-loop based
+// autorelease handler installed to successfully call the spelling api
 class AutoreleaseContext : boost::noncopyable
 {
 public:
@@ -73,8 +90,17 @@ public:
 	{
       AutoreleaseContext arContext;
 
+      // ensure the spell checker is available (and not damaged in some way)
+      // by checking for its language within an exception block
+      @try
+      {
+         [[NSSpellChecker sharedSpellChecker] language];
+         return Success();
+      }
+      CATCH_NS_EXCEPTION
 
-		return Success();
+      // keep compiler happy
+      return Success();
 	}
 
 public:
@@ -82,6 +108,24 @@ public:
    {
       AutoreleaseContext arContext;
 
+      @try
+      {
+         NSSpellChecker* spellChecker = [NSSpellChecker sharedSpellChecker];
+         NSString* nsWord = toNSString(word);
+         NSArray* results = [spellChecker checkString: nsWord
+                                          range: NSMakeRange(0, [nsWord length])
+                                          types: NSTextCheckingTypeSpelling
+                                          options: nil
+                                          inSpellDocumentWithTag: 0
+                                          orthography: NULL
+                                          wordCount: NULL];
+
+         *pCorrect = [results count] == 0;
+         return Success();
+      }
+      CATCH_NS_EXCEPTION
+
+      // keep compiler happy
       return Success();
    }
 
@@ -89,6 +133,24 @@ public:
    {
       AutoreleaseContext arContext;
 
+      @try
+      {
+         NSSpellChecker* spellChecker = [NSSpellChecker sharedSpellChecker];
+         NSString* nsWord = toNSString(word);
+         NSRange wordRange = NSMakeRange(0, [nsWord length]);
+         NSArray* results = [spellChecker guessesForWordRange: wordRange
+                                          inString: nsWord
+                                          language: [spellChecker language]
+                                          inSpellDocumentWithTag: 0];
+
+         for (NSString* result in results)
+            pSug->push_back(toStdString(result));
+
+         return Success();
+      }
+      CATCH_NS_EXCEPTION
+
+      // keep compiler happy
       return Success();
    }
 
@@ -130,8 +192,6 @@ public:
    }
 
 private:
-
-
    NSString* toNSString(const std::string& str)
    {
       const char* cStr = str.c_str();
