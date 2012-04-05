f85a1cef9f95292cc26a5cf3e15a9ae5b2aca401
rdayal@google.com <rdayal@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2012-04-05 18:50:07 +0000

    Make HasScrollHandlers extend HasHandlers.
    Fixes issue 6760.
    
    Review at: http://gwt-code-reviews.appspot.com/1610804/
    Patch by: t.broyer
    Review by: Jim Douglas
    
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@10935 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/event/dom/client/HasScrollHandlers.java
+++ user/src/com/google/gwt/event/dom/client/HasScrollHandlers.java
@@ -16,12 +16,13 @@
 package com.google.gwt.event.dom.client;
 
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.shared.HasHandlers;
 
 /**
  * A widget that implements this interface provides registration for
  * {@link ScrollHandler} instances.
  */
-public interface HasScrollHandlers {
+public interface HasScrollHandlers extends HasHandlers {
   /**
    * Adds a {@link ScrollEvent} handler.
    * 
