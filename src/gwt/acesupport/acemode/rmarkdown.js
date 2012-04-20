01e7735a4c80149cdd4382a0e67af78788881138
Joe Cheng <joe@rstudio.org>
2012-04-20 10:17:36 -0700

    Add folding for markdown

--- src/gwt/acesupport/acemode/rmarkdown.js
+++ src/gwt/acesupport/acemode/rmarkdown.js
@@ -21,11 +21,15 @@ define("mode/rmarkdown", function(require, exports, module) {
 var oop = require("ace/lib/oop");
 var MarkdownMode = require("ace/mode/markdown").Mode;
 var Tokenizer = require("ace/tokenizer").Tokenizer;
-var SweaveBackgroundHighlighter = require("mode/sweave_background_highlighter").SweaveBackgroundHighlighter;
 var RMarkdownHighlightRules = require("mode/rmarkdown_highlight_rules").RMarkdownHighlightRules;
+var SweaveBackgroundHighlighter = require("mode/sweave_background_highlighter").SweaveBackgroundHighlighter;
+var RCodeModel = require("mode/r_code_model").RCodeModel;
 
 var Mode = function(suppressHighlighting, doc, session) {
    this.$tokenizer = new Tokenizer(new RMarkdownHighlightRules().getRules());
+
+   this.codeModel = new RCodeModel(doc, this.$tokenizer, /^r-/);
+   this.foldingRules = this.codeModel;
    this.$sweaveBackgroundHighlighter = new SweaveBackgroundHighlighter(
          session,
          /^`{3,}\s*\{r(?:.*)\}\s*$/,
