085085fe1a1114671e3a95322d6eef15735377a0
JJ Allaire <jj@rstudio.org>
2012-05-01 13:26:25 -0700

    add another glib warning to stdout filter

--- src/cpp/session/modules/SessionConsole.cpp
+++ src/cpp/session/modules/SessionConsole.cpp
@@ -39,6 +39,7 @@ bool suppressOutput(const std::string& output)
 {
    // tokens to suppress
    const char * const kGlibWarningToken = "GLib-WARNING **: getpwuid_r()";
+   const char * const kGlibGObjectWarningToken = "GLib-GObject-WARNING **:";
    const char * const kAutoreleaseNoPool = "utoreleaseNoPool";
    const char * const kSelectInterrupted = "select: Interrupted system call";
    const char * const kNotAGitRepo = "Not a git repository";
@@ -46,6 +47,7 @@ bool suppressOutput(const std::string& output)
 
    // check tokens
    if (boost::algorithm::contains(output, kGlibWarningToken) ||
+       boost::algorithm::contains(output, kGlibGObjectWarningToken) ||
        boost::algorithm::contains(output, kAutoreleaseNoPool) ||
        boost::algorithm::contains(output, kSelectInterrupted) ||
        boost::algorithm::contains(output, kNotAGitRepo) ||
