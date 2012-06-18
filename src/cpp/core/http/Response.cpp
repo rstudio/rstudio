d53a61e05c5770317a1b5cb02cf96b5e0eed6a9d
JJ Allaire <jj@rstudio.org>
2012-06-18 15:18:25 -0400

    Revert "html escape http error status messages"
    This reverts commit d7d48bc5747b469221f82044349821ef20410e63.

--- src/cpp/core/http/Response.cpp
+++ src/cpp/core/http/Response.cpp
@@ -24,7 +24,6 @@
 #include <core/http/Util.hpp>
 #include <core/http/Cookie.hpp>
 #include <core/Hash.hpp>
-#include <core/StringUtils.hpp>
 
 namespace core {
 namespace http {
@@ -34,7 +33,7 @@ Response::Response()
 {
 }   
    
-const std::string& Response::statusMessage() const
+const std::string& Response::statusMessage() const    
 { 
    ensureStatusMessage();  
    return statusMessage_; 
@@ -147,9 +146,7 @@ void Response::setError(int statusCode, const std::string& message)
    setStatusCode(statusCode);
    removeCachingHeaders();
    setContentType("text/plain");
-   std::string escapedMessage = string_utils::htmlEscape(message,
-                                                         false);
-   setBodyUnencoded(escapedMessage);
+   setBodyUnencoded(message);
 }
    
 void Response::setError(const Error& error)
