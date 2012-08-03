cd76afc9a1ff823a2ad8fe2119a601f86e54f2ff
JJ Allaire <jj@rstudio.org>
2012-08-03 02:32:45 -0700

    add functions to save and restore undo state

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/ace/UndoManager.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/ace/UndoManager.java
@@ -13,6 +13,10 @@
 package org.rstudio.studio.client.workbench.views.source.editors.text.ace;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
 
 public class UndoManager extends JavaScriptObject
 {
@@ -22,4 +26,42 @@ public class UndoManager extends JavaScriptObject
    public native final JavaScriptObject peek() /*-{
       return this.peek();
    }-*/;
-}
+   
+   public final String saveState()
+   {
+      JavaScriptObject stateObj = saveStateNative();
+      return new JSONObject(stateObj).toString();
+   }
+   
+   public final void restoreState(String state)
+   {
+      JSONValue stateJson = JSONParser.parseStrict(state);
+      JSONObject stateObj = stateJson.isObject();
+      
+      JSONArray undoStack = stateObj.get("undoStack").isArray();
+      setUndoStack(undoStack.getJavaScriptObject());
+      
+      JSONArray redoStack = stateObj.get("redoStack").isArray();
+      setRedoStack(redoStack.getJavaScriptObject());
+   }
+   
+   
+   private native final JavaScriptObject saveStateNative() /*-{
+      var state = new Object();
+      state.undoStack = this.$undoStack; 
+      state.redoStack = this.$redoStack; 
+      return state;
+   }-*/;
+   
+   private native final void setUndoStack(JavaScriptObject undoStack) /*-{
+      this.$undoStack.length = 0;
+      for(var i=0,len=undoStack.length; i<len; i++)
+         this.$undoStack.push(undoStack[i]);
+   }-*/;
+   
+   private native final void setRedoStack(JavaScriptObject redoStack) /*-{
+       this.$redoStack.length = 0;
+       for(var i=0,len=redoStack.length; i<len; i++)
+         this.$redoStack.push(redoStack[i]);
+   }-*/;
+}   
