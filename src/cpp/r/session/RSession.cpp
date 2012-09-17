7691b187a72cf40c2be1f86c005675e72b6c4e78
JJ Allaire <jj@rstudio.org>
2012-09-17 15:27:09 -0400

    correct condition for quiet mode

--- src/cpp/r/session/RSession.cpp
+++ src/cpp/r/session/RSession.cpp
@@ -1196,7 +1196,8 @@ Error run(const ROptions& options, const RCallbacks& callbacks)
                        || !s_suspendedSessionPath.exists()
                        || options.rProfileOnResume;
 
-   bool quiet = s_suspendedSessionPath.exists();
+   bool quiet =  !restartContext().hasSessionState() &&
+                 s_suspendedSessionPath.exists();
 
    r::session::Callbacks cb;
    cb.showMessage = RShowMessage;
