e6d7f1e9749a5f7e05c1f9be1168917c0b48ba90
JJ Allaire <jj@rstudio.org>
2012-10-03 12:49:01 -0400

    enable debug build of rstudio installer on windows

--- package/win32/make-install-win64.bat
+++ package/win32/make-install-win64.bat
@@ -1,6 +1,8 @@
 REM setup variables
 setlocal
-set WIN64_BUILD_PATH=build64 
+set WIN64_BUILD_PATH=build64
+IF "%CMAKE_BUILD_TYPE%" == "" set CMAKE_BUILD_TYPE=Release
+IF "%CMAKE_BUILD_TYPE%" == "Debug" set WIN64_BUILD_PATH=build64-debug
 set MINGW64_PATH=%CD%\..\..\dependencies\windows\mingw64\bin
 set INSTALL_PATH=%1%
 
@@ -14,7 +16,7 @@ del CMakeCache.txt
 cmake -G"MinGW Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:String=%INSTALL_PATH% ^
       -DRSTUDIO_TARGET=SessionWin64 ^
-      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
       -DRSTUDIO_PACKAGE_BUILD=1 ^
       ..\..\..
 mingw32-make install
--- package/win32/make-package.bat
+++ package/win32/make-package.bat
@@ -8,24 +8,30 @@ REM Prepend Qt 4.8 SDK to path
 setlocal
 set PATH=C:\QtSDK\Desktop\Qt\4.8.0\mingw\bin;C:\QtSDK\mingw\bin;%PATH%
 
+
+REM Establish build dir
+set BUILD_DIR=build
+IF "%CMAKE_BUILD_TYPE%" == "" set CMAKE_BUILD_TYPE=Release
+IF "%CMAKE_BUILD_TYPE%" == "Debug" set BUILD_DIR=build-debug
+
 REM perform 32-bit build 
 cd "%PACKAGE_DIR%"
-mkdir build
-cd build
+mkdir "%BUILD_DIR%"
+cd "%BUILD_DIR%"
 del CMakeCache.txt
-rmdir /s /q build\_CPack_Packages
-cmake -G"MinGW Makefiles" -DRSTUDIO_TARGET=Desktop -DCMAKE_BUILD_TYPE=Release -DRSTUDIO_PACKAGE_BUILD=1 ..\..\..
+rmdir /s /q "%BUILD_DIR%\_CPack_Packages"
+cmake -G"MinGW Makefiles" -DRSTUDIO_TARGET=Desktop -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DRSTUDIO_PACKAGE_BUILD=1 ..\..\..
 mingw32-make 
 cd ..
 
 REM perform 64-bit build and install it into the 32-bit tree
 REM (but only do this if we are on win64)
-IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" call make-install-win64.bat "%PACKAGE_DIR%\build\src\cpp\session" %1
+IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" call make-install-win64.bat "%PACKAGE_DIR%\%BUILD_DIR%\src\cpp\session" %1
 
 REM create packages
-cd build
+cd "%BUILD_DIR%"
 cpack -G NSIS
-cpack -G ZIP
+IF "%CMAKE_BUILD_TYPE%" == "Release" cpack -G ZIP
 cd ..
 
 REM reset modified environment variables (PATH)
