e1b8c4ce968444acb26cac5d9e98bd0fc0b3dda6
JJ Allaire <jj@rstudio.org>
2012-10-03 13:31:04 -0400

    recycle bin support for windows

--- src/cpp/core/system/recycle_bin/Win32RecycleBin.cpp
+++ src/cpp/core/system/recycle_bin/Win32RecycleBin.cpp
@@ -14,13 +14,48 @@
 #include <core/Error.hpp>
 #include <core/FilePath.hpp>
 
+#include <windows.h>
+#include <shellapi.h>
+
 namespace core {
 namespace system {
 namespace recycle_bin {
       
 Error sendTo(const FilePath& filePath)
 {
-   return filePath.remove();
+   // get the path and double-null terminate
+   std::wstring wPath = filePath.absolutePathW();
+   std::vector<wchar_t> buffPath;
+   std::copy(wPath.begin(), wPath.end(), std::back_inserter(buffPath));
+   buffPath.push_back(L'\0');
+   buffPath.push_back(L'\0');
+
+   SHFILEOPSTRUCTW fileOp;
+   fileOp.hwnd = NULL;
+   fileOp.wFunc = FO_DELETE;
+   fileOp.pFrom = &(buffPath[0]);
+   fileOp.pTo = L"";
+   fileOp.fFlags = FOF_ALLOWUNDO |
+                   FOF_NOCONFIRMATION |
+                   FOF_NOERRORUI |
+                   FOF_SILENT;
+   fileOp.fAnyOperationsAborted = FALSE;
+   fileOp.hNameMappings = NULL;
+   fileOp.lpszProgressTitle = L"";
+
+   int result = ::SHFileOperationW(&fileOp);
+   if (result != 0)
+   {
+      Error error = systemError(boost::system::errc::protocol_error,
+                                ERROR_LOCATION);
+      error.addProperty("result", result);
+      error.addProperty("path", filePath);
+      return error;
+   }
+   else
+   {
+      return Success();
+   }
 }
 
 } // namespace recycle_bin
