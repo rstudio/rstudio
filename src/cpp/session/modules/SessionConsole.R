3cb65d95c9f7f4509747cb141addd67106d16ab9
JJ Allaire <jj@rstudio.org>
2013-01-08 13:59:34 -0500

    update copyright headers

--- src/cpp/session/modules/SessionConsole.R
+++ src/cpp/session/modules/SessionConsole.R
@@ -4,7 +4,9 @@
 #
 # Copyright (C) 2009-12 by RStudio, Inc.
 #
-# This program is licensed to you under the terms of version 3 of the
+# Unless you have received this program directly from RStudio pursuant
+# to the terms of a commercial license agreement with RStudio, then
+# this program is licensed to you under the terms of version 3 of the
 # GNU Affero General Public License. This program is distributed WITHOUT
 # ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
 # MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
