19b3a49929905b9ffa13534d9b263f3fb207f748
JJ Allaire <jj@rstudio.org>
2013-01-21 06:53:32 -0500

    allow recovery after failed fix

--- src/cpp/session/modules/SessionWorkspace.R
+++ src/cpp/session/modules/SessionWorkspace.R
@@ -92,21 +92,44 @@
 
 .rs.addFunction("registerFunctionEditor", function() {
 
+   # save default editor
    defaultEditor <- getOption("editor")
   
+   # ensure we have a scratch file
+   scratchFile <- tempfile()
+   cat("", file = scratchFile)
+   
    options(editor = function(name, file, title) {
-      
-      # use the RStudio editor for functions, otherwise delegate to default
-      if (is.function(name)) {
+       
+      # use internal editor for files and functions, otherwise 
+      # delegate to the default editor
+      if (is.null(name) || is.function(name)) {
          
-         # write the function source to a file
-         functionSrc <- .rs.deparseFunction(name, TRUE)
-         functionFile <- tempfile()
-         writeLines(functionSrc, functionFile)
+         # if no name then use file
+         if (is.null(name)) {
+            if (!is.null(file) && nzchar(file)) 
+               targetFile <- file
+            else
+               targetFile <- scratchFile
+         }
+         # otherwise it's a function, write it to a file for editing
+         else {
+            functionSrc <- .rs.deparseFunction(name, TRUE)
+            targetFile <- scratchFile
+            writeLines(functionSrc, targetFile)
+         }
          
          # invoke the RStudio editor on the file
-         if (.Call("rs_editFile", functionFile)) {
-            newFunc <- eval.parent(parse(functionFile))
+         if (.Call("rs_editFile", targetFile)) {
+
+            # try to parse it back in
+            newFunc <- try(eval.parent(parse(targetFile)),
+                           silent = TRUE)
+            if (inherits(newFunc, "try-error")) {
+               stop(newFunc, "You can attempt to correct the error using ",
+                    title, " = edit()")
+            }
+            
             return(newFunc)
          } 
          else {
