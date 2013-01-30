a92bac84d82bbb641f6d82197e81ec6c69298775
andrewbachmann@google.com <andrewbachmann@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2013-01-30 00:44:34 +0000

    correct RTL documentation
    Review by: goktug@google.com
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@11483 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/client/ui/CustomScrollPanel.java
+++ user/src/com/google/gwt/user/client/ui/CustomScrollPanel.java
@@ -557,7 +557,7 @@ public class CustomScrollPanel extends ScrollPanel {
    * corner.
    * 
    * <p>
-   * In RTL, the vertical scrollbar appears on the right.
+   * In RTL, the vertical scrollbar appears on the left.
    */
   private void maybeUpdateScrollbars() {
     if (!isAttached()) {
