ee5d509d718ddfbfaa63959d73dd965a37e494cb
skybrian@google.com <skybrian@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2013-02-04 21:11:34 +0000

    Add a webApiUsage property to control whether prefixed API's will be used.
    This is a simple implementation that doesn't include browser or
    feature-specific settings.
    
    Change-Id: I416794f99bafa78a9a0ef84ef698d372c8263613
    Review-Link: https://gwt-review.googlesource.com/#/c/1840/
    
    Review by: goktug@google.com
    
    git-svn-id: https://google-web-toolkit.googlecode.com/svn/trunk@11495 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/test/com/google/gwt/animation/AnimationSuite.java
+++ user/test/com/google/gwt/animation/AnimationSuite.java
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.animation;
 
+import com.google.gwt.animation.client.AnimationApiUsageTest;
 import com.google.gwt.animation.client.AnimationSchedulerImplTimerTest;
 import com.google.gwt.animation.client.AnimationTest;
 
@@ -28,6 +29,7 @@ public class AnimationSuite {
   public static Test suite() {
     TestSuite suite = new TestSuite("Tests of the animation package");
 
+    suite.addTestSuite(AnimationApiUsageTest.class);
     suite.addTestSuite(AnimationSchedulerImplTimerTest.class);
     suite.addTestSuite(AnimationTest.class);
 
