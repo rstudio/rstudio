8baece81d86a186582ccd47d2c6f4b7a4b962cc6
JJ Allaire <jj@rstudio.org>
2013-02-04 08:27:39 -0500

    look for Rtools v3.0

--- src/cpp/core/r_util/RToolsInfo.cpp
+++ src/cpp/core/r_util/RToolsInfo.cpp
@@ -81,7 +81,7 @@ RToolsInfo::RToolsInfo(const std::string& name, const FilePath& installPath)
       relativePathEntries.push_back("bin");
       relativePathEntries.push_back("gcc-4.6.3/bin");
    }
-   else if (name == "2.16")
+   else if (name == "2.16" || name == "3.0")
    {
       versionMin = "2.15.2";
       versionMax = "3.0.0";
