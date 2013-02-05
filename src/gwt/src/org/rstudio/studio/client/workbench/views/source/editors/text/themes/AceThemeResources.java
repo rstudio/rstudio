30de3921b4c8f31ff534d2e79c9aa1af671cb7da
Matt Morrison <matthewdmorrison@gmail.com>
2013-02-05 11:53:40 -0500

    add new themes
    Added neon.css and existing (but unused) .css themes to AceThemes.java
    and AceThemesResources.java, changed size of Preferences dialog box to
    accomodate extended theme list.

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/themes/AceThemeResources.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/themes/AceThemeResources.java
@@ -3,9 +3,7 @@
  *
  * Copyright (C) 2009-12 by RStudio, Inc.
  *
- * Unless you have received this program directly from RStudio pursuant
- * to the terms of a commercial license agreement with RStudio, then
- * this program is licensed to you under the terms of version 3 of the
+ * This program is licensed to you under the terms of version 3 of the
  * GNU Affero General Public License. This program is distributed WITHOUT
  * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
  * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
@@ -19,8 +17,26 @@ import org.rstudio.core.client.resources.StaticDataResource;
 
 public interface AceThemeResources extends ClientBundle
 {
-   @Source("textmate.css")
-   StaticDataResource textmate();
+   @Source("chrome.css")
+   StaticDataResource chrome();
+
+   @Source("clouds.css")
+   StaticDataResource clouds();
+
+   @Source("clouds_midnight.css")
+   StaticDataResource clouds_midnight();
+
+   @Source("cobalt.css")
+   StaticDataResource cobalt();
+
+   @Source("crimson_editor.css")
+   StaticDataResource crimson_editor();
+
+   @Source("dawn.css")
+   StaticDataResource dawn();
+
+   @Source("dreamweaver.css")
+   StaticDataResource dreamweaver();
 
    @Source("eclipse.css")
    StaticDataResource eclipse();
@@ -28,30 +44,55 @@ public interface AceThemeResources extends ClientBundle
    @Source("idle_fingers.css")
    StaticDataResource idle_fingers();
 
-   @Source("twilight.css")
-   StaticDataResource twilight();
+   @Source("kr_theme.css")
+   StaticDataResource kr_theme();
 
-   @Source("cobalt.css")
-   StaticDataResource cobalt();
+   @Source("merbivore.css")
+   StaticDataResource merbivore();
 
-   @Source("solarized_light.css")
-   StaticDataResource solarized();
+   @Source("merbivore_soft.css")
+   StaticDataResource merbivore_soft();
+
+   @Source("mono_industrial.css")
+   StaticDataResource mono_industrial();
+
+   @Source("monokai.css")
+   StaticDataResource monokai();
+
+   @Source("neon.css")
+   StaticDataResource neon();
+
+   @Source("pastel_on_dark.css")
+   StaticDataResource pastel_on_dark();
 
    @Source("solarized_dark.css")
-   StaticDataResource solarizedDark();
+   StaticDataResource solarized_dark();
+
+   @Source("solarized_light.css")
+   StaticDataResource solarized_light();
+
+   @Source("textmate.css")
+   StaticDataResource textmate();
 
    @Source("tomorrow.css")
    StaticDataResource tomorrow();
 
    @Source("tomorrow_night.css")
    StaticDataResource tomorrow_night();
-   
+
    @Source("tomorrow_night_blue.css")
    StaticDataResource tomorrow_night_blue();
-   
+
    @Source("tomorrow_night_bright.css")
-   StaticDataResource tomorrow_night_bright();  
-   
+   StaticDataResource tomorrow_night_bright();
+
    @Source("tomorrow_night_eighties.css")
-   StaticDataResource tomorrow_night_eighties(); 
+   StaticDataResource tomorrow_night_eighties();
+
+   @Source("twilight.css")
+   StaticDataResource twilight();
+
+   @Source("vibrant_ink.css")
+   StaticDataResource vibrant_ink();
+
 }
