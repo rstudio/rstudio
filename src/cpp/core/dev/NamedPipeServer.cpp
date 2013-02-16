6da12711f0c966267f0196e3d63d763f46cfdda5
JJ Allarie <jj@rstudio.org>
2013-02-16 08:44:02 -0500

    more work on named pipe server

--- src/cpp/core/dev/NamedPipeServer.cpp
+++ src/cpp/core/dev/NamedPipeServer.cpp
@@ -1,6 +1,8 @@
 #include "NamedPipeServer.hpp"
 
 #include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <boost/enable_shared_from_this.hpp>
 
 #include <core/Error.hpp>
 #include <core/Log.hpp>
@@ -8,6 +10,7 @@
 #include <core/http/Util.hpp>
 #include <core/http/Request.hpp>
 #include <core/http/Response.hpp>
+#include <core/http/RequestParser.hpp>
 
 using namespace core;
 
@@ -18,14 +21,78 @@ bool isConnectionTerminatedError(const Error& error)
    return false;
 }
 
-class NamedPipeHttpConnection : public HttpConnection, boost::noncopyable
+class NamedPipeHttpConnection :
+            public HttpConnection,
+            public boost::enable_shared_from_this<NamedPipeHttpConnection>,
+            boost::noncopyable
 {
 public:
-   NamedPipeHttpConnection(boost::asio::io_service& ioService, HANDLE hPipe)
-      : stream_(ioService, hPipe)
+   typedef boost::function<void(
+         boost::shared_ptr<NamedPipeHttpConnection>)> Handler;
+
+
+public:
+   NamedPipeHttpConnection(boost::asio::io_service& ioService,
+                           HANDLE hPipe,
+                           const Handler& handler)
+      : stream_(ioService, hPipe), handler_(handler)
    {
-      // parse the request (can use incremental parsing to stop
-      // reading when the request is fully submitted)
+   }
+
+   void readRequest()
+   {
+      boost::array<char, 8192> buffer;
+      http::RequestParser parser;
+
+      while (true)
+      {
+         boost::system::error_code ec;
+         std::size_t bytesRead = stream_.read_some(boost::asio::buffer(buffer),
+                                                   ec);
+         if (!ec)
+         {
+            // parse next chunk
+            http::RequestParser::status status = parser.parse(
+                                       request_,
+                                       buffer.data(),
+                                       buffer.data() + bytesRead);
+
+            // error - return bad request
+            if (status == core::http::RequestParser::error)
+            {
+               core::http::Response response;
+               response.setStatusCode(core::http::status::BadRequest);
+               sendResponse(response);
+
+               // this object now dies on it's own since we don't pass
+               // the shared ptr to the handler
+            }
+
+            // incomplete -- keep reading
+            else if (status == core::http::RequestParser::incomplete)
+            {
+               continue;
+            }
+
+            // got valid request -- handle it
+            else
+            {
+               handler_(NamedPipeHttpConnection::shared_from_this());
+               break;
+            }
+         }
+         else
+         {
+            // log if not connection terminated
+            Error error(ec, ERROR_LOCATION);
+            if (!isConnectionTerminatedError(error))
+               LOG_ERROR(error);
+
+            // request dies here
+            close();
+         }
+      }
+
 
    }
 
@@ -84,6 +151,7 @@ public:
 
 private:
    boost::asio::windows::stream_handle stream_;
+   Handler handler_;
    http::Request request_;
 };
 
