00102c9c607c0bb9e27f402379c176d50c532749
JJ Allarie <jj@rstudio.org>
2013-04-18 07:14:26 -0400

    Rtools 3.0 compatiblity with R-devel

--- src/cpp/core/r_util/RToolsInfo.cpp
+++ src/cpp/core/r_util/RToolsInfo.cpp
@@ -84,7 +84,7 @@ RToolsInfo::RToolsInfo(const std::string& name, const FilePath& installPath)
    else if (name == "2.16" || name == "3.0")
    {
       versionMin = "2.15.2";
-      versionMax = "3.0.0";
+      versionMax = "3.1.0";
       relativePathEntries.push_back("bin");
       relativePathEntries.push_back("gcc-4.6.3/bin");
    }
