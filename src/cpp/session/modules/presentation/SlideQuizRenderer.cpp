d22e997284e9a85dede123b79deb44115ed40e68
JJ Allaire <jj@rstudio.org>
2013-05-13 08:45:08 -0400

    stubs for quiz renderer

---
+++ src/cpp/session/modules/presentation/SlideQuizRenderer.cpp
@@ -0,0 +1,35 @@
+/*
+ * SlideQuizRenderer.cpp
+ *
+ * Copyright (C) 2009-12 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#include "SlideQuizRenderer.hpp"
+
+namespace session {
+namespace modules { 
+namespace presentation {
+
+namespace {
+
+
+} // anonymous namespace
+
+void renderQuiz(std::string* pHTML)
+{
+
+}
+
+} // namespace presentation
+} // namespace modules
+} // namesapce session
+
---
+++ src/cpp/session/modules/presentation/SlideQuizRenderer.hpp
@@ -0,0 +1,31 @@
+/*
+ * SlideQuizRenderer.hpp
+ *
+ * Copyright (C) 2009-12 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#ifndef SESSION_PRESENTATION_SLIDE_QUIZ_RENDERER_HPP
+#define SESSION_PRESENTATION_SLIDE_QUIZ_RENDERER_HPP
+
+#include <string>
+
+namespace session {
+namespace modules { 
+namespace presentation {
+
+void renderQuiz(std::string* pHTML);
+
+} // namespace presentation
+} // namespace modules
+} // namesapce session
+
+#endif // SESSION_PRESENTATION_SLIDE_QUIZ_RENDERER_HPP
