fb5051df57948287db49a42d6acfc9f6d4fec4b1
JJ Allaire <jj@rstudio.org>
2013-05-26 10:04:05 -0400

    call slide deck overlay hook

--- src/cpp/session/modules/presentation/SlideRequestHandler.cpp
+++ src/cpp/session/modules/presentation/SlideRequestHandler.cpp
@@ -726,6 +726,7 @@ void handlePresentationRootRequest(const std::string& path,
 
    // notify slide deck changed
    log().onSlideDeckChanged(slideDeck);
+   onSlideDeckChangedOverlay(slideDeck);
 
    // set preload to none for media
    vars["slides"] = boost::algorithm::replace_all_copy(
