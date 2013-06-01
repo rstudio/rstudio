0edafa41fd1f9fd55ba8b509c6ed224de1d5d669
Daniel Kurka <dankurka@google.com>
2013-06-01 03:05:10 +0000

    Revert "Adds onEnsureDebugId delegation to Composite."
    This reverts commit ff027f069482154b94c5b17434824992017d0f23
    
    Change-Id: Id162c4e9b553a5981f09d26a3d1112dbf085825e

--- user/src/com/google/gwt/user/client/ui/Composite.java
+++ user/src/com/google/gwt/user/client/ui/Composite.java
@@ -184,11 +184,6 @@ public abstract class Composite extends Widget implements IsRenderable {
       widget.onDetach();
     }
   }
-  
-  @Override
-  protected void onEnsureDebugId(String baseID) {
-    widget.onEnsureDebugId(baseID);
-  }
 
   @Override
   protected Element resolvePotentialElement() {
