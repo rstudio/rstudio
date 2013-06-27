f8dcbb8bc8e0352f930888c06a696f9e3754c369
JJ Allaire <jj@rstudio.org>
2013-06-27 07:57:42 -0700

    refine gwt stack trace filter

--- src/cpp/core/gwt/GwtLogHandler.cpp
+++ src/cpp/core/gwt/GwtLogHandler.cpp
@@ -91,6 +91,10 @@ bool isExceptionMechanismElement(const StackElement& element)
 
           ||
 
+          boost::algorithm::ends_with(element.methodName, "createStackTrace")
+
+          ||
+
           (boost::algorithm::starts_with(element.fileName,
                                         "com/google/gwt/emul/java/lang/")
           &&
