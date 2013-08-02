3b9805cdea7a7315604b35f08c8091071b3d0d2c
Matthew Dempsky <mdempsky@google.com>
2013-08-02 00:02:05 +0000

    Revert "Improve CompilationUnitInvalidator performance for large GWT compiles by reducing copying and preferring map-lookups over sequential scanning."
    This reverts commit e36d7ff5d7ae26782b56542f587b8ca9972d065b.
    
    Change-Id: I181f32e9349ea871acff15363342262441166b22

--- dev/core/src/com/google/gwt/dev/javac/CompilationUnitInvalidator.java
+++ dev/core/src/com/google/gwt/dev/javac/CompilationUnitInvalidator.java
@@ -16,13 +16,15 @@
 package com.google.gwt.dev.javac;
 
 import com.google.gwt.core.ext.TreeLogger;
-import com.google.gwt.thirdparty.guava.common.collect.HashMultimap;
-import com.google.gwt.thirdparty.guava.common.collect.Multimap;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 
 /**
  * Helper class to invalidate units in a set based on errors or references to
@@ -30,94 +32,50 @@ import java.util.Set;
  */
 public class CompilationUnitInvalidator {
 
-  /**
-   * Mutates {@code units} by retaining only valid units.  A unit is invalid if
-   * it
-   * <ul>
-   * <li>has errors,
-   * <li>depends on a member of {@code validClasses} that has errors,
-   * <li>depends on another member of {@code units} that has errors, or
-   * <li>references a source name that is not provided by a good member of
-   * {@code units} or {@code validClasses}.
-   * </ul>
-   */
   public static void retainValidUnits(TreeLogger logger,
       Collection<CompilationUnit> units, Map<String, CompiledClass> validClasses) {
     logger = logger.branch(TreeLogger.TRACE, "Removing invalidated units");
 
-    // Build a map of api-refs -> dependent units.
-    // This map excludes refs provided by good validClasses; it only contains
-    // dependencies that need to be provided by members of units.
-    Multimap<String, CompilationUnit> depsNeeded = HashMultimap.create();
-
-    // This set contains the source names of types provided by units, and after
-    // initial population, may shrink as problems are discovered in individual
-    // units.
-    Set<String> depsProvided = new HashSet<String>();
-
-    // These are all of the invalid units
-    Set<CompilationUnit> allBrokenUnits = new HashSet<CompilationUnit>();
-
-    // Populate depsNeeded, depsProvided, and allBrokenUnits with their initial values.
-    // At first, only compilation units that directly contain an error are known to be
-    // broken, not their dependencies.
+    // Assume all units are valid at first.
+    Set<CompilationUnit> currentlyValidUnits = new LinkedHashSet<CompilationUnit>();
+    Set<String> currentlyValidClasses = new HashSet<String>();
     for (CompilationUnit unit : units) {
-      if (unit.isError()) {
-        // It is bad and can be removed immediately
-        allBrokenUnits.add(unit);
-      } else {
-        // Update set of dependencies the unit provides
+      if (!unit.isError()) {
+        currentlyValidUnits.add(unit);
         for (CompiledClass cc : unit.getCompiledClasses()) {
-          depsProvided.add(cc.getSourceName());
-        }
-
-        // Update map of dependencies that the unit needs
-        for (String ref : unit.getDependencies().getApiRefs()) {
-          // Check validClasses
-          CompiledClass compiledClass = validClasses.get(ref);
-          if ((compiledClass == null) || compiledClass.getUnit().isError()) {
-            // we'll put this into the double-check pot
-            depsNeeded.put(ref, unit);
-          }
+          currentlyValidClasses.add(cc.getSourceName());
         }
       }
     }
+    for (Entry<String, CompiledClass> entry : validClasses.entrySet()) {
+      if (!entry.getValue().getUnit().isError()) {
+        currentlyValidClasses.add(entry.getKey());
+      }
+    }
 
-    // Repeatedly remove CompilationUnits that have a dependency that's known to
-    // be broken.
-    Multimap<String, CompilationUnit> missing;
+    boolean changed;
     do {
-      // Find the missing deps for this pass
-      missing = HashMultimap.create();
-      missing.putAll(depsNeeded);
-      missing.keySet().removeAll(depsProvided);
-
-      // Process the units with missing deps
-      for (Map.Entry<String, CompilationUnit> brokenEntry : missing.entries()) {
-        CompilationUnit brokenUnit = brokenEntry.getValue();
-
-        // Modify depsProvided for newly discovered broken units
-        // (side-effect add in 'if' condition)
-        if (allBrokenUnits.add(brokenUnit)) {
-          // Remove the broken unit from the provides set
-          for (CompiledClass cc : brokenUnit.getCompiledClasses()) {
-            depsProvided.remove(cc.getSourceName());
+      changed = false;
+      for (Iterator<CompilationUnit> it = currentlyValidUnits.iterator(); it.hasNext();) {
+        CompilationUnit unitToCheck = it.next();
+        List<String> invalidRefs = unitToCheck.getDependencies().findMissingApiRefs(
+            currentlyValidClasses);
+        if (invalidRefs.size() > 0) {
+          it.remove();
+          for (CompiledClass cc : unitToCheck.getCompiledClasses()) {
+            currentlyValidClasses.remove(cc.getSourceName());
           }
-
-          // Log it to maintain some logging compatibility with prior versions
-          // of this class.
+          changed = true;
           TreeLogger branch = logger.branch(TreeLogger.DEBUG,
-              "Compilation unit '" + brokenUnit
-              + "' is removed due to invalid reference(s):");
-          branch.log(TreeLogger.DEBUG, brokenEntry.getKey());
+              "Compilation unit '" + unitToCheck
+                  + "' is removed due to invalid reference(s):");
+          for (String ref : invalidRefs) {
+            branch.log(TreeLogger.DEBUG, ref);
+          }
         }
       }
+    } while (changed);
 
-      // Having found and removed some units with missing deps, remove their
-      // needs.
-      depsNeeded.keySet().removeAll(missing.keySet());
-    } while (!missing.isEmpty());
-
-    units.removeAll(allBrokenUnits);
+    units.retainAll(currentlyValidUnits);
   }
 }
