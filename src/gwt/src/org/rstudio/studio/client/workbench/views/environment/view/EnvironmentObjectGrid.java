03fee5c8919b63f792ba0569adc69ef10573d51b
Jonathan McPherson <jonathan@rstudio.com>
2013-08-30 15:01:39 -0700

    add simple columns to grid view (warning: ugly)

--- src/gwt/src/org/rstudio/studio/client/workbench/views/environment/view/EnvironmentObjectGrid.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/environment/view/EnvironmentObjectGrid.java
@@ -1,5 +1,8 @@
 package org.rstudio.studio.client.workbench.views.environment.view;
 
+import org.rstudio.core.client.StringUtil;
+import org.rstudio.studio.client.common.FilePathUtils;
+
 import com.google.gwt.cell.client.ClickableTextCell;
 import com.google.gwt.user.cellview.client.Column;
 
@@ -22,6 +25,42 @@ public class EnvironmentObjectGrid extends EnvironmentObjectDisplay
                   {
                      return object.rObject.getName();
                   }
-              });
+              }, "Name");
+      addColumn(new Column<RObjectEntry, String>(
+              new ClickableTextCell())
+              {
+                  @Override
+                  public String getValue(RObjectEntry object)
+                  {
+                     return object.rObject.getType();
+                  }
+              }, "Type");
+      addColumn(new Column<RObjectEntry, String>(
+              new ClickableTextCell())
+              {
+                  @Override
+                  public String getValue(RObjectEntry object)
+                  {
+                     return (new Integer(object.rObject.getLength())).toString();
+                  }
+              }, "Length");
+      addColumn(new Column<RObjectEntry, String>(
+              new ClickableTextCell())
+              {
+                  @Override
+                  public String getValue(RObjectEntry object)
+                  {
+                     return StringUtil.formatFileSize(object.rObject.getSize());
+                  }
+              }, "Size");
+      addColumn(new Column<RObjectEntry, String>(
+              new ClickableTextCell())
+              {
+                  @Override
+                  public String getValue(RObjectEntry object)
+                  {
+                     return object.rObject.getValue();
+                  }
+              }, "Value");
    }
 }
