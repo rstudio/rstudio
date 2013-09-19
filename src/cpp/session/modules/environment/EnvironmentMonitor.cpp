f09d5db40b594f4725e4d2385121927c9580be88
Jonathan McPherson <jonathan@rstudio.com>
2013-09-19 07:15:51 -0700

    fix crash in R some implementations on R_ReleaseObject(NULL)

--- src/cpp/session/modules/environment/EnvironmentMonitor.cpp
+++ src/cpp/session/modules/environment/EnvironmentMonitor.cpp
@@ -70,7 +70,6 @@ void removeVarFromList(std::vector<r::sexp::Variable>* pEnv,
 } // anonymous namespace
 
 EnvironmentMonitor::EnvironmentMonitor() :
-   environment_(NULL),
    initialized_(false),
    refreshOnInit_(false)
 {}
