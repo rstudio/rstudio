aa06d624bafbc63544ab4eaf0f50494b7b940cfd
JJ Allaire <jj@rstudio.org>
2013-10-07 14:27:04 -0400

    improve template for simply shiny app

--- src/cpp/session/resources/templates/shiny/ui.R
+++ src/cpp/session/resources/templates/shiny/ui.R
@@ -1,3 +1,10 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+# 
+# http://www.rstudio.com/shiny/
+#
+
 library(shiny)
 
 shinyUI(pageWithSidebar(
