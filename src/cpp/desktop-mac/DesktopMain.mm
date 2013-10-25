4bb5752b71408d56eeb95807493e9cc0702473a6
JJ Allaire <jj@rstudio.org>
2013-10-25 12:18:22 -0400

    fully reusable controller

--- src/cpp/desktop-mac/DesktopMain.mm
+++ src/cpp/desktop-mac/DesktopMain.mm
@@ -83,20 +83,50 @@
    return NO;
 }
 
-
-
 @end
 
 
 @interface WebViewController : NSWindowController<NSWindowDelegate> {
-   
+   WebView* webView_;
 }
+-(WebView*) webView;
 @end
 
 @implementation WebViewController
 
-- (id)initWithWindow:(NSWindow *)window
+- (WebView*) webView
 {
+   return webView_;
+}
+
+- (id)initWithURLRequest: (NSURLRequest*) request
+{
+   
+   // Style flags
+   NSUInteger windowStyle =
+     (NSTitledWindowMask | NSClosableWindowMask | NSResizableWindowMask);
+   
+   // Window bounds (x, y, width, height)
+   NSRect windowRect = NSMakeRect(100, 100, 1024, 768);
+   
+   NSWindow * window = [[[NSWindow alloc] initWithContentRect:windowRect
+                                                   styleMask:windowStyle
+                                                     backing:NSBackingStoreBuffered
+                                                       defer:NO] autorelease];   
+   [window setDelegate: self];
+
+   
+   webView_ = [[[WebView alloc] initWithFrame:NSMakeRect(100,100,1024,768)] autorelease];
+   [webView_ setUIDelegate: self];
+   [webView_ setFrameLoadDelegate: self];
+   
+   
+   [[webView_ mainFrame] loadRequest:request];
+   [window setContentView:webView_];
+   
+   [window setTitle: @"RStudio"];
+   [window makeKeyAndOrderFront: nil];
+   
    return [super initWithWindow: window];
 }
 
@@ -107,44 +137,10 @@
 
 - (WebView *)webView:(WebView *)sender createWebViewWithRequest:(NSURLRequest *)request
 {
-   // Style flags
-   NSUInteger windowStyle =
-   (NSTitledWindowMask |
-    NSClosableWindowMask |
-    NSResizableWindowMask);
-   
-   // Window bounds (x, y, width, height)
-   NSRect windowRect = NSMakeRect(100, 100, 1024, 768);
-   
-   NSWindow * window = [[NSWindow alloc] initWithContentRect:windowRect
-                                                   styleMask:windowStyle
-                                                     backing:NSBackingStoreBuffered
-                                                       defer:NO];
-   [window autorelease];
-   
-   // Window controller
-   WebViewController * windowController = [[WebViewController alloc] initWithWindow:window];
-   [window setDelegate: windowController];
-   
-   WebView *webView = [[[WebView alloc] initWithFrame:NSMakeRect(100,100,1024,768)] autorelease];
-   
-   [webView setUIDelegate: self];
-   [webView setFrameLoadDelegate: self];
-   
-   [[webView mainFrame] loadRequest:request];
-   
-   
-   
-   [window setContentView:webView];
-   
-   [window setTitle: @"RStudio Popup"];
-   [window makeKeyAndOrderFront: nil];
-   
-   
-   
-   return webView;
-   
+   WebViewController * windowController = [[WebViewController alloc] initWithURLRequest: request];   
+   return [windowController webView];
 }
+
 
 - (void)webView:(WebView *)webView windowScriptObjectAvailable:(WebScriptObject *)windowScriptObject {
    NSLog(@"%@ received %@", self, NSStringFromSelector(_cmd));
@@ -159,8 +155,6 @@
    NSLog(@"href: %@",href);
    
 }
-
-
 
 @end
 
@@ -180,55 +174,13 @@ int main(int argc, char* argv[])
    // This will initialize the global variable
    // 'NSApp' with the application instance.
    [NSApplication sharedApplication];
-
    
    // App Delegate
    AppDelegate* appDelegate = [[[AppDelegate alloc] init] autorelease];
    [NSApp setDelegate: appDelegate];
    
    
-   // Create a window:
-
-   // Style flags
-   NSUInteger windowStyle =
-   (NSTitledWindowMask |
-   NSClosableWindowMask |
-   NSResizableWindowMask);
-
-   // Window bounds (x, y, width, height)
-   NSRect windowRect = NSMakeRect(100, 100, 1024, 768);
-
-   NSWindow * window = [[NSWindow alloc] initWithContentRect:windowRect
-   styleMask:windowStyle
-   backing:NSBackingStoreBuffered
-   defer:NO];
-   [window autorelease];
-
-  
-   WebViewController * windowController = [[WebViewController alloc] initWithWindow:window];
-   [window setDelegate: windowController];
-   [windowController setWindowFrameAutosaveName: @"RStudio"];
-   
- 
-   
-
-   // Load content view
-   NSString *urlAddress = @"http://localhost:8787/webkit.nocache.html";
-   NSURL *url = [NSURL URLWithString:urlAddress];
-   NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
-   
-   WebView *webView = [[[WebView alloc] initWithFrame:NSMakeRect(100,100,1024,768)] autorelease];
-   [webView setUIDelegate: windowController];
-   [webView setFrameLoadDelegate: windowController];
-
-   
-   [[webView mainFrame] loadRequest:requestObj];
-   [window setContentView:webView];
-   
-   
-   
-   
-  
+   // create menubar
    id menubar = [[NSMenu new] autorelease];
    id appMenuItem = [[NSMenuItem new] autorelease];
    [menubar addItem:appMenuItem];
@@ -237,15 +189,24 @@ int main(int argc, char* argv[])
    id appName = [[NSProcessInfo processInfo] processName];
    id quitTitle = [@"Quit " stringByAppendingString:appName];
    id quitMenuItem = [[[NSMenuItem alloc] initWithTitle:quitTitle
-       action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
+                                                 action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
    [appMenu addItem:quitMenuItem];
    [appMenuItem setSubmenu:appMenu];
 
-   // Show window and run event loop
-   [window setTitle: @"RStudio"];
-   [window makeKeyAndOrderFront: nil];
-   [NSApp activateIgnoringOtherApps: YES];
+   
+   // Load content view
+   //NSString *urlAddress = @"http://localhost:8787/webkit.nocache.html";
+   NSString *urlAddress = @"http://www.google.com";
+   NSURL *url = [NSURL URLWithString:urlAddress];
+   NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+  
+   WebViewController * windowController = [[WebViewController alloc] initWithURLRequest: requestObj];
+   // TODO: this seems to work but it seems like it's too late?
+   [windowController setWindowFrameAutosaveName: @"RStudio"];
+   
 
+   [NSApp activateIgnoringOtherApps: YES];
+  
    [NSApp run];
 
    [pool drain];
