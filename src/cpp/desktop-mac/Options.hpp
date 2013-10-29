2a1c04541f95dfeaa2dcfc8b60d566341fbc2707
JJ Allaire <jj@rstudio.org>
2013-10-29 06:32:51 -0400

    initial work on options class

---
+++ src/cpp/desktop-mac/Options.hpp
@@ -0,0 +1,80 @@
+/*
+ * Options.hpp
+ *
+ * Copyright (C) 2009-12 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#ifndef DESKTOP_OPTIONS_HPP
+#define DESKTOP_OPTIONS_HPP
+
+#include <string>
+#include <vector>
+
+#include <boost/utility.hpp>
+
+#include <core/FilePath.hpp>
+
+#import <Foundation/NSArray.h>
+
+namespace desktop {
+
+class Options;
+Options& options();
+   
+class Options : boost::noncopyable
+{
+private:
+   Options();
+   friend Options& options();
+   
+public:
+   void initFromCommandLine(NSArray* arguments);
+
+   std::string portNumber() const;
+   std::string newPortNumber();
+   
+   std::string proportionalFont() const;
+   std::string fixedWidthFont() const;
+   void setFixedWidthFont(std::string font);
+   
+   double zoomLevel() const;
+   void setZoomLevel(double zoomLevel);
+      
+   core::FilePath scriptsPath() const;
+   void setScriptsPath(const core::FilePath& scriptsPath);
+   
+   core::FilePath executablePath() const;
+   core::FilePath supportingFilePath() const;
+   
+   core::FilePath wwwDocsPath() const;
+      
+   std::vector<std::string> ignoredUpdateVersions() const;
+   void setIgnoredUpdateVersions(const std::vector<std::string>& ignored);
+   
+   core::FilePath scratchTempDir(core::FilePath defaultPath=core::FilePath());
+   void cleanUpScratchTempDir();
+   
+   bool runDiagnostics() { return runDiagnostics_; }
+   
+private:   
+   core::FilePath scriptsPath_;
+   mutable core::FilePath executablePath_;
+   mutable core::FilePath supportingFilePath_;
+   mutable std::string portNumber_;
+   bool runDiagnostics_;
+   
+   
+};
+   
+} // namespace desktop
+
+#endif // DESKTOP_OPTIONS_HPP
---
+++ src/cpp/desktop-mac/Options.mm
@@ -0,0 +1,155 @@
+/*
+ * Options.mm
+ *
+ * Copyright (C) 2009-12 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+
+#include <core/FilePath.hpp>
+#include <core/Random.hpp>
+#include <core/SafeConvert.hpp>
+
+#import <Foundation/NSString.h>
+#import <Foundation/NSUserDefaults.h>
+#import <Foundation/NSDictionary.h>
+
+#import "Options.hpp"
+
+using namespace core;
+
+namespace desktop {
+   
+Options& options()
+{
+   static Options instance;
+   return instance;
+}
+   
+Options::Options()
+   : runDiagnostics_(false)
+{
+   // initialize user defaults
+   NSUserDefaults* prefs = [NSUserDefaults standardUserDefaults];
+   NSDictionary* defs = [NSDictionary dictionaryWithObjectsAndKeys:
+                             @"Lucida Grande", @"font.proportional",
+                             @"Monaco", @"font.fixed",
+                             nil];
+   [prefs registerDefaults:defs];
+}
+   
+void Options::initFromCommandLine(NSArray* arguments)
+{
+   for (NSString* arg in arguments)
+   {
+      if ([arg isEqualToString: @"--run-diagnostics"])
+         runDiagnostics_ = true;
+   }
+}
+
+std::string Options::portNumber() const
+{
+   // lookup / generate on demand
+   if (portNumber_.empty())
+   {
+      // Use a random-ish port number to avoid collisions between different
+      // instances of rdesktop-launched rsessions
+      int base = std::abs(random::uniformRandomInteger<int>());
+      int port = (base % 40000) + 8080;
+      portNumber_ = safe_convert::numberToString(port);
+   }
+   return portNumber_;
+}
+   
+std::string Options::newPortNumber()
+{
+   portNumber_.clear();
+   return portNumber();
+}
+
+std::string Options::proportionalFont() const
+{
+   NSUserDefaults* prefs = [NSUserDefaults standardUserDefaults];
+   return [[prefs stringForKey: @"font.proportional"] UTF8String];
+}
+
+std::string Options::fixedWidthFont() const
+{
+   NSUserDefaults* prefs = [NSUserDefaults standardUserDefaults];
+   return [[prefs stringForKey: @"font.fixed"] UTF8String];
+}
+
+void Options::setFixedWidthFont(std::string font)
+{
+   NSUserDefaults* prefs = [NSUserDefaults standardUserDefaults];
+   [prefs setObject: [NSString stringWithUTF8String: font.c_str()]
+             forKey: @"font.fixed"];
+}
+
+double Options::zoomLevel() const
+{
+   return 1.0;
+}
+   
+void Options::setZoomLevel(double zoomLevel)
+{
+      
+}
+
+FilePath Options::scriptsPath() const
+{
+   return FilePath();
+}
+   
+void Options::setScriptsPath(const FilePath& scriptsPath)
+{
+      
+}
+
+core::FilePath Options::executablePath() const
+{
+   return FilePath();
+}
+   
+FilePath Options::supportingFilePath() const
+{
+   return FilePath();
+}
+
+FilePath Options::wwwDocsPath() const
+{
+   return FilePath();
+}
+
+std::vector<std::string> Options::ignoredUpdateVersions() const
+{
+   return std::vector<std::string>();
+}
+   
+void Options::setIgnoredUpdateVersions(const std::vector<std::string>& ignored)
+{
+      
+}
+
+FilePath Options::scratchTempDir(FilePath defaultPath)
+{
+   return FilePath();
+}
+
+void Options::cleanUpScratchTempDir()
+{
+      
+}
+   
+   
+
+} // namespace desktop
+
