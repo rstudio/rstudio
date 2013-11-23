903479e1bf7fcdcdecab4a620bc8333b96c400d5
Jonathan McPherson <jonathan@rstudio.com>
2013-11-23 11:12:15 -0800

    First passing test (boot and check page title)

--- src/gwt/test/org/rstudio/studio/selenium/BootRStudio.java
+++ src/gwt/test/org/rstudio/studio/selenium/BootRStudio.java
@@ -7,20 +7,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.junit.Assert.*;
+
 public class BootRStudio  {
     public static void main(String[] args) {
         WebDriver driver = new FirefoxDriver();
 
         driver.get("http://localhost:8787/");
-        WebElement username = driver.findElement(By.name("username"));
-        username.sendKeys("Bob");
-
-        WebElement password = driver.findElement(By.name("password"));
-        password.sendKeys("b0bsp4ssw0rd");
-        password.submit();
 
         // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
+        assertEquals(driver.getTitle(), "RStudio");
 
         //Close the browser
         driver.quit();
