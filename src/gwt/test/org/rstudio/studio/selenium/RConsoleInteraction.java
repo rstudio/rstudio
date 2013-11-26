873f2317cd40993d3b8f0e35837a290d32737cd6
Jonathan McPherson <jonathan@rstudio.com>
2013-11-25 20:08:40 -0800

    Second passing test: make sure tab completion pops up

--- src/gwt/test/org/rstudio/studio/selenium/RConsoleInteraction.java
+++ src/gwt/test/org/rstudio/studio/selenium/RConsoleInteraction.java
@@ -3,6 +3,7 @@ package org.rstudio.studio.selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -12,87 +13,95 @@ import org.rstudio.core.client.ElementIds;
 
 import static org.junit.Assert.*;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test; 
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
-public class RConsoleInteraction  {
-   @Test
-   public void testRConsoleInteraction() throws MalformedURLException {
-       final WebDriver driver = new RemoteWebDriver(
-             new URL("http://localhost:9515/"), DesiredCapabilities.chrome());
-       
-       driver.get("http://localhost:8787/");
-       
+public class RConsoleInteraction {
+   @BeforeClass
+   public static void setUpBeforeClass() throws Exception {
+      driver_ = new RemoteWebDriver(
+            new URL("http://localhost:9515/"), DesiredCapabilities.chrome());
+      
+      driver_.get("http://localhost:8787/");
+      
        // Wait for the console panel to load
-       (new WebDriverWait(driver, 15)).until(new ExpectedCondition<Boolean>() {
-          public Boolean apply(WebDriver d) {
-             List<WebElement>elements = driver.findElements(By.id(
-                   ElementIds.getElementId(ElementIds.CONSOLE_INPUT)));
-             return elements.size() > 0;
-          }
-       });
-       
-       WebElement console = driver.findElement(By.id(
-             ElementIds.getElementId(ElementIds.SHELL_WIDGET)));
+      (new WebDriverWait(driver_, 15)).until(new ExpectedCondition<Boolean>() {
+         public Boolean apply(WebDriver d) {
+            List<WebElement>elements = driver_.findElements(By.id(
+                  ElementIds.getElementId(ElementIds.CONSOLE_INPUT)));
+            return elements.size() > 0;
+         }
+      });
 
-       // Test evaluating a basic expression with R 
-       console.sendKeys(Keys.chord(Keys.CONTROL, "l"));
-       console.sendKeys(Keys.ESCAPE);
-       console.sendKeys("40 + 2");
-       console.sendKeys(Keys.ENTER);
+      // Click on the shell
+      WebElement console = driver_.findElement(By.id(
+            ElementIds.getElementId(ElementIds.SHELL_WIDGET)));
 
-       final WebElement output = driver.findElement(By.id(
+      console.click();
+   }
+   
+   @AfterClass
+   public static void tearDownAfterClass() throws Exception {
+      driver_.quit();
+   }
+   
+   @Test
+   public void testBasicRInteraction() {
+       Actions do42 = new Actions(driver_);
+       do42.sendKeys(Keys.chord(Keys.CONTROL, "l"));
+       do42.sendKeys(Keys.ESCAPE);
+       do42.sendKeys("41 + 1");
+       do42.sendKeys(Keys.ENTER);
+       do42.perform();
+
+       final WebElement output = driver_.findElement(By.id(
              ElementIds.getElementId(ElementIds.CONSOLE_OUTPUT)));
 
-       (new WebDriverWait(driver, 5)).until(new ExpectedCondition<Boolean>() {
+       (new WebDriverWait(driver_, 5)).until(new ExpectedCondition<Boolean>() {
           public Boolean apply(WebDriver d) {
              String outputText = output.getText();
              return outputText.contains("42");
           }
        });
-       
-       // Test clearing the console
-       console.sendKeys(Keys.chord(Keys.CONTROL, "l"));
-
-       (new WebDriverWait(driver, 5)).until(new ExpectedCondition<Boolean>() {
-          public Boolean apply(WebDriver d) {
-             String outputText = output.getText();
-             return !outputText.contains("42");
-          }
-       });
-       
+   }
+   
+   @Test
+   public void testPopupCompletion() {
        // Test invoking and cancelling auto-complete
-
-       List<WebElement>elements = driver.findElements(By.id(
+       List<WebElement>elements = driver_.findElements(By.id(
              ElementIds.getElementId(ElementIds.POPUP_COMPLETIONS)));
        assertEquals(elements.size(), 0);
 
-       console.sendKeys(Keys.chord(Keys.CONTROL, "l"));
-       console.sendKeys("print");
-       console.sendKeys(Keys.TAB);
+       Actions popup = new Actions(driver_);
+       popup.sendKeys(Keys.ESCAPE);
+       popup.sendKeys("print");
+       popup.sendKeys(Keys.TAB);
+       popup.perform();
        
-       (new WebDriverWait(driver, 5)).until(new ExpectedCondition<Boolean>() {
+       (new WebDriverWait(driver_, 5)).until(new ExpectedCondition<Boolean>() {
           public Boolean apply(WebDriver d) {
-             List<WebElement>elements = driver.findElements(By.id(
+             List<WebElement>elements = driver_.findElements(By.id(
                    ElementIds.getElementId(ElementIds.POPUP_COMPLETIONS)));
              return elements.size() > 0;
           }
        });
 
-       console.sendKeys(Keys.ESCAPE);
+       Actions close = new Actions(driver_);
+       close.sendKeys(Keys.ESCAPE).perform();
 
-       (new WebDriverWait(driver, 5)).until(new ExpectedCondition<Boolean>() {
+       (new WebDriverWait(driver_, 5)).until(new ExpectedCondition<Boolean>() {
           public Boolean apply(WebDriver d) {
-             List<WebElement>elements = driver.findElements(By.id(
+             List<WebElement>elements = driver_.findElements(By.id(
                    ElementIds.getElementId(ElementIds.POPUP_COMPLETIONS)));
              return elements.size() == 0;
           }
        });
-
-       // Close the browser
-       driver.quit();
    }
+
+   private static WebDriver driver_;
 }
