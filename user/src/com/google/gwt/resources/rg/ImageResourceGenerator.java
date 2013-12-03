gwtproject/gwt

Removes recently added ImageResource.isStandalone.
Instance of adding isStandalone to ImageResource (a breaking change)
introduces subtype of ImageResourcePrototype to differentiate between
standalone and bundled images.

Change-Id: Ieeec29560694c548208f15c85e2e8874df4805c1

user/src/com/google/gwt/resources/rg/ImageResourceGenerator.java
@@ -72,8 +72,8 @@ public final class ImageResourceGenerator extends AbstractResourceGenerator
     }
 
     @Override
-    public boolean isStandalone() {
-      return false;
+    public Class<?> getResourceType() {
+      return ImageResourcePrototype.class;
     }
 
     public LocalizedImage addImage(TreeLogger logger, ResourceContext context,
@@ -259,7 +259,7 @@ public final class ImageResourceGenerator extends AbstractResourceGenerator
     protected String normalContentsFieldName;
     protected String rtlContentsFieldName;
 
-    public abstract boolean isStandalone();
+    public abstract Class<?> getResourceType();
 
     public abstract ImageRect getImageRect(ImageResourceDeclaration image);
 
@@ -304,8 +304,8 @@ public final class ImageResourceGenerator extends AbstractResourceGenerator
     }
 
     @Override
-    public boolean isStandalone() {
-      return true;
+    public Class<?> getResourceType() {
+      return ImageResourcePrototype.Standalone.class;
     }
 
     @Override
@@ -476,13 +476,14 @@ public final class ImageResourceGenerator extends AbstractResourceGenerator
       throws UnableToCompleteException {
     String name = method.getName();
 
+    ImageResourceDeclaration image = new ImageResourceDeclaration(method);
+    DisplayedImage bundle = getImage(image);
+
     SourceWriter sw = new StringSourceWriter();
-    sw.println("new " + ImageResourcePrototype.class.getName() + "(");
+    sw.println("new " + bundle.getResourceType().getCanonicalName() + "(");
     sw.indent();
     sw.println('"' + name + "\",");
 
-    ImageResourceDeclaration image = new ImageResourceDeclaration(method);
-    DisplayedImage bundle = getImage(image);
     ImageRect rect = bundle.getImageRect(image);
     if (rect == null) {
       throw new NullPointerException("No ImageRect ever computed for " + name);
@@ -500,8 +501,7 @@ public final class ImageResourceGenerator extends AbstractResourceGenerator
           + urlExpressions[1] + " : " + urlExpressions[0] + "),");
     }
     sw.println(rect.getLeft() + ", " + rect.getTop() + ", " + rect.getWidth() + ", "
-        + rect.getHeight() + ", " + rect.isAnimated() + ", " + rect.isLossy() + ", "
-        + bundle.isStandalone());
+        + rect.getHeight() + ", " + rect.isAnimated() + ", " + rect.isLossy());
 
     sw.outdent();
     sw.print(")");

user/test/com/google/gwt/resources/client/ImageResourceTest.java
@@ -171,7 +171,7 @@ public class ImageResourceTest extends GWTTestCase {
     // Make sure that the large, lossy image isn't bundled with the rest
     assertTrue(((ImageResourcePrototype) lossy).isLossy());
     assertTrue(!i64.getSafeUri().equals(lossy.getSafeUri()));
-    assertTrue(lossy.isStandalone());
+    assertTrue(lossy instanceof ImageResourcePrototype.Standalone);
 
     assertEquals(16, r.i16x16Vertical().getWidth());
     assertEquals(16, r.i16x16Vertical().getHeight());
@@ -200,10 +200,10 @@ public class ImageResourceTest extends GWTTestCase {
     // No image packing
     assertEquals(0, a.getTop());
     assertEquals(0, a.getLeft());
-    assertTrue(a.isStandalone());
+    assertTrue(a instanceof ImageResourcePrototype.Standalone);
     assertEquals(0, b.getTop());
     assertEquals(0, b.getLeft());
-    assertTrue(b.isStandalone());
+    assertTrue(b instanceof ImageResourcePrototype.Standalone);
   }
 
   /**
@@ -220,10 +220,10 @@ public class ImageResourceTest extends GWTTestCase {
     // No image packing
     assertEquals(0, a.getTop());
     assertEquals(0, a.getLeft());
-    assertTrue(a.isStandalone());
+    assertTrue(a instanceof ImageResourcePrototype.Standalone);
     assertEquals(0, b.getTop());
     assertEquals(0, b.getLeft());
-    assertTrue(b.isStandalone());
+    assertTrue(b instanceof ImageResourcePrototype.Standalone);
   }
 
   @SuppressWarnings("deprecation")

user/test/com/google/gwt/user/client/ui/ImageTest.java
@@ -31,6 +31,7 @@ import com.google.gwt.junit.client.WithProperties.Property;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.resources.client.ImageResource.ImageOptions;
+import com.google.gwt.resources.client.impl.ImageResourcePrototype;
 import com.google.gwt.user.client.Timer;
 
 /**
@@ -623,8 +624,7 @@ public class ImageTest extends GWTTestCase {
     Bundle b = GWT.create(Bundle.class);
     Image image = new Image(b.prettyPiccy());
     assertResourceWorked(image, b.prettyPiccy());
-
-    assertFalse(b.prettyPiccy().isStandalone());
+    assertFalse(b.prettyPiccy() instanceof ImageResourcePrototype.Standalone);
     assertEquals("clipped", getCurrentImageStateName(image));
   }
 
@@ -637,7 +637,7 @@ public class ImageTest extends GWTTestCase {
     image.setResource(b.prettyPiccy());
     assertResourceWorked(image, b.prettyPiccy());
 
-    assertFalse(b.prettyPiccy().isStandalone());
+    assertFalse(b.prettyPiccy() instanceof ImageResourcePrototype.Standalone);
     assertEquals("clipped", getCurrentImageStateName(image));
   }
 
@@ -646,7 +646,7 @@ public class ImageTest extends GWTTestCase {
     Image image = new Image(b.prettyPiccyStandalone());
     assertResourceWorked(image, b.prettyPiccyStandalone());
 
-    assertTrue(b.prettyPiccyStandalone().isStandalone());
+    assertTrue(b.prettyPiccyStandalone() instanceof ImageResourcePrototype.Standalone);
     assertEquals("unclipped", getCurrentImageStateName(image));
   }
 
@@ -656,7 +656,7 @@ public class ImageTest extends GWTTestCase {
     image.setResource(b.prettyPiccyStandalone());
     assertResourceWorked(image, b.prettyPiccyStandalone());
 
-    assertTrue(b.prettyPiccyStandalone().isStandalone());
+    assertTrue(b.prettyPiccyStandalone() instanceof ImageResourcePrototype.Standalone);
     assertEquals("unclipped", getCurrentImageStateName(image));
   }
 

rstudio/rstudio

user/src/com/google/gwt/resources/rg/ImageResourceGenerator.java
@@ -72,8 +72,8 @@ public final class ImageResourceGenerator extends AbstractResourceGenerator
     }
 
     @Override
-    public boolean isStandalone() {
-      return false;
+    public Class<?> getResourceType() {
+      return ImageResourcePrototype.Bundle.class;
     }
 
     public LocalizedImage addImage(TreeLogger logger, ResourceContext context,
@@ -259,7 +259,7 @@ public final class ImageResourceGenerator extends AbstractResourceGenerator
     protected String normalContentsFieldName;
     protected String rtlContentsFieldName;
 
-    public abstract boolean isStandalone();
+    public abstract Class<?> getResourceType();
 
     public abstract ImageRect getImageRect(ImageResourceDeclaration image);
 
@@ -304,8 +304,8 @@ public final class ImageResourceGenerator extends AbstractResourceGenerator
     }
 
     @Override
-    public boolean isStandalone() {
-      return true;
+    public Class<?> getResourceType() {
+      return ImageResourcePrototype.class;
     }
 
     @Override
@@ -476,13 +476,14 @@ public final class ImageResourceGenerator extends AbstractResourceGenerator
       throws UnableToCompleteException {
     String name = method.getName();
 
+    ImageResourceDeclaration image = new ImageResourceDeclaration(method);
+    DisplayedImage bundle = getImage(image);
+
     SourceWriter sw = new StringSourceWriter();
-    sw.println("new " + ImageResourcePrototype.class.getName() + "(");
+    sw.println("new " + bundle.getResourceType().getCanonicalName() + "(");
     sw.indent();
     sw.println('"' + name + "\",");
 
-    ImageResourceDeclaration image = new ImageResourceDeclaration(method);
-    DisplayedImage bundle = getImage(image);
     ImageRect rect = bundle.getImageRect(image);
     if (rect == null) {
       throw new NullPointerException("No ImageRect ever computed for " + name);
@@ -500,8 +501,7 @@ public final class ImageResourceGenerator extends AbstractResourceGenerator
           + urlExpressions[1] + " : " + urlExpressions[0] + "),");
     }
     sw.println(rect.getLeft() + ", " + rect.getTop() + ", " + rect.getWidth() + ", "
-        + rect.getHeight() + ", " + rect.isAnimated() + ", " + rect.isLossy() + ", "
-        + bundle.isStandalone());
+        + rect.getHeight() + ", " + rect.isAnimated() + ", " + rect.isLossy());
 
     sw.outdent();
     sw.print(")");

user/test/com/google/gwt/resources/client/ImageResourceTest.java
@@ -171,7 +171,7 @@ public class ImageResourceTest extends GWTTestCase {
     // Make sure that the large, lossy image isn't bundled with the rest
     assertTrue(((ImageResourcePrototype) lossy).isLossy());
     assertTrue(!i64.getSafeUri().equals(lossy.getSafeUri()));
-    assertTrue(lossy.isStandalone());
+    assertFalse(lossy instanceof ImageResourcePrototype.Bundle);
 
     assertEquals(16, r.i16x16Vertical().getWidth());
     assertEquals(16, r.i16x16Vertical().getHeight());
@@ -200,10 +200,10 @@ public class ImageResourceTest extends GWTTestCase {
     // No image packing
     assertEquals(0, a.getTop());
     assertEquals(0, a.getLeft());
-    assertTrue(a.isStandalone());
+    assertFalse(a instanceof ImageResourcePrototype.Bundle);
     assertEquals(0, b.getTop());
     assertEquals(0, b.getLeft());
-    assertTrue(b.isStandalone());
+    assertFalse(b instanceof ImageResourcePrototype.Bundle);
   }
 
   /**
@@ -220,10 +220,10 @@ public class ImageResourceTest extends GWTTestCase {
     // No image packing
     assertEquals(0, a.getTop());
     assertEquals(0, a.getLeft());
-    assertTrue(a.isStandalone());
+    assertFalse(a instanceof ImageResourcePrototype.Bundle);
     assertEquals(0, b.getTop());
     assertEquals(0, b.getLeft());
-    assertTrue(b.isStandalone());
+    assertFalse(b instanceof ImageResourcePrototype.Bundle);
   }
 
   @SuppressWarnings("deprecation")

user/test/com/google/gwt/user/client/ui/ImageTest.java
@@ -31,6 +31,7 @@ import com.google.gwt.junit.client.WithProperties.Property;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.resources.client.ImageResource.ImageOptions;
+import com.google.gwt.resources.client.impl.ImageResourcePrototype;
 import com.google.gwt.user.client.Timer;
 
 /**
@@ -623,8 +624,7 @@ public class ImageTest extends GWTTestCase {
     Bundle b = GWT.create(Bundle.class);
     Image image = new Image(b.prettyPiccy());
     assertResourceWorked(image, b.prettyPiccy());
-
-    assertFalse(b.prettyPiccy().isStandalone());
+    assertTrue(b.prettyPiccy() instanceof ImageResourcePrototype.Bundle);
     assertEquals("clipped", getCurrentImageStateName(image));
   }
 
@@ -637,7 +637,7 @@ public class ImageTest extends GWTTestCase {
     image.setResource(b.prettyPiccy());
     assertResourceWorked(image, b.prettyPiccy());
 
-    assertFalse(b.prettyPiccy().isStandalone());
+    assertTrue(b.prettyPiccy() instanceof ImageResourcePrototype.Bundle);
     assertEquals("clipped", getCurrentImageStateName(image));
   }
 
@@ -646,7 +646,7 @@ public class ImageTest extends GWTTestCase {
     Image image = new Image(b.prettyPiccyStandalone());
     assertResourceWorked(image, b.prettyPiccyStandalone());
 
-    assertTrue(b.prettyPiccyStandalone().isStandalone());
+    assertFalse(b.prettyPiccyStandalone() instanceof ImageResourcePrototype.Bundle);
     assertEquals("unclipped", getCurrentImageStateName(image));
   }
 
@@ -656,7 +656,7 @@ public class ImageTest extends GWTTestCase {
     image.setResource(b.prettyPiccyStandalone());
     assertResourceWorked(image, b.prettyPiccyStandalone());
 
-    assertTrue(b.prettyPiccyStandalone().isStandalone());
+    assertFalse(b.prettyPiccyStandalone() instanceof ImageResourcePrototype.Bundle);
     assertEquals("unclipped", getCurrentImageStateName(image));
   }
 