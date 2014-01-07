7dd85019938069ac8d058db64df6828949169dd2
Jens Nehlmeier <jens.nehlmeier@gmail.com>
2014-01-07 22:04:45 +0100

    Fixes OutputFileSetOnDirectory for non-canonical files.
    Bug: issue 8500
    Change-Id: I991e306dd53ba909bc46f6372d45faf027b8385e
    (cherry picked from commit 05fcece7b43422b126d265d89af1c08cc8d336cb)

--- dev/core/src/com/google/gwt/dev/util/OutputFileSetOnDirectory.java
+++ dev/core/src/com/google/gwt/dev/util/OutputFileSetOnDirectory.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -84,11 +84,7 @@ public class OutputFileSetOnDirectory extends OutputFileSet {
     }
   }
 
-  private File pathToFile(String path) {
-    File file = dir;
-    for (String part : (prefix + path).split("/")) {
-      file = new File(file, part);
-    }
-    return file;
+  private File pathToFile(String path) throws IOException {
+    return new File(dir, prefix + path).getCanonicalFile();
   }
 }
