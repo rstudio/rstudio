cbbb90ace16217f085c83d9a98a9864f5be18321
JJ Allaire <jj@rstudio.org>
2014-01-19 06:58:18 -0500

    add working dir utility

--- src/cpp/r/R/packages/rmarkdown/R/util.R
+++ src/cpp/r/R/packages/rmarkdown/R/util.R
@@ -3,3 +3,10 @@
 systemFile <- function(file) {
   system.file(file, package = "rmarkdown")
 }
+
+withWorkingDir <- function(dir, code) {
+  old <- setwd(dir)
+  on.exit(setwd(old))
+  force(code)
+} 
+
