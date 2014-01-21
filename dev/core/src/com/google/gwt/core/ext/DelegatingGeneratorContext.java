042c99deb079988e1fb128a471db6ec5b3526363
John Stalcup <stalcup@google.com>
2014-01-21 11:54:42 -0800

    Updates DelegatingGeneratorContext to avoid updating subclasses.
    Change-Id: Iefb949b935c8ee0b694f64209fe00334dcbf48ab

--- dev/core/src/com/google/gwt/core/ext/DelegatingGeneratorContext.java
+++ dev/core/src/com/google/gwt/core/ext/DelegatingGeneratorContext.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -96,6 +96,12 @@ public abstract class DelegatingGeneratorContext implements GeneratorContext {
   public boolean isProdMode() {
     return baseContext.isProdMode();
   }
+
+  @Override
+  public void reset() {
+    baseContext.reset();
+  }
+
   @Override
   public PrintWriter tryCreate(TreeLogger logger, String packageName, String simpleName) {
     return baseContext.tryCreate(logger, packageName, simpleName);
