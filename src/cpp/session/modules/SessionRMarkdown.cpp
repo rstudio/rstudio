9da6fd349aad50f8cbe59d8ef3fec8897d9a8898
JJ Allaire <jj@rstudio.org>
2014-02-02 14:08:08 -0500

    pandoc binaries for redhat/fedora

--- src/cpp/session/modules/SessionRMarkdown.cpp
+++ src/cpp/session/modules/SessionRMarkdown.cpp
@@ -33,27 +33,6 @@ namespace {
 
 void initPandocPath()
 {
-   // ensure that pandoc is on the path for all platforms save for
-   // redhat 5 (where we don't yet have a redistributable pandoc binary)
-#if !defined(_WIN32) && !defined(__APPLE__)
-   FilePath redhatReleaseFile("/etc/redhat-release");
-   if (redhatReleaseFile.exists())
-   {
-      // get redhat release info
-      std::string redhatRelease;
-      Error error = core::readStringFromFile(redhatReleaseFile, &redhatRelease);
-      if (error)
-         LOG_ERROR(error);
-
-      // check for redhat 5
-      if (!boost::algorithm::istarts_with(redhatRelease, "fedora") &&
-          boost::algorithm::icontains(redhatRelease, "release 5"))
-      {
-         return;
-      }
-   }
-#endif
-
    r::exec::RFunction sysSetenv("Sys.setenv");
    sysSetenv.addParam("RSTUDIO_PANDOC",
                       session::options().pandocPath().absolutePath());
