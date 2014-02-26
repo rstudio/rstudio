263200944efcc9af86c1cde0d33a3d4c99ba35a8
Jonathan McPherson <jonathan@rstudio.com>
2014-02-26 11:37:17 -0800

    include SHA1 in rmarkdown package archive name on Windows

--- dependencies/common/install-rmarkdown.cmd
+++ dependencies/common/install-rmarkdown.cmd
@@ -27,9 +27,17 @@ echo GithubRepo: rmarkdown >> DESCRIPTION
 echo GithubUsername: rstudio >> DESCRIPTION
 echo GithubRef: %RMARKDOWN_VERSION% >> DESCRIPTION
 echo GithubSHA1: %RMARKDOWN_SHA1% >> DESCRIPTION
+echo Repository: RStudioIDE >> DESCRIPTION
 
 REM create source package
 popd
-del /s /q rmarkdown*.tar.gz
+set RMARKDOWN_ARCHIVE_PATTERN=rmarkdown*.tar.gz
+del /s /q %RMARKDOWN_ARCHIVE_PATTERN%
 R CMD build rmarkdown
+
+REM modify filename to include SHA1
+for %%f in (%RMARKDOWN_ARCHIVE_PATTERN%) do set RMARKDOWN_ARCHIVE=%%f
+set RMARKDOWN_ARCHIVE_STEM=%RMARKDOWN_ARCHIVE:~0,-7%
+set RMARKDOWN_ARCHIVE_SHA1=%RMARKDOWN_ARCHIVE_STEM%_%RMARKDOWN_SHA1%.tar.gz
+move %RMARKDOWN_ARCHIVE% %RMARKDOWN_ARCHIVE_SHA1%
 
