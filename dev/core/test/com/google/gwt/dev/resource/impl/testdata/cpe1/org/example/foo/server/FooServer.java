9b4b848976037ebe536638d70ea6f11a456f0801
Roberto Lublinerman <rluble@google.com>
2014-03-24 08:54:11 -0700

    Fix package on FooServer.java (testdata).
    It was incorrectly modified in the update to Checkstyle 5.7.
    
    Change-Id: I6ce6d4304bd7d02804dc5f5d4c36d091901dce90

--- dev/core/test/com/google/gwt/dev/resource/impl/testdata/cpe1/org/example/foo/server/FooServer.java
+++ dev/core/test/com/google/gwt/dev/resource/impl/testdata/cpe1/org/example/foo/server/FooServer.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.gwt.dev.resource.impl.testdata.cpe2.org.example.foo.server;
+package com.google.gwt.dev.resource.impl.testdata.cpe1.org.example.foo.server;
 
 /**
  * Classes for testing resource infrastructure.
