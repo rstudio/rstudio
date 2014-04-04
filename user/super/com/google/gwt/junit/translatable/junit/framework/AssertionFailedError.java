9d76116f1d963690d5ded1dbbae1545eb65a2442
Kevin Cooney <kcooney@google.com>
2014-04-03 21:31:34 -0700

    Fix handling of null in AssertionFailedError(String).
    Previously, you would see "junit.framework.AssertionFailedError: null"
    if you pass in 'null' as the message for AssertionFailedError (which
    many Assert methods do). This was fixed in the core JUnit code
    in https://github.com/junit-team/junit/commit/1b809a4a2e
    
    Change-Id: I4d32ff789dc684d1427d9d0e53229fc15b53ab5c

--- user/super/com/google/gwt/junit/translatable/junit/framework/AssertionFailedError.java
+++ user/super/com/google/gwt/junit/translatable/junit/framework/AssertionFailedError.java
@@ -24,7 +24,7 @@ public class AssertionFailedError extends AssertionError {
   }
 
   public AssertionFailedError(String message) {
-    super(message);
+    super(message == null ? "" : message);
   }
 
 }
