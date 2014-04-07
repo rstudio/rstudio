b1066643c313563735339c6ad508a1b7de0d07e3
Lukas Laag <laaglu@gmail.com>
2014-04-07 11:15:24 +0200

    Restore support for derived DataResource in @url CSS extensions
    GWT supports references to Data and Image resource using the @url
    CSS extension. In 2.6.0, a test was introduced to check that the
    type of the method referenced in the @url extension is indeed of
    type DataResource or ImageResource. This test is too strict though
    and does not allow DataResource and ImageResource subclasses, which
    were allowed before 2.6.0. This patch makes the test more lenient
    to accept derived DataResource and ImageResource.
    
    Bug: issue 8564
    Change-Id: If81e5f1250e28a5a3150b26d3ffa290d780a26a0

---
+++ user/test/com/google/gwt/resources/rg/CustomImageResourceGenerator.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.resources.rg;
+
+import com.google.gwt.core.ext.TreeLogger;
+import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.gwt.core.ext.typeinfo.JMethod;
+import com.google.gwt.resources.client.impl.CustomImageResourcePrototype;
+import com.google.gwt.resources.ext.AbstractResourceGenerator;
+import com.google.gwt.resources.ext.ResourceContext;
+import com.google.gwt.resources.ext.ResourceGeneratorUtil;
+import com.google.gwt.safehtml.shared.UriUtils;
+import com.google.gwt.user.rebind.SourceWriter;
+import com.google.gwt.user.rebind.StringSourceWriter;
+
+import java.net.URL;
+
+/**
+ * Generator for {@link CustomImageResource}.
+ */
+public class CustomImageResourceGenerator extends AbstractResourceGenerator {
+
+  @Override
+  public String createAssignment(TreeLogger logger, ResourceContext context, JMethod method)
+      throws UnableToCompleteException {
+    URL[] resources = ResourceGeneratorUtil.findResources(logger, context, method);
+
+    if (resources.length != 1) {
+      logger.log(TreeLogger.ERROR, "Exactly one resource must be specified", null);
+      throw new UnableToCompleteException();
+    }
+
+    URL resource = resources[0];
+
+    SourceWriter sw = new StringSourceWriter();
+    sw.println("new " + CustomImageResourcePrototype.class.getName() + "(");
+    sw.indent();
+    sw.println('"' + method.getName() + "\",");
+    // We don't care about it actually working, so just use the resource URL
+    sw.println(UriUtils.class.getName() + ".fromTrustedString(\"" + resource.toExternalForm() + "\")");
+    sw.outdent();
+    sw.print(")");
+
+    return sw.toString();
+  }
+}
