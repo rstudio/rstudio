gwtproject/gwt

Fix that malformed parameter values break the app
  When reading parameters from URL, an exception in case of a malformed
  URI sequence, make the app fail.

  This happens for instance when initializing the GWT logger
  system which breaks when checking the existence of the 'logLevel'
  parameter in the URL.

  We can see this behaviour just adding the 'x=%E4' parameter to
  any GWT app which reads url parameters in the beginning.

Bug: issue 7741
Change-Id: I3f941e835f70c4caf9dd9caa0f14b63003001584

user/test/com/google/gwt/http/client/URLTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.http.client;
 
+import com.google.gwt.core.client.JavaScriptException;
 import com.google.gwt.junit.client.GWTTestCase;
 
 /**
@@ -136,6 +137,14 @@ public class URLTest extends GWTTestCase {
       URL.decodeQueryString(null);
       fail("Expected NullPointerException");
     } catch (NullPointerException ex) {
+      // expected exception was thrown
+    }
+
+    try {
+      // Malformed URI sequence
+      URL.decodeQueryString("%E4");
+      fail("Expected JavaScriptException");
+    } catch (JavaScriptException ex) {
       // expected exception was thrown
     }
 

rstudio/rstudio

user/test/com/google/gwt/http/client/URLTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.http.client;
 
+import com.google.gwt.core.client.JavaScriptException;
 import com.google.gwt.junit.client.GWTTestCase;
 
 /**
@@ -136,6 +137,14 @@ public class URLTest extends GWTTestCase {
       URL.decodeQueryString(null);
       fail("Expected NullPointerException");
     } catch (NullPointerException ex) {
+      // expected exception was thrown
+    }
+
+    try {
+      // Malformed URI sequence
+      URL.decodeQueryString("%E4");
+      fail("Expected JavaScriptException");
+    } catch (JavaScriptException ignored) {
       // expected exception was thrown
     }
 