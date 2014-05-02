gwtproject/gwt

Make JsInliner avoid inlining pinned methods.
At the Java AST level some methods might be pinned (e.g. the ones
appearing in Impl.getNameOf()) and those should not be inlined
by the JsInliner.

Change-Id: I74ccf8cdc078b07109e029d77b84fff89a9cacae

dev/core/src/com/google/gwt/dev/jjs/impl/GenerateJavaScriptAST.java
@@ -1261,6 +1261,10 @@ public class GenerateJavaScriptAST {
 
       JsFunction jsFunc = pop(); // body
 
+      if (!program.allowInliningOf(x)) {
+        jsProgram.disallowInliningOf(jsFunc);
+      }
+
       // Collect the resulting function to be considered by the JsInliner.
       if (methodsForJsInlining.contains(x)) {
         functionsForJsInlining.add(jsFunc);

dev/core/src/com/google/gwt/dev/js/JsInliner.java
@@ -868,9 +868,12 @@ public class JsInliner {
      */
     private JsFunction programFunction;
 
+    private JsProgram program;
+
     public InliningVisitor(JsProgram program, Set<JsNode> whitelist) {
-      invocationCountingVisitor.accept(program);
+      this.program = program;
       this.whitelist = whitelist;
+      invocationCountingVisitor.accept(program);
     }
 
     /**
@@ -995,6 +998,10 @@ public class JsInliner {
        */
       JsFunction invokedFunction = isFunction(x.getQualifier());
       if (invokedFunction == null) {
+        return;
+      }
+
+      if (!program.allowInliningOf(invokedFunction)) {
         return;
       }
 

dev/core/src/com/google/gwt/dev/js/ast/JsProgram.java
@@ -17,6 +17,7 @@ import com.google.gwt.dev.jjs.CorrelationFactory;
 import com.google.gwt.dev.jjs.CorrelationFactory.DummyCorrelationFactory;
 import com.google.gwt.dev.jjs.SourceInfo;
 import com.google.gwt.dev.jjs.SourceOrigin;
+import com.google.gwt.thirdparty.guava.common.collect.Sets;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -37,6 +38,8 @@ public final class JsProgram extends JsNode {
   private final Map<String, JsFunction> indexedFunctions = new HashMap<String, JsFunction>();
 
   private final Set<JsFunction> indexedFunctionSet = new HashSet<JsFunction>();
+
+  private final Set<JsFunction> preventInliningOf = Sets.newHashSet();
 
   private final JsScope objectScope;
 
@@ -141,6 +144,14 @@ public final class JsProgram extends JsNode {
     this.indexedFunctions.putAll(indexedFunctions);
     this.indexedFunctionSet.clear();
     this.indexedFunctionSet.addAll(indexedFunctions.values());
+  }
+
+  public void disallowInliningOf(JsFunction function) {
+    preventInliningOf.add(function);
+  }
+
+  public boolean allowInliningOf(JsFunction function) {
+    return !preventInliningOf.contains(function);
   }
 
   @Override

rstudio/rstudio

dev/core/src/com/google/gwt/dev/jjs/impl/GenerateJavaScriptAST.java
@@ -1261,6 +1261,10 @@ public class GenerateJavaScriptAST {
 
       JsFunction jsFunc = pop(); // body
 
+      if (!program.isInliningAllowed(x)) {
+        jsProgram.disallowInlining(jsFunc);
+      }
+
       // Collect the resulting function to be considered by the JsInliner.
       if (methodsForJsInlining.contains(x)) {
         functionsForJsInlining.add(jsFunc);

dev/core/src/com/google/gwt/dev/js/JsInliner.java
@@ -868,9 +868,12 @@ public class JsInliner {
      */
     private JsFunction programFunction;
 
+    private JsProgram program;
+
     public InliningVisitor(JsProgram program, Set<JsNode> whitelist) {
-      invocationCountingVisitor.accept(program);
+      this.program = program;
       this.whitelist = whitelist;
+      invocationCountingVisitor.accept(program);
     }
 
     /**
@@ -995,6 +998,10 @@ public class JsInliner {
        */
       JsFunction invokedFunction = isFunction(x.getQualifier());
       if (invokedFunction == null) {
+        return;
+      }
+
+      if (!program.isInliningAllowed(invokedFunction)) {
         return;
       }
 

dev/core/src/com/google/gwt/dev/js/ast/JsProgram.java
@@ -17,6 +17,7 @@ import com.google.gwt.dev.jjs.CorrelationFactory;
 import com.google.gwt.dev.jjs.CorrelationFactory.DummyCorrelationFactory;
 import com.google.gwt.dev.jjs.SourceInfo;
 import com.google.gwt.dev.jjs.SourceOrigin;
+import com.google.gwt.thirdparty.guava.common.collect.Sets;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -37,6 +38,8 @@ public final class JsProgram extends JsNode {
   private final Map<String, JsFunction> indexedFunctions = new HashMap<String, JsFunction>();
 
   private final Set<JsFunction> indexedFunctionSet = new HashSet<JsFunction>();
+
+  private final Set<JsFunction> preventInliningOf = Sets.newHashSet();
 
   private final JsScope objectScope;
 
@@ -141,6 +144,14 @@ public final class JsProgram extends JsNode {
     this.indexedFunctions.putAll(indexedFunctions);
     this.indexedFunctionSet.clear();
     this.indexedFunctionSet.addAll(indexedFunctions.values());
+  }
+
+  public void disallowInlining(JsFunction function) {
+    preventInliningOf.add(function);
+  }
+
+  public boolean isInliningAllowed(JsFunction function) {
+    return !preventInliningOf.contains(function);
   }
 
   @Override