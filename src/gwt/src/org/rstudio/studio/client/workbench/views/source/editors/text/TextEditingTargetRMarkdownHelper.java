176b4baf9ab0250410bd16e153d0409ea83a8e31
JJ Allaire <jj@rstudio.org>
2014-05-21 15:00:50 -0400

    add htmltools dependency for rmarkdown

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/TextEditingTargetRMarkdownHelper.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/TextEditingTargetRMarkdownHelper.java
@@ -141,6 +141,7 @@ public class TextEditingTargetRMarkdownHelper
          new Dependency[] {
            Dependency.cranPackage("knitr", "1.2"),
            Dependency.cranPackage("yaml", "2.1.5"),
+           Dependency.cranPackage("htmltools", "0.2.4"),
            Dependency.embeddedPackage("rmarkdown")
          }, 
          
