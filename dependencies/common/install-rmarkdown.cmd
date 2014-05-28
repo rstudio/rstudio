bb4e4f0acc6175474e41458c51ad56ddf255c27c
JJ Allaire <jj@rstudio.org>
2014-05-28 16:00:59 -0400

    parameterize windows install packages script

--- dependencies/common/install-rmarkdown.cmd
+++ dependencies/common/install-rmarkdown.cmd
@@ -2,42 +2,44 @@
 
 setlocal
 
+set PACKAGE=rmarkdown
+set PACKAGE_VERSION=master
+
 set PATH=%PATH%;%CD%\tools
 
 REM git clone if necessary
-set RMARKDOWN_DIR=rmarkdown
-if not exist "%RMARKDOWN_DIR%" (
-   git clone https://github.com/rstudio/rmarkdown.git
+set PACKAGE_DIR="%PACKAGE%"
+if not exist "%PACKAGE_DIR%" (
+   git clone "https://github.com/rstudio/%PACKAGE%.git"
 )
 
 REM clean and checkout target branch
-set RMARKDOWN_VERSION=master
-pushd "%RMARKDOWN_DIR%"
+pushd "%PACKAGE_DIR%"
 git checkout .
 git clean -df .
 git pull
-git checkout "%RMARKDOWN_VERSION%"
+git checkout "%PACKAGE_VERSION%"
 
 
 REM append GitHub fields to DESCRIPTION
-git rev-parse "%RMARKDOWN_VERSION%" > RMARKDOWN_SHA1
-set /p RMARKDOWN_SHA1= < RMARKDOWN_SHA1
-del RMARKDOWN_SHA1
-echo GithubRepo: rmarkdown >> DESCRIPTION
+git rev-parse "%PACKAGE_VERSION%" > PACKAGE_SHA1
+set /p PACKAGE_SHA1= < PACKAGE_SHA1
+del PACKAGE_SHA1
+echo GithubRepo: %PACKAGE% >> DESCRIPTION
 echo GithubUsername: rstudio >> DESCRIPTION
-echo GithubRef: %RMARKDOWN_VERSION% >> DESCRIPTION
-echo GithubSHA1: %RMARKDOWN_SHA1% >> DESCRIPTION
+echo GithubRef: %PACKAGE_VERSION% >> DESCRIPTION
+echo GithubSHA1: %PACKAGE_SHA1% >> DESCRIPTION
 echo Repository: RStudioIDE >> DESCRIPTION
 
 REM create source package
 popd
-set RMARKDOWN_ARCHIVE_PATTERN=rmarkdown*.tar.gz
-del /s /q %RMARKDOWN_ARCHIVE_PATTERN%
-R CMD build rmarkdown
+set PACKAGE_ARCHIVE_PATTERN="%PACKAGE%*.tar.gz"
+del /s /q %PACKAGE_ARCHIVE_PATTERN%
+R CMD build "%PACKAGE%"
 
 REM modify filename to include SHA1
-for %%f in (%RMARKDOWN_ARCHIVE_PATTERN%) do set RMARKDOWN_ARCHIVE=%%f
-set RMARKDOWN_ARCHIVE_STEM=%RMARKDOWN_ARCHIVE:~0,-7%
-set RMARKDOWN_ARCHIVE_SHA1=%RMARKDOWN_ARCHIVE_STEM%_%RMARKDOWN_SHA1%.tar.gz
-move %RMARKDOWN_ARCHIVE% %RMARKDOWN_ARCHIVE_SHA1%
+for %%f in (%PACKAGE_ARCHIVE_PATTERN%) do set PACKAGE_ARCHIVE=%%f
+set PACKAGE_ARCHIVE_STEM=%PACKAGE_ARCHIVE:~0,-7%
+set PACKAGE_ARCHIVE_SHA1=%PACKAGE_ARCHIVE_STEM%_%PACKAGE_SHA1%.tar.gz
+move %PACKAGE_ARCHIVE% %PACKAGE_ARCHIVE_SHA1%
 
