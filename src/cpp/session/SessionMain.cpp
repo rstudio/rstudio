5ec86f405dd20033711e6244ddc2d65003e7cf87
JJ Allaire <jj@rstudio.org>
2014-06-02 08:19:51 -0400

    require more recent version of shinyapps

--- src/cpp/session/SessionMain.cpp
+++ src/cpp/session/SessionMain.cpp
@@ -624,7 +624,7 @@ void handleClientInit(const boost::function<void()>& initFunction,
 
    // light up shinyapps-related UI features if shinyapps is installed
    bool shinyAppsInstalled = 
-      module_context::isPackageVersionInstalled("shinyapps", "0.2.1"); 
+      module_context::isPackageVersionInstalled("shinyapps", "0.3.5");
    sessionInfo["shinyapps_installed"] = shinyAppsInstalled;
    sessionInfo["allow_rmd_deployment"] = shinyAppsInstalled;
 
