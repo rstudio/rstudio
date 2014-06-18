fc86699db1c07f8cd804d5dafb083889df0c07f7
Jonathan McPherson <jonathan@rstudio.com>
2014-06-18 14:41:15 -0700

    add extensible hook for RStudio session initialization

---
+++ src/cpp/session/modules/SessionRHooks.cpp
@@ -0,0 +1,46 @@
+/*
+ * SessionRHooks.cpp
+ *
+ * Copyright (C) 2009-14 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#include <core/Exec.hpp>
+
+#include <r/RSexp.hpp>
+#include <r/RExec.hpp>
+
+#include <session/SessionModuleContext.hpp>
+
+#include <boost/bind.hpp>
+
+#include "SessionRHooks.hpp"
+
+namespace session {
+namespace modules { 
+namespace rhooks {
+
+using namespace core;
+
+Error initialize()
+{
+   using boost::bind;
+   using namespace module_context;
+   ExecBlock initBlock;
+   initBlock.addFunctions()
+      (bind(sourceModuleRFile, "SessionRHooks.R"));
+   return initBlock.execute();
+}
+
+} // namespace rhooks
+} // namespace modules
+} // namespace session
+
