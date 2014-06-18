056cfc1635d52d945a7114d17d30d8281f778939
JJ Allaire <jj@rstudio.org>
2014-06-18 15:46:28 -0400

    display all error content as (escaped) html

--- src/cpp/core/http/Response.cpp
+++ src/cpp/core/http/Response.cpp
@@ -234,14 +234,13 @@ void Response::setError(int statusCode, const std::string& message)
 {
    setStatusCode(statusCode);
    removeCachingHeaders();
-   setContentType("text/plain");
-   setBodyUnencoded(message);
+   setContentType("text/html");
+   setBodyUnencoded(string_utils::htmlEscape(message));
 }
 
 void Response::setNotFoundError(const std::string& uri)
 {
-   setError(http::status::NotFound,
-            string_utils::htmlEscape(uri) + " not found");
+   setError(http::status::NotFound, uri + " not found");
 }
    
 void Response::setError(const Error& error)
