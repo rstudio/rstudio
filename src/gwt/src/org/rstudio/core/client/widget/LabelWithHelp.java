a9d3362859c3e4bdf110debf2fd4fe9db7909d42
JJ Allaire <jj@rstudio.org>
2014-07-12 10:09:27 -0400

    factor LabelWithHelp out of spelling prefs

---
+++ src/gwt/src/org/rstudio/core/client/widget/LabelWithHelp.java
@@ -0,0 +1,35 @@
+/*
+ * LabelWithHelp.java
+ *
+ * Copyright (C) 2009-12 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+package org.rstudio.core.client.widget;
+
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+
+public class LabelWithHelp extends Composite
+{
+   public LabelWithHelp(String text, String helpTopic)
+   {
+      HorizontalPanel labelPanel = new HorizontalPanel();
+      Label label = new Label(text);
+      labelPanel.add(label);
+      HelpButton helpButton =  new HelpButton(helpTopic);
+      helpButton.getElement().getStyle().setMarginLeft(3, Unit.PX);
+      labelPanel.add(helpButton);
+      initWidget(labelPanel);
+   }  
+}
