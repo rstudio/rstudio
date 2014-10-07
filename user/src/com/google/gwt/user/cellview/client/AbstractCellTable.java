gwtproject/gwt

Allow to set emptyTableWidget and loadingIndicator in uibinder
Bug: issue 7121
Change-Id: I2cb4d10b3ffd3997d99a2771a6942bbb2b2d4841

user/src/com/google/gwt/user/cellview/client/AbstractCellTable.java
@@ -43,6 +43,7 @@ import com.google.gwt.safehtml.client.SafeHtmlTemplates;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+import com.google.gwt.uibinder.client.UiChild;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Window;
@@ -1536,6 +1537,7 @@ public abstract class AbstractCellTable<T> extends AbstractHasData<T> {
    * 
    * @param widget the empty table widget, or null to disable
    */
+  @UiChild(tagname="emptytablewidget", limit=1)
   public void setEmptyTableWidget(Widget widget) {
     this.emptyTableWidget = widget;
   }
@@ -1622,6 +1624,7 @@ public abstract class AbstractCellTable<T> extends AbstractHasData<T> {
    * 
    * @param widget the loading indicator, or null to disable
    */
+  @UiChild(tagname="loadingindicator", limit=1)
   public void setLoadingIndicator(Widget widget) {
     loadingIndicator = widget;
   }

rstudio/rstudio

Change-Id: Ie0d05e8a672d6e4166fe0fd29020e54e4e4f83cf

user/src/com/google/gwt/user/cellview/client/AbstractCellTable.java
@@ -44,6 +44,7 @@ import com.google.gwt.safehtml.client.SafeHtmlTemplates;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+import com.google.gwt.uibinder.client.UiChild;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Window;
@@ -1537,6 +1538,7 @@ public abstract class AbstractCellTable<T> extends AbstractHasData<T> {
    * 
    * @param widget the empty table widget, or null to disable
    */
+  @UiChild(tagname = "emptyTableWidget", limit = 1)
   public void setEmptyTableWidget(Widget widget) {
     this.emptyTableWidget = widget;
   }
@@ -1623,6 +1625,7 @@ public abstract class AbstractCellTable<T> extends AbstractHasData<T> {
    * 
    * @param widget the loading indicator, or null to disable
    */
+  @UiChild(tagname = "loadingIndicator", limit = 1)
   public void setLoadingIndicator(Widget widget) {
     loadingIndicator = widget;
   }