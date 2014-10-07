gwtproject/gwt

Refactor StorageMap.
This change also fixes failing MapInterfaceTest in DevMode.

Change-Id: If56cba6f4e759909d6bcb7b04860ddb24ad861f8

user/test/com/google/gwt/storage/client/MapInterfaceTest.java
@@ -679,6 +679,8 @@ public abstract class MapInterfaceTest<K, V> extends GWTTestCase {
       try {
         entrySet.retainAll(null);
         // Returning successfully is not ideal, but tolerated.
+      } catch (NullPointerException expected) {
+        // Expected in GWT client.
       } catch (JavaScriptException e) {
         // Expected in GWT client.
       }
@@ -1311,6 +1313,8 @@ public abstract class MapInterfaceTest<K, V> extends GWTTestCase {
       try {
         keySet.retainAll(null);
         // Returning successfully is not ideal, but tolerated.
+      } catch (NullPointerException expected) {
+        // Expected in GWT client.
       } catch (JavaScriptException e) {
         // Expected in GWT client.
       }
@@ -1480,6 +1484,8 @@ public abstract class MapInterfaceTest<K, V> extends GWTTestCase {
       try {
         values.removeAll(null);
         // Returning successfully is not ideal, but tolerated.
+      } catch (NullPointerException expected) {
+        // Expected in GWT client.
       } catch (JavaScriptException e) {
         // Expected in GWT client.
       }
@@ -1538,6 +1544,8 @@ public abstract class MapInterfaceTest<K, V> extends GWTTestCase {
       try {
         values.retainAll(null);
         // Returning successfully is not ideal, but tolerated.
+      } catch (NullPointerException expected) {
+        // Expected in GWT client.
       } catch (JavaScriptException e) {
         // Expected in GWT client.
       }

rstudio/rstudio

user/test/com/google/gwt/storage/client/MapInterfaceTest.java
@@ -19,7 +19,6 @@ package com.google.gwt.storage.client;
 import static java.util.Collections.singleton;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.JavaScriptException;
 import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.testing.TestUtils;
 
@@ -717,21 +716,15 @@ public abstract class MapInterfaceTest<K, V> extends GWTTestCase {
     final Entry<K, V> entryToAdd = mapEntry(null, null);
     try {
       entrySet.add(entryToAdd);
-      fail("Expected UnsupportedOperationException or JavaScriptException.");
-    } catch (UnsupportedOperationException e) {
-      // Expected.
-    } catch (JavaScriptException e) {
-      // Expected in GWT client.
+      fail("Expected UnsupportedOperationException.");
+    } catch (UnsupportedOperationException expected) {
     }
     assertInvariants(map);
 
     try {
       entrySet.addAll(singleton(entryToAdd));
-      fail("Expected UnsupportedOperationException or JavaScriptException.");
-    } catch (UnsupportedOperationException e) {
-      // Expected.
-    } catch (JavaScriptException e) {
-      // Expected in GWT client.
+      fail("Expected UnsupportedOperationException.");
+    } catch (UnsupportedOperationException expected) {
     }
     assertInvariants(map);
   }