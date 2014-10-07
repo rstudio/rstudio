9747044be65bed5fd6bfe4f4efc2613a3a6446ed
Kevin Ushey <kevinushey@gmail.com>
2014-10-07 11:03:23 -0700

    Bring back auto outdent fixes

--- src/gwt/acesupport/acemode/r_matching_brace_outdent.js
+++ src/gwt/acesupport/acemode/r_matching_brace_outdent.js
@@ -51,7 +51,7 @@ define("mode/r_matching_brace_outdent", function(require, exports, module)
          return false;
       };
 
-      this.autoOutdent = function(state, session, row) {
+      this.autoOutdent = function(state, session, row, codeModel) {
          if (row == 0)
             return 0;
 
@@ -65,7 +65,7 @@ define("mode/r_matching_brace_outdent", function(require, exports, module)
 
             if (!openBracePos || openBracePos.row == row) return 0;
 
-            var indent = this.codeModel.getIndentForOpenBrace(openBracePos);
+            var indent = codeModel.getIndentForOpenBrace(openBracePos);
             session.replace(new Range(row, 0, row, column-1), indent);
          }
 
@@ -73,7 +73,7 @@ define("mode/r_matching_brace_outdent", function(require, exports, module)
          if (match)
          {
             var column = match[1].length;
-            var indent = this.codeModel.getBraceIndent(row-1);
+            var indent = codeModel.getBraceIndent(row-1);
             session.replace(new Range(row, 0, row, column-1), indent);
          }
       };
