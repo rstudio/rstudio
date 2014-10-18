gwtproject/gwt

Add GSS support.
Second patch concerning the support of GSS in GWT. This patch
introduce the generator that will parse and compile a GSS file
to a css string and generate the implementation of the
corresponding CssResource interface. GSS support is disabled
by default. To enable it, you have to set the configuration
property "CssResource.enableGss" to true.

Change-Id: Iae743842c5707c0231b1c4eb290f7914b6f7a501

user/src/com/google/gwt/resources/gss/GwtGssFunctionMapProvider.java
@@ -1,0 +1,43 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.resources.gss;
+
+import com.google.gwt.resources.ext.ResourceContext;
+import com.google.gwt.thirdparty.common.css.compiler.ast.GssFunction;
+import com.google.gwt.thirdparty.common.css.compiler.gssfunctions.DefaultGssFunctionMapProvider;
+import com.google.gwt.thirdparty.guava.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+public class GwtGssFunctionMapProvider extends DefaultGssFunctionMapProvider {
+  private final ResourceContext context;
+
+  public GwtGssFunctionMapProvider(ResourceContext context) {
+    this.context = context;
+  }
+
+  @Override
+  public Map<String, GssFunction> get() {
+    Map<String, GssFunction> gssFunction = super.get();
+
+    return ImmutableMap.<String, GssFunction>builder().putAll(gssFunction)
+        // TODO add a namespace for specific gwt function ?
+        .put(EvalFunction.getName(), new EvalFunction())
+        .put(ValueFunction.getName(), new ValueFunction())
+        .put(ResourceUrlFunction.getName(), new ResourceUrlFunction(context))
+        .build();
+  }
+}

rstudio/rstudio

user/src/com/google/gwt/resources/gss/GwtGssFunctionMapProvider.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.resources.gss;
+
+import com.google.gwt.resources.ext.ResourceContext;
+import com.google.gwt.thirdparty.common.css.compiler.ast.GssFunction;
+import com.google.gwt.thirdparty.common.css.compiler.gssfunctions.DefaultGssFunctionMapProvider;
+import com.google.gwt.thirdparty.guava.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+/**
+ * {@link GssFunctionMapProvider} that adds the mapping of GssFunction implemented for GWT.
+ */
+public class GwtGssFunctionMapProvider extends DefaultGssFunctionMapProvider {
+  private final ResourceContext context;
+
+  public GwtGssFunctionMapProvider(ResourceContext context) {
+    this.context = context;
+  }
+
+  @Override
+  public Map<String, GssFunction> get() {
+    Map<String, GssFunction> gssFunctionMap = super.get();
+
+    return ImmutableMap.<String, GssFunction>builder().putAll(gssFunctionMap)
+        // TODO add a namespace for gwt-specific function ?
+        .put(EvalFunction.getName(), new EvalFunction())
+        .put(ValueFunction.getName(), new ValueFunction())
+        .put(ResourceUrlFunction.getName(), new ResourceUrlFunction(context))
+        .build();
+  }
+}