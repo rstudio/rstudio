2e92a5103243b1b4cda1b5dca27990db6a7093aa
Kevin Ushey <kevin@rstudio.com>
2014-10-30 13:53:22 -0700

    Use '[' surrounding for arguments of functions

--- src/gwt/src/org/rstudio/studio/client/workbench/views/console/shell/assist/CompletionRequester.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/console/shell/assist/CompletionRequester.java
@@ -146,7 +146,7 @@ public class CompletionRequester
                         {
                            newComp.add(new QualifiedName(
                                  argName,
-                                 "<" + functionName + ">"
+                                 "[" + functionName + "]"
                            ));
                         }
                      }
