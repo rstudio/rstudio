f0f5a02bb976a31b489988bd857b2726abd43e2d
Brian Slesinsky <skybrian@google.com>
2014-11-05 14:16:13 -0800

    DevMode: wait for CodeServer to start before enabling buttons
    This is to give CodeServer time to write the launcher files,
    so the user doesn't see a blank page.
    
    Bug: issue 8956
    Change-Id: I6966352652e4bcf83bf0bc39439eb245d7e9635a
    Review-Link: https://gwt-review.googlesource.com/#/c/10121/

--- dev/core/src/com/google/gwt/dev/shell/CodeServerListener.java
+++ dev/core/src/com/google/gwt/dev/shell/CodeServerListener.java
@@ -13,6 +13,7 @@
  */
 package com.google.gwt.dev.shell;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.core.ext.linker.ArtifactSet;
 import com.google.gwt.core.ext.linker.impl.StandardLinkerContext;
@@ -31,9 +32,15 @@ public interface CodeServerListener {
   int getSocketPort();
 
   /**
-   * Start the listener thread.
+   * Starts the code server. (It won't be ready until the {@link #waitUntilReady returns.)
    */
   void start();
+
+  /**
+   * Blocks until the code server ready to handle requests.
+   * @throws UnableToCompleteException if unable to start.
+   */
+  void waitUntilReady(TreeLogger logger) throws UnableToCompleteException;
 
   /**
    * Returns the URL to use in the browser for using this codeserver.
