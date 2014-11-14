36b645105f1f2a0354c51911f3418800010401d3
Kevin Ushey <kevin@rstudio.com>
2014-11-14 09:38:36 -0800

    eliminate 'mustWork' argument (unnecessary)

--- src/cpp/session/modules/SessionRCompletions.R
+++ src/cpp/session/modules/SessionRCompletions.R
@@ -767,7 +767,7 @@ assign(x = ".rs.acCompletionTypes",
                                                   excludeArgsFromObject,
                                                   filePath)
 {
-   filePath <- suppressWarnings(.rs.normalizePath(filePath, mustWork = FALSE))
+   filePath <- suppressWarnings(.rs.normalizePath(filePath))
    
    ## NOTE: these are passed in as lists of strings; convert to character
    additionalArgs <- as.character(additionalArgs)
