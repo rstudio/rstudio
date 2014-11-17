64b6deccf6b77c8fb616174336641744ced3d672
JJ Allaire <jj@rstudio.org>
2014-11-17 13:16:30 -0500

    windows installer work for qt 5.4

--- package/win32/make-package.bat
+++ package/win32/make-package.bat
@@ -4,9 +4,9 @@ set PACKAGE_DIR="%CD%"
 REM clean if requested
 if "%1" == "clean" call clean-build.bat
 
-REM Prepend Qt 5.3.2 SDK Mingw to path
+REM Prepend Qt 5.4 SDK Mingw to path
 setlocal
-set PATH=C:\Qt\Qt5.3.2\Tools\mingw482_32\bin;%PATH%
+set PATH=C:\Qt\Qt5.4.0\Tools\mingw491_32\bin;%PATH%
 
 REM Establish build dir
 set BUILD_DIR=build
