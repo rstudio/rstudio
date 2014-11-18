c123d4887524191e960dec037641935ebff57d15
John Stalcup <stalcup@google.com>
2014-11-17 18:04:12 -0800

    Changes OutputFileSet to overwrite on equal timestamps.
    Previously OutputFileSetOnDirectory took special care to not overwrite 
    a file in the output directory if it had the same timestamp as the 
    current artifact being written. For a long time this had no effect
    since Link.java deleted al output directory contents anyway and DevMode
    was  not using OutputFileSets to perform it's writes.
    
    But somewhat recently DevMode was changed to use 
    OutputFileSetOnDirectory and it resulted in a subtle change in behavior
    where generated files from 1 compile would not be overwritten by the 
    samely named (but differently contented) generated file on a subsequent
    compile.
    
    As a result running a prod mode test followed by a dev mode test would 
    fail as the dev mode test tried to reuse the incompatible prod mode
    test Foo.nocache.js file.
    
    Change-Id: I0916ab7933f9dfcb36b361f2e21657fac8174d67
    Review-Link: https://gwt-review.googlesource.com/#/c/10311/

--- dev/core/src/com/google/gwt/dev/util/OutputFileSet.java
+++ dev/core/src/com/google/gwt/dev/util/OutputFileSet.java
@@ -68,14 +68,15 @@ public abstract class OutputFileSet {
   }
 
   /**
-   * Opens a file for write. If writing to a zip file and the file already
-   * exists, there will be no effect. If writing to a directory, the file
-   * exists, and the given timestamp is older or equal to the file's
-   * current timestamp, there will be no effect.
+   * Opens a file for write. If writing to a zip file and the file already exists,
+   * there will be no effect. If writing to a directory, the file exists, and the
+   * given timestamp is older than the current timestamp, there will be no effect.
+   *
    * @param timeStampMillis last modified time in milliseconds, or
-   * TIMESTAMP_UNAVAILABLE to force an overwrite.
+   *          TIMESTAMP_UNAVAILABLE to force an overwrite.
    * @return the output stream to write to, possibly a NullOutputStream
    */
+  // TODO: revalidate whether any timestamp based overwrite prevention is needed.
   public OutputStream openForWrite(String path, long timeStampMillis)
       throws IOException {
     pathsSeen.add(path);
