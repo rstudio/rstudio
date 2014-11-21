gwtproject/gwt

Fixes an inconsistency that resulted in a bad AST.
In some cases the Pruner would leave the AST in an incosistent form
where there is a JNewInstance poiting to a JConstructor that is has
been already removed from the correspoinding class.

Suprisingly most passes seem not to care as, in fact, this constructor
is actually unreachable. However, under the correct (or should I say
wrong) circumstances MethodInliner fails due to inconsistent
expectations about the parameters of the method it inlines.

This patch also adds a verification stage after each transformation
in the optimization loop that is only run when assertions are enabled.

Change-Id: Iabcc6d4247205f6097e514d33a14bd4a1e9924a9

dev/core/src/com/google/gwt/dev/jjs/JavaToJavaScriptCompiler.java
@@ -92,6 +92,7 @@ import com.google.gwt.dev.jjs.impl.TypeRefDepsChecker;
 import com.google.gwt.dev.jjs.impl.TypeReferencesRecorder;
 import com.google.gwt.dev.jjs.impl.TypeTightener;
 import com.google.gwt.dev.jjs.impl.UnifyAst;
+import com.google.gwt.dev.jjs.impl.Verifier;
 import com.google.gwt.dev.jjs.impl.codesplitter.CodeSplitters;
 import com.google.gwt.dev.jjs.impl.codesplitter.MultipleDependencyGraphRecorder;
 import com.google.gwt.dev.jjs.impl.codesplitter.ReplaceRunAsyncs;
@@ -1501,6 +1502,7 @@ public abstract class JavaToJavaScriptCompiler {
     // Clinits might have become empty become empty.
     jprogram.typeOracle.recomputeAfterOptimizations(jprogram.getDeclaredTypes());
     OptimizerStats stats = new OptimizerStats(passName);
+    Verifier.assertProgramIsConsistent(jprogram);
     stats.add(Pruner.exec(jprogram, true, optimizerCtx).recordVisits(numNodes));
     stats.add(Finalizer.exec(jprogram, optimizerCtx).recordVisits(numNodes));
     stats.add(MakeCallsStatic.exec(jprogram, options.shouldAddRuntimeChecks(), optimizerCtx)

dev/core/src/com/google/gwt/dev/jjs/impl/EnumOrdinalizer.java
@@ -835,6 +835,7 @@ public class EnumOrdinalizer {
         type.setOrdinalized();
       }
     }
+    Verifier.assertProgramIsConsistent(program);
     return stats;
   }
 

dev/core/src/com/google/gwt/dev/jjs/impl/Finalizer.java
@@ -240,6 +240,8 @@ public class Finalizer {
     FinalizeVisitor finalizer = new FinalizeVisitor(optimizerCtx);
     finalizer.accept(program);
 
+    Verifier.assertProgramIsConsistent(program);
+
     return new OptimizerStats(NAME).recordModified(finalizer.getNumMods());
   }
 }

dev/core/src/com/google/gwt/dev/jjs/impl/MakeCallsStatic.java
@@ -502,6 +502,7 @@ public class MakeCallsStatic {
     rewriter.accept(program);
     stats.recordModified(rewriter.getNumMods());
     assert (rewriter.didChange() || toBeMadeStatic.isEmpty());
+    Verifier.assertProgramIsConsistent(program);
     return stats;
   }
 }

dev/core/src/com/google/gwt/dev/jjs/impl/MethodCallTightener.java
@@ -130,6 +130,7 @@ public class MethodCallTightener {
     OptimizerStats stats = new MethodCallTightener(program).execImpl(optimizerCtx);
     optimizerCtx.incOptimizationStep();
     optimizeEvent.end("didChange", "" + stats.didChange());
+    Verifier.assertProgramIsConsistent(program);
     return stats;
   }
 

dev/core/src/com/google/gwt/dev/jjs/impl/MethodInliner.java
@@ -578,6 +578,7 @@ public class MethodInliner {
       stats.recordModified(dceStats.getNumMods());
     }
     optimizerCtx.setLastStepFor(NAME, optimizerCtx.getOptimizationStep());
+    Verifier.assertProgramIsConsistent(program);
     return stats;
   }
 
@@ -585,7 +586,8 @@ public class MethodInliner {
    * Return the set of methods affected (because they are or callers of) by the modifications to the
    * given set functions.
    */
-  private Set<JMethod> affectedMethods(Set<JMethod> modifiedMethods, OptimizerContext optimizerCtx) {
+  private Set<JMethod> affectedMethods(Set<JMethod> modifiedMethods,
+      OptimizerContext optimizerCtx) {
     Set<JMethod> affectedMethods = Sets.newLinkedHashSet();
     if (modifiedMethods == null || modifiedMethods.size() == 0) {
       return affectedMethods;

dev/core/src/com/google/gwt/dev/jjs/impl/SameParameterValueOptimizer.java
@@ -243,6 +243,7 @@ public class SameParameterValueOptimizer {
         stats.recordModified(substituteParameterVisitor.getNumMods());
       }
     }
+    Verifier.assertProgramIsConsistent(program);
     return stats;
   }
 }

rstudio/rstudio

dev/core/src/com/google/gwt/dev/jjs/JavaToJavaScriptCompiler.java
@@ -70,6 +70,7 @@ import com.google.gwt.dev.jjs.impl.Finalizer;
 import com.google.gwt.dev.jjs.impl.FixAssignmentsToUnboxOrCast;
 import com.google.gwt.dev.jjs.impl.GenerateJavaScriptAST;
 import com.google.gwt.dev.jjs.impl.ImplementClassLiteralsAsFields;
+import com.google.gwt.dev.jjs.impl.JavaAstVerifier;
 import com.google.gwt.dev.jjs.impl.JavaToJavaScriptMap;
 import com.google.gwt.dev.jjs.impl.JsAbstractTextTransformer;
 import com.google.gwt.dev.jjs.impl.JsFunctionClusterer;
@@ -1501,6 +1502,7 @@ public abstract class JavaToJavaScriptCompiler {
     // Clinits might have become empty become empty.
     jprogram.typeOracle.recomputeAfterOptimizations(jprogram.getDeclaredTypes());
     OptimizerStats stats = new OptimizerStats(passName);
+    JavaAstVerifier.assertProgramIsConsistent(jprogram);
     stats.add(Pruner.exec(jprogram, true, optimizerCtx).recordVisits(numNodes));
     stats.add(Finalizer.exec(jprogram, optimizerCtx).recordVisits(numNodes));
     stats.add(MakeCallsStatic.exec(jprogram, options.shouldAddRuntimeChecks(), optimizerCtx)

dev/core/src/com/google/gwt/dev/jjs/impl/EnumOrdinalizer.java
@@ -835,6 +835,7 @@ public class EnumOrdinalizer {
         type.setOrdinalized();
       }
     }
+    JavaAstVerifier.assertProgramIsConsistent(program);
     return stats;
   }
 

dev/core/src/com/google/gwt/dev/jjs/impl/Finalizer.java
@@ -240,6 +240,8 @@ public class Finalizer {
     FinalizeVisitor finalizer = new FinalizeVisitor(optimizerCtx);
     finalizer.accept(program);
 
+    JavaAstVerifier.assertProgramIsConsistent(program);
+
     return new OptimizerStats(NAME).recordModified(finalizer.getNumMods());
   }
 }

dev/core/src/com/google/gwt/dev/jjs/impl/MakeCallsStatic.java
@@ -503,6 +503,7 @@ public class MakeCallsStatic {
     rewriter.accept(program);
     stats.recordModified(rewriter.getNumMods());
     assert (rewriter.didChange() || toBeMadeStatic.isEmpty());
+    JavaAstVerifier.assertProgramIsConsistent(program);
     return stats;
   }
 }

dev/core/src/com/google/gwt/dev/jjs/impl/MethodCallTightener.java
@@ -130,6 +130,7 @@ public class MethodCallTightener {
     OptimizerStats stats = new MethodCallTightener(program).execImpl(optimizerCtx);
     optimizerCtx.incOptimizationStep();
     optimizeEvent.end("didChange", "" + stats.didChange());
+    JavaAstVerifier.assertProgramIsConsistent(program);
     return stats;
   }
 

dev/core/src/com/google/gwt/dev/jjs/impl/MethodInliner.java
@@ -586,6 +586,7 @@ public class MethodInliner {
       stats.recordModified(dceStats.getNumMods());
     }
     optimizerCtx.setLastStepFor(NAME, optimizerCtx.getOptimizationStep());
+    JavaAstVerifier.assertProgramIsConsistent(program);
     return stats;
   }
 
@@ -593,7 +594,8 @@ public class MethodInliner {
    * Return the set of methods affected (because they are or callers of) by the modifications to the
    * given set functions.
    */
-  private Set<JMethod> affectedMethods(Set<JMethod> modifiedMethods, OptimizerContext optimizerCtx) {
+  private Set<JMethod> affectedMethods(Set<JMethod> modifiedMethods,
+      OptimizerContext optimizerCtx) {
     Set<JMethod> affectedMethods = Sets.newLinkedHashSet();
     if (modifiedMethods == null || modifiedMethods.size() == 0) {
       return affectedMethods;

dev/core/src/com/google/gwt/dev/jjs/impl/SameParameterValueOptimizer.java
@@ -243,6 +243,7 @@ public class SameParameterValueOptimizer {
         stats.recordModified(substituteParameterVisitor.getNumMods());
       }
     }
+    JavaAstVerifier.assertProgramIsConsistent(program);
     return stats;
   }
 }