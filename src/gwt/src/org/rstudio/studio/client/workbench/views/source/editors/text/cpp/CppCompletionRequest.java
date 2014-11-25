cfdf8b20e88fd8fc5213774edf124668a3d3c1c5
JJ Allaire <jj@rstudio.org>
2014-11-25 08:28:01 -0500

    cpp completion: correctly position cursor inside parens even when signature tooltips are disabled

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/cpp/CppCompletionRequest.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/cpp/CppCompletionRequest.java
@@ -267,13 +267,14 @@ public class CppCompletionRequest
       docDisplay_.setSelection(getReplacementSelection());
       docDisplay_.replaceSelection(insertText, true);
       
-      if (completion.hasParameters() && 
-          uiPrefs_.showSignatureTooltips().getValue())
+      if (completion.hasParameters())
       {
          Position pos = docDisplay_.getCursorPosition();
          pos = Position.create(pos.getRow(), pos.getColumn() - 1);
          docDisplay_.setSelectionRange(Range.fromPoints(pos, pos));
-         new CppCompletionSignatureTip(completion, docDisplay_);
+         
+         if (uiPrefs_.showSignatureTooltips().getValue())
+            new CppCompletionSignatureTip(completion, docDisplay_);
       }
    }
    
