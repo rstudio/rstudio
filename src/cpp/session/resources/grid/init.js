f24843e004d44bf59abbed53dda08b82d2ef3ae5
Jonathan McPherson <jonathan@rstudio.com>
2014-12-03 15:30:36 -0800

    implement filtering UX; support as-you-type/as-you-click filtering

--- src/cpp/session/resources/grid/init.js
+++ src/cpp/session/resources/grid/init.js
@@ -18,12 +18,22 @@
 // the data table itself
 var table;   
 
-// which column we're constructing a filter for, and functions to extract the
-// value from the filter UI for that column
-var filterColIdx = 0; 
-var getFilterColValue = function() { return ""; };
-var getFilterDisplayValue = function() { return ""; };
+var dismissActivePopup = function() { };
 
+// throttle to avoid redrawing the table too frequently (when filtering,
+// resizing, etc.)
+var debounce = function(func, wait) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			func.apply(context, args);
+		};
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+	};
+};
 
 // used to determine the step precision for a number--e.g.:
 // "10.4" has a step precision of 0.1
@@ -112,6 +122,217 @@ var runAfterSizing = function(func) {
       height = window.innerHeight;
     }
   }, 10);
+};
+
+
+var createNumericFilterUI = function(idx, col, onDismiss) {
+  var ele = document.createElement("div");
+  invokeFilterPopup(ele, function(popup) {
+    popup.className += " numericFilterPopup";
+    var min = col.col_min.toString();
+    var max = col.col_max.toString();
+    var val = table.columns(idx).search()[0];
+    if (val.indexOf("-") > 0) {
+      var range = val.split("-");
+      min = range[0];
+      max = range[1];
+    } else if (!isNaN(parseInt(val))) {
+      min = parseInt(val);
+      max = parseInt(val);
+    }
+    var minVal = document.createElement("div");
+    minVal.innerText = min;
+    minVal.className = "numMin selected";
+    popup.appendChild(minVal);
+    var maxVal = document.createElement("div");
+    maxVal.innerText = max;
+    maxVal.className = "numMax selected";
+    popup.appendChild(maxVal);
+    var slider = document.createElement("div");
+    slider.className = "numSlider";
+    popup.appendChild(slider);
+    var updateView = debounce(function() {
+      searchText = minVal.innerText === min && maxVal.innerText === max ? 
+        "" : minVal.innerText + "-" + maxVal.innerText;
+        table.columns(idx).search(searchText).draw();
+    }, 200);
+    $(slider).slider({
+      range:  true,
+      min:    col.col_min,
+      max:    col.col_max,
+      step:   Math.min(stepPrecision(col.col_min.toString()), 
+                       stepPrecision(col.col_max.toString())),
+      values: [min, max],
+      slide:  function(event, ui) {
+        minVal.innerText = ui.values[0];
+        maxVal.innerText = ui.values[1];
+        updateView();
+      }
+    });
+  }, onDismiss, false);
+  ele.innerText = "[...]";
+  return ele;
+};
+
+var createFactorFilterUI = function(idx, col, onDismiss) {
+  var ele = document.createElement("div");
+  var display = document.createElement("span");
+  display.innerHTML = "&nbsp;";
+  ele.appendChild(display);
+
+  var setValHandler = function(factor, text) {
+      return function(evt) {
+        table.columns(idx).search(factor.toString()).draw();
+        display.innerText = text;
+      };
+  };
+  invokeFilterPopup(ele, function(popup) {
+    var list = document.createElement("div");
+    list.className = "factorList";
+    var val = table.columns(idx).search()[0];
+    var current = val.length > 0 ? parseInt(val) : 0;
+    for (var i = 0; i < col.col_vals.length; i++) {
+      var opt = document.createElement("div");
+      opt.innerText = col.col_vals[i];
+      opt.className = "factorListItem";
+      opt.addEventListener("click", setValHandler(i + 1, col.col_vals[i]));
+      list.appendChild(opt);
+    }
+    popup.appendChild(list);
+  }, onDismiss, false);
+
+  return ele;
+};
+
+var createTextFilterUI = function(idx, col, onDismiss) {
+  var ele = document.createElement("div");
+  var input = document.createElement("input");
+  input.type = "text";
+  input.className = "textFilterBox";
+  input.value = table.columns(idx).search()[0];
+  var updateView = debounce(function() {
+          table.columns(idx).search(input.value).draw();
+    }, 200);
+  input.addEventListener("keyup", function(evt) {
+    // TODO: handle Esc
+    updateView();
+  });
+  input.addEventListener("blur", function(evt) {
+    onDismiss();
+  });
+  ele.addEventListener("click", function(evt) {
+    input.focus();
+    evt.preventDefault();
+    evt.stopPropagation();
+  });
+  ele.appendChild(input);
+  return ele;
+};
+
+var invokeFilterPopup = function (ele, buildPopup, onDismiss, dismissOnClick) {
+  var popup = null;
+
+  var dismissPopup = function() {
+    if (popup) {
+      document.body.removeChild(popup);
+      document.body.removeEventListener(checkLightDismiss);
+      popup = null;
+      onDismiss();
+      return true;
+    }
+    return false;
+  };
+  
+  var checkLightDismiss = function(evt) {
+    if (popup && (!dismissOnClick || !popup.contains(evt.target))) {
+      dismissPopup();
+    }
+  };
+
+  ele.addEventListener("click", function(evt) {
+    // dismiss any other popup
+    if (dismissActivePopup != dismissPopup) {
+      dismissActivePopup();
+    }
+    if (popup) {
+      dismissPopup();
+    } else {
+      popup = createFilterPopup();
+      buildPopup(popup);
+      popup.style.top = ($(ele).offset().top + 20) + "px";
+      popup.style.left = ($(ele).offset().left - 4)  + "px";
+      document.body.appendChild(popup);
+      document.body.addEventListener("click", checkLightDismiss);
+      dismissActivePopup = dismissPopup;
+    }
+    evt.preventDefault();
+    evt.stopPropagation();
+  });
+};
+
+var createFilterUI = function(idx, col) {
+  var host = document.createElement("div");
+  var val = null, ui = null;
+  host.className = "colFilter unfiltered";
+
+  var setUnfiltered = function() {
+    if (ui !== null) {
+      host.replaceChild(val, ui);
+      ui = null;
+    }
+    host.className = "colFilter unfiltered";
+    clear.style.display = "none";
+  };
+
+  var onDismiss = function() {
+    if (table.columns(idx).search()[0].length === 0) {
+      setUnfiltered();
+    }
+  };
+
+  var clear = document.createElement("img");
+  clear.src = "datatables/images/clear_filter.png";
+  clear.className = "clearFilter";
+  clear.style.display = "none";
+  clear.addEventListener("click", function(evt) {
+    dismissActivePopup();
+    table.columns(idx).search("").draw();
+    setUnfiltered();
+    evt.preventDefault();
+    evt.stopPropagation();
+  });
+  host.appendChild(clear);
+
+  val = document.createElement("div");
+  val.innerText = "All";
+  val.addEventListener("click", function(evt) {
+    if (col.col_type === "numeric") {
+      ui = createNumericFilterUI(idx, col, onDismiss);
+    } else if (col.col_type === "factor") {
+      ui = createFactorFilterUI(idx, col, onDismiss);
+    } else if (col.col_type === "character") {
+      ui = createTextFilterUI(idx, col, onDismiss);
+    }
+    if (ui) {
+      ui.className += " filterValue";
+      host.replaceChild(ui, val);
+      host.className = "colFilter filtered";
+      clear.style.display = "block";
+      ui.click();
+      evt.preventDefault();
+      evt.stopPropagation();
+    }
+  });
+
+  host.appendChild(val);
+  host.style.display = "none";
+  return host;
+};
+
+var createFilterPopup = function() {
+  var filterUI = document.createElement("div");
+  filterUI.className = "filterPopup";
+  return filterUI;
 };
 
 var showFilterUI = function(idx, col) {
@@ -236,7 +457,7 @@ var updateColFilterDisplay = function() {
                               .children[filterColIdx].children[1];
   filterDisplay.innerText = getFilterDisplayValue();
   filterDisplay.className = getFilterColValue().length > 0 ? 
-    "colFilter" : "colFilter unfiltered";
+    "colFilter filtered" : "colFilter unfiltered";
 };
 
 var createHeader = function(idx, col) {
@@ -253,28 +474,13 @@ var createHeader = function(idx, col) {
     th.title += " with " + col.col_vals.length + " levels";
   }
 
-  // add the filter controls
-  if (col.col_type === "numeric" ||
-      col.col_type === "factor" || 
-      col.col_type === "character") {
-    var filter = document.createElement("div");
-    filter.className = "colFilter unfiltered";
-    filter.style.display = "none";
-    filter.innerText = "All";
-    filter.tabIndex = 0;
-    filter.addEventListener("click", function(evt) {
-      if (document.getElementById("filterUI").style.display === "none")
-        showFilterUI(idx, col);
-      else
-      {
-        hideFilterUI();
-        if (idx != filterColIdx) 
-          showFilterUI(idx, col);
-      }
-      evt.stopPropagation();
-    });
+  if (col.col_type === "numeric" || 
+      col.col_type === "character" ||
+      col.col_type === "factor")  {
+    var filter = createFilterUI(idx, col);
     th.appendChild(filter);
   }
+
   return th;
 };
 
@@ -388,24 +594,6 @@ var initDataTable = function() {
 
 $(document).ready(function() {
   runAfterSizing(initDataTable);
-
-  document.getElementById("filterForm")
-          .addEventListener("submit", function(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-    hideFilterUI();
-    updateColFilterDisplay();
-    table.columns(filterColIdx).search(getFilterColValue()).draw();
-  }, false);
-
-  document.getElementById("clearFilter")
-          .addEventListener("click", function(evt) {
-    hideFilterUI();
-    getFilterColValue = function() { return ""; };
-    getFilterDisplayValue = function() { return "All"; };
-    updateColFilterDisplay();
-    table.columns(filterColIdx).search("").draw();
-  }, false);
 });
 
 
@@ -414,17 +602,15 @@ $(document).ready(function() {
 // called from RStudio to toggle the filter UI 
 window.setFilterUIVisible = function(visible) {
   var thead = document.getElementById("data_cols");
+  if (!visible) {
+    // clear all the filter data
+    $("#data").DataTable().columns().search("");
+  }
   for (var i = 0; i < thead.children.length; i++) {
     if (thead.children[i].children.length > 1) {
       var filter = thead.children[i].children[1];
       filter.style.display = visible ? "block" : "none";
-      filter.className = "colFilter unfiltered";
-      filter.innerText = "All";
     }
-  }
-  if (!visible) {
-    // clear all the filter data
-    $("#data").DataTable().columns().search("");
   }
   sizeDataTable(false);
 };
