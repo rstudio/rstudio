3e878641ea0e4bdee673a01f2c022db0d618f776
Kevin Ushey <kevin@rstudio.com>
2014-12-12 10:25:53 -0800

    one more line

--- src/cpp/session/modules/SessionPackages.R
+++ src/cpp/session/modules/SessionPackages.R
@@ -757,6 +757,7 @@ if (identical(as.character(Sys.info()["sysname"]), "Darwin") &&
          "# Hello, world!",
          "#",
          "# This is an example function named 'hello' which prints 'Hello, world!'.",
+         "#",
          "# You can learn more about package authoring with RStudio at:",
          "#",
          "#   http://r-pkgs.had.co.nz/",
