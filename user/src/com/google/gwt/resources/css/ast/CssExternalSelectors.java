2eeea49cacfd3e890eab78903659d428ff38049b
Daniel Kurka <dankurka@google.com>
2014-12-18 17:53:12 +0100

    Ensure iteration order for external classes.
    Switching to Java8 makes Css2Gss fail since it depends on iteration
    order.
    
    Change-Id: Ica070c5d2523bdc9e1f5ff97f8237ecac76d22f9

--- user/src/com/google/gwt/resources/css/ast/CssExternalSelectors.java
+++ user/src/com/google/gwt/resources/css/ast/CssExternalSelectors.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,7 +15,7 @@
  */
 package com.google.gwt.resources.css.ast;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -25,7 +25,7 @@ import java.util.Set;
  */
 public class CssExternalSelectors extends CssNode {
 
-  private final Set<String> classes = new HashSet<String>();
+  private final Set<String> classes = new LinkedHashSet<String>();
 
   public Set<String> getClasses() {
     return classes;
@@ -35,10 +35,10 @@ public class CssExternalSelectors extends CssNode {
   public boolean isStatic() {
     return true;
   }
-  
+
+  @Override
   public void traverse(CssVisitor visitor, Context context) {
     visitor.visit(this, context);
     visitor.endVisit(this, context);
   }
-
 }
