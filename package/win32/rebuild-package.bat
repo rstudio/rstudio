5308562147b6a0058fe2bec1bb7fab3722b350a3
kevinushey <kevinushey@gmail.com>
2015-01-09 12:16:40 -0800

    scripts for faster rebuilding of packaged rstudio on windows

---
+++ package/win32/rebuild-package.bat
@@ -0,0 +1,37 @@
+
+set PACKAGE_DIR="%CD%"
+
+REM Prepend Qt 5.4 SDK Mingw to path
+setlocal
+set PATH=C:\Qt\Qt5.4.0\Tools\mingw491_32\bin;%PATH%
+
+REM Establish build dir
+set BUILD_DIR=build
+IF "%CMAKE_BUILD_TYPE%" == "" set CMAKE_BUILD_TYPE=Release
+IF "%CMAKE_BUILD_TYPE%" == "Debug" set BUILD_DIR=build-debug
+
+REM perform 32-bit build 
+cd "%PACKAGE_DIR%"
+mkdir "%BUILD_DIR%"
+cd "%BUILD_DIR%"
+cmake -G"MinGW Makefiles" -DRSTUDIO_TARGET=Desktop -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DRSTUDIO_PACKAGE_BUILD=1 ..\..\..
+mingw32-make -j10
+cd ..
+
+REM perform 64-bit build and install it into the 32-bit tree
+REM (but only do this if we are on win64)
+IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" call rebuild-install-win64.bat "%PACKAGE_DIR%\%BUILD_DIR%\src\cpp\session" %1
+
+REM create packages
+cd "%BUILD_DIR%"
+cpack -G NSIS
+IF "%CMAKE_BUILD_TYPE%" == "Release" cpack -G ZIP
+cd ..
+
+REM reset modified environment variables (PATH)
+endlocal
+
+
+
+
+
