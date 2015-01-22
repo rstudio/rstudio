e3310a16a6b8511825e32a4f847fe4f518313c40
JJ Allaire <jj@rstudio.org>
2015-01-21 19:05:05 -0500

    more namespace work

--- src/cpp/core/include/core/Error.hpp
+++ src/cpp/core/include/core/Error.hpp
@@ -174,7 +174,7 @@ std::ostream& operator<<(std::ostream& os, const ErrorLocation& location);
 } // namespace core 
 } // namespace rstudio
 
-#define ERROR_LOCATION core::ErrorLocation( \
+#define ERROR_LOCATION rstudio::core::ErrorLocation( \
       BOOST_CURRENT_FUNCTION,__FILE__,__LINE__)
 
 #define CATCH_UNEXPECTED_EXCEPTION \
