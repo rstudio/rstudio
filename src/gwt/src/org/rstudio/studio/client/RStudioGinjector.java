72895805efd71abc62de383af7ce898eb55ab15b
JJ Allaire <jj@rstudio.org>
2015-03-20 14:34:25 -0400

    more work on snippet editor

--- src/gwt/src/org/rstudio/studio/client/RStudioGinjector.java
+++ src/gwt/src/org/rstudio/studio/client/RStudioGinjector.java
@@ -58,6 +58,7 @@ import org.rstudio.studio.client.workbench.commands.Commands;
 import org.rstudio.studio.client.workbench.model.RemoteFileSystemContext;
 import org.rstudio.studio.client.workbench.model.Session;
 import org.rstudio.studio.client.workbench.prefs.model.UIPrefs;
+import org.rstudio.studio.client.workbench.snippets.ui.EditSnippetsDialog;
 import org.rstudio.studio.client.workbench.views.console.shell.assist.CompletionRequester;
 import org.rstudio.studio.client.workbench.views.console.shell.assist.HelpStrategy;
 import org.rstudio.studio.client.workbench.views.console.shell.assist.RCompletionManager;
@@ -111,6 +112,7 @@ public interface RStudioGinjector extends Ginjector
    void injectMembers(PathBreadcrumbWidget pathBreadcrumbWidget);
    void injectMembers(LintManager manager);
    void injectMembers(CompletionRequester requester);
+   void injectMembers(EditSnippetsDialog dialog);
    
    public static final RStudioGinjector INSTANCE = GWT.create(RStudioGinjector.class);
 
--- src/gwt/src/org/rstudio/studio/client/workbench/snippets/ui/EditSnippetsDialog.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/snippets/ui/EditSnippetsDialog.java
@@ -16,36 +16,65 @@
 package org.rstudio.studio.client.workbench.snippets.ui;
 
 
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import org.rstudio.core.client.BrowseCap;
 import org.rstudio.core.client.Size;
+import org.rstudio.core.client.command.KeyboardShortcut;
 import org.rstudio.core.client.dom.DomMetrics;
 import org.rstudio.core.client.widget.ModalDialogBase;
+import org.rstudio.core.client.widget.ModalDialogTracker;
 import org.rstudio.core.client.widget.ThemedButton;
 import org.rstudio.core.client.widget.WidgetListBox;
+import org.rstudio.studio.client.RStudioGinjector;
+import org.rstudio.studio.client.application.events.EventBus;
+import org.rstudio.studio.client.common.HelpLink;
 import org.rstudio.studio.client.common.filetypes.FileTypeRegistry;
 import org.rstudio.studio.client.common.filetypes.TextFileType;
+import org.rstudio.studio.client.workbench.ui.FontSizeManager;
 import org.rstudio.studio.client.workbench.views.source.editors.text.AceEditor;
+import org.rstudio.studio.client.workbench.views.source.editors.text.DocDisplay;
+import org.rstudio.studio.client.workbench.views.source.editors.text.TextDisplay;
+import org.rstudio.studio.client.workbench.views.source.editors.text.TextEditingTarget;
 
+import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
-public class EditSnippetsDialog extends ModalDialogBase
+public class EditSnippetsDialog extends ModalDialogBase implements TextDisplay
 {
    public EditSnippetsDialog()
    {
       setText("Edit Snippets");
-      ThemedButton okButton = new ThemedButton("Close", new ClickHandler() {
+      RStudioGinjector.INSTANCE.injectMembers(this);
+      
+      addCancelButton();
+      ThemedButton saveButton = new ThemedButton("Save", new ClickHandler() {
          public void onClick(ClickEvent event) 
          {
-            closeDialog();
+            attemptSaveAndClose();
          }
       });
-      addCancelButton(okButton);
+      addButton(saveButton);
+      
+      addLeftWidget(new HelpLink("Using Code Snippets", "code_snippets"));
+   }
+   
+   @Inject
+   void initialize(EventBus events, FontSizeManager fontSizeManager)
+   {
+      events_ = events;
+      fontSizeManager_ = fontSizeManager;
    }
    
    @Override
@@ -83,13 +112,19 @@ public class EditSnippetsDialog extends ModalDialogBase
       panel_.addWest(snippetTypes_, 150);
      
       // editor
-      editor_ = new AceEditor();
-      editor_.setFileType(FileTypeRegistry.SNIPPETS);
+      docDisplay_ = new AceEditor();
+      docDisplay_.setFileType(FileTypeRegistry.SNIPPETS);
       SimplePanel panel = new SimplePanel();
       panel.addStyleName("EditDialog");
       panel.getElement().getStyle().setMarginLeft(8, Unit.PX);
-      panel.setWidget(editor_);
+      panel.setWidget(docDisplay_.asWidget());
       panel_.add(panel);
+      
+      
+      TextEditingTarget.syncFontSize(releaseOnDismiss_, 
+            events_, 
+            this, 
+            fontSizeManager_); 
       
       // default to R
       updateEditor(FileTypeRegistry.R);
@@ -97,11 +132,81 @@ public class EditSnippetsDialog extends ModalDialogBase
       return panel_;
    }
    
+   @Override
+   public void onDialogShown()
+   {
+      docDisplay_.focus();
+   }
+   
+   private void attemptSaveAndClose()
+   {
+      closeDialog();
+   }
+   
+   @Override
+   public void onActivate()
+   {
+      docDisplay_.onActivate();
+   }
+
+   @Override
+   public void adaptToFileType(TextFileType fileType)
+   {
+      docDisplay_.setFileType(fileType, true);
+   }
+
+   @Override
+   public void setFontSize(double size)
+   {
+      docDisplay_.setFontSize(size);
+   }
+   
+   @Override
+   public Widget asWidget()
+   {
+      return this;
+   }
+   
+   @Override
+   public void onPreviewNativeEvent(Event.NativePreviewEvent event)
+   {
+      if (!ModalDialogTracker.isTopMost(this))
+         return;
+
+      if (event.getTypeInt() == Event.ONKEYDOWN)
+      {
+         NativeEvent ne = event.getNativeEvent();
+         int mod = KeyboardShortcut.getModifierValue(ne);
+         if ((mod == KeyboardShortcut.META || 
+             (mod == KeyboardShortcut.CTRL && !BrowseCap.hasMetaKey())))
+         {
+            if (ne.getKeyCode() == 'S')
+            {
+               ne.preventDefault();
+               ne.stopPropagation();
+               event.cancel();
+               attemptSaveAndClose();
+            }
+         }  
+      }
+      
+      super.onPreviewNativeEvent(event);
+   }
+   
+   @Override
+   public void onUnload()
+   {
+      super.onUnload();
+      
+      while (releaseOnDismiss_.size() > 0)
+         releaseOnDismiss_.remove(0).removeHandler();
+   }
+   
    private void updateEditor(TextFileType fileType)
    { 
       String snippetText = getSnippetText(
                               fileType.getEditorLanguage().getModeName());
-      editor_.setCode(snippetText, false);
+      docDisplay_.setCode(snippetText, false);
    }
    
    private static native String getSnippetText(String mode) /*-{
@@ -111,7 +216,13 @@ public class EditSnippetsDialog extends ModalDialogBase
    
    private DockLayoutPanel panel_;
    private WidgetListBox<SnippetType> snippetTypes_;
-   private AceEditor editor_;
+   private DocDisplay docDisplay_;
+   
+   private final ArrayList<HandlerRegistration> releaseOnDismiss_ =
+         new ArrayList<HandlerRegistration>();
+   
+   private EventBus events_;
+   private FontSizeManager fontSizeManager_;
   
 
 }
