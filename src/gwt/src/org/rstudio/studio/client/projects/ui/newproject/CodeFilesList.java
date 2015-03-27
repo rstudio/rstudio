f193cf8feaa6b156addbb0b0f182ffd9b5630923
Kevin Ushey <kevin@rstudio.com>
2015-03-27 12:08:40 -0700

    allow multiselect in add/remove pkg listbox

--- src/gwt/src/org/rstudio/studio/client/projects/ui/newproject/CodeFilesList.java
+++ src/gwt/src/org/rstudio/studio/client/projects/ui/newproject/CodeFilesList.java
@@ -52,7 +52,7 @@ public class CodeFilesList extends Composite
       
       HorizontalPanel dictionariesPanel = new HorizontalPanel();
       listBox_ = new ListBox();
-      listBox_.setMultipleSelect(false);
+      listBox_.setMultipleSelect(true);
       listBox_.addStyleName(RES.styles().codeFilesListBox());
       listBox_.getElement().<SelectElement>cast().setSize(3);
       dictionariesPanel.add(listBox_);
@@ -119,13 +119,10 @@ public class CodeFilesList extends Composite
       @Override
       public void onClick(ClickEvent event)
       {
-         // get selected index
-         int index = listBox_.getSelectedIndex();
-         if (index != -1)
-            listBox_.removeItem(index);
+         while (listBox_.getSelectedIndex() != -1)
+            listBox_.removeItem(listBox_.getSelectedIndex());
       }
    };
- 
    
    private SmallButton createButton(String caption)
    {
