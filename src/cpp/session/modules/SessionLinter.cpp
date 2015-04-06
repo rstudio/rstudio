f07752818bd10409aa15b2dba1a015e37729e2a5
Kevin Ushey <kevin@rstudio.com>
2015-04-06 15:30:58 -0700

    remove unused code; use safe file reader

--- src/cpp/session/modules/SessionLinter.cpp
+++ src/cpp/session/modules/SessionLinter.cpp
@@ -44,7 +44,7 @@
 #include <r/RUtil.hpp>
 
 #include <core/r_util/RSourceIndex.hpp>
-#include <core/FileUtils.hpp>
+#include <core/FileSerializer.hpp>
 #include <core/collection/Tree.hpp>
 #include <core/collection/Stack.hpp>
 
@@ -525,7 +525,14 @@ SEXP rs_lintRFile(SEXP filePathSEXP)
       return r::sexp::create(builder, &protect);
    }
    
-   std::string rCode = file_utils::readFile(filePath);
+   std::string rCode;
+   Error error = core::readStringFromFile(filePath, &rCode);
+   if (error)
+   {
+      LOG_ERROR(error);
+      return R_NilValue;
+   }
+   
    ParseResults results = parse(rCode, filePath, std::string());
    const std::vector<LintItem>& lint = results.lint().get();
    
@@ -548,22 +555,6 @@ SEXP rs_lintRFile(SEXP filePathSEXP)
    }
    
    return r::sexp::create(builder, &protect);
-}
-
-static std::wstring s_rCode;
-
-SEXP rs_loadString(SEXP rCodeSEXP)
-{
-   s_rCode = string_utils::utf8ToWide(CHAR(STRING_ELT(rCodeSEXP, 0)));
-   return R_NilValue;
-}
-
-SEXP rs_parse(SEXP rCodeSEXP)
-{
-   std::string code = r::sexp::safeAsString(rCodeSEXP);
-   ParseResults results = parse(code, FilePath(), std::string());
-   r::sexp::Protect protect;
-   return r::sexp::create((int) results.lint().size(), &protect);
 }
 
 void onNAMESPACEchanged()
@@ -804,8 +795,6 @@ core::Error initialize()
    projects::projectContext().subscribeToFileMonitor("Diagnostics", cb);
    
    RS_REGISTER_CALL_METHOD(rs_lintRFile, 1);
-   RS_REGISTER_CALL_METHOD(rs_loadString, 1);
-   RS_REGISTER_CALL_METHOD(rs_parse, 1);
    
    ExecBlock initBlock;
    initBlock.addFunctions()
