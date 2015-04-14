gwtproject/gwt

Make calls to String instance methods in JSNI be a compiler error.
Strings in GWT have been from some time native JavaScript strings
whose instance method calls are devirtualized. Calling String
instance methods from JSNI is disallowed, and now it is handled
correctly as a compile error.

This patch also emits a warning when calling Object instance
methods through JSNI. Calling Object instance methods on String,
Array or Jso instances is undefined behaviour.

Change-Id: Ifd3b76b103eb9d5afc8223df1e9ed8671f726216
Bugs: issue 9166.

dev/core/test/com/google/gwt/dev/jjs/impl/ArrayNormalizerTest.java
@@ -13,6 +13,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.dev.jjs.ast.JMethod;
 import com.google.gwt.dev.jjs.ast.JProgram;
 
@@ -64,7 +65,7 @@ public class ArrayNormalizerTest extends OptimizerTestBase {
   }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean optimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     program.addEntryMethod(findMainMethod(program));
     boolean didChange = true;
     // TODO(jbrosenberg): remove loop when Pruner/CFA interaction is perfect.

dev/core/test/com/google/gwt/dev/jjs/impl/CompileTimeConstantsReplacerTest.java
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.dev.jjs.ast.Context;
 import com.google.gwt.dev.jjs.ast.JDeclaredType;
@@ -106,7 +107,7 @@ public class CompileTimeConstantsReplacerTest extends OptimizerTestBase {
   }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean optimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     CompileTimeConstantsReplacer.exec(program);
     return true;
   }

dev/core/test/com/google/gwt/dev/jjs/impl/EnumOrdinalizerTest.java
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.dev.jjs.ast.Context;
 import com.google.gwt.dev.jjs.ast.JDeclarationStatement;
@@ -1096,7 +1097,7 @@ public class EnumOrdinalizerTest extends OptimizerTestBase {
   }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean optimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     /*
      * EnumOrdinalizer depends MakeCallsStatic and MethodInliner running before
      * it runs, since they cleanup the internal structure of an enum class to

dev/core/test/com/google/gwt/dev/jjs/impl/ImplicitUpcastAnalyzerTest.java
@@ -13,6 +13,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.dev.javac.testing.impl.JavaResourceBase;
 import com.google.gwt.dev.javac.testing.impl.MockJavaResource;
 import com.google.gwt.dev.jjs.SourceInfo;
@@ -125,7 +126,7 @@ public class ImplicitUpcastAnalyzerTest extends OptimizerTestBase {
   }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean optimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     program.addEntryMethod(findMainMethod(program));
     TypeTightener.exec(program);
     return false;

dev/core/test/com/google/gwt/dev/jjs/impl/PrunerTest.java
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.dev.jjs.ast.JMethod;
 import com.google.gwt.dev.jjs.ast.JProgram;
 import com.google.gwt.dev.jjs.ast.js.JsniMethodBody;
@@ -367,7 +368,7 @@ public class PrunerTest extends OptimizerTestBase {
  }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean optimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     program.addEntryMethod(findMainMethod(program));
     boolean didChange = false;
     // TODO(jbrosenberg): remove loop when Pruner/CFA interaction is perfect.

dev/core/test/com/google/gwt/dev/jjs/impl/UnifyAstTest.java
@@ -13,6 +13,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.dev.javac.testing.impl.JavaResourceBase;
 import com.google.gwt.dev.javac.testing.impl.MockJavaResource;
@@ -480,7 +481,7 @@ public class UnifyAstTest extends OptimizerTestBase {
   }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean optimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     program.addEntryMethod(findMainMethod(program));
     return false;
   }

rstudio/rstudio

dev/core/test/com/google/gwt/dev/jjs/impl/ArrayNormalizerTest.java
@@ -13,6 +13,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.dev.jjs.ast.JMethod;
 import com.google.gwt.dev.jjs.ast.JProgram;
 
@@ -64,7 +65,7 @@ public class ArrayNormalizerTest extends OptimizerTestBase {
   }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean doOptimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     program.addEntryMethod(findMainMethod(program));
     boolean didChange = true;
     // TODO(jbrosenberg): remove loop when Pruner/CFA interaction is perfect.

dev/core/test/com/google/gwt/dev/jjs/impl/CompileTimeConstantsReplacerTest.java
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.dev.jjs.ast.Context;
 import com.google.gwt.dev.jjs.ast.JDeclaredType;
@@ -106,7 +107,7 @@ public class CompileTimeConstantsReplacerTest extends OptimizerTestBase {
   }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean doOptimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     CompileTimeConstantsReplacer.exec(program);
     return true;
   }

dev/core/test/com/google/gwt/dev/jjs/impl/EnumOrdinalizerTest.java
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.dev.jjs.ast.Context;
 import com.google.gwt.dev.jjs.ast.JDeclarationStatement;
@@ -1096,7 +1097,7 @@ public class EnumOrdinalizerTest extends OptimizerTestBase {
   }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean doOptimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     /*
      * EnumOrdinalizer depends MakeCallsStatic and MethodInliner running before
      * it runs, since they cleanup the internal structure of an enum class to

dev/core/test/com/google/gwt/dev/jjs/impl/ImplicitUpcastAnalyzerTest.java
@@ -13,6 +13,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.dev.javac.testing.impl.JavaResourceBase;
 import com.google.gwt.dev.javac.testing.impl.MockJavaResource;
 import com.google.gwt.dev.jjs.SourceInfo;
@@ -125,7 +126,7 @@ public class ImplicitUpcastAnalyzerTest extends OptimizerTestBase {
   }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean doOptimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     program.addEntryMethod(findMainMethod(program));
     TypeTightener.exec(program);
     return false;

dev/core/test/com/google/gwt/dev/jjs/impl/PrunerTest.java
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.dev.jjs.ast.JMethod;
 import com.google.gwt.dev.jjs.ast.JProgram;
 import com.google.gwt.dev.jjs.ast.js.JsniMethodBody;
@@ -367,7 +368,7 @@ public class PrunerTest extends OptimizerTestBase {
  }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean doOptimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     program.addEntryMethod(findMainMethod(program));
     boolean didChange = false;
     // TODO(jbrosenberg): remove loop when Pruner/CFA interaction is perfect.

dev/core/test/com/google/gwt/dev/jjs/impl/UnifyAstTest.java
@@ -13,6 +13,7 @@
  */
 package com.google.gwt.dev.jjs.impl;
 
+import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.dev.javac.testing.impl.JavaResourceBase;
 import com.google.gwt.dev.javac.testing.impl.MockJavaResource;
@@ -480,7 +481,7 @@ public class UnifyAstTest extends OptimizerTestBase {
   }
 
   @Override
-  protected boolean optimizeMethod(JProgram program, JMethod method) {
+  protected boolean doOptimizeMethod(TreeLogger logger, JProgram program, JMethod method) {
     program.addEntryMethod(findMainMethod(program));
     return false;
   }