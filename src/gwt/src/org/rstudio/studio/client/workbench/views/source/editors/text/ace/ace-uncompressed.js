67f9bdcc0d38c7ca9655fe7fa46e5e5c7376de71
Kevin Ushey <kevin@rstudio.com>
2015-04-24 12:03:30 -0700

    bind 'cmd + alt + click' to multi-select

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/ace/ace-uncompressed.js
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/ace/ace-uncompressed.js
@@ -51011,20 +51011,10 @@ function onMouseDown(e) {
     var screenCursor = screenAnchor;
     
     var selectionMode;
-    if (editor.$mouseHandler.$enableJumpToDef) {
-        if (ctrl && alt || accel && alt)
-            selectionMode = "add";
-        else if (alt)
-            selectionMode = "block";
-    } else {
-        if (accel && !alt) {
-            selectionMode = "add";
-            if (!isMultiSelect && shift)
-                return;
-        } else if (alt) {
-            selectionMode = "block";
-        }
-    }
+    if (ctrl && alt || accel && alt)
+        selectionMode = "add";
+    else if (alt)
+        selectionMode = "block";
     
     if (selectionMode && useragent.isMac && ev.ctrlKey) {
         editor.$mouseHandler.cancelContextMenu();
