ad11e0ea642e0ffd5517bf0d9cc1dfd5630bde7f
Kevin Ushey <kevin@rstudio.com>
2015-05-29 10:24:26 -0700

    provide '{Begin/End}PasteEvent' hooks

--- src/gwt/src/org/rstudio/studio/client/application/ui/impl/DesktopApplicationHeader.java
+++ src/gwt/src/org/rstudio/studio/client/application/ui/impl/DesktopApplicationHeader.java
@@ -16,8 +16,10 @@ package org.rstudio.studio.client.application.ui.impl;
 
 import java.util.ArrayList;
 
+import org.rstudio.core.client.Debug;
 import org.rstudio.core.client.command.CommandBinder;
 import org.rstudio.core.client.command.Handler;
+import org.rstudio.core.client.command.KeyboardShortcut;
 import org.rstudio.core.client.command.impl.DesktopMenuCallback;
 import org.rstudio.core.client.js.JsObject;
 import org.rstudio.core.client.theme.res.ThemeResources;
@@ -36,6 +38,8 @@ import org.rstudio.studio.client.application.ui.ApplicationHeader;
 import org.rstudio.studio.client.application.ui.GlobalToolbar;
 import org.rstudio.studio.client.common.GlobalDisplay;
 import org.rstudio.studio.client.common.debugging.ErrorManager;
+import org.rstudio.studio.client.events.BeginPasteEvent;
+import org.rstudio.studio.client.events.EndPasteEvent;
 import org.rstudio.studio.client.server.ServerError;
 import org.rstudio.studio.client.server.ServerRequestCallback;
 import org.rstudio.studio.client.workbench.codesearch.CodeSearch;
@@ -55,7 +59,13 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Event.NativePreviewHandler;
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -214,6 +224,28 @@ public class DesktopApplicationHeader implements ApplicationHeader
    @Handler
    void onPasteDummy()
    {
+      eventBus_.fireEvent(new BeginPasteEvent());
+      keyListener_ = Event.addNativePreviewHandler(new NativePreviewHandler()
+      {
+         
+         @Override
+         public void onPreviewNativeEvent(NativePreviewEvent event)
+         {
+            // Defer so that this event can go through and execute unabated
+            Scheduler.get().scheduleDeferred(new ScheduledCommand()
+            {
+               
+               @Override
+               public void execute()
+               {
+                  eventBus_.fireEvent(new EndPasteEvent());
+                  keyListener_.removeHandler();
+                  keyListener_ = null;
+               }
+            });
+         }
+      });
+      
       Desktop.getFrame().clipboardPaste();
    }
 
@@ -364,4 +396,5 @@ public class DesktopApplicationHeader implements ApplicationHeader
    private IgnoredUpdates ignoredUpdates_;
    private boolean ignoredUpdatesDirty_ = false;
    private ApplicationQuit appQuit_; 
+   private HandlerRegistration keyListener_;
 }
