d2b041ff92932cd25239c0a31a642ee3893da45f
JJ Allaire <jj@rstudio.org>
2015-06-14 07:25:07 -0400

    dev.new() now creates a new platform-specific device (e.g. quartz) if there is already an active RStudio device

--- src/cpp/r/session/RSession.cpp
+++ src/cpp/r/session/RSession.cpp
@@ -1689,6 +1689,11 @@ bool isDefaultPrompt(const std::string& prompt)
    return prompt == r::options::getOption<std::string>("prompt");
 }
 
+bool isServerMode()
+{
+   return s_options.serverMode;
+}
+
 const FilePath& userHomePath()
 {
    return s_options.userHomePath;
