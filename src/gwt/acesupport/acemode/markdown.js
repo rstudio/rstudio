bdc4ee2aa39f61e3a83ce97fde8a3b25c4b70831
Kevin Ushey <kevin@rstudio.com>
2015-06-17 13:58:51 -0700

    reindent (3-space indent)

--- src/gwt/acesupport/acemode/markdown.js
+++ src/gwt/acesupport/acemode/markdown.js
@@ -59,26 +59,28 @@ var MarkdownHighlightRules = require("mode/markdown_highlight_rules").MarkdownHi
 var MarkdownFoldMode = require("mode/markdown_folding").FoldMode;
 
 var Mode = function() {
-    this.HighlightRules = MarkdownHighlightRules;
+   this.HighlightRules = MarkdownHighlightRules;
 
-    this.createModeDelegates({
-        "js-": JavaScriptMode,
-        "xml-": XmlMode,
-        "html-": HtmlMode
-    });
+   this.createModeDelegates({
+      "js-": JavaScriptMode,
+      "xml-": XmlMode,
+      "html-": HtmlMode
+   });
 
-    this.foldingRules = new MarkdownFoldMode();
+   this.foldingRules = new MarkdownFoldMode();
 };
 oop.inherits(Mode, TextMode);
 
 (function() {
-    this.type = "text";
-    this.blockComment = {start: "<!--", end: "-->"};
+   this.type = "text";
+   this.blockComment = {start: "<!--", end: "-->"};
 
-    this.getNextLineIndent = function(state, line, tab) {
-        return this.$getIndent(line);
-    };
-    this.$id = "mode/markdown";
+   this.getNextLineIndent = function(state, line, tab) {
+      return this.$getIndent(line);
+   };
+
+
+   this.$id = "mode/markdown";
 }).call(Mode.prototype);
 
 exports.Mode = Mode;
