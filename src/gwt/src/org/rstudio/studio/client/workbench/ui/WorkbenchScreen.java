df8e54619026bd2d3231707ee062b3007a741f24
JJ Allaire <jj@rstudio.org>
2015-06-23 16:37:53 -0400

    only use custom device pixel ratio on mac desktop (that's the only place we know for sure it works reliably)

--- src/gwt/src/org/rstudio/studio/client/workbench/ui/WorkbenchScreen.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/ui/WorkbenchScreen.java
@@ -265,10 +265,13 @@ public class WorkbenchScreen extends Composite
                Math.max(deckPanelSize.width, 0),
                Math.max(deckPanelSize.height - Toolbar.DEFAULT_HEIGHT, 0));
       
+      double devicePixelRatio = 1.0;
+      if (BrowseCap.isMacintoshDesktop())
+         devicePixelRatio = BrowseCap.devicePixelRatio();
       WorkbenchMetrics metrics = WorkbenchMetrics.create(consoleWidth,
                                                          plotsSize.width,
                                                          plotsSize.height,
-                                                         BrowseCap.devicePixelRatio());
+                                                         devicePixelRatio);
 
       // make sure we don't send very similar metrics values twice (it is
       // an expensive operation since it involves at least 2 http requests)
