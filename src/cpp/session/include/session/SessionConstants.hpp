5730477860a40aa3e997870a90dcbf155f87b557
JJ Allaire <jj@rstudio.org>
2015-06-27 00:13:46 +0200

    add constant for rstudio user identity display

--- src/cpp/session/include/session/SessionConstants.hpp
+++ src/cpp/session/include/session/SessionConstants.hpp
@@ -19,6 +19,7 @@
 #define kEventsPending                    "ep"
 
 #define kRStudioUserIdentity              "RSTUDIO_USER_IDENTITY"
+#define kRStudioUserIdentityDisplay       "RSTUDIO_USER_IDENTITY_DISPLAY"
 #define kRStudioLimitRpcClientUid         "RSTUDIO_LIMIT_RPC_CLIENT_UID"
 #define kRSessionPortNumber               "RSTUDIO_SESSION_PORT"
 #define kRSessionStandalonePortNumber     "RSTUDIO_STANDALONE_PORT"
