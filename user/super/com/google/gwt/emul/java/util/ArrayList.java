gwtproject/gwt

Add extra array helper class.
Change-Id: Ifa07e71e0b16ce063282138d6a499fabab9f3786

user/super/com/google/gwt/emul/java/util/ArrayList.java
@@ -20,8 +20,7 @@ import static com.google.gwt.core.shared.impl.InternalPreconditions.checkElement
 import static com.google.gwt.core.shared.impl.InternalPreconditions.checkPositionIndex;
 import static com.google.gwt.core.shared.impl.InternalPreconditions.checkPositionIndexes;
 
-import com.google.gwt.lang.Array;
-
+import java.internal.ArrayHelper;
 import java.io.Serializable;
 
 /**
@@ -55,7 +54,7 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>,
   }-*/;
 
   private void insertAt(int index, Object[] values) {
-    Array.nativeArrayInsert(values, 0, array, index, values.length);
+    ArrayHelper.nativeArrayInsert(values, 0, array, index, values.length);
   }
 
   /**
@@ -189,7 +188,7 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>,
 
   @Override
   public Object[] toArray() {
-    return Array.cloneSubrange(array, 0, array.length);
+    return ArrayHelper.cloneSubrange(array, 0, array.length);
   }
 
   /*
@@ -200,7 +199,7 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>,
   public <T> T[] toArray(T[] out) {
     int size = array.length;
     if (out.length < size) {
-      out = Array.createFrom(out, size);
+      out = ArrayHelper.createFrom(out, size);
     }
     for (int i = 0; i < size; ++i) {
       out[i] = (T) array[i];

user/super/com/google/gwt/emul/java/util/Arrays.java
@@ -25,8 +25,8 @@ import static com.google.gwt.core.shared.impl.InternalPreconditions.checkPositio
 
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.UnsafeNativeLong;
-import com.google.gwt.lang.Array;
 
+import java.internal.ArrayHelper;
 import java.io.Serializable;
 
 /**
@@ -78,7 +78,7 @@ public class Arrays {
      */
     @Override
     public Object[] toArray() {
-      return Array.clone(array);
+      return ArrayHelper.cloneSubrange(array, 0, array.length);
     }
 
     /*
@@ -89,7 +89,7 @@ public class Arrays {
     public <T> T[] toArray(T[] out) {
       int size = size();
       if (out.length < size) {
-        out = Array.createFrom(out, size);
+        out = ArrayHelper.createFrom(out, size);
       }
       for (int i = 0; i < size; ++i) {
         out[i] = (T) array[i];
@@ -502,7 +502,7 @@ public class Arrays {
 
   public static <T> T[] copyOfRange(T[] original, int from, int to) {
     int newLength = getLengthFromRange(from, to);
-    T[] copy = Array.createFrom(original, newLength);
+    T[] copy = ArrayHelper.createFrom(original, newLength);
     System.arraycopy(original, from, copy, 0,
         Math.min(original.length - from, newLength));
     return copy;

user/super/com/google/gwt/emul/java/util/EnumMap.java
@@ -18,7 +18,7 @@ package java.util;
 import static com.google.gwt.core.shared.impl.InternalPreconditions.checkArgument;
 import static com.google.gwt.core.shared.impl.InternalPreconditions.checkState;
 
-import com.google.gwt.lang.Array;
+import java.internal.ArrayHelper;
 
 /**
  * A {@link java.util.Map} of {@link Enum}s. <a
@@ -210,7 +210,7 @@ public class EnumMap<K extends Enum<K>, V> extends AbstractMap<K, V> {
 
   private void init(EnumMap<K, ? extends V> m) {
     keySet = m.keySet.clone();
-    values = Array.clone(m.values);
+    values = ArrayHelper.cloneSubrange(m.values, 0, m.values.length);
   }
 
   private V set(int ordinal, V value) {

user/super/com/google/gwt/emul/java/util/EnumSet.java
@@ -21,7 +21,8 @@ import static com.google.gwt.core.shared.impl.InternalPreconditions.checkNotNull
 import static com.google.gwt.core.shared.impl.InternalPreconditions.checkState;
 
 import com.google.gwt.core.client.impl.SpecializeMethod;
-import com.google.gwt.lang.Array;
+
+import java.internal.ArrayHelper;
 
 /**
  * A {@link java.util.Set} of {@link Enum}s. <a
@@ -129,7 +130,7 @@ public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
 
     @Override
     public EnumSet<E> clone() {
-      E[] clonedSet = Array.clone(set);
+      E[] clonedSet = ArrayHelper.cloneSubrange(set, 0, set.length);
       return new EnumSetImpl<E>(all, clonedSet, size);
     }
 
@@ -176,7 +177,7 @@ public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
 
   public static <E extends Enum<E>> EnumSet<E> allOf(Class<E> elementType) {
     E[] all = elementType.getEnumConstants();
-    E[] set = Array.clone(all);
+    E[] set = ArrayHelper.cloneSubrange(all, 0, all.length);
     return new EnumSetImpl<E>(all, set, all.length);
   }
 
@@ -184,7 +185,7 @@ public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
     EnumSetImpl<E> s = (EnumSetImpl<E>) other;
     E[] all = s.all;
     E[] oldSet = s.set;
-    E[] newSet = Array.createFrom(oldSet);
+    E[] newSet = ArrayHelper.createFrom(oldSet, oldSet.length);
     for (int i = 0, c = oldSet.length; i < c; ++i) {
       if (oldSet[i] == null) {
         newSet[i] = all[i];
@@ -216,7 +217,7 @@ public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
 
   public static <E extends Enum<E>> EnumSet<E> noneOf(Class<E> elementType) {
     E[] all = elementType.getEnumConstants();
-    return new EnumSetImpl<E>(all, Array.createFrom(all), 0);
+    return new EnumSetImpl<E>(all, ArrayHelper.createFrom(all, all.length), 0);
   }
 
   public static <E extends Enum<E>> EnumSet<E> of(E first) {
@@ -235,7 +236,7 @@ public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
     checkArgument(from.compareTo(to) <= 0, "%s > %s", from, to);
 
     E[] all = from.getDeclaringClass().getEnumConstants();
-    E[] set = Array.createFrom(all);
+    E[] set = ArrayHelper.createFrom(all, all.length);
 
     // Inclusive
     int start = from.ordinal();

rstudio/rstudio

user/super/com/google/gwt/emul/java/util/ArrayList.java
@@ -15,13 +15,12 @@
  */
 package java.util;
 
-import com.google.gwt.lang.Array;
-
 import static java.internal.InternalPreconditions.checkArgument;
 import static java.internal.InternalPreconditions.checkElementIndex;
 import static java.internal.InternalPreconditions.checkPositionIndex;
 import static java.internal.InternalPreconditions.checkPositionIndexes;
 
+import java.internal.ArrayHelper;
 import java.io.Serializable;
 
 /**
@@ -55,7 +54,7 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>,
   }-*/;
 
   private void insertAt(int index, Object[] values) {
-    Array.nativeArrayInsert(values, 0, array, index, values.length);
+    ArrayHelper.arrayInsert(values, 0, array, index, values.length);
   }
 
   /**
@@ -188,7 +187,7 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>,
 
   @Override
   public Object[] toArray() {
-    return Array.cloneSubrange(array, 0, array.length);
+    return ArrayHelper.clone(array, 0, array.length);
   }
 
   /*
@@ -199,7 +198,7 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>,
   public <T> T[] toArray(T[] out) {
     int size = array.length;
     if (out.length < size) {
-      out = Array.createFrom(out, size);
+      out = ArrayHelper.createFrom(out, size);
     }
     for (int i = 0; i < size; ++i) {
       out[i] = (T) array[i];

user/super/com/google/gwt/emul/java/util/Arrays.java
@@ -18,15 +18,15 @@ package java.util;
 
 import static com.google.gwt.core.client.impl.Coercions.ensureInt;
 
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.core.client.UnsafeNativeLong;
-import com.google.gwt.lang.Array;
-
 import static java.internal.InternalPreconditions.checkArgument;
 import static java.internal.InternalPreconditions.checkArraySize;
 import static java.internal.InternalPreconditions.checkElementIndex;
 import static java.internal.InternalPreconditions.checkPositionIndexes;
 
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.UnsafeNativeLong;
+
+import java.internal.ArrayHelper;
 import java.io.Serializable;
 
 /**
@@ -78,7 +78,7 @@ public class Arrays {
      */
     @Override
     public Object[] toArray() {
-      return Array.clone(array);
+      return ArrayHelper.clone(array, 0, array.length);
     }
 
     /*
@@ -89,7 +89,7 @@ public class Arrays {
     public <T> T[] toArray(T[] out) {
       int size = size();
       if (out.length < size) {
-        out = Array.createFrom(out, size);
+        out = ArrayHelper.createFrom(out, size);
       }
       for (int i = 0; i < size; ++i) {
         out[i] = (T) array[i];
@@ -502,7 +502,7 @@ public class Arrays {
 
   public static <T> T[] copyOfRange(T[] original, int from, int to) {
     int newLength = getLengthFromRange(from, to);
-    T[] copy = Array.createFrom(original, newLength);
+    T[] copy = ArrayHelper.createFrom(original, newLength);
     System.arraycopy(original, from, copy, 0,
         Math.min(original.length - from, newLength));
     return copy;
@@ -1428,7 +1428,7 @@ public class Arrays {
     var temp = array.slice(fromIndex, toIndex);
     temp.sort(@com.google.gwt.lang.LongLib::compare(Lcom/google/gwt/lang/LongLibBase$LongEmul;Lcom/google/gwt/lang/LongLibBase$LongEmul;));
     var n = toIndex - fromIndex;
-    @com.google.gwt.lang.Array::nativeArraycopy(Ljava/lang/Object;ILjava/lang/Object;II)(
+    @java.internal.ArrayHelper::arrayCopy(Ljava/lang/Object;ILjava/lang/Object;II)(
         temp, 0, array, fromIndex, n)
   }-*/;
 
@@ -1451,7 +1451,7 @@ public class Arrays {
       return a - b;
     });
     var n = toIndex - fromIndex;
-    @com.google.gwt.lang.Array::nativeArraycopy(Ljava/lang/Object;ILjava/lang/Object;II)(
+    @java.internal.ArrayHelper::arrayCopy(Ljava/lang/Object;ILjava/lang/Object;II)(
         temp, 0, array, fromIndex, n)
   }-*/;
 

user/super/com/google/gwt/emul/java/util/EnumMap.java
@@ -15,10 +15,10 @@
  */
 package java.util;
 
-import com.google.gwt.lang.Array;
-
 import static java.internal.InternalPreconditions.checkArgument;
 import static java.internal.InternalPreconditions.checkState;
+
+import java.internal.ArrayHelper;
 
 /**
  * A {@link java.util.Map} of {@link Enum}s. <a
@@ -210,7 +210,7 @@ public class EnumMap<K extends Enum<K>, V> extends AbstractMap<K, V> {
 
   private void init(EnumMap<K, ? extends V> m) {
     keySet = m.keySet.clone();
-    values = Array.clone(m.values);
+    values = ArrayHelper.clone(m.values, 0, m.values.length);
   }
 
   private V set(int ordinal, V value) {

user/super/com/google/gwt/emul/java/util/EnumSet.java
@@ -15,13 +15,14 @@
  */
 package java.util;
 
-import com.google.gwt.core.client.impl.SpecializeMethod;
-import com.google.gwt.lang.Array;
-
 import static java.internal.InternalPreconditions.checkArgument;
 import static java.internal.InternalPreconditions.checkElement;
 import static java.internal.InternalPreconditions.checkNotNull;
 import static java.internal.InternalPreconditions.checkState;
+
+import com.google.gwt.core.client.impl.SpecializeMethod;
+
+import java.internal.ArrayHelper;
 
 /**
  * A {@link java.util.Set} of {@link Enum}s. <a
@@ -129,7 +130,7 @@ public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
 
     @Override
     public EnumSet<E> clone() {
-      E[] clonedSet = Array.clone(set);
+      E[] clonedSet = ArrayHelper.clone(set, 0, set.length);
       return new EnumSetImpl<E>(all, clonedSet, size);
     }
 
@@ -176,7 +177,7 @@ public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
 
   public static <E extends Enum<E>> EnumSet<E> allOf(Class<E> elementType) {
     E[] all = elementType.getEnumConstants();
-    E[] set = Array.clone(all);
+    E[] set = ArrayHelper.clone(all, 0, all.length);
     return new EnumSetImpl<E>(all, set, all.length);
   }
 
@@ -184,7 +185,7 @@ public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
     EnumSetImpl<E> s = (EnumSetImpl<E>) other;
     E[] all = s.all;
     E[] oldSet = s.set;
-    E[] newSet = Array.createFrom(oldSet);
+    E[] newSet = ArrayHelper.createFrom(oldSet, oldSet.length);
     for (int i = 0, c = oldSet.length; i < c; ++i) {
       if (oldSet[i] == null) {
         newSet[i] = all[i];
@@ -216,7 +217,7 @@ public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
 
   public static <E extends Enum<E>> EnumSet<E> noneOf(Class<E> elementType) {
     E[] all = elementType.getEnumConstants();
-    return new EnumSetImpl<E>(all, Array.createFrom(all), 0);
+    return new EnumSetImpl<E>(all, ArrayHelper.createFrom(all, all.length), 0);
   }
 
   public static <E extends Enum<E>> EnumSet<E> of(E first) {
@@ -235,7 +236,7 @@ public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
     checkArgument(from.compareTo(to) <= 0, "%s > %s", from, to);
 
     E[] all = from.getDeclaringClass().getEnumConstants();
-    E[] set = Array.createFrom(all);
+    E[] set = ArrayHelper.createFrom(all, all.length);
 
     // Inclusive
     int start = from.ordinal();