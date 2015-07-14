gwtproject/gwt

Cut dependency from emul to JsDate.
Change-Id: I768c8efdf616edf33a28819c7279cfd5c521bd9f

user/super/com/google/gwt/emul/java/lang/System.java
@@ -18,11 +18,12 @@ package java.lang;
 import static java.internal.InternalPreconditions.checkArrayType;
 import static java.internal.InternalPreconditions.checkNotNull;
 
-import com.google.gwt.core.client.JsDate;
 import com.google.gwt.core.client.impl.Impl;
 
 import java.internal.ArrayHelper;
 import java.io.PrintStream;
+
+import javax.internal.JsDateFactory;
 
 /**
  * General-purpose low-level utility methods. GWT only supports a limited subset
@@ -90,7 +91,7 @@ public final class System {
   }
 
   public static long currentTimeMillis() {
-    return (long) JsDate.now();
+    return (long) JsDateFactory.now();
   }
 
   /**

user/super/com/google/gwt/emul/java/util/Random.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -21,29 +21,29 @@
  * licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * INCLUDES MODIFICATIONS BY RICHARD ZSCHECH AS WELL AS GOOGLE.
  */
 package java.util;
 
-import com.google.gwt.core.client.JsDate;
-
 import static java.internal.InternalPreconditions.checkCriticalArgument;
 import static java.internal.InternalPreconditions.checkNotNull;
+
+import javax.internal.JsDateFactory;
 
 /**
  * This class provides methods that generates pseudo-random numbers of different
  * types, such as {@code int}, {@code long}, {@code double}, and {@code float}.
  * It follows the algorithms specified in the JRE javadoc.
- * 
+ *
  * This emulated version of Random is not serializable.
  */
 public class Random {
@@ -77,7 +77,7 @@ public class Random {
       twoToTheXMinus48[i] = twoToTheXMinus48Tmp;
       twoToTheXMinus48Tmp *= 0.5;
     }
-    
+
     double twoToTheXMinus24Tmp = 1.0;
     for (int i = 24; i >= 0; i--) {
       twoToTheXMinus24[i] = twoToTheXMinus24Tmp;
@@ -94,7 +94,7 @@ public class Random {
    * The second Gaussian generated number.
    */
   private double nextNextGaussian;
-  
+
   /**
    * The high 24 bits of the 48=bit seed value.
    */
@@ -104,17 +104,17 @@ public class Random {
    * The low 24 bits of the 48=bit seed value.
    */
   private double seedlo;
-  
+
   /**
    * Construct a random generator with the current time of day in milliseconds
    * plus a unique counter value as the initial state.
-   * 
+   *
    * @see #setSeed
    */
   public Random() {
     // JsDate.now used instead of System.currentTimeMillis()
     // as it returns a double in order to avoid the use os LongLib.
-    double seed = uniqueSeed++ + JsDate.now();
+    double seed = uniqueSeed++ + JsDateFactory.now();
     int hi = (int) Math.floor(seed * twoToTheMinus24) & 0xffffff;
     int lo = (int) (seed - (hi * twoToThe24));
     setSeed(hi, lo);
@@ -123,7 +123,7 @@ public class Random {
   /**
    * Construct a random generator with the given {@code seed} as the initial
    * state.
-   * 
+   *
    * @param seed the seed that will determine the initial state of this random
    *          number generator.
    * @see #setSeed
@@ -135,7 +135,7 @@ public class Random {
   /**
    * Returns the next pseudo-random, uniformly distributed {@code boolean} value
    * generated by this generator.
-   * 
+   *
    * @return a pseudo-random, uniformly distributed boolean value.
    */
   public boolean nextBoolean() {
@@ -145,7 +145,7 @@ public class Random {
   /**
    * Modifies the {@code byte} array by a random sequence of {@code byte}s
    * generated by this random number generator.
-   * 
+   *
    * @param buf non-null array to contain the new random {@code byte}s.
    * @see #next
    */
@@ -168,7 +168,7 @@ public class Random {
   /**
    * Generates a normally distributed random {@code double} number between 0.0
    * inclusively and 1.0 exclusively.
-   * 
+   *
    * @return a random {@code double} in the range [0.0 - 1.0)
    * @see #nextFloat
    */
@@ -180,7 +180,7 @@ public class Random {
   /**
    * Generates a normally distributed random {@code float} number between 0.0
    * inclusively and 1.0 exclusively.
-   * 
+   *
    * @return float a random {@code float} number between [0.0 and 1.0)
    * @see #nextDouble
    */
@@ -195,7 +195,7 @@ public class Random {
    * Marsaglia, as described by Donald E. Knuth in <i>The Art of Computer
    * Programming, Volume 2: Seminumerical Algorithms</i>, section 3.4.1,
    * subsection C, algorithm P.
-   * 
+   *
    * @return a random {@code double}
    * @see #nextDouble
    */
@@ -205,7 +205,7 @@ public class Random {
       haveNextNextGaussian = false;
       return nextNextGaussian;
     }
-    
+
     double v1, v2, s;
     do {
       // Generates two independent random variables U1, U2
@@ -225,7 +225,7 @@ public class Random {
   /**
    * Generates a uniformly distributed 32-bit {@code int} value from the random
    * number sequence.
-   * 
+   *
    * @return a uniformly distributed {@code int} value.
    * @see java.lang.Integer#MAX_VALUE
    * @see java.lang.Integer#MIN_VALUE
@@ -240,7 +240,7 @@ public class Random {
    * Returns a new pseudo-random {@code int} value which is uniformly
    * distributed between 0 (inclusively) and the value of {@code n}
    * (exclusively).
-   * 
+   *
    * @param n the exclusive upper border of the range [0 - n).
    * @return a random {@code int}.
    */
@@ -257,11 +257,11 @@ public class Random {
     } while (bits - val + (n - 1) < 0);
     return (int) val;
   }
-  
+
   /**
    * Generates a uniformly distributed 64-bit integer value from the random
    * number sequence.
-   * 
+   *
    * @return 64-bit random integer.
    * @see java.lang.Integer#MAX_VALUE
    * @see java.lang.Integer#MIN_VALUE
@@ -276,7 +276,7 @@ public class Random {
   /**
    * Modifies the seed a using linear congruential formula presented in <i>The
    * Art of Computer Programming, Volume 2</i>, Section 3.2.1.
-   * 
+   *
    * @param seed the seed that alters the state of the random number generator.
    * @see #next
    * @see #Random()
@@ -285,13 +285,13 @@ public class Random {
   public synchronized void setSeed(long seed) {
     setSeed((int) ((seed >> 24) & 0xffffff), (int) (seed & 0xffffff));
   }
-  
+
   /**
    * Returns a pseudo-random uniformly distributed {@code int} value of the
    * number of bits specified by the argument {@code bits} as described by
    * Donald E. Knuth in <i>The Art of Computer Programming, Volume 2:
    * Seminumerical Algorithms</i>, section 3.2.1.
-   * 
+   *
    * @param bits number of bits of the returned value.
    * @return a pseudo-random generated int number.
    * @see #nextBytes
@@ -305,7 +305,7 @@ public class Random {
   protected synchronized int next(int bits) {
     return (int) nextInternal(bits);
   }
-  
+
   private synchronized double nextInternal(int bits) {
     double hi = seedhi * multiplierLo + seedlo * multiplierHi;
     double lo = seedlo * multiplierLo + 0xb;
@@ -313,10 +313,10 @@ public class Random {
     hi += carry;
     lo -= carry * twoToThe24;
     hi %= twoToThe24;
-    
+
     seedhi = hi;
     seedlo = lo;
-    
+
     if (bits <= 24) {
       // High bits of seedhi, shifted right
       return Math.floor(seedhi * twoToTheXMinus24[bits]);
@@ -334,7 +334,7 @@ public class Random {
       return dval;
     }
   }
-  
+
   // seedhi and seedlo should be integers from 0 to 2^24 - 1
   private synchronized void setSeed(int seedhi, int seedlo) {
     this.seedhi = seedhi ^ 0x5de;

user/super/com/google/gwt/emul/javax/internal/package-info.java
@@ -1,0 +1,25 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/**
+ * Contains internal classes used for GWT's JRE implementation.
+ *
+ * <p>The classes in here are not meant to be used by GWT users at all and can change at any time.
+ * Classes that go in here play an important supporting role in GWT's JRE implementation. They
+ * should also be mostly transpilable with other Java to JavaScript compilers. This means that they
+ * can not refer to any GWT specific implementations (no classes outside of the JRE).
+ */
+package javax.internal;

rstudio/rstudio

user/super/com/google/gwt/emul/java/lang/System.java
@@ -18,11 +18,10 @@ package java.lang;
 import static java.internal.InternalPreconditions.checkArrayType;
 import static java.internal.InternalPreconditions.checkNotNull;
 
-import com.google.gwt.core.client.JsDate;
-
 import java.internal.ArrayHelper;
 import java.io.PrintStream;
 
+import javaemul.internal.DateUtil;
 import javaemul.internal.HashCodes;
 
 /**
@@ -91,7 +90,7 @@ public final class System {
   }
 
   public static long currentTimeMillis() {
-    return (long) JsDate.now();
+    return (long) DateUtil.now();
   }
 
   /**

user/super/com/google/gwt/emul/java/util/Random.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -21,29 +21,29 @@
  * licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * INCLUDES MODIFICATIONS BY RICHARD ZSCHECH AS WELL AS GOOGLE.
  */
 package java.util;
 
-import com.google.gwt.core.client.JsDate;
-
 import static java.internal.InternalPreconditions.checkCriticalArgument;
 import static java.internal.InternalPreconditions.checkNotNull;
+
+import javaemul.internal.DateUtil;
 
 /**
  * This class provides methods that generates pseudo-random numbers of different
  * types, such as {@code int}, {@code long}, {@code double}, and {@code float}.
  * It follows the algorithms specified in the JRE javadoc.
- * 
+ *
  * This emulated version of Random is not serializable.
  */
 public class Random {
@@ -77,7 +77,7 @@ public class Random {
       twoToTheXMinus48[i] = twoToTheXMinus48Tmp;
       twoToTheXMinus48Tmp *= 0.5;
     }
-    
+
     double twoToTheXMinus24Tmp = 1.0;
     for (int i = 24; i >= 0; i--) {
       twoToTheXMinus24[i] = twoToTheXMinus24Tmp;
@@ -94,7 +94,7 @@ public class Random {
    * The second Gaussian generated number.
    */
   private double nextNextGaussian;
-  
+
   /**
    * The high 24 bits of the 48=bit seed value.
    */
@@ -104,17 +104,17 @@ public class Random {
    * The low 24 bits of the 48=bit seed value.
    */
   private double seedlo;
-  
+
   /**
    * Construct a random generator with the current time of day in milliseconds
    * plus a unique counter value as the initial state.
-   * 
+   *
    * @see #setSeed
    */
   public Random() {
     // JsDate.now used instead of System.currentTimeMillis()
     // as it returns a double in order to avoid the use os LongLib.
-    double seed = uniqueSeed++ + JsDate.now();
+    double seed = uniqueSeed++ + DateUtil.now();
     int hi = (int) Math.floor(seed * twoToTheMinus24) & 0xffffff;
     int lo = (int) (seed - (hi * twoToThe24));
     setSeed(hi, lo);
@@ -123,7 +123,7 @@ public class Random {
   /**
    * Construct a random generator with the given {@code seed} as the initial
    * state.
-   * 
+   *
    * @param seed the seed that will determine the initial state of this random
    *          number generator.
    * @see #setSeed
@@ -135,7 +135,7 @@ public class Random {
   /**
    * Returns the next pseudo-random, uniformly distributed {@code boolean} value
    * generated by this generator.
-   * 
+   *
    * @return a pseudo-random, uniformly distributed boolean value.
    */
   public boolean nextBoolean() {
@@ -145,7 +145,7 @@ public class Random {
   /**
    * Modifies the {@code byte} array by a random sequence of {@code byte}s
    * generated by this random number generator.
-   * 
+   *
    * @param buf non-null array to contain the new random {@code byte}s.
    * @see #next
    */
@@ -168,7 +168,7 @@ public class Random {
   /**
    * Generates a normally distributed random {@code double} number between 0.0
    * inclusively and 1.0 exclusively.
-   * 
+   *
    * @return a random {@code double} in the range [0.0 - 1.0)
    * @see #nextFloat
    */
@@ -180,7 +180,7 @@ public class Random {
   /**
    * Generates a normally distributed random {@code float} number between 0.0
    * inclusively and 1.0 exclusively.
-   * 
+   *
    * @return float a random {@code float} number between [0.0 and 1.0)
    * @see #nextDouble
    */
@@ -195,7 +195,7 @@ public class Random {
    * Marsaglia, as described by Donald E. Knuth in <i>The Art of Computer
    * Programming, Volume 2: Seminumerical Algorithms</i>, section 3.4.1,
    * subsection C, algorithm P.
-   * 
+   *
    * @return a random {@code double}
    * @see #nextDouble
    */
@@ -205,7 +205,7 @@ public class Random {
       haveNextNextGaussian = false;
       return nextNextGaussian;
     }
-    
+
     double v1, v2, s;
     do {
       // Generates two independent random variables U1, U2
@@ -225,7 +225,7 @@ public class Random {
   /**
    * Generates a uniformly distributed 32-bit {@code int} value from the random
    * number sequence.
-   * 
+   *
    * @return a uniformly distributed {@code int} value.
    * @see java.lang.Integer#MAX_VALUE
    * @see java.lang.Integer#MIN_VALUE
@@ -240,7 +240,7 @@ public class Random {
    * Returns a new pseudo-random {@code int} value which is uniformly
    * distributed between 0 (inclusively) and the value of {@code n}
    * (exclusively).
-   * 
+   *
    * @param n the exclusive upper border of the range [0 - n).
    * @return a random {@code int}.
    */
@@ -257,11 +257,11 @@ public class Random {
     } while (bits - val + (n - 1) < 0);
     return (int) val;
   }
-  
+
   /**
    * Generates a uniformly distributed 64-bit integer value from the random
    * number sequence.
-   * 
+   *
    * @return 64-bit random integer.
    * @see java.lang.Integer#MAX_VALUE
    * @see java.lang.Integer#MIN_VALUE
@@ -276,7 +276,7 @@ public class Random {
   /**
    * Modifies the seed a using linear congruential formula presented in <i>The
    * Art of Computer Programming, Volume 2</i>, Section 3.2.1.
-   * 
+   *
    * @param seed the seed that alters the state of the random number generator.
    * @see #next
    * @see #Random()
@@ -285,13 +285,13 @@ public class Random {
   public synchronized void setSeed(long seed) {
     setSeed((int) ((seed >> 24) & 0xffffff), (int) (seed & 0xffffff));
   }
-  
+
   /**
    * Returns a pseudo-random uniformly distributed {@code int} value of the
    * number of bits specified by the argument {@code bits} as described by
    * Donald E. Knuth in <i>The Art of Computer Programming, Volume 2:
    * Seminumerical Algorithms</i>, section 3.2.1.
-   * 
+   *
    * @param bits number of bits of the returned value.
    * @return a pseudo-random generated int number.
    * @see #nextBytes
@@ -305,7 +305,7 @@ public class Random {
   protected synchronized int next(int bits) {
     return (int) nextInternal(bits);
   }
-  
+
   private synchronized double nextInternal(int bits) {
     double hi = seedhi * multiplierLo + seedlo * multiplierHi;
     double lo = seedlo * multiplierLo + 0xb;
@@ -313,10 +313,10 @@ public class Random {
     hi += carry;
     lo -= carry * twoToThe24;
     hi %= twoToThe24;
-    
+
     seedhi = hi;
     seedlo = lo;
-    
+
     if (bits <= 24) {
       // High bits of seedhi, shifted right
       return Math.floor(seedhi * twoToTheXMinus24[bits]);
@@ -334,7 +334,7 @@ public class Random {
       return dval;
     }
   }
-  
+
   // seedhi and seedlo should be integers from 0 to 2^24 - 1
   private synchronized void setSeed(int seedhi, int seedlo) {
     this.seedhi = seedhi ^ 0x5de;

user/super/com/google/gwt/emul/javaemul/internal/package-info.java
@@ -1,0 +1,25 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/**
+ * Contains internal classes used for GWT's JRE implementation.
+ *
+ * <p>The classes in here are not meant to be used by GWT users at all and can change at any time.
+ * Classes that go in here play an important supporting role in GWT's JRE implementation. They
+ * should also be mostly transpilable with other Java to JavaScript compilers. This means that they
+ * can not refer to any GWT specific implementations (no classes outside of the JRE).
+ */
+package javaemul.internal;