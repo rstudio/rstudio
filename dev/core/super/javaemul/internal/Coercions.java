gwtproject/gwt

Move coercions into the JRE.
Change-Id: Ib27c26c5594e8c96914132b747f1205fd729aa55

user/super/com/google/gwt/emul/java/internal/Coercions.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.gwt.core.client.impl;
+package java.internal;
 
 /**
  * Private implementation class for GWT. This API should not be

user/super/com/google/gwt/emul/java/math/BitLevel.java
@@ -34,7 +34,7 @@
  */
 package java.math;
 
-import static com.google.gwt.core.client.impl.Coercions.ensureInt;
+import static java.internal.Coercions.ensureInt;
 
 /**
  * Static library that provides all the <b>bit level</b> operations for

user/super/com/google/gwt/emul/java/security/MessageDigest.java
@@ -15,7 +15,7 @@
  */
 package java.security;
 
-import static com.google.gwt.core.client.impl.Coercions.ensureInt;
+import static java.internal.Coercions.ensureInt;
 
 /**
  * Message Digest algorithm - <a href=

user/super/com/google/gwt/emul/java/util/Arrays.java
@@ -16,8 +16,7 @@
 
 package java.util;
 
-import static com.google.gwt.core.client.impl.Coercions.ensureInt;
-
+import static java.internal.Coercions.ensureInt;
 import static java.internal.InternalPreconditions.checkArgument;
 import static java.internal.InternalPreconditions.checkArraySize;
 import static java.internal.InternalPreconditions.checkElementIndex;

user/super/com/google/gwt/emul/java/util/Collections.java
@@ -15,8 +15,7 @@
  */
 package java.util;
 
-import static com.google.gwt.core.client.impl.Coercions.ensureInt;
-
+import static java.internal.Coercions.ensureInt;
 import static java.internal.InternalPreconditions.checkArgument;
 import static java.internal.InternalPreconditions.checkElementIndex;
 

user/super/com/google/gwt/emul/java/util/HashMap.java
@@ -15,7 +15,7 @@
  */
 package java.util;
 
-import static com.google.gwt.core.client.impl.Coercions.ensureInt;
+import static java.internal.Coercions.ensureInt;
 
 import java.io.Serializable;
 

rstudio/rstudio

dev/core/super/javaemul/internal/Coercions.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.gwt.core.client.impl;
+package javaemul.internal;
 
 /**
  * Private implementation class for GWT. This API should not be

user/super/com/google/gwt/emul/java/math/BitLevel.java
@@ -34,7 +34,7 @@
  */
 package java.math;
 
-import static com.google.gwt.core.client.impl.Coercions.ensureInt;
+import static javaemul.internal.Coercions.ensureInt;
 
 /**
  * Static library that provides all the <b>bit level</b> operations for

user/super/com/google/gwt/emul/java/security/MessageDigest.java
@@ -15,7 +15,7 @@
  */
 package java.security;
 
-import static com.google.gwt.core.client.impl.Coercions.ensureInt;
+import static javaemul.internal.Coercions.ensureInt;
 
 /**
  * Message Digest algorithm - <a href=

user/super/com/google/gwt/emul/java/util/Arrays.java
@@ -16,12 +16,12 @@
 
 package java.util;
 
-import static com.google.gwt.core.client.impl.Coercions.ensureInt;
-
 import static java.internal.InternalPreconditions.checkArgument;
 import static java.internal.InternalPreconditions.checkArraySize;
 import static java.internal.InternalPreconditions.checkElementIndex;
 import static java.internal.InternalPreconditions.checkPositionIndexes;
+
+import static javaemul.internal.Coercions.ensureInt;
 
 import java.internal.ArrayHelper;
 import java.io.Serializable;

user/super/com/google/gwt/emul/java/util/Collections.java
@@ -15,10 +15,10 @@
  */
 package java.util;
 
-import static com.google.gwt.core.client.impl.Coercions.ensureInt;
-
 import static java.internal.InternalPreconditions.checkArgument;
 import static java.internal.InternalPreconditions.checkElementIndex;
+
+import static javaemul.internal.Coercions.ensureInt;
 
 import java.io.Serializable;
 

user/super/com/google/gwt/emul/java/util/HashMap.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,7 +15,7 @@
  */
 package java.util;
 
-import static com.google.gwt.core.client.impl.Coercions.ensureInt;
+import static javaemul.internal.Coercions.ensureInt;
 
 import java.io.Serializable;
 
@@ -23,7 +23,7 @@ import java.io.Serializable;
  * Implementation of Map interface based on a hash table. <a
  * href="http://java.sun.com/j2se/1.5.0/docs/api/java/util/HashMap.html">[Sun
  * docs]</a>
- * 
+ *
  * @param <K> key type
  * @param <V> value type
  */