35841a52f859c0318d3ca16abdb1e7709f0565b3
Jonathan McPherson <jonathan@rstudio.com>
2015-08-14 13:58:13 -0700

    specify SHA1 thumbprint of desired code signing cert

--- package/win32/codesign.bat
+++ package/win32/codesign.bat
@@ -1,13 +1,6 @@
-
 set SIGN_TARGET=%1%
 
-"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool" sign /fd sha256 /v /ac "cert\After_10-10-10_MSCV-VSClass3.cer" /s MY /n "RStudio, Inc." /t http://timestamp.VeriSign.com/scripts/timstamp.dll %SIGN_TARGET%
+"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool" sign /sha1 E5D84022D15FD10154AA7A75EC6CA055EE7E52A7 /v /ac "cert\After_10-10-10_MSCV-VSClass3.cer" /s MY /n "RStudio, Inc." /t http://timestamp.VeriSign.com/scripts/timstamp.dll %SIGN_TARGET%
+
 "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool" verify /v /kp %SIGN_TARGET%
  
-
-
-
-
-
-
-
