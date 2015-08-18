32ce5e83863a105abec9db7f2cfdb14aee4355b2
Lorenzo Simionato <simionato@google.com>
2015-08-18 09:13:47 -0400

    Make @JsFunction work with browsers that don't support '__proto__'.
    Note that the implementation is not equivalent to setting the prototype.
    For example, 'instanceof' will not work and if new properties are added
    to the object, they will not be reflected in the lambda.
    I think both of those shouldn't be a big problem though.
    
    Change-Id: I2324b558d276210bdd394466f141f6fc3196dc40
    Review-Link: https://gwt-review.googlesource.com/#/c/13390/

--- dev/core/super/com/google/gwt/dev/jjs/intrinsic/com/google/gwt/lang/JavaClassHierarchySetupUtil.java
+++ dev/core/super/com/google/gwt/dev/jjs/intrinsic/com/google/gwt/lang/JavaClassHierarchySetupUtil.java
@@ -151,7 +151,15 @@ public class JavaClassHierarchySetupUtil {
   public static native JavaScriptObject makeLambdaFunction(JavaScriptObject samMethod,
       JavaScriptObject instance) /*-{
     var lambda = function() { return samMethod.apply(lambda, arguments); }
-    lambda.__proto__ = instance;
+
+    if (lambda.__proto__) {
+      lambda.__proto__ = instance;
+    } else {
+      for (var prop in instance) {
+        lambda[prop] = instance[prop];
+      }
+    }
+
     return lambda;
   }-*/;
 
