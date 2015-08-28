783c71ab9bcf69feb8a075d224df586bab6c723e
Goktug Gokdogan <goktug@google.com>
2015-08-28 15:01:43 -0700

    Introduces a suite for LongLibGwtTest.
    It was missing the suite so it wasn't executed part of our
    smoke tests.
    
    Change-Id: I96b54690e495e9a33feafd26c213e76b7be3a082

--- user/test/com/google/gwt/langtest/client/LongLibGwtTest.java
+++ user/test/com/google/gwt/langtest/client/LongLibGwtTest.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.gwt.langtest;
+package com.google.gwt.langtest.client;
 
 import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.lang.LongLibTestBase;
