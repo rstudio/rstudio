84e9bc932f71b8b622bd8797720a8f596eedacb8
Kevin Ushey <kevin@rstudio.com>
2015-09-01 10:09:05 -0700

    fix indentation; better mechanism to ensure operator tokens highlighted

--- src/gwt/acesupport/acemode/c_cpp_highlight_rules.js
+++ src/gwt/acesupport/acemode/c_cpp_highlight_rules.js
@@ -43,20 +43,20 @@ var c_cppHighlightRules = function() {
 
    var keywords = lang.arrayToMap([
       "alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand",
-       "bitor", "bool", "break", "case", "catch", "char", "char16_t",
-       "char32_t", "class", "compl", "const", "constexpr",
-       "const_cast", "continue", "decltype", "default", "delete",
-       "do", "double", "dynamic_cast", "else", "enum", "explicit",
-       "export", "extern", "false", "float", "for", "friend", "goto",
-       "if", "inline", "int", "in", "long", "mutable", "namespace",
-       "new", "noexcept", "not", "not_eq", "nullptr", "or", "or_eq",
-       "private", "protected", "public", "register",
-       "reinterpret_cast", "return", "short", "signed", "sizeof",
-       "static", "static_assert", "static_cast", "struct", "switch",
-       "template", "this", "thread_local", "throw", "true", "try",
-       "typedef", "typeid", "typeof", "typename", "union", "unsigned",
-       "using", "virtual", "void", "volatile", "wchar_t", "while",
-       "xor", "xor_eq"
+      "bitor", "bool", "break", "case", "catch", "char", "char16_t",
+      "char32_t", "class", "compl", "const", "constexpr",
+      "const_cast", "continue", "decltype", "default", "delete",
+      "do", "double", "dynamic_cast", "else", "enum", "explicit",
+      "export", "extern", "false", "float", "for", "friend", "goto",
+      "if", "inline", "int", "in", "long", "mutable", "namespace",
+      "new", "noexcept", "not", "not_eq", "nullptr", "or", "or_eq",
+      "private", "protected", "public", "register",
+      "reinterpret_cast", "return", "short", "signed", "sizeof",
+      "static", "static_assert", "static_cast", "struct", "switch",
+      "template", "this", "thread_local", "throw", "true", "try",
+      "typedef", "typeid", "typeof", "typename", "union", "unsigned",
+      "using", "virtual", "void", "volatile", "wchar_t", "while",
+      "xor", "xor_eq"
    ]);
 
    var preProcTokens = [
@@ -174,7 +174,7 @@ var c_cppHighlightRules = function() {
             merge : false,
             regex : reOperatorTokens
          }, {
-            token : "punctuation.operator",
+            token : "keyword.punctuation.operator",
             merge : false,
             regex : "\\?|\\:|\\,|\\;|\\.|\\\\"
          }, {
