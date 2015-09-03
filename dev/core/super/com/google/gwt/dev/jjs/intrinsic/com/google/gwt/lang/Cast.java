gwtproject/gwt

Double and Boolean implemented as unboxed raw types.
TODO: Update EqualityNormalizer to deal with following cases:

unknown != null/unknown == null (can't use the !!x/!x version)
unknown (!=|==) double/boolean (because 1 == true :( )
a != b|a == b where typeof(a) != typeof(b) and a or b is one of
string, boolean, double, because again, insane things like
"1" == 1.

I'll do this in a followon patchset later today when I figure out
whether I want to try and encode this in the matrix the class uses,
or in some other way.

Amazingly, all CompilerSuite/EmulSuite tests pass even though this
logic is currently wrong.

I'll revert the Hello.java and gradle changes in a later patchset
after I write tests.

Change-Id: I3a11e91be6ceb0d72b727a43c28581ebef6d70e3

dev/core/super/com/google/gwt/dev/jjs/intrinsic/com/google/gwt/lang/Cast.java
@@ -41,11 +41,20 @@ final class Cast {
    */
   private static JavaScriptObject stringCastMap;
 
+  // the next two are implemented exactly as the former
+  private static JavaScriptObject doubleCastMap;
+  private static JavaScriptObject booleanCastMap;
+
   @HasNoSideEffects
   static native boolean canCast(Object src, JavaScriptObject dstId) /*-{
     return @com.google.gwt.lang.Cast::isJavaString(*)(src) &&
         !!@com.google.gwt.lang.Cast::stringCastMap[dstId] ||
-        src.@java.lang.Object::castableTypeMap && !!src.@java.lang.Object::castableTypeMap[dstId];
+        @com.google.gwt.lang.Cast::isJavaDouble(*)(src) &&
+        !!@com.google.gwt.lang.Cast::doubleCastMap[dstId] ||
+        src.@java.lang.Object::castableTypeMap && !!src.@java.lang.Object::castableTypeMap[dstId] ||
+        // this occurs last because it is much rarer and less likely to be in hot code
+        @com.google.gwt.lang.Cast::isJavaBoolean(*)(src) &&
+        !!@com.google.gwt.lang.Cast::booleanCastMap[dstId];
   }-*/;
 
   @HasNoSideEffects
@@ -67,6 +76,16 @@ final class Cast {
 
   static Object dynamicCastToString(Object src) {
     checkType(src == null || isJavaString(src));
+    return src;
+  }
+
+  static Object dynamicCastToNumber(Object src) {
+    checkType(src == null || isJavaDouble(src));
+    return src;
+  }
+
+  static Object dynamicCastToBoolean(Object src) {
+    checkType(src == null || isJavaBoolean(src));
     return src;
   }
 
@@ -145,7 +164,7 @@ final class Cast {
 
   /**
    * Uses the not operator to perform a null-check; do NOT use on anything that
-   * could be a String.
+   * could be a String, 'unboxed' Double, or 'unboxed' Boolean.
    */
   static native boolean isNotNull(Object src) /*-{
     // Coerce to boolean.
@@ -154,7 +173,7 @@ final class Cast {
 
   /**
    * Uses the not operator to perform a null-check; do NOT use on anything that
-   * could be a String.
+   * could be a String, 'unboxed' Double, or 'unboxed' Boolean.
    */
   static native boolean isNull(Object src) /*-{
     return !src;
@@ -265,6 +284,26 @@ final class Cast {
   @HasNoSideEffects
   static native boolean isJavaString(Object src) /*-{
     return typeof(src) === "string";
+  }-*/;
+
+  /**
+   * Returns whether the Object is a Java Double.
+   *
+   * Java Numbers are translated to JavaScript numbers.
+   */
+  @HasNoSideEffects
+  static native boolean isJavaDouble(Object src) /*-{
+    return typeof(src) === "number";
+  }-*/;
+
+  /**
+   * Returns whether the Object is a Java Boolean. (*)
+   *
+   * Java Booleans are translated to JavaScript booleans.
+   */
+  @HasNoSideEffects
+  static native boolean isJavaBoolean(Object src) /*-{
+    return typeof(src) === "boolean";
   }-*/;
 
   /**

rstudio/rstudio

dev/core/super/com/google/gwt/dev/jjs/intrinsic/com/google/gwt/lang/Cast.java
@@ -39,13 +39,23 @@ final class Cast {
    * NOTE: it is important that the field is left uninitialized so that Cast does not
    * require a clinit.
    */
+  // NOTE: if any of these three are edited, update JProgram.DispatchType's constructor
   private static JavaScriptObject stringCastMap;
+
+  // the next two are implemented exactly as the former
+  private static JavaScriptObject doubleCastMap;
+  private static JavaScriptObject booleanCastMap;
 
   @HasNoSideEffects
   static native boolean canCast(Object src, JavaScriptObject dstId) /*-{
     return @com.google.gwt.lang.Cast::isJavaString(*)(src) &&
         !!@com.google.gwt.lang.Cast::stringCastMap[dstId] ||
-        src.@java.lang.Object::castableTypeMap && !!src.@java.lang.Object::castableTypeMap[dstId];
+        src.@java.lang.Object::castableTypeMap && !!src.@java.lang.Object::castableTypeMap[dstId] ||
+        @com.google.gwt.lang.Cast::isJavaDouble(*)(src) &&
+        !!@com.google.gwt.lang.Cast::doubleCastMap[dstId] ||
+        // this occurs last because it is much rarer and less likely to be in hot code
+        @com.google.gwt.lang.Cast::isJavaBoolean(*)(src) &&
+        !!@com.google.gwt.lang.Cast::booleanCastMap[dstId];
   }-*/;
 
   @HasNoSideEffects
@@ -65,8 +75,19 @@ final class Cast {
     return src;
   }
 
+  // NOTE: if any of these three are edited, update JProgram.DispatchType's constructor
   static Object dynamicCastToString(Object src) {
     checkType(src == null || isJavaString(src));
+    return src;
+  }
+
+  static Object dynamicCastToDouble(Object src) {
+    checkType(src == null || isJavaDouble(src));
+    return src;
+  }
+
+  static Object dynamicCastToBoolean(Object src) {
+    checkType(src == null || isJavaBoolean(src));
     return src;
   }
 
@@ -145,7 +166,7 @@ final class Cast {
 
   /**
    * Uses the not operator to perform a null-check; do NOT use on anything that
-   * could be a String.
+   * could be a String, 'unboxed' Double, or 'unboxed' Boolean.
    */
   static native boolean isNotNull(Object src) /*-{
     // Coerce to boolean.
@@ -154,7 +175,7 @@ final class Cast {
 
   /**
    * Uses the not operator to perform a null-check; do NOT use on anything that
-   * could be a String.
+   * could be a String, 'unboxed' Double, or 'unboxed' Boolean.
    */
   static native boolean isNull(Object src) /*-{
     return !src;
@@ -257,6 +278,7 @@ final class Cast {
     return o;
   }
 
+  // NOTE: if any of these three are edited, update JProgram.DispatchType's constructor
   /**
    * Returns whether the Object is a Java String.
    *
@@ -265,6 +287,26 @@ final class Cast {
   @HasNoSideEffects
   static native boolean isJavaString(Object src) /*-{
     return typeof(src) === "string";
+  }-*/;
+
+  /**
+   * Returns whether the Object is a Java Double.
+   *
+   * Java Numbers are translated to JavaScript numbers.
+   */
+  @HasNoSideEffects
+  static native boolean isJavaDouble(Object src) /*-{
+    return typeof(src) === "number";
+  }-*/;
+
+  /**
+   * Returns whether the Object is a Java Boolean. (*)
+   *
+   * Java Booleans are translated to JavaScript booleans.
+   */
+  @HasNoSideEffects
+  static native boolean isJavaBoolean(Object src) /*-{
+    return typeof(src) === "boolean";
   }-*/;
 
   /**