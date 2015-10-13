51bf38d2794fa1587d3df70997db358f0158e559
Kevin Ushey <kevinushey@gmail.com>
2015-10-13 09:13:43 -0700

    check for empty doc id

--- src/cpp/session/modules/SessionRCompletions.cpp
+++ src/cpp/session/modules/SessionRCompletions.cpp
@@ -456,6 +456,11 @@ SEXP rs_getKnitParamsForDocument(SEXP documentIdSEXP)
    using namespace source_database;
    
    std::string documentId = r::sexp::asString(documentIdSEXP);
+   
+   // Check for empty doc ID (can happen if this is requested e.g. from console)
+   if (documentId.empty())
+      return R_NilValue;
+   
    boost::shared_ptr<SourceDocument> pDoc(new SourceDocument());
    
    Error error = get(documentId, pDoc);
