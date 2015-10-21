gwtproject/gwt

Don't emit unnecessary synthetic overrides.
Change-Id: I43fab8f002de32efbf63d329f792461fc787cba6

dev/core/src/com/google/gwt/dev/jjs/impl/ComputeOverridesAndImplementDefaultMethods.java
@@ -291,6 +291,8 @@ public class ComputeOverridesAndImplementDefaultMethods {
       } else {
         // Creates a forwarding method to act as the place holder for this accidental override.
         implementingMethod = JjsUtils.createForwardingMethod(type, method);
+        implementingMethod.setSyntheticOverride();
+
         if (method.isFinal()) {
           // To keep consistency we reset the final mark
           method.setFinal(false);

dev/core/src/com/google/gwt/dev/jjs/impl/JsInteropRestrictionChecker.java
@@ -300,6 +300,12 @@ public class JsInteropRestrictionChecker {
   private void checkNoStaticJsPropertyCalls() {
     new JVisitor() {
       @Override
+      public boolean visit(JMethod x, Context ctx) {
+        // Skip unnecessary synthetic override, as they will not be generated.
+        return !JjsUtils.isUnnecessarySyntheticOverride(x);
+      }
+
+      @Override
       public void endVisit(JMethodCall x, Context ctx) {
         JMethod target = x.getTarget();
         if (x.isStaticDispatchOnly() && target.isJsPropertyAccessor()) {

rstudio/rstudio

dev/core/src/com/google/gwt/dev/jjs/impl/ComputeOverridesAndImplementDefaultMethods.java
@@ -291,6 +291,8 @@ public class ComputeOverridesAndImplementDefaultMethods {
       } else {
         // Creates a forwarding method to act as the place holder for this accidental override.
         implementingMethod = JjsUtils.createForwardingMethod(type, method);
+        implementingMethod.setSyntheticAccidentalOverride();
+
         if (method.isFinal()) {
           // To keep consistency we reset the final mark
           method.setFinal(false);

dev/core/src/com/google/gwt/dev/jjs/impl/JsInteropRestrictionChecker.java
@@ -374,6 +374,12 @@ public class JsInteropRestrictionChecker {
   private void checkNoStaticJsPropertyCalls() {
     new JVisitor() {
       @Override
+      public boolean visit(JMethod x, Context ctx) {
+        // Skip unnecessary synthetic override, as they will not be generated.
+        return !JjsUtils.isUnnecessarySyntheticAccidentalOverride(x);
+      }
+
+      @Override
       public void endVisit(JMethodCall x, Context ctx) {
         JMethod target = x.getTarget();
         if (x.isStaticDispatchOnly() && target.isJsPropertyAccessor()) {