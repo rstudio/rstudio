332ea02d0cbd0952e40039cb9ffd385b306af1bb
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2015-10-28 15:23:26 -0700

    Adding java8 patterns for test exclusion.
    Author: Goktug Gokdogan
    Change-Id: Ibfaa119e7cb150cb386ad36a333ac743db73f8d7

---
+++ user/test/com/google/gwt/emultest/EmulJava8Suite.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest;
+
+import com.google.gwt.emultest.java8.util.OptionalTest;
+import com.google.gwt.junit.tools.GWTTestSuite;
+
+import junit.framework.Test;
+
+/**
+ * Test Java8 JRE emulations.
+ */
+public class EmulJava8Suite {
+
+  public static Test suite() {
+    GWTTestSuite suite = new GWTTestSuite("Tests for com.google.gwt.emul.java8");
+
+    //-- java.util
+    suite.addTestSuite(OptionalTest.class);
+
+    return suite;
+  }
+}
