eb419a1dc94edb11029b0efe01c458b7de06ae80
Kevin Ushey <kevinushey@gmail.com>
2015-10-28 14:58:45 -0700

    handle 'may not respond to selector' warnings

--- src/cpp/desktop-mac/GwtCallbacks.mm
+++ src/cpp/desktop-mac/GwtCallbacks.mm
@@ -314,19 +314,37 @@ private:
    }
 }
 
+- (void) performClipboardAction: (SEL) selector
+{
+   WKWebView* view = [[[NSApp mainWindow] windowController] webView];
+   if (view == nil) {
+      NSString* errorMsg = [NSString stringWithFormat: @"nil webView on clipboard action %@", NSStringFromSelector(selector)];
+      LOG_ERROR_MESSAGE([errorMsg UTF8String]);
+      return;
+   }
+
+   if ([view respondsToSelector: selector]) {
+      [view performSelector: selector withObject: view];
+   } else {
+      NSString* errorMsg = [NSString stringWithFormat: @"@webView does not respond to selector %@", NSStringFromSelector(selector)];
+      LOG_ERROR_MESSAGE([errorMsg UTF8String]);
+      return;
+   }
+}
+
 - (void) clipboardCut
 {
-   [[[[NSApp mainWindow] windowController] webView] cut: self];
+   [self performClipboardAction: @selector(cut:)];
 }
 
 - (void) clipboardCopy
 {
-   [[[[NSApp mainWindow] windowController] webView] copy: self];
+   [self performClipboardAction: @selector(copy:)];
 }
 
 - (void) clipboardPaste
 {
-   [[[[NSApp mainWindow] windowController] webView] paste: self];
+   [self performClipboardAction: @selector(paste:)];
 }
 
 - (NSString*) getUriForPath: (NSString*) path
