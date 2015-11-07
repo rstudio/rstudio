rstudio/rstudio

commit 64e3270a9a02eea66fb9beab9e423a14f60b6cec

Refactor property/member identification for JsInterop.
Change-Id: I7e4d2bc0675bb08f013c69993c1fd602f12a8ca1

dev/core/src/com/google/gwt/dev/javac/JsInteropUtil.java
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.dev.javac;
 
+import com.google.gwt.dev.jjs.ast.HasJsInfo.JsMemberType;
 import com.google.gwt.dev.jjs.ast.JClassType;
 import com.google.gwt.dev.jjs.ast.JConstructor;
 import com.google.gwt.dev.jjs.ast.JDeclaredType;
@@ -22,9 +23,7 @@ import com.google.gwt.dev.jjs.ast.JField;
 import com.google.gwt.dev.jjs.ast.JInterfaceType;
 import com.google.gwt.dev.jjs.ast.JMember;
 import com.google.gwt.dev.jjs.ast.JMethod;
-import com.google.gwt.dev.jjs.ast.JMethod.JsPropertyAccessorType;
 import com.google.gwt.dev.jjs.ast.JPrimitiveType;
-import com.google.gwt.thirdparty.guava.common.base.Strings;
 
 import org.eclipse.jdt.internal.compiler.ast.Annotation;
 import org.eclipse.jdt.internal.compiler.lookup.AnnotationBinding;
@@ -46,7 +45,7 @@ public final class JsInteropUtil {
   public static void maybeSetJsInteropProperties(JDeclaredType type, Annotation... annotations) {
     AnnotationBinding jsType = JdtUtil.getAnnotation(annotations, JSTYPE_CLASS);
     String namespace = maybeGetJsNamespace(annotations);
-    String exportName = maybeGetJsExportName(annotations, "");
+    String exportName = maybeGetJsExportName(annotations);
     String jsPrototype = JdtUtil.getAnnotationParameterString(jsType, "prototype");
     boolean isJsNative = jsPrototype != null;
     if (isJsNative) {
@@ -55,7 +54,7 @@ public final class JsInteropUtil {
       exportName = jsPrototype.substring(indexOf + 1);
     }
     boolean isJsType = jsType != null;
-    boolean isClassWideExport = exportName != null;
+    boolean isClassWideExport = JdtUtil.getAnnotation(annotations, JSEXPORT_CLASS) != null;
     boolean isJsFunction = JdtUtil.getAnnotation(annotations, JSFUNCTION_CLASS) != null;
     boolean canBeImplementedExternally =
         (type instanceof JInterfaceType && (isJsType || isJsFunction))
@@ -84,61 +83,60 @@ public final class JsInteropUtil {
   }
 
   public static void maybeSetJsInteropProperties(JMethod method, Annotation... annotations) {
-    setJsInteropProperties(method, annotations);
-    if (JdtUtil.getAnnotation(annotations, JSPROPERTY_CLASS) != null) {
-      setJsPropertyProperties(method, null);
-    }
+    boolean isPropertyAccessor = JdtUtil.getAnnotation(annotations, JSPROPERTY_CLASS) != null;
+    setJsInteropProperties(method, annotations, isPropertyAccessor);
   }
 
   public static void maybeSetJsInteropPropertiesNew(JMethod method, Annotation... annotations) {
-    AnnotationBinding annotation = getInteropAnnotation(annotations, "JsMethod");
     if (getInteropAnnotation(annotations, "JsOverlay") != null) {
       method.setJsOverlay();
     }
 
+    AnnotationBinding annotation = getInteropAnnotation(annotations, "JsMethod");
     if (annotation == null) {
       annotation = getInteropAnnotation(annotations, "JsConstructor");
     }
-    AnnotationBinding jsPropertyAnnotation = getInteropAnnotation(annotations, "JsProperty");
     if (annotation == null) {
-      annotation = jsPropertyAnnotation;
+      annotation = getInteropAnnotation(annotations, "JsProperty");
     }
-    setJsInteropPropertiesNew(method, annotations, annotation);
-    if (jsPropertyAnnotation != null) {
-      setJsPropertyProperties(
-          method, JdtUtil.getAnnotationParameterString(jsPropertyAnnotation, "name"));
-    }
+
+    boolean isPropertyAccessor = getInteropAnnotation(annotations, "JsProperty") != null;
+    setJsInteropPropertiesNew(method, annotations, annotation, isPropertyAccessor);
   }
 
   public static void maybeSetJsInteropProperties(JField field, Annotation... annotations) {
-    setJsInteropProperties(field, annotations);
+    setJsInteropProperties(field, annotations, false);
   }
 
   public static void maybeSetJsInteropPropertiesNew(JField field, Annotation... annotations) {
     AnnotationBinding annotation = getInteropAnnotation(annotations, "JsProperty");
-    setJsInteropPropertiesNew(field, annotations, annotation);
+    setJsInteropPropertiesNew(field, annotations, annotation, false);
   }
 
-  private static void setJsInteropProperties(JMember member, Annotation... annotations) {
-    String namespace = maybeGetJsNamespace(annotations);
-    String exportName = maybeGetJsExportName(annotations, computeName(member));
-    member.setJsMemberInfo(namespace, exportName, exportName != null);
+  private static void setJsInteropProperties(
+      JMember member, Annotation[] annotations, boolean isPropertyAccessor) {
+    boolean hasExport = JdtUtil.getAnnotation(annotations, JSEXPORT_CLASS) != null;
 
     /* Apply class wide JsInterop annotations */
 
     boolean ignore = JdtUtil.getAnnotation(annotations, JSNOEXPORT_CLASS) != null;
-    if (ignore || (!member.isPublic() && !isNativeConstructor(member)) || exportName != null) {
+    if (ignore || (!member.isPublic() && !isNativeConstructor(member)) || !hasExport) {
       return;
     }
+
+    String namespace = maybeGetJsNamespace(annotations);
+    String exportName = maybeGetJsExportName(annotations);
+    JsMemberType memberType = getJsMemberType(member, isPropertyAccessor);
+    member.setJsMemberInfo(memberType, namespace, exportName, hasExport);
 
     JDeclaredType enclosingType = member.getEnclosingType();
 
     if (enclosingType.isJsType() && member.needsDynamicDispatch()) {
-      member.setJsMemberInfo(namespace, computeName(member), true);
+      member.setJsMemberInfo(memberType, namespace, exportName, true);
     }
 
     if (enclosingType.isClassWideExport() && !member.needsDynamicDispatch()) {
-      member.setJsMemberInfo(namespace, computeName(member), true);
+      member.setJsMemberInfo(memberType, namespace, exportName, true);
     }
   }
 
@@ -146,8 +144,8 @@ public final class JsInteropUtil {
     return member instanceof JConstructor && member.getEnclosingType().isJsNative();
   }
 
-  private static void setJsInteropPropertiesNew(
-      JMember member, Annotation[] annotations, AnnotationBinding memberAnnotation) {
+  private static void setJsInteropPropertiesNew(JMember member, Annotation[] annotations,
+      AnnotationBinding memberAnnotation, boolean isAccessor) {
     if (getInteropAnnotation(annotations, "JsIgnore") != null) {
       return;
     }
@@ -159,25 +157,34 @@ public final class JsInteropUtil {
 
     String namespace = JdtUtil.getAnnotationParameterString(memberAnnotation, "namespace");
     String name = JdtUtil.getAnnotationParameterString(memberAnnotation, "name");
-    member.setJsMemberInfo(namespace, name == null ? computeName(member) : name, true);
+    JsMemberType memberType = getJsMemberType(member, isAccessor);
+    member.setJsMemberInfo(memberType, namespace, name, true);
   }
 
-  private static void setJsPropertyProperties(JMethod method, String name) {
-    if (method.getParams().size() == 1 && method.getType() == JPrimitiveType.VOID) {
-      method.setJsPropertyInfo(name, JsPropertyAccessorType.SETTER);
-    } else if (method.getParams().isEmpty() && method.getType() != JPrimitiveType.VOID) {
-      method.setJsPropertyInfo(name, JsPropertyAccessorType.GETTER);
-    } else {
-      method.setJsPropertyInfo(name, JsPropertyAccessorType.UNDEFINED);
+  private static JsMemberType getJsMemberType(JMember member, boolean isPropertyAccessor) {
+    if (member instanceof JField) {
+      return JsMemberType.PROPERTY;
     }
+    if (member instanceof JConstructor) {
+      return JsMemberType.CONSTRUCTOR;
+    }
+    if (isPropertyAccessor) {
+      return getJsPropertyAccessorType((JMethod) member);
+    }
+    return JsMemberType.METHOD;
+  }
+
+  private static JsMemberType getJsPropertyAccessorType(JMethod method) {
+    if (method.getParams().size() == 1 && method.getType() == JPrimitiveType.VOID) {
+      return JsMemberType.SETTER;
+    } else if (method.getParams().isEmpty() && method.getType() != JPrimitiveType.VOID) {
+      return JsMemberType.GETTER;
+    }
+    return JsMemberType.UNDEFINED_ACCESSOR;
   }
 
   private static AnnotationBinding getInteropAnnotation(Annotation[] annotations, String name) {
     return JdtUtil.getAnnotation(annotations, "jsinterop.annotations." + name);
-  }
-
-  private static String computeName(JMember member) {
-    return member instanceof JConstructor ? "" : member.getName();
   }
 
   private static String maybeGetJsNamespace(Annotation[] annotations) {
@@ -185,12 +192,8 @@ public final class JsInteropUtil {
     return JdtUtil.getAnnotationParameterString(jsNamespace, "value");
   }
 
-  private static String maybeGetJsExportName(Annotation[] annotations, String calculatedName) {
-    AnnotationBinding jsExport = JdtUtil.getAnnotation(annotations, JSEXPORT_CLASS);
-    if (jsExport == null) {
-      return null;
-    }
-    String value = JdtUtil.getAnnotationParameterString(jsExport, "value");
-    return Strings.isNullOrEmpty(value) ? calculatedName : value;
+  private static String maybeGetJsExportName(Annotation[] annotations) {
+    AnnotationBinding annotation = JdtUtil.getAnnotation(annotations, JSEXPORT_CLASS);
+    return JdtUtil.getAnnotationParameterString(annotation, "value");
   }
 }

dev/core/src/com/google/gwt/dev/jjs/ast/HasJsInfo.java
@@ -15,14 +15,116 @@
  */
 package com.google.gwt.dev.jjs.ast;
 
+import com.google.gwt.dev.javac.JsInteropUtil;
+
+import java.beans.Introspector;
+
 /**
  * Abstracts JsInterop information for the AST nodes.
  */
 public interface HasJsInfo extends HasJsName {
+  /**
+   * Indicates type of JsMember.
+   */
+  enum JsMemberType {
+    /**
+     * Not a js member.
+     */
+    NONE,
+    /**
+     * A JsConstructor.
+     */
+    CONSTRUCTOR {
+      @Override
+      public String computeName(JMember member) {
+        return "";
+      }
+    },
+    /**
+     * A JsMethod.
+     */
+    METHOD,
+    /**
+     * A JsProperty.
+     */
+    PROPERTY,
+    /**
+     * A getter JsProperty accessor. Usually in the form of getX()/isX().
+     */
+    GETTER("get") {
+      @Override
+      public String computeName(JMember member) {
+        String methodName = member.getName();
+        if (startsWithCamelCase(methodName, "get")) {
+          return Introspector.decapitalize(methodName.substring(3));
+        }
+        if (startsWithCamelCase(methodName, "is")) {
+          return Introspector.decapitalize(methodName.substring(2));
+        }
+        return JsInteropUtil.INVALID_JSNAME;
+      }
+      @Override
+      public boolean isPropertyAccessor() {
+        return true;
+      }
+    },
+    /**
+     * A setter JsProperty accessor. Usually in the form of setX(x).
+     */
+    SETTER("set") {
+      @Override
+      public String computeName(JMember member) {
+        String methodName = member.getName();
+        if (startsWithCamelCase(methodName, "set")) {
+          return Introspector.decapitalize(methodName.substring(3));
+        }
+        return JsInteropUtil.INVALID_JSNAME;
+      }
+      @Override
+      public boolean isPropertyAccessor() {
+        return true;
+      }
+    },
+    /**
+     * A property accessor but doesn't match setter/getter patterns.
+     */
+    UNDEFINED_ACCESSOR;
 
-  void setJsMemberInfo(String namespace, String name, boolean exported);
+    private String accessorKey;
+
+    private JsMemberType() { }
+
+    private JsMemberType(String accessorKey) {
+      this.accessorKey = accessorKey;
+    }
+
+    public String getPropertyAccessorKey() {
+      return accessorKey;
+    }
+
+    public boolean isPropertyAccessor() {
+      return getPropertyAccessorKey() != null;
+    }
+
+    public String computeName(JMember member) {
+      return member.getName();
+    }
+
+    private static boolean startsWithCamelCase(String string, String prefix) {
+      return string.length() > prefix.length() && string.startsWith(prefix)
+          && Character.isUpperCase(string.charAt(prefix.length()));
+    }
+  }
+
+  void setJsMemberInfo(JsMemberType type, String namespace, String name, boolean exported);
+
+  JsMemberType getJsMemberType();
 
   boolean isJsNative();
 
   boolean isJsOverlay();
+
+  boolean canBeReferencedExternally();
+
+  boolean isJsInteropEntryPoint();
 }

dev/core/src/com/google/gwt/dev/jjs/ast/JDeclaredType.java
@@ -360,6 +360,10 @@ public abstract class JDeclaredType extends JReferenceType
     return methods;
   }
 
+  public Iterable<JMember> getMembers() {
+    return Iterables.<JMember>concat(fields, methods);
+  }
+
   @Override
   public boolean isJsType() {
     return isJsType;
@@ -394,22 +398,16 @@ public abstract class JDeclaredType extends JReferenceType
     return false;
   }
 
+  @Override
   public boolean canBeReferencedExternally() {
     if (isJsType()) {
       return true;
     }
-    for (JMethod method : getMethods()) {
-      if (method.canBeCalledExternally()) {
+    for (JMember member : getMembers()) {
+      if (member.canBeReferencedExternally()) {
         return true;
       }
     }
-
-    for (JField field : getFields()) {
-      if (field.canBeReferencedExternally()) {
-        return true;
-      }
-    }
-
     return false;
   }
 
@@ -613,14 +611,17 @@ public abstract class JDeclaredType extends JReferenceType
     return null;
   }
 
+  @Override
   public String getJsName() {
     return Strings.isNullOrEmpty(jsName) ? getSimpleName() : jsName;
   }
 
+  @Override
   public String getJsNamespace() {
     return jsNamespace;
   }
 
+  @Override
   public String getQualifiedJsName() {
     return jsNamespace.isEmpty() ? getJsName() : jsNamespace + "." + getJsName();
   }

dev/core/src/com/google/gwt/dev/jjs/ast/JField.java
@@ -80,6 +80,7 @@ public class JField extends JVariable implements JMember {
   public static final JField NULL_FIELD = new JField(SourceOrigin.UNKNOWN, "nullField", null,
       JReferenceType.NULL_TYPE, false, Disposition.FINAL);
 
+  private JsMemberType jsMembertype = JsMemberType.NONE;
   private String jsName;
   private String jsNamespace;
   private boolean exported;
@@ -131,16 +132,25 @@ public class JField extends JVariable implements JMember {
   }
 
   @Override
-  public void setJsMemberInfo(String namespace, String name, boolean exported) {
-    this.jsName = name;
+  public void setJsMemberInfo(
+      JsMemberType jsMembertype, String namespace, String name, boolean exported) {
+    this.jsMembertype = jsMembertype;
+    this.jsName = name != null ? name : jsMembertype.computeName(this);
     this.jsNamespace = namespace;
     this.exported = exported;
   }
 
-  public boolean isJsInteropEntryPoint() {
-    return exported && isStatic();
+  @Override
+  public JsMemberType getJsMemberType() {
+    return jsMembertype;
   }
 
+  @Override
+  public boolean isJsInteropEntryPoint() {
+    return exported && isStatic() && !isJsNative();
+  }
+
+  @Override
   public boolean canBeReferencedExternally() {
     return exported;
   }
@@ -159,10 +169,6 @@ public class JField extends JVariable implements JMember {
   @Override
   public boolean isAbstract() {
     return false;
-  }
-
-  public boolean isJsProperty() {
-    return jsName != null;
   }
 
   @Override

dev/core/src/com/google/gwt/dev/jjs/ast/JMember.java
@@ -21,7 +21,7 @@ import com.google.gwt.dev.jjs.HasSourceInfo;
  * Abstracts class members (i.e. fields and methods).
  */
 public interface JMember extends HasEnclosingType, HasName, HasJsInfo, HasType, HasSourceInfo,
-    CanBeSetFinal, CanBeStatic, CanBeAbstract {
+    CanBeSetFinal, CanBeStatic, CanBeAbstract, CanHaveSuppressedWarnings {
 
   boolean isPublic();
 

dev/core/src/com/google/gwt/dev/jjs/ast/JMethod.java
@@ -26,7 +26,6 @@ import com.google.gwt.dev.util.StringInterner;
 import com.google.gwt.dev.util.collect.Lists;
 import com.google.gwt.thirdparty.guava.common.collect.Sets;
 
-import java.beans.Introspector;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -40,76 +39,7 @@ import java.util.Set;
 /**
  * A Java method implementation.
  */
-public class JMethod extends JNode implements JMember, CanBeAbstract, CanHaveSuppressedWarnings {
-
-  /**
-   * Indicates whether a method is a JsProperty accessor.
-   */
-  public enum JsPropertyAccessorType {
-    /**
-     * Not a property accessor.
-     */
-    NONE,
-    /**
-     * A getter property accessor. Usually in the form of getX()/isX().
-     */
-    GETTER("get") {
-      @Override
-      public String computeName(JMethod member) {
-        String methodName = member.getName();
-        if (startsWithCamelCase(methodName, "get")) {
-          return Introspector.decapitalize(methodName.substring(3));
-        }
-        if (startsWithCamelCase(methodName, "is")) {
-          return Introspector.decapitalize(methodName.substring(2));
-        }
-        return JsInteropUtil.INVALID_JSNAME;
-      }
-    },
-    /**
-     * A setter JsProperty accessor. Usually in the form of setX(x).
-     */
-    SETTER("set") {
-      @Override
-      public String computeName(JMethod member) {
-        String methodName = member.getName();
-        if (startsWithCamelCase(methodName, "set")) {
-          return Introspector.decapitalize(methodName.substring(3));
-        }
-        return JsInteropUtil.INVALID_JSNAME;
-      }
-    },
-    /**
-     * A property accessor but doesn't match setter/getter patterns.
-     */
-    UNDEFINED;
-
-    private String key;
-
-    JsPropertyAccessorType() {
-    }
-
-    JsPropertyAccessorType(String key) {
-      this.key = key;
-    }
-
-    public String getKey() {
-      return key;
-    }
-
-    public boolean isPropertyAccessor() {
-      return key != null;
-    }
-
-    public String computeName(JMethod method) {
-      return method.getName();
-    }
-
-    private static boolean startsWithCamelCase(String string, String prefix) {
-      return string.length() > prefix.length() && string.startsWith(prefix)
-          && Character.isUpperCase(string.charAt(prefix.length()));
-    }
-  }
+public class JMethod extends JNode implements JMember, CanBeAbstract {
 
   public static final Comparator<JMethod> BY_SIGNATURE_COMPARATOR = new Comparator<JMethod>() {
     @Override
@@ -121,7 +51,7 @@ public class JMethod extends JNode implements JMember, CanBeAbstract, CanHaveSup
   private String jsName;
   private boolean exported;
   private String jsNamespace;
-  private JsPropertyAccessorType jsPropertyType = JsPropertyAccessorType.NONE;
+  private JsMemberType jsMemberType = JsMemberType.NONE;
   private Specialization specialization;
   private InliningMode inliningMode = InliningMode.NORMAL;
   private boolean preventDevirtualization = false;
@@ -132,17 +62,20 @@ public class JMethod extends JNode implements JMember, CanBeAbstract, CanHaveSup
   private boolean isJsOverlay = false;
 
   @Override
-  public void setJsMemberInfo(String namespace, String name, boolean exported) {
-    this.jsName = name;
+  public void setJsMemberInfo(JsMemberType type, String namespace, String name, boolean exported) {
+    this.jsMemberType = type;
+    this.jsName = name != null ? name : type.computeName(this);
     this.jsNamespace = namespace;
     this.exported = exported;
   }
 
+  @Override
   public boolean isJsInteropEntryPoint() {
     return exported && !needsDynamicDispatch() && !isJsNative();
   }
 
-  public boolean canBeCalledExternally() {
+  @Override
+  public boolean canBeReferencedExternally() {
     if (exported || isJsFunctionMethod()) {
       return true;
     }
@@ -209,58 +142,38 @@ public class JMethod extends JNode implements JMember, CanBeAbstract, CanHaveSup
   }
 
   /**
-   * Returns {@code true} if this method is the first JsMethod in the method hierarchy that exposes
-   * an existing non-JsMethod inside a class.
+   * Returns {@code true} if this method is the first JsMember in the method hierarchy that exposes
+   * an existing non-JsMember inside a class.
    */
-  public boolean exposesNonJsMethod() {
-    if (isInterfaceMethod() || !isOrOverridesJsMethod()) {
+  public boolean exposesNonJsMember() {
+    if (isInterfaceMethod() || getJsMemberType() == JsMemberType.NONE) {
       return false;
     }
 
-    boolean hasNonJsMethodParent = false;
+    boolean hasNonJsMemberParent = false;
     for (JMethod overriddenMethod : overriddenMethods) {
-      if (!overriddenMethod.isOrOverridesJsMethod()) {
-        hasNonJsMethodParent = true;
+      if (overriddenMethod.getJsMemberType() == JsMemberType.NONE) {
+        hasNonJsMemberParent = true;
       }
-      if (overriddenMethod.exposesNonJsMethod()) {
+      if (overriddenMethod.exposesNonJsMember()) {
         return false; // some other method already exposed this method.
       }
     }
 
-    return hasNonJsMethodParent;
+    return hasNonJsMemberParent;
   }
 
-  public boolean isOrOverridesJsMethod() {
-    if (jsName != null) {
-      return true;
+  @Override
+  public JsMemberType getJsMemberType() {
+    if (jsMemberType != JsMemberType.NONE) {
+      return jsMemberType;
     }
     for (JMethod overriddenMethod : getOverriddenMethods()) {
-      if (overriddenMethod.jsName != null) {
-        return true;
+      if (overriddenMethod.jsMemberType != JsMemberType.NONE) {
+        return overriddenMethod.jsMemberType;
       }
     }
-    return false;
-  }
-
-  public void setJsPropertyInfo(String jsName, JsPropertyAccessorType jsPropertyType) {
-    this.jsName = jsName != null ? jsName : jsPropertyType.computeName(this);
-    this.jsPropertyType = jsPropertyType;
-  }
-
-  public JsPropertyAccessorType getJsPropertyAccessorType() {
-    if (isJsPropertyAccessor()) {
-      return jsPropertyType;
-    }
-    for (JMethod overriddenMethod : getOverriddenMethods()) {
-      if (overriddenMethod.isJsPropertyAccessor()) {
-        return overriddenMethod.jsPropertyType;
-      }
-    }
-    return JsPropertyAccessorType.NONE;
-  }
-
-  private boolean isJsPropertyAccessor() {
-    return jsPropertyType != JsPropertyAccessorType.NONE;
+    return JsMemberType.NONE;
   }
 
   private boolean isJsFunctionMethod() {
@@ -284,6 +197,7 @@ public class JMethod extends JNode implements JMember, CanBeAbstract, CanHaveSup
     return (isAbstract || body == null) && jsName != null;
   }
 
+  @Override
   public boolean isJsOverlay() {
     return isJsOverlay || (getEnclosingType() != null && getEnclosingType().isJsoType());
   }
@@ -690,6 +604,7 @@ public class JMethod extends JNode implements JMember, CanBeAbstract, CanHaveSup
     }
   }
 
+  @Override
   public boolean isPrivate() {
     return access == AccessModifier.PRIVATE.ordinal();
   }
@@ -704,6 +619,7 @@ public class JMethod extends JNode implements JMember, CanBeAbstract, CanHaveSup
     return isStatic;
   }
 
+  @Override
   public boolean isSynthetic() {
     return isSynthetic;
   }

dev/core/src/com/google/gwt/dev/jjs/impl/ControlFlowAnalyzer.java
@@ -616,7 +616,7 @@ public class ControlFlowAnalyzer {
           // Returning from this method passes a value from JavaScript into Java.
           maybeRescueJavaScriptObjectPassingIntoJava(method.getType());
         }
-        if (method.canBeCalledExternally() || method.canBeImplementedExternally()) {
+        if (method.canBeReferencedExternally() || method.canBeImplementedExternally()) {
           for (JParameter param : method.getParams()) {
             // Parameters in JsExport, JsType, JsFunction methods should not be pruned in order to
             // keep the API intact.
@@ -683,7 +683,7 @@ public class ControlFlowAnalyzer {
       JDeclaredType declaredType = (JDeclaredType) type;
 
       for (JMethod method : declaredType.getMethods()) {
-        if (method.canBeCalledExternally()) {
+        if (method.canBeReferencedExternally()) {
           rescue(method);
         }
       }

dev/core/src/com/google/gwt/dev/jjs/impl/ControlFlowRecorder.java
@@ -137,7 +137,7 @@ public class ControlFlowRecorder extends JVisitor {
           overriddenMethodName);
     }
 
-    if (x.canBeCalledExternally()) {
+    if (x.canBeReferencedExternally()) {
       stringAnalyzableTypeEnvironment.recordExportedMethodInType(currentMethodName, typeName);
     }
 

commit 51f96f25d7768fd7907b6a1300f60cfe85b0c3d4

Restore some behavior for old annotations.
The regression was introduced in
64e3270a9a02eea66fb9beab9e423a14f60b6cec

Change-Id: I3f27ea193f54b50ed8d1b1a69098f42c4274cb32

dev/core/src/com/google/gwt/dev/javac/JsInteropUtil.java
@@ -54,7 +54,8 @@ public final class JsInteropUtil {
       exportName = jsPrototype.substring(indexOf + 1);
     }
     boolean isJsType = jsType != null;
-    boolean isClassWideExport = JdtUtil.getAnnotation(annotations, JSEXPORT_CLASS) != null;
+    boolean isClassWideExport =
+        isJsNative || JdtUtil.getAnnotation(annotations, JSEXPORT_CLASS) != null;
     boolean isJsFunction = JdtUtil.getAnnotation(annotations, JSFUNCTION_CLASS) != null;
     boolean canBeImplementedExternally =
         (type instanceof JInterfaceType && (isJsType || isJsFunction))
@@ -116,18 +117,20 @@ public final class JsInteropUtil {
   private static void setJsInteropProperties(
       JMember member, Annotation[] annotations, boolean isPropertyAccessor) {
     boolean hasExport = JdtUtil.getAnnotation(annotations, JSEXPORT_CLASS) != null;
+    JsMemberType memberType = getJsMemberType(member, isPropertyAccessor);
+    String namespace = maybeGetJsNamespace(annotations);
+    String exportName = maybeGetJsExportName(annotations);
+    if (hasExport) {
+      member.setJsMemberInfo(memberType, namespace, exportName, true);
+      return;
+    }
 
     /* Apply class wide JsInterop annotations */
 
     boolean ignore = JdtUtil.getAnnotation(annotations, JSNOEXPORT_CLASS) != null;
-    if (ignore || (!member.isPublic() && !isNativeConstructor(member)) || !hasExport) {
+    if (ignore || (!member.isPublic() && !isNativeConstructor(member)) || hasExport) {
       return;
     }
-
-    String namespace = maybeGetJsNamespace(annotations);
-    String exportName = maybeGetJsExportName(annotations);
-    JsMemberType memberType = getJsMemberType(member, isPropertyAccessor);
-    member.setJsMemberInfo(memberType, namespace, exportName, hasExport);
 
     JDeclaredType enclosingType = member.getEnclosingType();
 