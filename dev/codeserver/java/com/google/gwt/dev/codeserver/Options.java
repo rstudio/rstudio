gwtproject/gwt

Enables JsInterop by default & introduces -generateJsInteropExports.
Change-Id: I294517a48ced6002a570b64e5cbd33ab9f6fb173
Review-Link: https://gwt-review.googlesource.com/#/c/13773/

dev/codeserver/java/com/google/gwt/dev/codeserver/Options.java
@@ -21,6 +21,7 @@ import com.google.gwt.dev.ArgProcessorBase;
 import com.google.gwt.dev.cfg.ModuleDef;
 import com.google.gwt.dev.jjs.JsOutputOption;
 import com.google.gwt.dev.util.arg.ArgHandlerClosureFormattedOutput;
+import com.google.gwt.dev.util.arg.ArgHandlerGenerateExports;
 import com.google.gwt.dev.util.arg.ArgHandlerIncrementalCompile;
 import com.google.gwt.dev.util.arg.ArgHandlerJsInteropMode;
 import com.google.gwt.dev.util.arg.ArgHandlerLogLevel;
@@ -29,6 +30,7 @@ import com.google.gwt.dev.util.arg.ArgHandlerScriptStyle;
 import com.google.gwt.dev.util.arg.ArgHandlerSetProperties;
 import com.google.gwt.dev.util.arg.ArgHandlerSourceLevel;
 import com.google.gwt.dev.util.arg.OptionClosureFormattedOutput;
+import com.google.gwt.dev.util.arg.OptionGenerateExports;
 import com.google.gwt.dev.util.arg.OptionIncrementalCompile;
 import com.google.gwt.dev.util.arg.OptionJsInteropMode;
 import com.google.gwt.dev.util.arg.OptionLogLevel;
@@ -86,7 +88,8 @@ public class Options {
   private SourceLevel sourceLevel = SourceLevel.DEFAULT_SOURCE_LEVEL;
   private boolean failOnError = false;
   private int compileTestRecompiles = 0;
-  private OptionJsInteropMode.Mode jsInteropMode = OptionJsInteropMode.Mode.NONE;
+  private OptionJsInteropMode.Mode jsInteropMode = OptionJsInteropMode.Mode.JS_RC;
+  private boolean generateJsInteropExports = false;
   private OptionMethodNameDisplayMode.Mode methodNameDisplayMode =
       OptionMethodNameDisplayMode.Mode.NONE;
   private boolean closureFormattedOutput = false;
@@ -306,6 +309,10 @@ public class Options {
     return jsInteropMode;
   }
 
+  boolean shouldGenerateJsInteropExports() {
+    return generateJsInteropExports;
+  }
+
   JsOutputOption getOutput() {
     if (output == null) {
       return isIncrementalCompileEnabled() ? JsOutputOption.OBFUSCATED : JsOutputOption.PRETTY;
@@ -402,6 +409,17 @@ public class Options {
         public void setJsInteropMode(OptionJsInteropMode.Mode mode) {
           Options.this.jsInteropMode = mode;
         }}));
+      registerHandler(new ArgHandlerGenerateExports(new OptionGenerateExports() {
+        @Override
+        public boolean shouldGenerateJsInteropExports() {
+          return Options.this.generateJsInteropExports;
+        }
+
+        @Override
+        public void setGenerateJsInteropExports(boolean generateExports) {
+          Options.this.generateJsInteropExports = generateExports;
+        }
+      }));
       registerHandler(new ArgHandlerMethodNameDisplayMode(new OptionMethodNameDisplayMode() {
         @Override
         public OptionMethodNameDisplayMode.Mode getMethodNameDisplayMode() {
@@ -412,8 +430,7 @@ public class Options {
         public void setMethodNameDisplayMode(Mode mode) {
           Options.this.methodNameDisplayMode = mode;
         }
-      }) {
-      });
+      }));
       registerHandler(new ArgHandlerClosureFormattedOutput(new OptionClosureFormattedOutput() {
         @Override
         public boolean isClosureCompilerFormatEnabled() {

dev/core/src/com/google/gwt/dev/DevMode.java
@@ -35,6 +35,7 @@ import com.google.gwt.dev.util.Util;
 import com.google.gwt.dev.util.arg.ArgHandlerDeployDir;
 import com.google.gwt.dev.util.arg.ArgHandlerDisableUpdateCheck;
 import com.google.gwt.dev.util.arg.ArgHandlerExtraDir;
+import com.google.gwt.dev.util.arg.ArgHandlerGenerateExports;
 import com.google.gwt.dev.util.arg.ArgHandlerIncrementalCompile;
 import com.google.gwt.dev.util.arg.ArgHandlerJsInteropMode;
 import com.google.gwt.dev.util.arg.ArgHandlerMethodNameDisplayMode;
@@ -247,6 +248,7 @@ public class DevMode extends DevModeBase implements RestartServerCallback {
       registerHandler(new ArgHandlerMethodNameDisplayMode(options));
       registerHandler(new ArgHandlerSourceLevel(options));
       registerHandler(new ArgHandlerJsInteropMode(options));
+      registerHandler(new ArgHandlerGenerateExports(options));
       registerHandler(new ArgHandlerIncrementalCompile(options));
       registerHandler(new ArgHandlerModuleName(options) {
         @Override

dev/core/src/com/google/gwt/dev/PrecompileTaskArgProcessor.java
@@ -37,6 +37,7 @@ import com.google.gwt.dev.util.arg.ArgHandlerEnableClosureCompiler;
 import com.google.gwt.dev.util.arg.ArgHandlerFragmentCount;
 import com.google.gwt.dev.util.arg.ArgHandlerFragmentMerge;
 import com.google.gwt.dev.util.arg.ArgHandlerGenDir;
+import com.google.gwt.dev.util.arg.ArgHandlerGenerateExports;
 import com.google.gwt.dev.util.arg.ArgHandlerIncrementalCompileWarnings;
 import com.google.gwt.dev.util.arg.ArgHandlerJsInteropMode;
 import com.google.gwt.dev.util.arg.ArgHandlerJsonSoyc;
@@ -81,6 +82,7 @@ class PrecompileTaskArgProcessor extends CompileArgProcessor {
     registerHandler(new ArgHandlerGenDir(options));
     registerHandler(new ArgHandlerIncrementalCompileWarnings());
     registerHandler(new ArgHandlerJsInteropMode(options));
+    registerHandler(new ArgHandlerGenerateExports(options));
     registerHandler(new ArgHandlerMethodNameDisplayMode(options));
     registerHandler(new ArgHandlerMissingDepsFile());
     registerHandler(new ArgHandlerNamespace(options));

dev/core/src/com/google/gwt/dev/jjs/JJSOptions.java
@@ -24,6 +24,7 @@ import com.google.gwt.dev.util.arg.OptionEnableAssertions;
 import com.google.gwt.dev.util.arg.OptionEnableClosureCompiler;
 import com.google.gwt.dev.util.arg.OptionFragmentCount;
 import com.google.gwt.dev.util.arg.OptionFragmentsMerge;
+import com.google.gwt.dev.util.arg.OptionGenerateExports;
 import com.google.gwt.dev.util.arg.OptionIncrementalCompile;
 import com.google.gwt.dev.util.arg.OptionInlineLiteralParameters;
 import com.google.gwt.dev.util.arg.OptionJsInteropMode;
@@ -54,7 +55,7 @@ public interface JJSOptions extends OptionOptimize,
     OptionRemoveDuplicateFunctions, OptionStrict,
     OptionSoycHtmlDisabled, OptionEnableClosureCompiler,
     OptionFragmentsMerge, OptionFragmentCount, OptionSourceLevel, OptionNamespace,
-    OptionCheckedMode, OptionJsInteropMode, OptionUseDetailedTypeIds,
+    OptionCheckedMode, OptionJsInteropMode, OptionGenerateExports, OptionUseDetailedTypeIds,
     OptionAllowJDTConstantInlining, OptionMethodNameDisplayMode,
     OptionClosureFormattedOutput {
 }

user/src/com/google/gwt/junit/JUnitShell.java
@@ -54,6 +54,7 @@ import com.google.gwt.dev.util.arg.ArgHandlerEnableAssertions;
 import com.google.gwt.dev.util.arg.ArgHandlerEnableClosureCompiler;
 import com.google.gwt.dev.util.arg.ArgHandlerExtraDir;
 import com.google.gwt.dev.util.arg.ArgHandlerGenDir;
+import com.google.gwt.dev.util.arg.ArgHandlerGenerateExports;
 import com.google.gwt.dev.util.arg.ArgHandlerIncrementalCompile;
 import com.google.gwt.dev.util.arg.ArgHandlerJsInteropMode;
 import com.google.gwt.dev.util.arg.ArgHandlerLocalWorkers;
@@ -290,6 +291,7 @@ public class JUnitShell extends DevMode {
       registerHandler(new ArgHandlerOptimize(options));
       registerHandler(new ArgHandlerIncrementalCompile(options));
       registerHandler(new ArgHandlerJsInteropMode(options));
+      registerHandler(new ArgHandlerGenerateExports(options));
       registerHandler(new ArgHandlerSetProperties(options));
       registerHandler(new ArgHandlerClosureFormattedOutput(options));
 

rstudio/rstudio

dev/codeserver/java/com/google/gwt/dev/codeserver/Options.java
@@ -21,6 +21,7 @@ import com.google.gwt.dev.ArgProcessorBase;
 import com.google.gwt.dev.cfg.ModuleDef;
 import com.google.gwt.dev.jjs.JsOutputOption;
 import com.google.gwt.dev.util.arg.ArgHandlerClosureFormattedOutput;
+import com.google.gwt.dev.util.arg.ArgHandlerGenerateJsInteropExports;
 import com.google.gwt.dev.util.arg.ArgHandlerIncrementalCompile;
 import com.google.gwt.dev.util.arg.ArgHandlerJsInteropMode;
 import com.google.gwt.dev.util.arg.ArgHandlerLogLevel;
@@ -29,6 +30,7 @@ import com.google.gwt.dev.util.arg.ArgHandlerScriptStyle;
 import com.google.gwt.dev.util.arg.ArgHandlerSetProperties;
 import com.google.gwt.dev.util.arg.ArgHandlerSourceLevel;
 import com.google.gwt.dev.util.arg.OptionClosureFormattedOutput;
+import com.google.gwt.dev.util.arg.OptionGenerateJsInteropExports;
 import com.google.gwt.dev.util.arg.OptionIncrementalCompile;
 import com.google.gwt.dev.util.arg.OptionJsInteropMode;
 import com.google.gwt.dev.util.arg.OptionLogLevel;
@@ -86,7 +88,8 @@ public class Options {
   private SourceLevel sourceLevel = SourceLevel.DEFAULT_SOURCE_LEVEL;
   private boolean failOnError = false;
   private int compileTestRecompiles = 0;
-  private OptionJsInteropMode.Mode jsInteropMode = OptionJsInteropMode.Mode.NONE;
+  private OptionJsInteropMode.Mode jsInteropMode = OptionJsInteropMode.Mode.JS_RC;
+  private boolean generateJsInteropExports = false;
   private OptionMethodNameDisplayMode.Mode methodNameDisplayMode =
       OptionMethodNameDisplayMode.Mode.NONE;
   private boolean closureFormattedOutput = false;
@@ -306,6 +309,10 @@ public class Options {
     return jsInteropMode;
   }
 
+  boolean shouldGenerateJsInteropExports() {
+    return generateJsInteropExports;
+  }
+
   JsOutputOption getOutput() {
     if (output == null) {
       return isIncrementalCompileEnabled() ? JsOutputOption.OBFUSCATED : JsOutputOption.PRETTY;
@@ -402,6 +409,17 @@ public class Options {
         public void setJsInteropMode(OptionJsInteropMode.Mode mode) {
           Options.this.jsInteropMode = mode;
         }}));
+      registerHandler(new ArgHandlerGenerateJsInteropExports(new OptionGenerateJsInteropExports() {
+        @Override
+        public boolean shouldGenerateJsInteropExports() {
+          return Options.this.generateJsInteropExports;
+        }
+
+        @Override
+        public void setGenerateJsInteropExports(boolean generateExports) {
+          Options.this.generateJsInteropExports = generateExports;
+        }
+      }));
       registerHandler(new ArgHandlerMethodNameDisplayMode(new OptionMethodNameDisplayMode() {
         @Override
         public OptionMethodNameDisplayMode.Mode getMethodNameDisplayMode() {
@@ -412,8 +430,7 @@ public class Options {
         public void setMethodNameDisplayMode(Mode mode) {
           Options.this.methodNameDisplayMode = mode;
         }
-      }) {
-      });
+      }));
       registerHandler(new ArgHandlerClosureFormattedOutput(new OptionClosureFormattedOutput() {
         @Override
         public boolean isClosureCompilerFormatEnabled() {

dev/core/src/com/google/gwt/dev/DevMode.java
@@ -35,6 +35,7 @@ import com.google.gwt.dev.util.Util;
 import com.google.gwt.dev.util.arg.ArgHandlerDeployDir;
 import com.google.gwt.dev.util.arg.ArgHandlerDisableUpdateCheck;
 import com.google.gwt.dev.util.arg.ArgHandlerExtraDir;
+import com.google.gwt.dev.util.arg.ArgHandlerGenerateJsInteropExports;
 import com.google.gwt.dev.util.arg.ArgHandlerIncrementalCompile;
 import com.google.gwt.dev.util.arg.ArgHandlerJsInteropMode;
 import com.google.gwt.dev.util.arg.ArgHandlerMethodNameDisplayMode;
@@ -247,6 +248,7 @@ public class DevMode extends DevModeBase implements RestartServerCallback {
       registerHandler(new ArgHandlerMethodNameDisplayMode(options));
       registerHandler(new ArgHandlerSourceLevel(options));
       registerHandler(new ArgHandlerJsInteropMode(options));
+      registerHandler(new ArgHandlerGenerateJsInteropExports(options));
       registerHandler(new ArgHandlerIncrementalCompile(options));
       registerHandler(new ArgHandlerModuleName(options) {
         @Override

dev/core/src/com/google/gwt/dev/PrecompileTaskArgProcessor.java
@@ -37,6 +37,7 @@ import com.google.gwt.dev.util.arg.ArgHandlerEnableClosureCompiler;
 import com.google.gwt.dev.util.arg.ArgHandlerFragmentCount;
 import com.google.gwt.dev.util.arg.ArgHandlerFragmentMerge;
 import com.google.gwt.dev.util.arg.ArgHandlerGenDir;
+import com.google.gwt.dev.util.arg.ArgHandlerGenerateJsInteropExports;
 import com.google.gwt.dev.util.arg.ArgHandlerIncrementalCompileWarnings;
 import com.google.gwt.dev.util.arg.ArgHandlerJsInteropMode;
 import com.google.gwt.dev.util.arg.ArgHandlerJsonSoyc;
@@ -81,6 +82,7 @@ class PrecompileTaskArgProcessor extends CompileArgProcessor {
     registerHandler(new ArgHandlerGenDir(options));
     registerHandler(new ArgHandlerIncrementalCompileWarnings());
     registerHandler(new ArgHandlerJsInteropMode(options));
+    registerHandler(new ArgHandlerGenerateJsInteropExports(options));
     registerHandler(new ArgHandlerMethodNameDisplayMode(options));
     registerHandler(new ArgHandlerMissingDepsFile());
     registerHandler(new ArgHandlerNamespace(options));

dev/core/src/com/google/gwt/dev/jjs/JJSOptions.java
@@ -24,6 +24,7 @@ import com.google.gwt.dev.util.arg.OptionEnableAssertions;
 import com.google.gwt.dev.util.arg.OptionEnableClosureCompiler;
 import com.google.gwt.dev.util.arg.OptionFragmentCount;
 import com.google.gwt.dev.util.arg.OptionFragmentsMerge;
+import com.google.gwt.dev.util.arg.OptionGenerateJsInteropExports;
 import com.google.gwt.dev.util.arg.OptionIncrementalCompile;
 import com.google.gwt.dev.util.arg.OptionInlineLiteralParameters;
 import com.google.gwt.dev.util.arg.OptionJsInteropMode;
@@ -54,7 +55,7 @@ public interface JJSOptions extends OptionOptimize,
     OptionRemoveDuplicateFunctions, OptionStrict,
     OptionSoycHtmlDisabled, OptionEnableClosureCompiler,
     OptionFragmentsMerge, OptionFragmentCount, OptionSourceLevel, OptionNamespace,
-    OptionCheckedMode, OptionJsInteropMode, OptionUseDetailedTypeIds,
+    OptionCheckedMode, OptionJsInteropMode, OptionGenerateJsInteropExports, OptionUseDetailedTypeIds,
     OptionAllowJDTConstantInlining, OptionMethodNameDisplayMode,
     OptionClosureFormattedOutput {
 }

user/src/com/google/gwt/junit/JUnitShell.java
@@ -54,6 +54,7 @@ import com.google.gwt.dev.util.arg.ArgHandlerEnableAssertions;
 import com.google.gwt.dev.util.arg.ArgHandlerEnableClosureCompiler;
 import com.google.gwt.dev.util.arg.ArgHandlerExtraDir;
 import com.google.gwt.dev.util.arg.ArgHandlerGenDir;
+import com.google.gwt.dev.util.arg.ArgHandlerGenerateJsInteropExports;
 import com.google.gwt.dev.util.arg.ArgHandlerIncrementalCompile;
 import com.google.gwt.dev.util.arg.ArgHandlerJsInteropMode;
 import com.google.gwt.dev.util.arg.ArgHandlerLocalWorkers;
@@ -290,6 +291,7 @@ public class JUnitShell extends DevMode {
       registerHandler(new ArgHandlerOptimize(options));
       registerHandler(new ArgHandlerIncrementalCompile(options));
       registerHandler(new ArgHandlerJsInteropMode(options));
+      registerHandler(new ArgHandlerGenerateJsInteropExports(options));
       registerHandler(new ArgHandlerSetProperties(options));
       registerHandler(new ArgHandlerClosureFormattedOutput(options));
 