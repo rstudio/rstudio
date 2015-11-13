e3e7cfc456b63ed007ca2b0a5d57d7b32da75ffa
Randy Lai <randy.cs.lai@gmail.com>
2015-11-13 15:46:25 -0500

    add applescript support

---
+++ src/cpp/desktop-mac/ScriptCommand.h
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+@interface doJavaScriptCommand : NSScriptCommand
+
+-(id) performDefaultImplementation;
+
+@end
+
+@interface doRscriptCommand : NSScriptCommand
+
+-(id) performDefaultImplementation;
+
+@end
---
+++ src/cpp/desktop-mac/ScriptCommand.mm
@@ -0,0 +1,47 @@
+#import <Foundation/Foundation.h>
+#import "ScriptCommand.h"
+#import "MainFrameController.h"
+
+
+@implementation doJavaScriptCommand
+
+-(id) performDefaultImplementation
+{
+    NSString *script = [self directParameter];
+    if (!script || [script isEqualToString:@""])
+        return [NSNumber numberWithBool:NO];
+   [[MainFrameController instance] evaluateJavaScript: script];
+
+   return [NSNumber numberWithBool:YES];
+}
+
+@end
+
+
+@implementation doRscriptCommand
+
+-(id) performDefaultImplementation
+{
+    NSString *script = [self directParameter];
+    if (!script || [script isEqualToString:@""])
+        return [NSNumber numberWithBool:NO];
+    script = [script stringByReplacingOccurrencesOfString:@"\\" withString:@"\\\\"];
+    script = [script stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
+    script = [script stringByReplacingOccurrencesOfString:@"\n" withString:@"\\n"];
+    NSString *jscript = [NSString stringWithFormat:
+      @"var input = document.getElementById('rstudio_console_input'); \
+      var textarea = input.getElementsByTagName('textarea')[0]; \
+      textarea.value += \"%@\"; \
+      var e = document.createEvent('KeyboardEvent'); \
+      e.initKeyboardEvent('input'); \
+      textarea.dispatchEvent(e); \
+      var e = document.createEvent('KeyboardEvent'); \
+      e.initKeyboardEvent('keydown'); \
+      Object.defineProperty(e, 'keyCode', {'value' : 13}); \
+      input.dispatchEvent(e);", script];
+   [[MainFrameController instance] evaluateJavaScript: jscript];
+
+   return [NSNumber numberWithBool:YES];
+}
+
+@end
