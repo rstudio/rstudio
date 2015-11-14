4f5a235aa9a3f221bf73db3022f4fb74d0b4fe18
Randy Lai <randy.cs.lai@gmail.com>
2015-11-14 01:20:03 -0500

    add delays between successive cmd's

--- src/cpp/desktop-mac/ScriptCommand.mm
+++ src/cpp/desktop-mac/ScriptCommand.mm
@@ -14,16 +14,40 @@
     script = [script stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
     script = [script stringByReplacingOccurrencesOfString:@"\n" withString:@"\\n"];
     NSString *jscript = [NSString stringWithFormat:
-      @"var input = document.getElementById('rstudio_console_input'); \
-      var textarea = input.getElementsByTagName('textarea')[0]; \
-      textarea.value += \"%@\"; \
-      var e = document.createEvent('KeyboardEvent'); \
-      e.initKeyboardEvent('input'); \
-      textarea.dispatchEvent(e); \
-      var e = document.createEvent('KeyboardEvent'); \
-      e.initKeyboardEvent('keydown'); \
-      Object.defineProperty(e, 'keyCode', {'value' : 13}); \
-      input.dispatchEvent(e);", script];
+      @"if (typeof evaluateRscript == 'undefined') { \
+         var evaluateRscript = function(){ \
+              var queue = []; \
+              var running = 0; \
+              var run_queue = function(){ \
+                   if (queue.length > 0 && running == 0){ \
+                        running = 1; \
+                        evaluate(queue[0]); \
+                        queue.splice(0, 1); \
+                        setTimeout(function(){ \
+                             running = 0; \
+                             run_queue(); \
+                        }, 100); \
+                   } \
+              }; \
+              var evaluate = function(str){ \
+                   var input = document.getElementById('rstudio_console_input'); \
+                   var textarea = input.getElementsByTagName('textarea')[0]; \
+                   textarea.value += str; \
+                   var e = document.createEvent('KeyboardEvent'); \
+                   e.initKeyboardEvent('input'); \
+                   textarea.dispatchEvent(e); \
+                   var e = document.createEvent('KeyboardEvent'); \
+                   e.initKeyboardEvent('keydown'); \
+                   Object.defineProperty(e, 'keyCode', {'value' : 13}); \
+                   input.dispatchEvent(e); \
+              }; \
+              return function(str) { \
+                   queue.push(str); \
+                   run_queue(); \
+              } \
+         }(); \
+      }; \
+      evaluateRscript(\"%@\");", script];
    [[MainFrameController instance] evaluateJavaScript: jscript];
 
    return [NSNumber numberWithBool:YES];
