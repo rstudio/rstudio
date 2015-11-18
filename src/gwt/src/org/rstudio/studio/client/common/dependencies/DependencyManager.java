539c20f83635d1d16f0b106585d533c62d91bba0
JJ Allaire <jj@rstudio.org>
2015-11-18 05:50:25 -0500

    install knitr from source and add it's other dependencies explicitly (so they are installed as binaries)
    - need to do this because CRAN has not yet built the knitr 1.11 binary

--- src/gwt/src/org/rstudio/studio/client/common/dependencies/DependencyManager.java
+++ src/gwt/src/org/rstudio/studio/client/common/dependencies/DependencyManager.java
@@ -153,11 +153,17 @@ public class DependencyManager implements InstallShinyEvent.Handler
    private ArrayList<Dependency> rmarkdownDependencies()
    {
       ArrayList<Dependency> deps = new ArrayList<Dependency>();
-      deps.add(Dependency.cranPackage("knitr", "1.11"));
+      deps.add(Dependency.cranPackage("evaluate", "0.7.2"));
+      deps.add(Dependency.cranPackage("digest", "0.6"));
+      deps.add(Dependency.cranPackage("formatR", "1.1"));
+      deps.add(Dependency.cranPackage("highr", "0.3"));
+      deps.add(Dependency.cranPackage("markdown", "0.7"));
+      deps.add(Dependency.cranPackage("stringr", "0.6"));
       deps.add(Dependency.cranPackage("yaml", "2.1.5"));
       deps.add(Dependency.cranPackage("htmltools", "0.2.4"));
       deps.add(Dependency.cranPackage("caTools", "1.14"));
       deps.add(Dependency.cranPackage("bitops", "1.0-6"));
+      deps.add(Dependency.cranPackage("knitr", "1.11", true));
       deps.add(Dependency.cranPackage("rmarkdown", "0.8.1", true));
       return deps;
    }
