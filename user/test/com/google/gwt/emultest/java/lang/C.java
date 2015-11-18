22134798bf19fec84659f85865d1621f4a642a18
Daniel Kurka <dankurka@google.com>
2015-11-17 21:09:58 -0800

    Remove JSNI references in C.java.
    Change-Id: I2ecfd7b068c0c368aaa06e313dde8143e7d3fdd6
    Review-Link: https://gwt-review.googlesource.com/#/c/13989/

--- user/test/com/google/gwt/emultest/java/lang/C.java
+++ user/test/com/google/gwt/emultest/java/lang/C.java
@@ -15,8 +15,6 @@
  */
 package com.google.gwt.emultest.java.lang;
 
-import com.google.gwt.testing.TestUtils;
-
 import javaemul.internal.ArrayHelper;
 
 /**
@@ -43,25 +41,13 @@ public class C {
   public static final long LONG_VALUE = 1234567890123456L;
 
   public static String getLargeCharArrayString() {
-    if (TestUtils.isJvm()) {
-      StringBuffer buffer = new StringBuffer();
-      int len = numberOfCopies();
-      for (int i = 0; i < len; ++i) {
-        buffer.append(CHAR_ARRAY_STRING);
-      }
-      return buffer.toString();
+    StringBuffer buffer = new StringBuffer();
+    int len = numberOfCopies();
+    for (int i = 0; i < len; ++i) {
+      buffer.append(CHAR_ARRAY_STRING);
     }
-    return getLargeCharArrayString0();
+    return buffer.toString();
   }
-
-  private static native String getLargeCharArrayString0() /*-{
-    var value = ""
-    var len = @C::numberOfCopies()();
-    for (var i = 0; i < len; ++i) {
-      value += @C::CHAR_ARRAY_STRING;
-    }
-    return value;
-  }-*/;
 
   public static char[] getLargeCharArrayValue() {
     char[] largeCharArray;
