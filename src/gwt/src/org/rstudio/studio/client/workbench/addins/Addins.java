e93db29cf76644d767c59c5857b5e5a1c3502ae0
Kevin Ushey <kevinushey@gmail.com>
2015-12-03 11:44:42 -0800

    javascript objects for R Addins

---
+++ src/gwt/src/org/rstudio/studio/client/workbench/addins/Addins.java
@@ -0,0 +1,24 @@
+package org.rstudio.studio.client.workbench.addins;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+import org.rstudio.core.client.js.JsMap;
+
+public class Addins
+{
+   public static class RAddin extends JavaScriptObject
+   {
+      protected RAddin() {}
+      
+      public final native String getName() /*-{ return this["name"]; }-*/;
+      public final native String getPackage() /*-{ return this["package"]; }-*/;
+      public final native String getTitle() /*-{ return this["title"]; }-*/;
+      public final native String getDescription() /*-{ return this["description"]; }-*/;
+      public final native String getBinding() /*-{ return this["binding"]; }-*/;
+   }
+   
+   public static class RAddins extends JsMap<RAddin>
+   {
+      protected RAddins() {}
+   }
+}
