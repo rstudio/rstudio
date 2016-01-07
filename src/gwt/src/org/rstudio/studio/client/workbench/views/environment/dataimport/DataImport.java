dfa312d1cf99c6f821d86ffea712834f2c6614c9
Javier Luraschi <javierluraschi@hotmail.com>
2016-01-07 07:17:41 -0800

    add support to browse files and update path in data import dialog

--- src/gwt/src/org/rstudio/studio/client/workbench/views/environment/dataimport/DataImport.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/environment/dataimport/DataImport.java
@@ -1,27 +1,32 @@
 package org.rstudio.studio.client.workbench.views.environment.dataimport;
 
-import org.rstudio.core.client.widget.ThemedButton;
+import org.rstudio.core.client.widget.FileChooserTextBox;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiFactory;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
-public class DataImport extends Composite {
+public class DataImport extends Composite
+{
+   private static DataImportUiBinder uiBinder = GWT
+         .create(DataImportUiBinder.class);
 
-	private static DataImportUiBinder uiBinder = GWT.create(DataImportUiBinder.class);
+   interface DataImportUiBinder extends UiBinder<Widget, DataImport>
+   {
+   }
 
-	@UiField TextBox textResource_;
-	@UiField ThemedButton buttonBrowse_;
-	
-	interface DataImportUiBinder extends UiBinder<Widget, DataImport> {
-	}
-
-	public DataImport() {
-		initWidget(uiBinder.createAndBindUi(this));
-		setWidth("400px");
-	}
-
+   public DataImport()
+   {
+      initWidget(uiBinder.createAndBindUi(this));
+      setWidth("400px");
+   }
+   
+   @UiFactory
+   FileChooserTextBox makeSomeWidget() {
+      FileChooserTextBox fileChooserTextBox = new FileChooserTextBox("File/URL:", null);
+      fileChooserTextBox.setReadOnly(false);
+      return fileChooserTextBox;
+   }
 }
--- src/gwt/src/org/rstudio/studio/client/workbench/views/environment/dataimport/DataImportDialog.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/environment/dataimport/DataImportDialog.java
@@ -2,13 +2,16 @@ package org.rstudio.studio.client.workbench.views.environment.dataimport;
 
 import org.rstudio.core.client.widget.ModalDialog;
 import org.rstudio.core.client.widget.OperationWithInput;
+import org.rstudio.studio.client.common.FileDialogs;
+import org.rstudio.studio.client.workbench.WorkbenchContext;
+import org.rstudio.studio.client.workbench.model.RemoteFileSystemContext;
 
 import com.google.gwt.user.client.ui.Widget;
 
 public class DataImportDialog extends ModalDialog<String>
 {
-
-   public DataImportDialog(String caption, OperationWithInput<String> operation)
+   public DataImportDialog(String caption,
+                           OperationWithInput<String> operation)
    {
       super(caption, operation);
       setOkButtonCaption("Import");
