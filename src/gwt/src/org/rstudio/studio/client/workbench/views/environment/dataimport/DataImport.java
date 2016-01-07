cbfb09144d332bfeae6e5fbb8771307ac45fcfb9
Javier Luraschi <javierluraschi@hotmail.com>
2016-01-07 10:59:47 -0800

    add readr dependency check before opening data import dialog and simple code generation

--- src/gwt/src/org/rstudio/studio/client/workbench/views/environment/dataimport/DataImport.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/environment/dataimport/DataImport.java
@@ -1,3 +1,18 @@
+/*
+ * DataImport.java
+ *
+ * Copyright (C) 2009-16 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
 package org.rstudio.studio.client.workbench.views.environment.dataimport;
 
 import org.rstudio.core.client.widget.FileChooserTextBox;
@@ -5,14 +20,16 @@ import org.rstudio.core.client.widget.FileChooserTextBox;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiFactory;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class DataImport extends Composite
 {
    private static DataImportUiBinder uiBinder = GWT
          .create(DataImportUiBinder.class);
-
+   
    interface DataImportUiBinder extends UiBinder<Widget, DataImport>
    {
    }
@@ -28,5 +45,17 @@ public class DataImport extends Composite
       FileChooserTextBox fileChooserTextBox = new FileChooserTextBox("File/URL:", null);
       fileChooserTextBox.setReadOnly(false);
       return fileChooserTextBox;
+   }
+   
+   @UiField
+   FileChooserTextBox fileChooserTextBox_;
+   
+   public DataImportOptions getOptions()
+   {
+      DataImportOptions options = new DataImportOptions();
+      options.setDataName("dataset");
+      options.setImportLocation(fileChooserTextBox_.getText());
+      
+      return options;
    }
 }
--- src/gwt/src/org/rstudio/studio/client/workbench/views/environment/dataimport/DataImportDialog.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/environment/dataimport/DataImportDialog.java
@@ -1,31 +1,47 @@
+/*
+ * DataImportDialog.java
+ *
+ * Copyright (C) 2009-16 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
 package org.rstudio.studio.client.workbench.views.environment.dataimport;
 
 import org.rstudio.core.client.widget.ModalDialog;
 import org.rstudio.core.client.widget.OperationWithInput;
-import org.rstudio.studio.client.common.FileDialogs;
-import org.rstudio.studio.client.workbench.WorkbenchContext;
-import org.rstudio.studio.client.workbench.model.RemoteFileSystemContext;
 
 import com.google.gwt.user.client.ui.Widget;
 
-public class DataImportDialog extends ModalDialog<String>
+public class DataImportDialog extends ModalDialog<DataImportOptions>
 {
+   private DataImport dataImport_;
+   
    public DataImportDialog(String caption,
-                           OperationWithInput<String> operation)
+                           OperationWithInput<DataImportOptions> operation)
    {
       super(caption, operation);
       setOkButtonCaption("Import");
+      
+      dataImport_ = new DataImport();
    }
 
    @Override
-   protected String collectInput()
+   protected DataImportOptions collectInput()
    {
-      return null;
+      return dataImport_.getOptions();
    }
 
    @Override
    protected Widget createMainWidget()
    {
-      return new DataImport();
+      return dataImport_;
    }
 }
