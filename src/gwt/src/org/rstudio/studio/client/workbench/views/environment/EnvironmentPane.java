17cddda0953b0985c1d083e639d78bbad717a685
JJ Allaire <jj@rstudio.org>
2016-01-20 08:32:40 -0500

    command management for new data import commands

--- src/gwt/src/org/rstudio/studio/client/workbench/views/environment/EnvironmentPane.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/environment/EnvironmentPane.java
@@ -416,15 +416,9 @@ public class EnvironmentPane extends WorkbenchPane
    {
       ToolbarPopupMenu menu = new ToolbarPopupMenu();
       
-      if (prefs_.useDataImport().getValue())
-      {
-         menu.addItem(commands_.importDatasetFromCSV().createMenuItem(false));
-      }
-      else
-      {
-         menu.addItem(commands_.importDatasetFromFile().createMenuItem(false));
-         menu.addItem(commands_.importDatasetFromURL().createMenuItem(false));
-      }
+      menu.addItem(commands_.importDatasetFromCSV().createMenuItem(false));
+      menu.addItem(commands_.importDatasetFromFile().createMenuItem(false));
+      menu.addItem(commands_.importDatasetFromURL().createMenuItem(false));
       
       dataImportButton_ = new ToolbarButton(
               "Import Dataset",
