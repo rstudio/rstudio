8450f41953f51b3d1de22d59d50e0eed0f7a2a5b
Roberto Lublinerman <rluble@google.com>
2016-01-20 10:41:31 -0800

    Improve optimzed tests stack traces by @DoNotInline assertXXX.
    Change-Id: Ia10ed28321fca3b4ede7d47a5c5e5139eca893e5

--- user/super/com/google/gwt/junit/translatable/junit/framework/Assert.java
+++ user/super/com/google/gwt/junit/translatable/junit/framework/Assert.java
@@ -15,58 +15,73 @@
  */
 package junit.framework;
 
+import javaemul.internal.annotations.DoNotInline;
+
 /**
  * Translatable version of JUnit's <code>Assert</code>.
  */
 public class Assert {
+  @DoNotInline
   public static void assertEquals(boolean obj1, boolean obj2) {
     assertEquals("", obj1, obj2);
   }
 
+  @DoNotInline
   public static void assertEquals(byte obj1, byte obj2) {
     assertEquals("", obj1, obj2);
   }
 
+  @DoNotInline
   public static void assertEquals(char obj1, char obj2) {
     assertEquals("", obj1, obj2);
   }
 
+  @DoNotInline
   public static void assertEquals(double obj1, double obj2, double delta) {
     assertEquals("", obj1, obj2, delta);
   }
 
+  @DoNotInline
   public static void assertEquals(float obj1, float obj2, float delta) {
     assertEquals("", obj1, obj2, delta);
   }
 
+  @DoNotInline
   public static void assertEquals(int expected, int actual) {
     assertEquals("", expected, actual);
   }
 
+  @DoNotInline
   public static void assertEquals(long obj1, long obj2) {
     assertEquals("", obj1, obj2);
   }
 
+  @DoNotInline
   public static void assertEquals(Object obj1, Object obj2) {
     assertEquals("", obj1, obj2);
   }
 
+  @DoNotInline
   public static void assertEquals(short expected, short actual) {
     assertEquals("", expected, actual);
   }
 
+  @DoNotInline
   public static void assertEquals(String str, boolean obj1, boolean obj2) {
     assertEquals(str, Boolean.valueOf(obj1), Boolean.valueOf(obj2));
   }
 
+  @DoNotInline
   public static void assertEquals(String str, byte obj1, byte obj2) {
     assertEquals(str, Byte.valueOf(obj1), Byte.valueOf(obj2));
   }
 
+  @DoNotInline
   public static void assertEquals(String str, char obj1, char obj2) {
     assertEquals(str, Character.valueOf(obj1), Character.valueOf(obj2));
   }
 
+  @DoNotInline
   public static void assertEquals(String str, double obj1, double obj2,
       double delta) {
     if (obj1 == obj2) {
@@ -78,6 +93,7 @@ public class Assert {
     }
   }
 
+  @DoNotInline
   public static void assertEquals(String str, float obj1, float obj2,
       float delta) {
     if (obj1 == obj2) {
@@ -89,16 +105,19 @@ public class Assert {
     }
   }
 
+  @DoNotInline
   public static void assertEquals(String msg, int expected, int actual) {
     if (expected != actual) {
       failNotEquals(msg, expected, actual);
     }
   }
 
+  @DoNotInline
   public static void assertEquals(String str, long obj1, long obj2) {
     assertEquals(str, new Long(obj1), new Long(obj2));
   }
 
+  @DoNotInline
   public static void assertEquals(String msg, Object obj1, Object obj2) {
     if (obj1 == null && obj2 == null) {
       return;
@@ -111,40 +130,49 @@ public class Assert {
     failNotEquals(msg, obj1, obj2);
   }
 
+  @DoNotInline
   public static void assertEquals(String str, short obj1, short obj2) {
     assertEquals(str, Short.valueOf(obj1), Short.valueOf(obj2));
   }
 
+  @DoNotInline
   public static void assertEquals(String obj1, String obj2) {
     assertEquals("", obj1, obj2);
   }
 
+  @DoNotInline
   public static void assertEquals(String message, String expected, String actual) {
     assertEquals(message, (Object) expected, (Object) actual);
   }
 
+  @DoNotInline
   public static void assertFalse(boolean condition) {
     assertFalse(null, condition);
   }
 
+  @DoNotInline
   public static void assertFalse(String message, boolean condition) {
     assertEquals(message, false, condition);
   }
 
+  @DoNotInline
   public static void assertNotNull(Object obj) {
     assertNotNull(null, obj);
   }
 
+  @DoNotInline
   public static void assertNotNull(String msg, Object obj) {
     if (obj == null) {
       fail(concatMessages(msg, "expected: not null, actual: null"));
     }
   }
 
+  @DoNotInline
   public static void assertNotSame(Object obj1, Object obj2) {
     assertNotSame(null, obj1, obj2);
   }
 
+  @DoNotInline
   public static void assertNotSame(String msg, Object obj1, Object obj2) {
     if (obj1 != obj2) {
       return;
@@ -153,18 +181,22 @@ public class Assert {
     fail(concatMessages(msg, "expected: not same as " + format(obj1) + ", actual: same"));
   }
 
+  @DoNotInline
   public static void assertNull(Object obj) {
     assertNull(null, obj);
   }
 
+  @DoNotInline
   public static void assertNull(String msg, Object obj) {
     assertEquals(msg, null, obj);
   }
 
+  @DoNotInline
   public static void assertSame(Object obj1, Object obj2) {
     assertSame(null, obj1, obj2);
   }
 
+  @DoNotInline
   public static void assertSame(String msg, Object obj1, Object obj2) {
     if (obj1 == obj2) {
       return;
@@ -173,18 +205,22 @@ public class Assert {
     failNotSame(msg, obj1, obj2);
   }
 
+  @DoNotInline
   public static void assertTrue(boolean condition) {
     assertTrue(null, condition);
   }
 
+  @DoNotInline
   public static void assertTrue(String message, boolean condition) {
     assertEquals(message, true, condition);
   }
 
+  @DoNotInline
   public static void fail() {
     fail(null);
   }
 
+  @DoNotInline
   public static void fail(String message) {
     if (message == null) {
       message = "failed";
@@ -193,16 +229,19 @@ public class Assert {
     throw new AssertionFailedError(message);
   }
 
+  @DoNotInline
   public static void failNotEquals(String message, Object expected,
       Object actual) {
     fail(concatMessages(message, "expected: " + format(expected) + ", actual: " + format(actual)));
   }
 
+  @DoNotInline
   public static void failNotSame(String message, Object expected, Object actual) {
     fail(concatMessages(
         message, "expected: same as " + format(expected) + ", actual: " + format(actual)));
   }
 
+  @DoNotInline
   public static void failSame(String message) {
     fail(concatMessages(message, "expected: not same, actual: same"));
   }
