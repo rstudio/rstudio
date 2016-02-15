0ee079842400d085e11f653f5f8de41f6ebefae1
Thomas Broyer <t.broyer@gmail.com>
2016-02-15 11:31:22 +0100

    Uncomment test after JDT upgrade
    Change-Id: Ib2c93d7ab386ac560ca585f19815886b2c1f8d3a

--- dev/core/test/com/google/gwt/dev/jjs/impl/Java7AstTest.java
+++ dev/core/test/com/google/gwt/dev/jjs/impl/Java7AstTest.java
@@ -93,14 +93,8 @@ public class Java7AstTest extends JJSTestBase {
         "Object o = null; char s = (char) o;");
 
     assertEqualBlock(
-        "Object o = null; char s = (char) ((Character) o).charValue();",
-        "Object o = null; char s = (char) o;");
-
-    // This is the expected behaviour, however JDT 3.8.3 emits a compiler error.
-    // TODO(rluble): uncomment when JDT is updated.
-    // assertEqualBlock(
-    //    "Number o = 1; int s = (int) ((Integer) o).intValue();",
-    //    "Number o= 1 ; int s = (int) o;");
+        "Number o = null; int s = (int) ((Integer) o).intValue();",
+        "Number o = null; int s = (int) o;");
   }
 
   private void assertEqualExpression(String type, String expected, String expression)
