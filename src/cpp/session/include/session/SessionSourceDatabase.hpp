b7f0d5f315e42f391f81f6f399f38955cc357a4b
Jonathan McPherson <jonathan@rstudio.com>
2016-02-16 12:17:58 -0800

    implement reverse path-to-id lookup in source database

--- src/cpp/session/include/session/SessionSourceDatabase.hpp
+++ src/cpp/session/include/session/SessionSourceDatabase.hpp
@@ -190,6 +190,8 @@ core::Error remove(const std::string& id);
 core::Error removeAll();
 core::Error getPath(const std::string& id, std::string* pPath);
 core::Error getPath(const std::string& id, core::FilePath* pPath);
+core::Error getId(const std::string& path, std::string* pId);
+core::Error getId(const core::FilePath& path, std::string* pId);
 
 // source database events
 struct Events : boost::noncopyable
