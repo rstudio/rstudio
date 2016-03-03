0a5f70d3794f7ae759264884566d1162b50e5932
Jonathan McPherson <jonathan@rstudio.com>
2016-03-02 16:52:02 -0800

    vagrant: add ldap client provisioning script (WIP)

--- vagrant/provision-ldap-server.sh
+++ vagrant/provision-ldap-server.sh
@@ -19,3 +19,7 @@ apt-get install -y ldap-utils
 # populate users
 ldapadd -x -D cn=admin,dc=nodomain -w rsp-pass -f /rstudio/vagrant/rstudiousers.ldif
 
+# rewire RStudio Server instances to use newly provisioned server
+ssh -i /rstudio/vagrant/vagrant_key_rsa -o StrictHostKeyChecking=no vagrant@192.168.55.101 "sudo /rstudio/vagrant/provision-ldap-client.sh"
+ssh -i /rstudio/vagrant/vagrant_key_rsa -o StrictHostKeyChecking=no vagrant@192.168.55.102 "sudo /rstudio/vagrant/provision-ldap-client.sh"
+
