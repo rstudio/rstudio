8a403ba92288c81d9e82378c02d4249fcf7c4f7d
Jonathan McPherson <jonathan@rstudio.com>
2016-04-11 11:37:20 -0700

    add stubs for notebook error management module

--- src/cpp/session/modules/rmarkdown/SessionRmdNotebook.cpp
+++ src/cpp/session/modules/rmarkdown/SessionRmdNotebook.cpp
@@ -21,6 +21,7 @@
 #include "NotebookOutput.hpp"
 #include "NotebookHtmlWidgets.hpp"
 #include "NotebookExec.hpp"
+#include "NotebookErrors.hpp"
 
 #include <iostream>
 
@@ -240,7 +241,8 @@ Error initialize()
       (bind(module_context::sourceModuleRFile, "SessionRmdNotebook.R"))
       (bind(initOutput))
       (bind(initCache))
-      (bind(initHtmlWidgets));
+      (bind(initHtmlWidgets))
+      (bind(initErrors));
 
    return initBlock.execute();
 }
