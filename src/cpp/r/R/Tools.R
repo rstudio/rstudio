7009889d07b5a28df505fcd21bc1194a669acebe
Kevin Ushey <kevin@rstudio.com>
2016-04-28 12:10:56 -0700

    move to 'Tools.R'

--- src/cpp/r/R/Tools.R
+++ src/cpp/r/R/Tools.R
@@ -827,6 +827,44 @@ assign(envir = .rs.Env, ".rs.getVar", function(name)
    })()
 })
 
+.rs.addFunction("listBuilder", function()
+{
+   (function() {
+      capacity_ <- 1024
+      index_ <- 0
+      data_ <- vector("list", capacity_)
+      
+      append <- function(data) {
+         
+         # increment index and check capacity
+         index_ <<- index_ + 1
+         if (index_ > capacity_) {
+            capacity_ <<- capacity_ * 2
+            data_[capacity_] <<- list(NULL)
+         }
+         
+         # append data
+         if (is.null(data))
+            data_[index_] <<- list(NULL)
+         else
+            data_[[index_]] <<- data
+      }
+      
+      data <- function() {
+         data_[seq_len(index_)]
+      }
+      
+      clear <- function() {
+         capacity_ <<- 1024
+         index_ <<- 0
+         data_ <<- vector("list", capacity_)
+      }
+      
+      list(append = append, clear = clear, data = data)
+      
+   })()
+})
+
 .rs.addFunction("regexMatches", function(pattern, x) {
    matches <- gregexpr(pattern, x, perl = TRUE)[[1]]
    starts <- attr(matches, "capture.start")
