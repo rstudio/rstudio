gwtproject/gwt

Emulate new methods for java.util.Map in Java 8.
Change-Id: I492cb49faf2b839cf7939d3eabae3c2a3fdfeb54

user/super/com/google/gwt/emul/java/util/Map.java
@@ -15,6 +15,14 @@
  */
 package java.util;
 
+import static javaemul.internal.InternalPreconditions.checkCriticalNotNull;
+import static javaemul.internal.InternalPreconditions.checkNotNull;
+
+import java.io.Serializable;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 /**
  * Abstract interface for maps.
  *
@@ -26,7 +34,7 @@ public interface Map<K, V> {
   /**
    * Represents an individual map entry.
    */
-  public interface Entry<K, V> {
+  interface Entry<K, V> {
     @Override
     boolean equals(Object o);
 
@@ -38,9 +46,80 @@ public interface Map<K, V> {
     int hashCode();
 
     V setValue(V value);
+
+    static <K extends Comparable<? super K>, V> Comparator<Map.Entry<K,V>> comparingByKey() {
+      return comparingByKey(Comparator.naturalOrder());
+    }
+
+    static <K, V> Comparator<Map.Entry<K, V>> comparingByKey(Comparator<? super K> cmp) {
+      checkCriticalNotNull(cmp);
+      return (Comparator<Map.Entry<K, V>> & Serializable)
+          (a, b) -> cmp.compare(a.getKey(), b.getKey());
+    }
+
+    static <K, V extends Comparable<? super V>> Comparator<Map.Entry<K,V>> comparingByValue() {
+      return comparingByValue(Comparator.naturalOrder());
+    }
+
+    static <K, V> Comparator<Map.Entry<K, V>> comparingByValue(Comparator<? super V> cmp) {
+      return (Comparator<Map.Entry<K, V>> & Serializable)
+          (a, b) -> cmp.compare(a.getValue(), b.getValue());
+    }
   }
 
   void clear();
+
+  default V compute(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+    checkNotNull(remappingFunction);
+
+    V oldValue = get(key);
+    V newValue = remappingFunction.apply(key, oldValue);
+    if (newValue == null) {
+      if (oldValue != null || containsKey(key)) {
+        remove(key);
+      }
+      return null;
+    }
+
+    put(key, newValue);
+    return newValue;
+  }
+
+  default V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
+    checkNotNull(mappingFunction);
+
+    V oldValue = get(key);
+    if (oldValue != null) {
+      return oldValue;
+    }
+
+    V newValue = mappingFunction.apply(key);
+    if (newValue != null) {
+      put(key, newValue);
+      return newValue;
+    }
+
+    return null;
+  }
+
+  default V computeIfPresent(K key,
+                             BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+    checkNotNull(remappingFunction);
+
+    V oldValue = get(key);
+    if (oldValue == null) {
+      return null;
+    }
+
+    V newValue = remappingFunction.apply(key, oldValue);
+    if (newValue != null) {
+      put(key, newValue);
+      return newValue;
+    }
+
+    remove(key);
+    return null;
+  }
 
   boolean containsKey(Object key);
 
@@ -51,7 +130,19 @@ public interface Map<K, V> {
   @Override
   boolean equals(Object o);
 
+  default void forEach(BiConsumer<? super K, ? super V> consumer) {
+    checkNotNull(consumer);
+    for (Entry<K, V> entry : entrySet()) {
+      consumer.accept(entry.getKey(), entry.getValue());
+    }
+  }
+
   V get(Object key);
+
+  default V getOrDefault(Object key, V defaultValue) {
+    V currentValue = get(key);
+    return (currentValue == null && !containsKey(key)) ? defaultValue : currentValue;
+  }
 
   @Override
   int hashCode();
@@ -60,11 +151,60 @@ public interface Map<K, V> {
 
   Set<K> keySet();
 
+  default V merge(K key, V value, BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
+    checkNotNull(remappingFunction);
+    checkNotNull(value);
+    V currentValue = get(key);
+    V newValue = currentValue == null ? value : remappingFunction.apply(currentValue, value);
+    if (newValue == null) {
+      remove(key);
+    } else {
+      put(key, newValue);
+    }
+    return newValue;
+  }
+
   V put(K key, V value);
+
+  default V putIfAbsent(K key, V value) {
+    V currentValue = get(key);
+    return currentValue != null ? currentValue : put(key, value);
+  }
 
   void putAll(Map<? extends K, ? extends V> t);
 
   V remove(Object key);
+
+  default boolean remove(Object key, Object value) {
+    Object currentValue = get(key);
+    if (!Objects.equals(currentValue, value) || (currentValue == null && !containsKey(key))) {
+      return false;
+    }
+    remove(key);
+    return true;
+  }
+
+  default V replace(K key, V value) {
+    V currentValue = get(key);
+    return (currentValue == null && !containsKey(key)) ? null : put(key, value);
+  }
+
+  default boolean replace(K key, V oldValue, V newValue) {
+    Object currentValue = get(key);
+    if (!Objects.equals(currentValue, oldValue) || (currentValue == null && !containsKey(key))) {
+      return false;
+    }
+    put(key, newValue);
+    return true;
+  }
+
+  default void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
+    checkNotNull(function);
+    for (Entry<K, V> entry : entrySet()) {
+      V value = function.apply(entry.getKey(), entry.getValue());
+      entry.setValue(value);
+    }
+  }
 
   int size();
 

rstudio/rstudio

user/super/com/google/gwt/emul/java/util/Map.java
@@ -15,6 +15,14 @@
  */
 package java.util;
 
+import static javaemul.internal.InternalPreconditions.checkCriticalNotNull;
+import static javaemul.internal.InternalPreconditions.checkNotNull;
+
+import java.io.Serializable;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 /**
  * Abstract interface for maps.
  *
@@ -26,7 +34,7 @@ public interface Map<K, V> {
   /**
    * Represents an individual map entry.
    */
-  public interface Entry<K, V> {
+  interface Entry<K, V> {
     @Override
     boolean equals(Object o);
 
@@ -38,9 +46,70 @@ public interface Map<K, V> {
     int hashCode();
 
     V setValue(V value);
+
+    static <K extends Comparable<? super K>, V> Comparator<Map.Entry<K,V>> comparingByKey() {
+      return comparingByKey(Comparator.naturalOrder());
+    }
+
+    static <K, V> Comparator<Map.Entry<K, V>> comparingByKey(Comparator<? super K> cmp) {
+      checkCriticalNotNull(cmp);
+      return (Comparator<Map.Entry<K, V>> & Serializable)
+          (a, b) -> cmp.compare(a.getKey(), b.getKey());
+    }
+
+    static <K, V extends Comparable<? super V>> Comparator<Map.Entry<K,V>> comparingByValue() {
+      return comparingByValue(Comparator.naturalOrder());
+    }
+
+    static <K, V> Comparator<Map.Entry<K, V>> comparingByValue(Comparator<? super V> cmp) {
+      checkCriticalNotNull(cmp);
+      return (Comparator<Map.Entry<K, V>> & Serializable)
+          (a, b) -> cmp.compare(a.getValue(), b.getValue());
+    }
   }
 
   void clear();
+
+  default V compute(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+    checkNotNull(remappingFunction);
+
+    V value = remappingFunction.apply(key, get(key));
+    if (value != null) {
+      put(key, value);
+    } else {
+      remove(key);
+    }
+    return value;
+  }
+
+  default V computeIfAbsent(K key, Function<? super K, ? extends V> remappingFunction) {
+    checkNotNull(remappingFunction);
+
+    V value = get(key);
+    if (value == null) {
+      value = remappingFunction.apply(key);
+      if (value != null) {
+        put(key, value);
+      }
+    }
+    return value;
+  }
+
+  default V computeIfPresent(K key,
+                             BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+    checkNotNull(remappingFunction);
+
+    V value = get(key);
+    if (value != null) {
+      value = remappingFunction.apply(key, value);
+      if (value != null) {
+        put(key, value);
+      } else {
+        remove(key);
+      }
+    }
+    return value;
+  }
 
   boolean containsKey(Object key);
 
@@ -51,7 +120,19 @@ public interface Map<K, V> {
   @Override
   boolean equals(Object o);
 
+  default void forEach(BiConsumer<? super K, ? super V> consumer) {
+    checkNotNull(consumer);
+    for (Entry<K, V> entry : entrySet()) {
+      consumer.accept(entry.getKey(), entry.getValue());
+    }
+  }
+
   V get(Object key);
+
+  default V getOrDefault(Object key, V defaultValue) {
+    V currentValue = get(key);
+    return (currentValue == null && !containsKey(key)) ? defaultValue : currentValue;
+  }
 
   @Override
   int hashCode();
@@ -60,11 +141,59 @@ public interface Map<K, V> {
 
   Set<K> keySet();
 
+  default V merge(K key, V value, BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
+    checkNotNull(remappingFunction);
+    checkNotNull(value);
+
+    V currentValue = get(key);
+    V newValue = currentValue == null ? value : remappingFunction.apply(currentValue, value);
+    if (newValue == null) {
+      remove(key);
+    } else {
+      put(key, newValue);
+    }
+    return newValue;
+  }
+
   V put(K key, V value);
+
+  default V putIfAbsent(K key, V value) {
+    V currentValue = get(key);
+    return currentValue != null ? currentValue : put(key, value);
+  }
 
   void putAll(Map<? extends K, ? extends V> t);
 
   V remove(Object key);
+
+  default boolean remove(Object key, Object value) {
+    Object currentValue = get(key);
+    if (!Objects.equals(currentValue, value) || (currentValue == null && !containsKey(key))) {
+      return false;
+    }
+    remove(key);
+    return true;
+  }
+
+  default V replace(K key, V value) {
+    return containsKey(key) ? put(key, value) : null;
+  }
+
+  default boolean replace(K key, V oldValue, V newValue) {
+    Object currentValue = get(key);
+    if (!Objects.equals(currentValue, oldValue) || (currentValue == null && !containsKey(key))) {
+      return false;
+    }
+    put(key, newValue);
+    return true;
+  }
+
+  default void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
+    checkNotNull(function);
+    for (Entry<K, V> entry : entrySet()) {
+      entry.setValue(function.apply(entry.getKey(), entry.getValue()));
+    }
+  }
 
   int size();
 