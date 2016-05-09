8965927a1189efaf81c62688a4ae841525ec4942
Jonathan McPherson <jonathan@rstudio.com>
2016-05-09 13:25:16 -0700

    more precise value for plot size

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/rmd/TextEditingTargetNotebook.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/rmd/TextEditingTargetNotebook.java
@@ -1574,9 +1574,12 @@ public class TextEditingTargetNotebook
    
    private int getPlotWidth()
    {
-      // subtract some space to account for padding (10px on either side) and
-      // ensure the plot doesn't grow arbitrarily large
-      return Math.min(docDisplay_.getPixelWidth() - 30,
+      // subtract some space to account for padding; ensure the plot doesn't
+      // grow arbitrarily large. note that this value must total the amount of
+      // space outside the element (here, 2 * (10px margin + 1px border)); since
+      // we stretch the plot to fit the space it will scale in unpredictable
+      // ways if it doesn't fit exactly
+      return Math.min(docDisplay_.getPixelWidth() - 22,
                       ChunkOutputUi.MAX_PLOT_WIDTH);
    }
    
