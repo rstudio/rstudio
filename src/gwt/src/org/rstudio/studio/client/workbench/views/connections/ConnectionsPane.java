3d1dc6c72fc7aa99c2e96b427ca1ce37e5d91118
JJ Allaire <jj@rstudio.org>
2016-05-13 20:19:20 -0400

    stub in data grid for connections tab

--- src/gwt/src/org/rstudio/studio/client/workbench/views/connections/ConnectionsPane.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/connections/ConnectionsPane.java
@@ -12,12 +12,21 @@
  */
 package org.rstudio.studio.client.workbench.views.connections;
 
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.cellview.client.DataGrid;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.cellview.client.TextHeader;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.view.client.ProvidesKey;
+import com.google.gwt.view.client.SingleSelectionModel;
 import com.google.inject.Inject;
-import org.rstudio.core.client.widget.HorizontalCenterPanel;
+
+import org.rstudio.core.client.theme.RStudioDataGridResources;
+import org.rstudio.core.client.theme.RStudioDataGridStyle;
 import org.rstudio.core.client.widget.Toolbar;
 import org.rstudio.studio.client.workbench.ui.WorkbenchPane;
+import org.rstudio.studio.client.workbench.views.connections.model.Connection;
 
 public class ConnectionsPane extends WorkbenchPane implements ConnectionsPresenter.Display
 {
@@ -25,7 +34,49 @@ public class ConnectionsPane extends WorkbenchPane implements ConnectionsPresent
    public ConnectionsPane()
    {
       super("Connections");
+      
+      keyProvider_ = new ProvidesKey<Connection>() {
+         @Override
+         public Object getKey(Connection connection)
+         {
+            return connection.hashCode();
+         }
+      };
+      
+      selectionModel_ = new SingleSelectionModel<Connection>();
+      
+      connectionsDataGrid_ = new DataGrid<Connection>(1000, RES, keyProvider_);
+      connectionsDataGrid_.setSelectionModel(selectionModel_);
+     
+      typeColumn_ = new TextColumn<Connection>() {
+         @Override
+         public String getValue(Connection connection)
+         {
+            return connection.getType();
+         }
+      };
+      
+      connectionsDataGrid_.addColumn(typeColumn_, new TextHeader("Type"));
+      connectionsDataGrid_.setColumnWidth(typeColumn_, "20px");
+            
+      // Name ----
+      nameColumn_ = new TextColumn<Connection>() {
+         @Override
+         public String getValue(Connection connection)
+         {
+            return connection.getName();
+         }
+      };
+      
+      connectionsDataGrid_.addColumn(nameColumn_, new TextHeader("Name"));
+      connectionsDataGrid_.setColumnWidth(nameColumn_, "120px");
+      
+      
+      dataProvider_ = new ListDataProvider<Connection>();
+      dataProvider_.addDataDisplay(connectionsDataGrid_);
+      
       ensureWidget();
+      
    }
    
    @Override
@@ -38,8 +89,33 @@ public class ConnectionsPane extends WorkbenchPane implements ConnectionsPresent
    @Override 
    protected Widget createMainWidget()
    {
-      Label label = new Label("Under Construction");
-      label.getElement().getStyle().setColor("#888");
-      return new HorizontalCenterPanel(label, 100);
+      return connectionsDataGrid_;
+   }
+   
+   
+   private final DataGrid<Connection> connectionsDataGrid_; 
+   
+   private final TextColumn<Connection> typeColumn_;
+   private final TextColumn<Connection> nameColumn_;
+   
+   private final ProvidesKey<Connection> keyProvider_;
+   private final SingleSelectionModel<Connection> selectionModel_;
+   private final ListDataProvider<Connection> dataProvider_;
+   
+   // Resources, etc ----
+   public interface Resources extends RStudioDataGridResources
+   {
+      @Source({RStudioDataGridStyle.RSTUDIO_DEFAULT_CSS})
+      Styles dataGridStyle();
+   }
+   
+   public interface Styles extends RStudioDataGridStyle
+   {
+   }
+   
+   private static final Resources RES = GWT.create(Resources.class);
+   
+   static {
+      RES.dataGridStyle().ensureInjected();
    }
 }
