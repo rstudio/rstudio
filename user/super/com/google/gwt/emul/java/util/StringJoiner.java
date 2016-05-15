gwtproject/gwt

Fix failing tests for precondition checks in java8 classes.
Fixes test regressions introduced in f7c7e5

Change-Id: I0b049247078a21e90e066c135a5689db73f18d1f

user/super/com/google/gwt/emul/java/util/StringJoiner.java
@@ -15,6 +15,8 @@
  */
 package java.util;
 
+import static javaemul.internal.InternalPreconditions.checkNotNull;
+
 /**
  * See <a href="https://docs.oracle.com/javase/8/docs/api/java/util/StringJoiner.html">
  * the official Java API doc</a> for details.
@@ -33,6 +35,10 @@ public final class StringJoiner {
   }
 
   public StringJoiner(CharSequence delimiter, CharSequence prefix, CharSequence suffix) {
+    // TODO: null.toString() does not throw exception
+    checkNotNull(delimiter, "delimiter");
+    checkNotNull(prefix, "prefix");
+    checkNotNull(suffix, "suffix");
     this.delimiter = delimiter.toString();
     this.prefix = prefix.toString();
     this.suffix = suffix.toString();
@@ -64,6 +70,8 @@ public final class StringJoiner {
   }
 
   public StringJoiner setEmptyValue(CharSequence emptyValue) {
+    // TODO: null.toString() does not throw exception
+    checkNotNull(emptyValue);
     this.emptyValue = emptyValue.toString();
     return this;
   }

rstudio/rstudio

user/super/com/google/gwt/emul/java/util/StringJoiner.java
@@ -15,6 +15,8 @@
  */
 package java.util;
 
+import static javaemul.internal.InternalPreconditions.checkCriticalNotNull;
+
 /**
  * See <a href="https://docs.oracle.com/javase/8/docs/api/java/util/StringJoiner.html">
  * the official Java API doc</a> for details.
@@ -33,6 +35,11 @@ public final class StringJoiner {
   }
 
   public StringJoiner(CharSequence delimiter, CharSequence prefix, CharSequence suffix) {
+    // TODO: null.toString() does not throw exception
+    checkCriticalNotNull(delimiter, "delimiter");
+    checkCriticalNotNull(prefix, "prefix");
+    checkCriticalNotNull(suffix, "suffix");
+
     this.delimiter = delimiter.toString();
     this.prefix = prefix.toString();
     this.suffix = suffix.toString();
@@ -64,6 +71,9 @@ public final class StringJoiner {
   }
 
   public StringJoiner setEmptyValue(CharSequence emptyValue) {
+    // TODO: null.toString() does not throw exception
+    checkCriticalNotNull(emptyValue);
+
     this.emptyValue = emptyValue.toString();
     return this;
   }