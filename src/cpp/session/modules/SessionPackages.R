5adf442652ffdaaef506d203e37e935ba509d08f
Kevin Ushey <kevinushey@gmail.com>
2016-06-14 12:37:46 -0700

    cleanups re: package skeleton + devtools options

--- src/cpp/session/modules/SessionPackages.R
+++ src/cpp/session/modules/SessionPackages.R
@@ -601,29 +601,27 @@ if (identical(as.character(Sys.info()["sysname"]), "Darwin") &&
    # Protect against vectors with length > 1
    getDevtoolsOption <- function(optionName, default, collapse = " ")
    {
-      option <- getOption(optionName)
-      if (!is.null(option))
-      {
-         if (length(option) > 0)
-            paste(option, collapse = collapse)
-         else
-            option
-      }
-      else default
+      devtoolsDesc <- getOption("devtools.desc")
+      if (!length(devtoolsDesc))
+         return(default)
+      
+      option <- devtoolsDesc[[optionName]]
+      if (is.null(option))
+         return(default)
+      
+      paste(option, collapse = collapse)
    }
    
-   `%||%` <- function(x, y)
-      if (is.null(x)) y else x
    
-   Author <- getDevtoolsOption("devtools.name", "Who wrote it")
+   Author <- getDevtoolsOption("Author", "Who wrote it")
    
    Maintainer <- getDevtoolsOption(
-      "devtools.desc.author",
-      "Who to complain to <yourfault@somewhere.net>"
+      "Maintainer",
+      "The package maintainer <yourself@somewhere.net>"
    )
    
    License <- getDevtoolsOption(
-      "devtools.desc.license",
+      "License",
       "What license is it under?",
       ", "
    )
@@ -640,15 +638,6 @@ if (identical(as.character(Sys.info()["sysname"]), "Darwin") &&
       Encoding = "UTF-8",
       LazyData = "true"
    )
-   
-   # If we filled in an 'Authors@R' field for Authors (e.g. provided for new
-   # 'devtools'), then name this field as such.
-   if (isTRUE(grepl("^\\s*(?:as\\.person|person|c)\\(", DESCRIPTION[["Maintainer"]], perl = TRUE)))
-   {
-      maintainerIdx <- which(names(DESCRIPTION) == "Maintainer")
-      names(DESCRIPTION)[[maintainerIdx]] <- "Authors@R"
-      DESCRIPTION[["Author"]] <- NULL
-   }
    
    # Create a NAMESPACE file
    NAMESPACE <- c(
