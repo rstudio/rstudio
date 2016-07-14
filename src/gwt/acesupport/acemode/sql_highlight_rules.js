fe0d12c8adad16d34c45f25c6eed415c1ef7a2bb
Javier Luraschi <javierluraschi@hotmail.com>
2016-07-13 23:30:33 -0700

    add insert and into as sql highlight keywords

--- src/gwt/acesupport/acemode/sql_highlight_rules.js
+++ src/gwt/acesupport/acemode/sql_highlight_rules.js
@@ -27,7 +27,7 @@ define("mode/sql_highlight_rules", ["require", "exports", "module"], function(re
     var keywords = (
         "select|insert|update|delete|from|where|and|or|group|by|order|limit|offset|having|as|case|" +
         "when|else|end|type|left|right|join|on|outer|desc|asc|union|create|table|primary|key|if|" +
-        "foreign|not|references|default|null|inner|cross|natural|database|drop|grant"
+        "foreign|not|references|default|null|inner|cross|natural|database|drop|grant|into|values"
     );
 
     var builtinConstants = (
