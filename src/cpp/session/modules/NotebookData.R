decf426f7a56043aecac2747b3e8b504fd702ff1
Javier Luraschi <javierluraschi@hotmail.com>
2016-08-03 14:01:00 -0700

    add support for parameterized sql chunks and output values

--- src/cpp/session/modules/NotebookData.R
+++ src/cpp/session/modules/NotebookData.R
@@ -70,23 +70,77 @@
   )
 })
 
-.rs.addFunction("runSqlForDataCapture", function(query, outputFile, options)
+.rs.addFunction("runSqlForDataCapture", function(sql, outputFile, options)
 {
   # precreate directories if needed
   dir.create(dirname(outputFile), recursive = TRUE, showWarnings = FALSE)
 
-  maxRows <- if (is.null(options$max.print)) 1000 else as.numeric(options$max.print)
-  maxRows <- if (is.null(options$sql.max.print)) maxRows else as.numeric(options$sql.max.print)
+  max.print <- if (is.null(options$max.print)) 1000 else as.numeric(options$max.print)
+  max.print <- if (is.null(options$sql.max.print)) 1000 else as.numeric(options$sql.max.print)
 
   conn <- get(options$connection, envir = globalenv())
+  if (is.null(conn)) stop(
+    "The 'connection' option (DBI connection) is required for sql chunks."
+  )
+
+  # Return char vector of sql interpolation param names
+  varnames_from_sql = function(conn, sql) {
+    varPos = DBI::sqlParseVariables(conn, sql)
+    if (length(varPos$start) > 0) {
+      varNames = substring(sql, varPos$start, varPos$end)
+      sub("^\\?", "", varNames)
+    }
+  }
+
+  # Vectorized version of exists
+  mexists = function(x, env = globalenv(), inherits = TRUE) {
+    vapply(x, exists, logical(1), where = env, inherits = inherits)
+  }
+
+  # Interpolate a sql query based on the variables in an environment
+  interpolate_from_env = function(conn, sql, env = globalenv(), inherits = TRUE) {
+    names = unique(varnames_from_sql(conn, sql))
+    names_missing = names[!mexists(names, env, inherits)]
+    if (length(names_missing) > 0) {
+      stop("Object(s) not found: ", paste('"', names_missing, '"', collapse = ", "))
+    }
+
+    args = if (length(names) > 0) setNames(
+      mget(names, envir = env, inherits = inherits), names
+    )
+
+    do.call(DBI::sqlInterpolate, c(list(conn, sql), args))
+  }
+
+  # extract options
+  varname = options$output.var
+
+  # execute query -- when we are printing with an enforced max.print we
+  # use dbFetch so as to only pull down the required number of records
+  query = interpolate_from_env(conn, sql)
+  if (is.null(varname) && max.print > 0) {
+    res = DBI::dbSendQuery(conn, query)
+    data = if (!DBI::dbHasCompleted(res) || (DBI::dbGetRowCount(res) > 0))
+              DBI::dbFetch(res, n = max.print)
+    DBI::dbClearResult(res)
+  } else {
+    data = DBI::dbGetQuery(conn, query)
+  }
 
   res <- DBI::dbSendQuery(conn, query)
   x <- if (!DBI::dbHasCompleted(res) || (DBI::dbGetRowCount(res) > 0))
-            DBI::dbFetch(res, n = maxRows)
+            DBI::dbFetch(res, n = max.print)
   DBI::dbClearResult(res)
 
-  save(
-    x, 
-    file = outputFile
-  )
+  # assign varname if requested, otherwise print
+  if (!is.null(varname)) {
+    assign(varname, data, envir = globalenv())
+  }
+  else {
+    x <- data
+    save(
+      x, 
+      file = outputFile
+    )
+  }
 })
