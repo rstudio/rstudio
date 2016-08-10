95bffa19a6246a4dd2c83f6b1aedb6fc8c3d35de
Jonathan McPherson <jonathan@rstudio.com>
2016-08-10 14:48:33 -0700

    vagrant: set appropriate permissions on authorized_keys

--- vagrant/provision-common-user.sh
+++ vagrant/provision-common-user.sh
@@ -5,6 +5,9 @@ if [ -f /rstudio/vagrant/vagrant_key_rsa.pub ]; then
     cat /rstudio/vagrant/vagrant_key_rsa.pub >> ~/.ssh/authorized_keys
 fi
 
+# set appropriate permissions on keys
+chmod 600 ~/.ssh/authorized_keys
+
 # run overlay script if present
 if [ -f ./install-overlay-debian ]; then
     ./install-overlay-debian
