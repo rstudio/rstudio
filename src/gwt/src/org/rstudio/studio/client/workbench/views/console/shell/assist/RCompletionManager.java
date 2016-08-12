de8725ba95ccd4cd1513733b25871a514efc67d6
Kevin Ushey <kevin@rstudio.com>
2016-08-12 16:08:20 -0700

    allow autocompletion for R code within roxygen examples

--- src/gwt/src/org/rstudio/studio/client/workbench/views/console/shell/assist/RCompletionManager.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/console/shell/assist/RCompletionManager.java
@@ -77,6 +77,7 @@ import org.rstudio.studio.client.workbench.views.source.editors.text.ace.RInfixD
 import org.rstudio.studio.client.workbench.views.source.editors.text.ace.Range;
 import org.rstudio.studio.client.workbench.views.source.editors.text.ace.Token;
 import org.rstudio.studio.client.workbench.views.source.editors.text.ace.TokenCursor;
+import org.rstudio.studio.client.workbench.views.source.editors.text.ace.TokenIterator;
 import org.rstudio.studio.client.workbench.views.source.editors.text.events.PasteEvent;
 import org.rstudio.studio.client.workbench.views.source.editors.text.r.RCompletionToolTip;
 import org.rstudio.studio.client.workbench.views.source.editors.text.r.SignatureToolTipManager;
@@ -1100,7 +1101,8 @@ public class RCompletionManager implements CompletionManager
    
    private boolean isLineInRoxygenComment(String line)
    {
-      return line.matches("^\\s*#+'\\s*[^\\s].*");
+      Pattern pattern = Pattern.create("^\\s*#+'");
+      return pattern.test(line);
    }
    
    private boolean isLineInComment(String line)
@@ -1431,10 +1433,30 @@ public class RCompletionManager implements CompletionManager
       if (firstLine.matches("^\\s*[?].*"))
          return new AutocompletionContext(token, AutocompletionContext.TYPE_HELP);
       
-      // escape early for roxygen
+      // escape early for (non-examples) roxygen
       if (firstLine.matches("\\s*#+'.*"))
-         return new AutocompletionContext(
-               token, AutocompletionContext.TYPE_ROXYGEN);
+      {
+         TokenIterator it = docDisplay_.getTokenIterator(input_.getCursorPosition());
+         for (Token currentToken = it.getCurrentToken();
+              currentToken != null;
+              currentToken = it.stepBackward())
+         {
+            if (it.bwdToMatchingToken())
+               continue;
+            
+            String tokenContents = currentToken.getValue();
+            if (tokenContents.startsWith("@"))
+            {
+               // if we discovered an '@examples' tag, then we want to use R code style completion
+               if (tokenContents.equals("@examples"))
+                  break;
+
+               return new AutocompletionContext(
+                     token,
+                     AutocompletionContext.TYPE_ROXYGEN);
+            }
+         }
+      }
       
       // If the token has '$' or '@', add in the autocompletion context --
       // note that we still need parent contexts to give more information
