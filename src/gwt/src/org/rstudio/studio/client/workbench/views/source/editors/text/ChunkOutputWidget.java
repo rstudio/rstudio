7584b41d7ef93b4e38181c71c3f2ea4a5e8a7c51
Javier Luraschi <javierluraschi@hotmail.com>
2016-08-27 00:49:05 -0700

    fix satellite window issue where clicks to gallery were being lost

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/ChunkOutputWidget.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/ChunkOutputWidget.java
@@ -847,6 +847,14 @@ public class ChunkOutputWidget extends Composite
          // need to switch to gallery mode 
          final ChunkOutputStream stream = (ChunkOutputStream)presenter_;
          final ChunkOutputGallery gallery = new ChunkOutputGallery(this);
+
+         // replace the stream with the gallery (the stream will live on inside
+         // the gallery's console page). Perform this operation before adding pages,
+         // otherwise, in some situations (satellite chunks), attachPresenter()
+         // would perform the attachment by temporarily deattaching which causes
+         // all the events to be lost and clicks to stop working. Instead, attach
+         // the gallery before to maintain the event listers assigned in addPage().
+         attachPresenter(gallery);
          
          // extract all the pages from the stream and populate the gallery
          List<ChunkOutputPage> pages = stream.extractPages();
@@ -860,9 +868,6 @@ public class ChunkOutputWidget extends Composite
             gallery.addPage(page);
          }
          
-         // replace the stream with the gallery (the stream will live on inside
-         // the gallery's console page)
-         attachPresenter(gallery);
          syncHeight(false, false);
       }
    }
