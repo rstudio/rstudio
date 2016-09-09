487867deff2be06aeedb22a647610dbaff1cc0ec
Kevin Ushey <kevin@rstudio.com>
2016-09-09 11:04:39 -0700

    respect 'fig.asp' for notebook plots

--- src/cpp/session/modules/rmarkdown/NotebookExec.cpp
+++ src/cpp/session/modules/rmarkdown/NotebookExec.cpp
@@ -123,6 +123,11 @@ void ChunkExecContext::connect()
    // extract knitr figure options if present
    double figWidth = options_.getOverlayOption("fig.width", 0.0);
    double figHeight = options_.getOverlayOption("fig.height", 0.0);
+   
+   // if 'fig.asp' is set, then use that to override 'fig.height'
+   double figAsp = options_.getOverlayOption("fig.asp", 0.0);
+   if (figAsp != 0.0)
+      figHeight = figWidth * figAsp;
 
    // begin capturing plots 
    connections_.push_back(events().onPlotOutput.connect(
