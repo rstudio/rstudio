8a6a4364fb9abb87335a75ff639ba7fa29daa77b
Gary Ritchie <gary@rstudio.com>
2016-10-06 16:43:02 -0700

    WIP on plumbing for an XTerm.js-based widget.

---
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/terminal/xterm/XTermNative.java
@@ -0,0 +1,29 @@
+/*
+ * XTermNative.java
+ *
+ * Copyright (C) 2009-16 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+package org.rstudio.studio.client.workbench.views.terminal.xterm;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.dom.client.Element;
+
+public class XTermNative extends JavaScriptObject
+{
+   public static native XTermNative createTerminal(Element container) /*-{
+      term = new Terminal({cursorBlink: false});
+      term.open(container);
+      return term;
+   }-*/;
+   
+}
