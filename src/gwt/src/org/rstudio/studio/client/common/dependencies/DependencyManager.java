65f29efc75f47cc506bea59ecc7eb9078813e807
JJ Allaire <jj@rstudio.org>
2016-10-21 10:56:19 -0400

    update rmarkdown dependencies: htmltools v0.3.5; rprojroot v1.0

--- src/gwt/src/org/rstudio/studio/client/common/dependencies/DependencyManager.java
+++ src/gwt/src/org/rstudio/studio/client/common/dependencies/DependencyManager.java
@@ -211,13 +211,14 @@ public class DependencyManager implements InstallShinyEvent.Handler,
       deps.add(Dependency.cranPackage("stringr", "0.6"));
       deps.add(Dependency.cranPackage("yaml", "2.1.5"));
       deps.add(Dependency.cranPackage("Rcpp", "0.11.5"));
-      deps.add(Dependency.cranPackage("htmltools", "0.2.4"));
+      deps.add(Dependency.cranPackage("htmltools", "0.3.5"));
       deps.add(Dependency.cranPackage("caTools", "1.14"));
       deps.add(Dependency.cranPackage("bitops", "1.0-6"));
       deps.add(Dependency.cranPackage("knitr", "1.14", true));
       deps.add(Dependency.cranPackage("jsonlite", "0.9.19"));
       deps.add(Dependency.cranPackage("base64enc", "0.1-3"));
       deps.add(Dependency.cranPackage("tibble", "1.1"));
+      deps.add(Dependency.cranPackage("rprojroot", "1.0"));
       deps.add(Dependency.embeddedPackage("rmarkdown"));
       return deps;
    }
