8f425f12e80a912eb984afebc9e3360e59cdbf43
Jonathan McPherson <jonathan@rstudio.com>
2016-11-22 16:19:25 -0800

    add support for JavaScript and CSS chunks in the notebook

--- src/cpp/session/modules/SessionRmdNotebook.R
+++ src/cpp/session/modules/SessionRmdNotebook.R
@@ -296,9 +296,25 @@ assign(".rs.notebookVersion", envir = .rs.toolsEnv(), "1.0")
       # determine whether we include source code (respect chunk options)
       includeSource <- isTRUE(context$echo) && isTRUE(context$include)
       
-      # if we have no chunk outputs, just show source code (respecting
-      # chunk options as appropriate)
-      if (is.null(chunkId)) {
+      if (identical(context$engine, "js") || identical(context$engine, "css")) {
+         # these engines never show code; ensure they're marked for evaluation
+         # and then emit contents literally wrapped in the appropriate tags
+         htmlOutput <- ""
+         if (isTRUE(context$eval)) {
+            if (identical(context$engine, "js")) {
+               htmlOutput <- paste(
+                  c('<script type="text/javascript">', code, '</script>'),
+                    collapse = '\n')
+            } else if (identical(context$engine, "css")) {
+               htmlOutput <- paste(
+                  c('<style type="text/css">', code, '</style>'),
+                    collapse = '\n')
+            }
+         }
+         return(knitr::asis_output(htmlOutput))
+      } else if (is.null(chunkId)) {
+         # if we have no chunk outputs, just show source code (respecting
+         # chunk options as appropriate)
          if (includeSource) {
             attributes <- list(class = .rs.rnb.engineToCodeClass(context$engine))
             
