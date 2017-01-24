59b8c0c42a491cba6dcfab04f8fd18c396a30364
Javier Luraschi <javierluraschi@hotmail.com>
2017-01-24 10:53:35 -0800

    fallback to download.file if curl not installed

--- src/cpp/session/modules/SessionDataImportV2.R
+++ src/cpp/session/modules/SessionDataImportV2.R
@@ -354,7 +354,7 @@
          cacheDataCode <- append(cacheDataCode, list(
             paste(
                downloadCondition,
-               "curl::curl_download(",
+               if (.rs.isPackageInstalled("curl")) "curl::curl_download(" else "download.file(",
                cacheUrlName,
                ", ",
                cacheVariableName,
