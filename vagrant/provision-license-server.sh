f630bad6683dd42b61b91801e7a3f7ed37f0e190
Jonathan McPherson <jonathan@rstudio.com>
2017-01-25 12:18:47 -0800

    install development-friendly configuration for license server

--- vagrant/provision-license-server.sh
+++ vagrant/provision-license-server.sh
@@ -8,7 +8,14 @@ wget "https://s3.amazonaws.com/rstudio-license-server/rsp-license-server-1.0.0-x
 # install it 
 sudo dpkg -i "rsp-license-server-1.0.0-x86_64.deb"
 
-# start it!
-service rsp-license-server start
+# replace the default configuration file with one that has more development-
+# friendly settings
+sudo cp /rstudio/vagrant/rsp-license-server.conf /etc/rsp-license-server.conf
+
+# the service needs to be activated before it can be started; run these
+# commands after the VM is provisioned:
+# 
+# sudo service rsp-license-server activate <your-license-key>
+# sudo service rsp-license-server start
 
 popd
