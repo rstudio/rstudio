69ded47653f8fe3fce6b974bde2639bbbc8af949
Kevin Ushey <kevinushey@gmail.com>
2017-01-31 17:08:57 -0800

    fix printing of data.frame containing date

--- src/cpp/session/modules/NotebookData.R
+++ src/cpp/session/modules/NotebookData.R
@@ -165,7 +165,7 @@
                           factor = "fctr",
                           POSIXt = "dttm",
                           difftime = "time",
-                          Date = date,
+                          Date = "date",
                           data.frame = class(x)[[1]],
                           tbl_df = "tibble",
                           NULL
