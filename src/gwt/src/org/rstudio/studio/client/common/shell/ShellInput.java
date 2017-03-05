1cd987b5d554b1622aaa812d5fb854de6259b3c6
Gary Ritchie <gary@rstudio.com>
2017-03-04 16:25:50 -0800

    deal with out-of-order RPC calls for terminal input

--- src/gwt/src/org/rstudio/studio/client/common/shell/ShellInput.java
+++ src/gwt/src/org/rstudio/studio/client/common/shell/ShellInput.java
@@ -18,20 +18,38 @@ import com.google.gwt.core.client.JavaScriptObject;
 
 public class ShellInput extends JavaScriptObject
 {
+   // This tells server to flush any queued input, ignoring sequence. Used
+   // in the unlikely situation we need to wrap an int.
+   public static final int FLUSH_SEQUENCE = -2;
+   
+   // Tells server to ignore sequences and use the input as it arrives.
+   public static final int IGNORE_SEQUENCE = -1;
+
    protected ShellInput()
    {
    }
    
-   public native static ShellInput create(String text, boolean echoInput) /*-{
+   public native static ShellInput create(int sequence, String text, boolean echoInput) /*-{
       return {
+         sequence: sequence,
          interrupt: false,
          text: text,
          echo_input: echoInput
       };
    }-*/;
-   
+
+   public native static ShellInput create(String text, boolean echoInput) /*-{
+      return {
+         sequence: -1,
+         interrupt: false,
+         text: text,
+         echo_input: echoInput
+      };
+   }-*/;
+    
    public native static ShellInput createInterrupt() /*-{
       return {
+         sequence: -1,
          interrupt: true,
          text: "",
          echo_input: true
@@ -40,6 +58,10 @@ public class ShellInput extends JavaScriptObject
    
    
    
+   public native final boolean getSequence() /*-{
+      return this.sequence;
+   }-*/;
+
    public native final boolean getInterrupt() /*-{
       return this.interrupt;
    }-*/;
--- src/gwt/src/org/rstudio/studio/client/workbench/views/terminal/TerminalSession.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/terminal/TerminalSession.java
@@ -262,8 +262,27 @@ public class TerminalSession extends XTermWidget
          inputQueue_.setLength(0);
       }
 
+      // On Windows, rapid typing sometimes causes RPC messages for writeStandardInput
+      // to arrive out of sequence in the terminal; send a sequence number with each
+      // message so server can put messages back in order
+      if (inputSequence_ == ShellInput.FLUSH_SEQUENCE)
+      {
+         // Last message has flushed server, start tracking sequences again.
+         inputSequence_ = 0;
+      }
+      else if (inputSequence_ >= Integer.MAX_VALUE - 100)
+      {
+         // Very diligent typist!  Tell server to flush its input
+         // queue, temporarily ignoring sequences.
+         inputSequence_ = ShellInput.FLUSH_SEQUENCE;
+      }
+      else
+      {
+         inputSequence_++;
+      }
+
       consoleProcess_.writeStandardInput(
-            ShellInput.create(userInput,  true /* echo input*/), 
+            ShellInput.create(inputSequence_, userInput,  true /* echo input*/), 
             new VoidServerRequestCallback() {
 
                @Override
@@ -572,6 +591,7 @@ public class TerminalSession extends XTermWidget
    private boolean connecting_;
    private boolean terminating_;
    private StringBuilder inputQueue_ = new StringBuilder();
+   private int inputSequence_ = -1;
    private boolean newTerminal_ = true;
    private int cols_ = ConsoleProcessInfo.DEFAULT_COLS;
    private int rows_ = ConsoleProcessInfo.DEFAULT_ROWS;;
