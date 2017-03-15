5cb1d3fb57c0aee252367c81e9800962d1c0fd4c
Javier Luraschi <javierluraschi@hotmail.com>
2017-03-15 09:39:06 -0700

    fix 2x icons gwt warnings

--- src/gwt/src/org/rstudio/core/client/resources/ImageResource2x.java
+++ src/gwt/src/org/rstudio/core/client/resources/ImageResource2x.java
@@ -74,6 +74,7 @@ public class ImageResource2x implements ImageResource
       return getResource().getTop();
    }
 
+   @SuppressWarnings("deprecation")
    @Override
    public String getURL()
    {
