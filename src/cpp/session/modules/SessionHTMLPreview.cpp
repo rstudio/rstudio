2011c4ec28ecd6088016309f92d82cc71635541f
Jonathan McPherson <jonathan@rstudio.com>
2017-04-12 14:54:59 -0700

    use new RStudio MathJax URL

--- src/cpp/session/modules/SessionHTMLPreview.cpp
+++ src/cpp/session/modules/SessionHTMLPreview.cpp
@@ -775,11 +775,20 @@ void modifyOutputForPreview(std::string* pOutput)
 #endif
    }
 
-   // serve mathjax locally
+   // serve mathjax locally from this directory
    std::string previewMathjax = "mathjax-26";
+
+   // replace old mathjax CDN for compatibility with R packages that still use
+   // the deprecated URL
    boost::algorithm::replace_first(
         *pOutput,
         "https://cdn.mathjax.org/mathjax/latest",
+        previewMathjax);
+
+   // replace RStudio MathJax URL
+   boost::algorithm::replace_first(
+        *pOutput,
+        "https://mathjax.rstudio.com/latest",
         previewMathjax);
 }
 
