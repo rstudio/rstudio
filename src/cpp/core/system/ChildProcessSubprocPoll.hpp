8222e930e846842dcf21dbb98d4e196b861e36c7
Gary Ritchie <gary@rstudio.com>
2017-04-19 17:32:58 -0700

    refactor to eliminate duplicate code between posix/win32, and encapsulate confusing logic for subprocess checking/recent output polling

---
+++ src/cpp/core/system/ChildProcessSubprocPoll.hpp
@@ -0,0 +1,65 @@
+/*
+ * ChildProcessSubprocPoll.hpp
+ *
+ * Copyright (C) 2009-17 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#ifndef CORE_SYSTEM_CHILD_PROCESS_SUBPROC_POLL_HPP
+#define CORE_SYSTEM_CHILD_PROCESS_SUBPROC_POLL_HPP
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/function.hpp>
+
+#include <core/system/System.hpp>
+
+namespace rstudio {
+namespace core {
+namespace system {
+
+// Updates two polling-based flags for a process.
+//
+// hasSubprocess(): does the process have any child processes?
+// hasRecentOutput(): did the process recently report receiving output
+//
+// Pass function to perform the actual subproc check to the constructor, or
+// NULL to not do any subprocess checking.
+//
+class ChildProcessSubprocPoll : boost::noncopyable
+{
+public:
+//   typedef bool (*hasSubprocCheck)(PidType);
+
+   ChildProcessSubprocPoll(PidType pid, boost::function<bool (PidType pid)> subProcCheck);
+
+   virtual ~ChildProcessSubprocPoll() {}
+
+   void poll(bool hadOutput);
+   void stop();
+
+   bool hasSubprocess() const;
+   bool hasRecentOutput() const;
+
+private:
+   PidType pid_;
+   boost::posix_time::ptime checkSubProcAfter_;
+   boost::posix_time::ptime resetRecentOutputAfter_;
+   bool hasSubprocess_;
+   bool hasRecentOutput_;
+   bool stopped_;
+   boost::function<bool (PidType pid)> subProcCheck_;
+};
+
+} // namespace system
+} // namespace core
+} // namespace rstudio
+
+#endif // CORE_SYSTEM_CHILD_PROCESS_SUBPROC_POLL_HPP
---
+++ src/cpp/core/system/ChildProcessSubprocPollTests.cpp
@@ -0,0 +1,172 @@
+/*
+ * ChildProcessSubprocPollTests.cpp
+ *
+ * Copyright (C) 2009-17 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#include "ChildProcessSubprocPoll.hpp"
+
+#include <boost/bind.hpp>
+#include <boost/asio/deadline_timer.hpp>
+
+#include <tests/TestThat.hpp>
+
+namespace rstudio {
+namespace core {
+namespace system {
+namespace tests {
+
+namespace {
+
+void blockingwait(int ms)
+{
+   boost::asio::io_service io;
+   boost::asio::deadline_timer timer(io, boost::posix_time::milliseconds(ms));
+   timer.wait();
+}
+
+class NoSubProcPollingFixture
+{
+public:
+   NoSubProcPollingFixture(PidType pid)
+      :
+        poller_(pid, NULL)
+   {}
+
+   ChildProcessSubprocPoll poller_;
+};
+
+class SubProcPollingFixture
+{
+public:
+   SubProcPollingFixture(PidType pid)
+      :
+        poller_(pid, boost::bind(&SubProcPollingFixture::checkSubproc, this, _1)),
+        checkReturns_(false),
+        callerPid_(0),
+        checkCalled_(false)
+   {}
+
+   bool checkSubproc(PidType pid)
+   {
+      callerPid_ = pid;
+      checkCalled_ = true;
+      return checkReturns_;
+   }
+
+   void clearFlags()
+   {
+      callerPid_ = 0;
+      checkCalled_ = false;
+   }
+
+   ChildProcessSubprocPoll poller_;
+   bool checkReturns_;
+
+   PidType callerPid_;
+   bool checkCalled_;
+};
+
+} // anonymous namespace
+
+context("ChildProcess polling support class")
+{
+   test_that("initial state without subproc polling matches expectation")
+   {
+      PidType pid = 12345;
+      NoSubProcPollingFixture test(pid);
+
+      // we start with the flags set to true
+      expect_true(test.poller_.hasRecentOutput());
+      expect_true(test.poller_.hasSubprocess());
+   }
+
+   test_that("recent input state without subproc polling doesn't change immediately")
+   {
+      PidType pid = 12345;
+      NoSubProcPollingFixture test(pid);
+
+      test.poller_.poll(true);
+      expect_true(test.poller_.hasRecentOutput());
+      test.poller_.poll(false);
+      expect_true(test.poller_.hasRecentOutput()); // timeout hasn't expired
+   }
+
+   test_that("recent input state without subproc polling does change after timeout")
+   {
+      PidType pid = 12345;
+      NoSubProcPollingFixture test(pid);
+
+      test.poller_.poll(true);
+      expect_true(test.poller_.hasRecentOutput());
+      blockingwait(1100); // longer than timeout, but hate to do this in unit test
+      test.poller_.poll(false);
+      expect_false(test.poller_.hasRecentOutput()); // now it flips to false
+      test.poller_.poll(true);
+      expect_true(test.poller_.hasRecentOutput()); // but right back to true
+   }
+
+   test_that("initial state for subproc polling matches expectations")
+   {
+      PidType pid = 12345;
+      SubProcPollingFixture test(pid);
+
+      // we start with the flags set to true
+      expect_true(test.poller_.hasRecentOutput());
+      expect_true(test.poller_.hasSubprocess());
+   }
+
+   test_that("childproc checked when recent output and timeout expires")
+   {
+      PidType pid = 12345;
+      SubProcPollingFixture test(pid);
+
+      test.checkReturns_ = false;
+      test.poller_.poll(true);
+      expect_false(test.checkCalled_); // polling timeout hasn't passed
+      blockingwait(300); // longer than the timeout
+      test.poller_.poll(false); // still not longer than the recent output timeout!
+      expect_true(test.checkCalled_);
+      expect_true(test.poller_.hasSubprocess() == test.checkReturns_);
+      expect_true(test.poller_.hasRecentOutput());
+      expect_true(test.callerPid_ == pid);
+   }
+
+   test_that("lack of recent output prevents childproc checking")
+   {
+      PidType pid = 12345;
+      SubProcPollingFixture test(pid);
+
+      test.checkReturns_ = false;
+      test.poller_.poll(false);
+      expect_false(test.checkCalled_);
+      blockingwait(300); // long enough for childproc polling
+      test.poller_.poll(false); // not longer than the output timeout
+      expect_true(test.checkCalled_);
+      expect_true(test.poller_.hasSubprocess() == test.checkReturns_);
+      expect_true(test.poller_.hasRecentOutput());
+      expect_true(test.callerPid_ == pid);
+
+      test.clearFlags();
+      test.poller_.poll(false); // no recent output
+      blockingwait(800); // this plus earlier delay should timeout recent output
+      test.poller_.poll(false);
+      expect_false(test.checkCalled_); // because of no recent output
+      expect_false(test.poller_.hasRecentOutput());
+      expect_true(test.poller_.hasSubprocess() == test.checkReturns_);
+   }
+}
+
+} // namespace tests
+} // namespace system
+} // namespace core
+} // namespace rstudio
