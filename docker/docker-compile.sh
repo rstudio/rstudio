2d18d467cb1c58c48d74e1556512e89cdb358217
Jonathan McPherson <jonathan@rstudio.com>
2017-04-26 12:27:31 -0700

    move script and add some comments

--- docker/docker-compile.sh
+++ docker/docker-compile.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+# RStudio Docker Compilation Helper
+#
+# The purpose of this script is to make it easy to compile under Docker on 
+# a development machine, so it's easy to iterate when making platform-specific
+# changes or configuration changes.
+# 
+# The syntax is as follows:
+#
+#     docker-compile.sh IMAGE-NAME FLAVOR-NAME [VERSION]
+#
+# where the image name is the platform and architecture, the flavor name is
+# the kind of package you wish to build (desktop or server), and the version
+# is the number to assign to the resulting build.
+#
+# For example:
+# 
+#     docker-compile.sh centos7-amd64 desktop 1.2.345
+#
+# will produce an RPM of RStudio Desktop from the 64bit CentOS 7 container, 
+# with build version 1.2.345, in your package/linux/ directory.
+#
+
 # friendly names for arguments
 IMAGE=$1
 FLAVOR=$2
@@ -8,10 +30,14 @@ VERSION=$3
 # abort on error
 set -e
 
+# move to the repo root (script's grandparent directory)
+cd "$(dirname ${BASH_SOURCE[0]})/.."
+REPO=$(basename $(pwd))
+
 # print usage if no argument supplied
 if [ -z "$IMAGE" ] || [ -z "$FLAVOR" ]; then
     echo -e "Compiles RStudio inside a Docker container."
-    echo -e "Usage: docker-compile image-name flavor-name [version]\n"
+    echo -e "Usage: docker-compile.sh image-name flavor-name [version]\n"
     echo -e "Valid images:\n"
     ls -f docker/jenkins/Dockerfile.* | sed -e 's/.*Dockerfile.//'
     echo -e "\nValid flavors:\n"
@@ -19,11 +45,6 @@ if [ -z "$IMAGE" ] || [ -z "$FLAVOR" ]; then
     echo -e "server"
     exit 1
 fi
-
-# make sure we're running from the directory of this script, and infer the 
-# name of the repository
-cd "$(dirname ${BASH_SOURCE[0]})"
-REPO=$(basename $(pwd))
 
 # check to see if there's already a built image
 IMAGEID=`docker images $REPO:$IMAGE --format "{{.ID}}"`
