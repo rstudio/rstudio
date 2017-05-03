7c37831683d7ad07c719501af8ceab1b8497652f
Kevin Ushey <kevinushey@gmail.com>
2017-05-03 16:13:50 -0700

    drop 'data.table' class for 'data.frame' objects in viewer

--- src/cpp/session/modules/SessionDataViewer.R
+++ src/cpp/session/modules/SessionDataViewer.R
@@ -215,6 +215,11 @@
 })
 
 .rs.addFunction("toDataFrame", function(x, name, flatten) {
+   
+   # if we have a data.table, convert it to a data.frame explicitly
+   if (inherits(x, "data.table"))
+      x <- as.data.frame(x)
+   
   # if it's not already a frame, coerce it to a frame
   if (!is.data.frame(x)) {
     frame <- NULL
