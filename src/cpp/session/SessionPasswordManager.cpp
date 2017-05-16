f56c59fbeed13f43892d68d44aade43290198a7f
Gary Ritchie <gary@rstudio.com>
2017-05-16 13:53:27 -0700

    oops, missed adding these

---
+++ src/cpp/session/SessionPasswordManager.cpp
@@ -0,0 +1,143 @@
+/*
+ * SessionPasswordManager.cpp
+ *
+ * Copyright (C) 2009-17 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#include <session/SessionPasswordManager.hpp>
+
+#include <core/RegexUtils.hpp>
+
+using namespace rstudio::core;
+
+namespace rstudio {
+namespace session {
+namespace console_process {
+
+void PasswordManager::attach(
+                  boost::shared_ptr<console_process::ConsoleProcess> pCP,
+                  bool showRememberOption)
+{
+   pCP->setPromptHandler(boost::bind(&PasswordManager::handlePrompt,
+                                       this,
+                                       pCP->handle(),
+                                       _1,
+                                       showRememberOption,
+                                       _2));
+
+   pCP->onExit().connect(boost::bind(&PasswordManager::onExit,
+                                       this,
+                                       pCP->handle(),
+                                       _1));
+}
+
+bool PasswordManager::handlePrompt(const std::string& cpHandle,
+                                   const std::string& prompt,
+                                   bool showRememberOption,
+                                   ConsoleProcess::Input* pInput)
+{
+   // is this a password prompt?
+   boost::smatch match;
+   if (regex_utils::match(prompt, match, promptPattern_))
+   {
+      // see if it matches any of our existing cached passwords
+      std::vector<CachedPassword>::const_iterator it =
+                  std::find_if(passwords_.begin(),
+                               passwords_.end(),
+                               boost::bind(&hasPrompt, _1, prompt));
+      if (it != passwords_.end())
+      {
+         // cached password
+         *pInput = ConsoleProcess::Input(it->password + "\n", false);
+      }
+      else
+      {
+         // prompt for password
+         std::string password;
+         bool remember;
+         if (promptHandler_(prompt, showRememberOption, &password, &remember))
+         {
+
+            // cache the password (but also set the remember flag so it
+            // will be removed from the cache when the console process
+            // exits if the user chose not to remember).
+            CachedPassword cachedPassword;
+            cachedPassword.cpHandle = cpHandle;
+            cachedPassword.prompt = prompt;
+            cachedPassword.password = password;
+            cachedPassword.remember = remember;
+            passwords_.push_back(cachedPassword);
+
+            // interactively entered password
+            *pInput = ConsoleProcess::Input(password + "\n", false);
+         }
+         else
+         {
+            // user cancelled
+            *pInput = ConsoleProcess::Input();
+         }
+      }
+
+      return true;
+   }
+   // not a password prompt so ignore
+   else
+   {
+      return false;
+   }
+}
+
+void PasswordManager::onExit(const std::string& cpHandle,
+                             int exitCode)
+{
+   // if a process exits with an error then remove any cached
+   // passwords which originated from that process
+   if (exitCode != EXIT_SUCCESS)
+   {
+      passwords_.erase(std::remove_if(passwords_.begin(),
+                                      passwords_.end(),
+                                      boost::bind(&hasHandle, _1, cpHandle)),
+                       passwords_.end());
+   }
+
+   // otherwise remove any cached password for this process which doesn't
+   // have its remember flag set
+   else
+   {
+      passwords_.erase(std::remove_if(passwords_.begin(),
+                                      passwords_.end(),
+                                      boost::bind(&forgetOnExit, _1, cpHandle)),
+                       passwords_.end());
+   }
+}
+
+bool PasswordManager::hasPrompt(const CachedPassword& cachedPassword,
+                                const std::string& prompt)
+{
+   return cachedPassword.prompt == prompt;
+}
+
+bool PasswordManager::hasHandle(const CachedPassword& cachedPassword,
+                                const std::string& cpHandle)
+{
+   return cachedPassword.cpHandle == cpHandle;
+}
+
+bool PasswordManager::forgetOnExit(const CachedPassword& cachedPassword,
+                                   const std::string& cpHandle)
+{
+   return hasHandle(cachedPassword, cpHandle) && !cachedPassword.remember;
+}
+
+} // namespace console_process
+} // namespace session
+} // namespace rstudio
