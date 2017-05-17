7162862263238764a143c2e8754712f3428922c5
Gary Ritchie <gary@rstudio.com>
2017-05-17 12:19:00 -0700

    java copyright updates

--- src/gwt/src/org/rstudio/studio/client/workbench/views/viewer/ViewerPane.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/viewer/ViewerPane.java
@@ -1,7 +1,7 @@
 /*
  * ViewerPane.java
  *
- * Copyright (C) 2009-12 by RStudio, Inc.
+ * Copyright (C) 2009-17 by RStudio, Inc.
  *
  * This program is licensed to you under the terms of version 3 of the
  * GNU Affero General Public License. This program is distributed WITHOUT
