c4850153d66b9fee202a162c2e76435714ce7364
Kevin Ushey <kevinushey@gmail.com>
2017-06-13 16:29:10 -0700

    roll back unnecessary changes

--- docker/docker-compile.sh
+++ docker/docker-compile.sh
@@ -78,9 +78,6 @@ if [ -n "$VERSION" ]; then
     ENV="RSTUDIO_VERSION_MAJOR=${SPLIT[0]} RSTUDIO_VERSION_MINOR=${SPLIT[1]} RSTUDIO_VERSION_PATCH=${SPLIT[2]}"
 fi
 
-if [ "${VARIANT}" = "server" ]; then
-    EXCLUDE_QT_SDK="--exclude-qt-sdk"
-fi
-
-docker run --rm -v $(pwd):/src $REPO:$IMAGE bash -c "cd /src/dependencies/linux && ./install-dependencies-$INSTALLER ${EXCLUDE_QT_SDK} && cd /src/package/linux && $ENV ./make-$FLAVOR-package $PACKAGE clean $VARIANT"
+# run compile step
+docker run --rm -v $(pwd):/src $REPO:$IMAGE bash -c "cd /src/dependencies/linux && ./install-dependencies-$INSTALLER --exclude-qt-sdk && cd /src/package/linux && $ENV ./make-$FLAVOR-package $PACKAGE clean $VARIANT"
 
