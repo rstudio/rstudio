e1f1eb0a118d5413c78b93feaa2f3f7689a2b911
Daniel Kurka <dankurka@google.com>
2017-07-14 11:41:48 -0700

    Trap window.onerror by default.
    Changes gwt.uncaughtexceptionhander.windowonerror
    to REPORT_IF_NO_HANDLER.
    This is a good default for most GWT applications.
    see: https://gwt-review.googlesource.com/#/c/18540/
    
    
    Change-Id: I2a1cb3e89aacfd14315841835aed26927e1ff944

--- user/test/com/google/gwt/core/client/WindowOnErrorTest.java
+++ user/test/com/google/gwt/core/client/WindowOnErrorTest.java
@@ -27,7 +27,7 @@ public class WindowOnErrorTest extends GWTTestCase {
 
   @Override
   public String getModuleName() {
-    return "com.google.gwt.core.WindowOnError";
+    return "com.google.gwt.core.Core";
   }
 
   @Override
