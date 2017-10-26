rstudio/rstudio

commit 9258dd48450737820d547cc85e9f3776b7beb1db

remove most usages of String.equals()

src/gwt/src/org/rstudio/studio/client/workbench/views/buildtools/BuildCommands.java
@@ -32,7 +32,7 @@ public class BuildCommands
       
       // adapt or remove package commands if this isn't a package
       String type = sessionInfo.getBuildToolsType();
-      if (!type.equals(SessionInfo.BUILD_TOOLS_PACKAGE))
+      if (type == SessionInfo.BUILD_TOOLS_PACKAGE)
       {
          commands.devtoolsLoadAll().remove();
          commands.buildSourcePackage().remove();
@@ -51,20 +51,20 @@ public class BuildCommands
       }
       
       // remove makefile commands if this isn't a makefile
-      if (type.equals(SessionInfo.BUILD_TOOLS_CUSTOM) ||
-          type.equals(SessionInfo.BUILD_TOOLS_WEBSITE))
+      if (type == SessionInfo.BUILD_TOOLS_CUSTOM ||
+          type == SessionInfo.BUILD_TOOLS_WEBSITE)
       {
          commands.rebuildAll().remove();
       }
       
-      if (type.equals(SessionInfo.BUILD_TOOLS_CUSTOM) ||
-          type.equals(SessionInfo.BUILD_TOOLS_PACKAGE))
+      if (type == SessionInfo.BUILD_TOOLS_CUSTOM ||
+          type == SessionInfo.BUILD_TOOLS_PACKAGE)
       {
          commands.cleanAll().remove();
       }
       
       // remove all other commands if there are no build tools
-      if (type.equals(SessionInfo.BUILD_TOOLS_NONE))
+      if (type == SessionInfo.BUILD_TOOLS_NONE)
       {
          commands.buildAll().remove();
          commands.rebuildAll().remove();

commit 6f9f8f0f20a2685324f3aef016f6e48b3abd5f07

hide package commands when not in a package project (fixes #1674)
this is a regression from 9258dd48450737820d547cc85e9f3776b7beb1db,
in which the logic was accidentally inverted when switching from
String.equals to the equivalent direct comparison.

src/gwt/src/org/rstudio/studio/client/workbench/views/buildtools/BuildCommands.java
@@ -32,7 +32,7 @@ public class BuildCommands
       
       // adapt or remove package commands if this isn't a package
       String type = sessionInfo.getBuildToolsType();
-      if (type == SessionInfo.BUILD_TOOLS_PACKAGE)
+      if (type != SessionInfo.BUILD_TOOLS_PACKAGE)
       {
          commands.devtoolsLoadAll().remove();
          commands.buildSourcePackage().remove();