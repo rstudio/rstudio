rstudio/rstudio

commit e2b9ba1b5c7ba338aa1619af6766e987003642c1

tweaks to data viewer rownames synthesis

src/cpp/session/modules/SessionDataViewer.R
@@ -183,9 +183,10 @@
    if (is.data.frame(x))
    {
       info <- .row_names_info(x, type = 0L)
-      if (is.na(info)[[1]])
+      if (is.integer(info) && is.na(info[[1]]))
       {
-         range <- seq(from = start, to = start + len)
+         n <- info[[2]]
+         range <- seq(from = start, to = min(n, start + len))
          return(as.character(range))
       }
    }

commit a39872678484b9b555eb34a3b920645eef39d05e

allow for negative (automatic) row names in data viewer; fixes #1784
addresses a regression from commit:

e2b9ba1b5c7ba338aa1619af6766e987003642c1

which causes row numbers to be negative in the data viewer when row
names are auto-generated numbers.

src/cpp/session/modules/SessionDataViewer.R
@@ -185,7 +185,9 @@
       info <- .row_names_info(x, type = 0L)
       if (is.integer(info) && is.na(info[[1]]))
       {
-         n <- info[[2]]
+         # the second element indicates the number of rows, and is negative if they're 
+         # automatic
+         n <- abs(info[[2]])
          range <- seq(from = start, to = min(n, start + len))
          return(as.character(range))
       }