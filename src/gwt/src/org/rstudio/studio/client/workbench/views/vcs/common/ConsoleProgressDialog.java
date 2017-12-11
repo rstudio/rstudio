rstudio/rstudio

commit 2b7bff8f28b37d73d6063338b511cedd9caa5fae

emit 'branch' + 'push' output to single dialog

src/gwt/src/org/rstudio/studio/client/workbench/views/vcs/common/ConsoleProgressDialog.java
@@ -76,8 +76,6 @@ public class ConsoleProgressDialog extends ProgressDialog
          throw new IllegalArgumentException(
                "Invalid combination of arguments to ConsoleProgressDialog");
       }
-
-      consoleProcess_ = consoleProcess;
       
       if (getInteractionMode() != ConsoleProcessInfo.INTERACTION_NEVER)
       {
@@ -94,14 +92,48 @@ public class ConsoleProgressDialog extends ProgressDialog
          display_.setReadOnly(true);
          outputWriter_ = display_;
       }
-
-      stopButton().addClickHandler(this);
       
       if (!StringUtil.isNullOrEmpty(initialOutput))
       {
          outputWriter_.consoleWriteOutput(initialOutput);
       }
 
+      if (exitCode != null)
+         setExitCode(exitCode);
+     
+      // interaction-always mode is a shell -- customize ui accordingly
+      if (getInteractionMode() == ConsoleProcessInfo.INTERACTION_ALWAYS)
+      {
+         stopButton().setText("Close");
+         
+         hideProgress();
+         
+         int height = Window.getClientHeight() - 150;
+         int width = Math.min(800, Window.getClientWidth() - 150);
+         Style style = display_.getShellWidget().getElement().getStyle();
+         style.setHeight(height, Unit.PX);
+         style.setWidth(width, Unit.PX);
+      }
+      
+      attachToProcess(consoleProcess);
+   }
+   
+   @Override
+   protected Widget createDisplayWidget(Object param)
+   {
+      display_ = new ConsoleProgressWidget();
+      display_.setMaxOutputLines(getMaxOutputLines());
+      display_.setSuppressPendingInput(true);
+      return display_.getShellWidget();
+   }
+   
+   public void attachToProcess(final ConsoleProcess consoleProcess)
+   {
+      consoleProcess_ = consoleProcess;
+      
+      stopButton().setText("Stop");
+      stopButton().addClickHandler(this);
+      
       if (consoleProcess != null)
       {
          addHandlerRegistration(consoleProcess.addConsolePromptHandler(this));
@@ -131,37 +163,10 @@ public class ConsoleProgressDialog extends ProgressDialog
          @Override
          public void onClose(CloseEvent<PopupPanel> popupPanelCloseEvent)
          {
-            if (consoleProcess_ != null)
-               consoleProcess_.reap(new VoidServerRequestCallback());
+            if (consoleProcess != null)
+               consoleProcess.reap(new VoidServerRequestCallback());
          }
       });
-
-      if (exitCode != null)
-         setExitCode(exitCode);
-     
-      // interaction-always mode is a shell -- customize ui accordingly
-      if (getInteractionMode() == ConsoleProcessInfo.INTERACTION_ALWAYS)
-      {
-         stopButton().setText("Close");
-         
-         hideProgress();
-         
-         int height = Window.getClientHeight() - 150;
-         int width = Math.min(800, Window.getClientWidth() - 150);
-         Style style = display_.getShellWidget().getElement().getStyle();
-         style.setHeight(height, Unit.PX);
-         style.setWidth(width, Unit.PX);
-      }
-
-   }
-   
-   @Override
-   protected Widget createDisplayWidget(Object param)
-   {
-      display_ = new ConsoleProgressWidget();
-      display_.setMaxOutputLines(getMaxOutputLines());
-      display_.setSuppressPendingInput(true);
-      return display_.getShellWidget();
    }
 
    public ConsoleProcess getConsoleProcess()
@@ -187,19 +192,29 @@ public class ConsoleProgressDialog extends ProgressDialog
          return false;
       }
    }
+   
+   public void writeOutput(String output)
+   {
+      maybeShowOnOutput(output);
+      outputWriter_.consoleWriteOutput(output);
+   }
+   
+   public void writePrompt(String prompt)
+   {
+      maybeShowOnOutput(prompt);
+      outputWriter_.consoleWritePrompt(prompt);
+   }
 
    @Override
    public void onConsoleOutput(ConsoleOutputEvent event)
    {
-      maybeShowOnOutput(event.getOutput());
-      outputWriter_.consoleWriteOutput(event.getOutput());
+      writeOutput(event.getOutput());
    }
    
    @Override
    public void onConsolePrompt(ConsolePromptEvent event)
    {
-      maybeShowOnOutput(event.getPrompt());
-      outputWriter_.consoleWritePrompt(event.getPrompt());
+      writePrompt(event.getPrompt());
    }
 
    @Override
@@ -321,7 +336,7 @@ public class ConsoleProgressDialog extends ProgressDialog
    
    private boolean showOnOutput_ = false;
    
-   private final ConsoleProcess consoleProcess_;
+   private ConsoleProcess consoleProcess_;
  
    private final ShellOutputWriter outputWriter_;
   

src/cpp/session/modules/SessionGit.cpp
@@ -586,6 +586,17 @@ public:
       s_branches = *pBranches;
       return Success();
    }
+   
+   core::Error listRemotes(std::vector<std::string>* pRemotes)
+   {
+      std::string output;
+      Error error = runGit(ShellArgs() << "remote", &output);
+      if (error)
+         return error;
+      
+      *pRemotes = split(output);
+      return Success();
+   }
 
    core::Error checkout(const std::string& id,
                         boost::shared_ptr<ConsoleProcess>* ppCP)
@@ -791,6 +802,16 @@ public:
       }
 
       return createConsoleProc(args, "Git Push", ppCP);
+   }
+   
+   core::Error pushBranch(const std::string& branch,
+                          const std::string& remote,
+                          boost::shared_ptr<ConsoleProcess>* ppCP)
+   {
+      return createConsoleProc(
+               ShellArgs() << "push" << "-u" << remote << branch,
+               "Git Push",
+               ppCP);
    }
 
    core::Error pull(boost::shared_ptr<ConsoleProcess>* ppCP)
@@ -1595,6 +1616,26 @@ Error vcsPush(const json::JsonRpcRequest& request,
    return Success();
 }
 
+Error vcsPushBranch(const json::JsonRpcRequest& request,
+                    json::JsonRpcResponse* pResponse)
+{
+   std::string branch, remote;
+   Error error = json::readParams(request.params, &branch, &remote);
+   if (error)
+      return error;
+   
+   boost::shared_ptr<ConsoleProcess> pCP;
+   error = s_git_.pushBranch(branch, remote, &pCP);
+   if (error)
+      return error;
+
+   ask_pass::setActiveWindow(request.sourceWindow);
+
+   pResponse->setResult(pCP->toJson());
+
+   return Success();
+}
+
 Error vcsPull(const json::JsonRpcRequest& request,
               json::JsonRpcResponse* pResponse)
 {
@@ -1757,6 +1798,16 @@ Error vcsSetIgnores(const json::JsonRpcRequest& request,
    return Success();
 }
 
+Error vcsListRemotes(const json::JsonRpcRequest& request,
+                     json::JsonRpcResponse* pResponse)
+{
+   std::vector<std::string> remotes;
+   Error error = s_git_.listRemotes(&remotes);
+   if (error)
+      LOG_ERROR(error);
+   pResponse->setResult(json::toJsonArray(remotes));
+   return Success();
+}
 
 std::string getUpstream(const std::string& branch = std::string())
 {
@@ -2991,6 +3042,7 @@ core::Error initialize()
       (bind(registerRpcMethod, "git_all_status", vcsAllStatus))
       (bind(registerRpcMethod, "git_commit", vcsCommit))
       (bind(registerRpcMethod, "git_push", vcsPush))
+      (bind(registerRpcMethod, "git_push_branch", vcsPushBranch))
       (bind(registerRpcMethod, "git_pull", vcsPull))
       (bind(registerRpcMethod, "git_diff_file", vcsDiffFile))
       (bind(registerRpcMethod, "git_apply_patch", vcsApplyPatch))
@@ -3004,6 +3056,7 @@ core::Error initialize()
       (bind(registerRpcMethod, "git_init_repo", vcsInitRepo))
       (bind(registerRpcMethod, "git_get_ignores", vcsGetIgnores))
       (bind(registerRpcMethod, "git_set_ignores", vcsSetIgnores))
+      (bind(registerRpcMethod, "git_list_remotes", vcsListRemotes))
       (bind(registerRpcMethod, "git_github_remote_url", vcsGithubRemoteUrl));
    error = initBlock.execute();
    if (error)

src/gwt/src/org/rstudio/studio/client/common/vcs/GitServerOperations.java
@@ -15,6 +15,7 @@
 package org.rstudio.studio.client.common.vcs;
 
 import com.google.gwt.core.client.JsArray;
+import com.google.gwt.core.client.JsArrayString;
 
 import org.rstudio.core.client.files.FileSystemItem;
 import org.rstudio.core.client.jsonrpc.RpcObjectList;
@@ -69,6 +70,8 @@ public interface GitServerOperations extends VCSServerOperations
                         ServerRequestCallback<ConsoleProcess> requestCallback);
    
    void gitListBranches(ServerRequestCallback<BranchesInfo> requestCallback);
+   
+   void gitListRemotes(ServerRequestCallback<JsArrayString> requestCallback);
 
    void gitCheckout(String id,
                     ServerRequestCallback<ConsoleProcess> requestCallback);
@@ -123,6 +126,10 @@ public interface GitServerOperations extends VCSServerOperations
                       ServerRequestCallback<Void> requestCallback);
 
    void gitPush(ServerRequestCallback<ConsoleProcess> requestCallback);
+   
+   void gitPushBranch(String branch,
+                      String remote,
+                      ServerRequestCallback<ConsoleProcess> requestCallback);
 
    void gitPull(ServerRequestCallback<ConsoleProcess> requestCallback);
    

src/gwt/src/org/rstudio/studio/client/server/remote/RemoteServer.java
@@ -2153,6 +2153,12 @@ public class RemoteServer implements Server
    {
       sendRequest(RPC_SCOPE, GIT_LIST_BRANCHES, requestCallback);
    }
+   
+   @Override
+   public void gitListRemotes(ServerRequestCallback<JsArrayString> requestCallback)
+   {
+      sendRequest(RPC_SCOPE, GIT_LIST_REMOTES, requestCallback);
+   }
 
    @Override
    public void gitCheckout(String id,
@@ -2205,6 +2211,18 @@ public class RemoteServer implements Server
       sendRequest(RPC_SCOPE, GIT_PUSH,
                   new ConsoleProcessCallbackAdapter(requestCallback));
    }
+   
+   public void gitPushBranch(String branch,
+                             String remote,
+                             ServerRequestCallback<ConsoleProcess> requestCallback)
+   {
+      JSONArray params = new JSONArray();
+      params.set(0, new JSONString(branch));
+      params.set(1, new JSONString(remote));
+      sendRequest(RPC_SCOPE, GIT_PUSH_BRANCH, params,
+                  new ConsoleProcessCallbackAdapter(requestCallback));
+   }
+   
 
    @Override
    public void vcsClone(VcsCloneOptions options,
@@ -5180,9 +5198,11 @@ public class RemoteServer implements Server
    private static final String GIT_FULL_STATUS = "git_full_status";
    private static final String GIT_CREATE_BRANCH = "git_create_branch";
    private static final String GIT_LIST_BRANCHES = "git_list_branches";
+   private static final String GIT_LIST_REMOTES = "git_list_remotes";
    private static final String GIT_CHECKOUT = "git_checkout";
    private static final String GIT_COMMIT = "git_commit";
    private static final String GIT_PUSH = "git_push";
+   private static final String GIT_PUSH_BRANCH = "git_push_branch";
    private static final String GIT_PULL = "git_pull";
    private static final String ASKPASS_COMPLETED = "askpass_completed";
    private static final String CREATE_SSH_KEY = "create_ssh_key";

src/gwt/src/org/rstudio/studio/client/workbench/views/vcs/CreateBranchDialog.java
@@ -1,6 +1,113 @@
+/*
+ * CreateBranchDialog.java
+ *
+ * Copyright (C) 2009-17 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
 package org.rstudio.studio.client.workbench.views.vcs;
 
-public class CreateBranchDialog
-{
+import org.rstudio.core.client.widget.ModalDialog;
+import org.rstudio.core.client.widget.OperationWithInput;
 
+import com.google.gwt.core.client.JsArrayString;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class CreateBranchDialog extends ModalDialog<CreateBranchDialog.Input>
+{
+   public static class Input
+   {
+      public Input(String branch, String remote, boolean push)
+      {
+         branch_ = branch;
+         remote_ = remote;
+         push_ = push;
+      }
+      
+      public final String getBranch()
+      {
+         return branch_;
+      }
+      
+      public final String getRemote()
+      {
+         return remote_;
+      }
+      
+      public final boolean getPush()
+      {
+         return push_;
+      }
+      
+      private final String branch_;
+      private final String remote_;
+      private final boolean push_;
+   }
+
+   @Override
+   protected Input collectInput()
+   {
+      return new Input(
+            tbBranch_.getValue().trim(),
+            tbRemote_.getValue().trim(),
+            cbPush_.getValue());
+   }
+   
+   public CreateBranchDialog(final String caption,
+                             final JsArrayString remotes,
+                             final OperationWithInput<Input> operation)
+   {
+      super(caption, operation);
+      
+      setOkButtonCaption("Create");
+      
+      container_ = new VerticalPanel();
+      tbBranch_ = new TextBox();
+      tbRemote_ = new TextBox();
+      cbPush_ = new CheckBox("Push branch to remote");
+      
+      final String remote = remotes.length() == 0 ? "origin" : remotes.get(0);
+      tbRemote_.setValue(remote);
+      
+      cbPush_.setValue(true);
+      
+      Grid grid = new Grid(2, 2);
+      grid.setWidget(0, 0, new Label("Branch:"));
+      grid.setWidget(0, 1, tbBranch_);
+      grid.setWidget(1, 0, new Label("Remote:"));
+      grid.setWidget(1, 1, tbRemote_);
+      
+      container_.add(grid);
+      container_.add(cbPush_);
+   }
+
+   @Override
+   protected Widget createMainWidget()
+   {
+      return container_;
+   }
+   
+   @Override
+   public void showModal()
+   {
+      super.showModal();
+      tbBranch_.setFocus(true);
+   }
+   
+   private final VerticalPanel container_;
+   private final TextBox tbBranch_;
+   private final TextBox tbRemote_;
+   private final CheckBox cbPush_;
 }

src/gwt/src/org/rstudio/studio/client/workbench/views/vcs/CreateBranchToolbarButton.java
@@ -25,13 +25,16 @@ import org.rstudio.core.client.widget.OperationWithInput;
 import org.rstudio.core.client.widget.ToolbarButton;
 import org.rstudio.studio.client.common.GlobalDisplay;
 import org.rstudio.studio.client.common.console.ConsoleProcess;
+import org.rstudio.studio.client.common.console.ProcessExitEvent;
 import org.rstudio.studio.client.common.icons.StandardIcons;
 import org.rstudio.studio.client.common.vcs.BranchesInfo;
 import org.rstudio.studio.client.common.vcs.GitServerOperations;
 import org.rstudio.studio.client.server.ServerError;
 import org.rstudio.studio.client.server.ServerRequestCallback;
+import org.rstudio.studio.client.workbench.views.vcs.CreateBranchDialog.Input;
 import org.rstudio.studio.client.workbench.views.vcs.common.ConsoleProgressDialog;
 
+import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.inject.Inject;
@@ -56,21 +59,41 @@ public class CreateBranchToolbarButton extends ToolbarButton
    @Override
    public void onClick(ClickEvent event)
    {
-      globalDisplay_.promptForText(
-            "Create Branch",
-            "Create a new branch:",
-            "",
-            new OperationWithInput<String>()
-            {
-               @Override
-               public void execute(String newBranch)
-               {
-                  onCreateBranch(newBranch);
-               }
-            });
+      gitServer_.gitListRemotes(new ServerRequestCallback<JsArrayString>()
+      {
+         @Override
+         public void onResponseReceived(JsArrayString remotes)
+         {
+            onListRemotes(remotes);
+         }
+
+         @Override
+         public void onError(ServerError error)
+         {
+            Debug.logError(error);
+         }
+      });
    }
    
-   private void onCreateBranch(final String newBranch)
+   private void onListRemotes(final JsArrayString remotes)
+   {
+      // TODO: If we have no remote branch set up, should we instruct
+      // the user to first set up that remote?
+      CreateBranchDialog dialog = new CreateBranchDialog(
+            "New Branch",
+            remotes,
+            new OperationWithInput<CreateBranchDialog.Input>()
+            {
+               @Override
+               public void execute(Input input)
+               {
+                  onCreateBranch(input);
+               }
+            });
+      dialog.showModal();
+   }
+   
+   private void onCreateBranch(final Input input)
    {
       gitServer_.gitListBranches(
             new ServerRequestCallback<BranchesInfo>()
@@ -78,7 +101,7 @@ public class CreateBranchToolbarButton extends ToolbarButton
                @Override
                public void onResponseReceived(BranchesInfo branchesInfo)
                {
-                  onBranchInfoReceived(newBranch, branchesInfo);
+                  onBranchInfoReceived(input, branchesInfo);
                }
                
                @Override
@@ -89,7 +112,7 @@ public class CreateBranchToolbarButton extends ToolbarButton
             });
    }
    
-   private void onBranchInfoReceived(final String newBranch,
+   private void onBranchInfoReceived(final Input input,
                                      final BranchesInfo branchesInfo)
    {
       // If we have a branch of this name already, prompt the
@@ -99,7 +122,7 @@ public class CreateBranchToolbarButton extends ToolbarButton
       boolean hasBranch = false;
       for (String branch : JsUtil.asIterable(branchesInfo.getBranches()))
       {
-         if (branch.equals(newBranch))
+         if (branch.equals(input.getBranch()))
          {
             hasBranch = true;
             break;
@@ -109,7 +132,7 @@ public class CreateBranchToolbarButton extends ToolbarButton
       if (hasBranch)
       {
          String message =
-               "A branch named '" + newBranch + "' already exists. Would " +
+               "A branch named '" + input.getBranch() + "' already exists. Would " +
                "you like to check out that branch, or overwrite that branch?";
          
          List<String> labels = new ArrayList<String>();
@@ -122,7 +145,7 @@ public class CreateBranchToolbarButton extends ToolbarButton
             @Override
             public void execute()
             {
-               onCheckout(newBranch);
+               onCheckout(input);
             }
          });
          operations.add(new Operation()
@@ -130,7 +153,7 @@ public class CreateBranchToolbarButton extends ToolbarButton
             @Override
             public void execute()
             {
-               onCreate(newBranch);
+               onCreate(input);
             }
          });
          
@@ -144,20 +167,31 @@ public class CreateBranchToolbarButton extends ToolbarButton
       }
       else
       {
-         onCreate(newBranch);
+         onCreate(input);
       }
    }
    
-   private void onCreate(final String newBranch)
+   private void onCreate(final Input input)
    {
       gitServer_.gitCreateBranch(
-            newBranch,
+            input.getBranch(),
             new ServerRequestCallback<ConsoleProcess>()
             {
                @Override
                public void onResponseReceived(ConsoleProcess process)
                {
-                  showConsoleProcessDialog(process);
+                  final ConsoleProgressDialog dialog =
+                        new ConsoleProgressDialog(process, gitServer_);
+                  dialog.showModal();
+                  dialog.writePrompt("> git checkout -B '" + input.getBranch() + "'\n");
+                  process.addProcessExitHandler(new ProcessExitEvent.Handler()
+                  {
+                     @Override
+                     public void onProcessExit(ProcessExitEvent event)
+                     {
+                        onCreateBranchFinished(input, event, dialog);
+                     }
+                  });
                }
                
                @Override
@@ -169,10 +203,37 @@ public class CreateBranchToolbarButton extends ToolbarButton
             });
    }
    
-   private void onCheckout(final String newBranch)
+   private void onCreateBranchFinished(final Input input,
+                                       final ProcessExitEvent event,
+                                       final ConsoleProgressDialog dialog)
+   {
+      if (event.getExitCode() == 0 && input.getPush())
+      {
+         dialog.writePrompt("> git push -u " + input.getRemote() + " " + input.getBranch() + "\n");
+         gitServer_.gitPushBranch(
+               input.getBranch(),
+               input.getRemote(),
+               new ServerRequestCallback<ConsoleProcess>()
+               {
+                  @Override
+                  public void onResponseReceived(ConsoleProcess process)
+                  {
+                     dialog.attachToProcess(process);
+                  }
+
+                  @Override
+                  public void onError(ServerError error)
+                  {
+                     Debug.logError(error);
+                  }
+               });
+      }
+   }
+   
+   private void onCheckout(final Input input)
    {
       gitServer_.gitCheckout(
-            newBranch,
+            input.getBranch(),
             new ServerRequestCallback<ConsoleProcess>()
             {
                @Override

src/gwt/src/org/rstudio/studio/client/workbench/views/vcs/git/GitPane.java
@@ -132,7 +132,6 @@ public class GitPane extends WorkbenchPane implements Display
       historyButton_.setText(width > 680 ? "History" : "");
       moreButton_.setText(width > 680 ? "More" : "");
       createBranchToolbarButton_.setText(width > 780 ? "Create Branch" : "");
-      
    }
 
    @Override

commit 36b13dfdf0c4f89a22f12c9cb659b0ad2e33a8a4

save console process before computing interaction mode; fixes #1810
this change fixes an issue introduced in
2b7bff8f28b37d73d6063338b511cedd9caa5fae; because getInteractionMode()
uses the saved console process, it needs to be set before we check the
interaction mode.

src/gwt/src/org/rstudio/studio/client/workbench/views/vcs/common/ConsoleProgressDialog.java
@@ -76,6 +76,9 @@ public class ConsoleProgressDialog extends ProgressDialog
          throw new IllegalArgumentException(
                "Invalid combination of arguments to ConsoleProgressDialog");
       }
+
+      // save console process so that we can compute interaction mode below
+      consoleProcess_ = consoleProcess;
       
       if (getInteractionMode() != ConsoleProcessInfo.INTERACTION_NEVER)
       {