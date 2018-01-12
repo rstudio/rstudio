3caa866264066fe0ff817dd3840bb5639ac461f9
Gary <gary@rstudio.com>
2018-01-12 12:37:28 -0800

    Show project in dock icon (Mac/QtWebEngine)
    Resolves #1954

---
+++ src/cpp/desktop/DockTileView.hpp
@@ -0,0 +1,33 @@
+/*
+ * DockTileView.hpp
+ *
+ * Copyright (C) 2009-18 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#ifndef DESKTOP_DOCK_TILE_VIEW_HPP
+#define DESKTOP_DOCK_TILE_VIEW_HPP
+
+#include <QString>
+
+namespace rstudio {
+namespace desktop {
+
+class DockTileView
+{
+public:
+   static void setLabel(const QString& label);
+};
+
+} // namespace desktop
+} // namespace rstudio
+
+#endif // DESKTOP_DOCK_TILE_VIEW_HPP
