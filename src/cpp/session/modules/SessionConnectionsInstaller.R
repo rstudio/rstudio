479b92eea1a13529aebb0655cbf295081d638f46
Javier Luraschi <javierluraschi@hotmail.com>
2018-02-23 14:53:07 -0800

    support to check and install odbc prereqs

--- src/cpp/session/modules/SessionConnectionsInstaller.R
+++ src/cpp/session/modules/SessionConnectionsInstaller.R
@@ -13,16 +13,21 @@
 #
 #
 
-odbc_bundle_name <- function(placeholder) {
+odbc_bundle_os_name <- function() {
   os_mapping <- list (
     Linux = "linux",
     Windows = "windows",
     Darwin = "osx"
   )
+  
   if (!Sys.info()["sysname"] %in% names(os_mapping))
     stop("Operating system \"", Sys.info()["sysname"], "\" is unsupported")
   
-  os_name <- os_mapping[[Sys.info()[["sysname"]]]]
+  os_mapping[[Sys.info()[["sysname"]]]]
+}
+
+odbc_bundle_name <- function(placeholder) {
+  os_name <- odbc_bundle_os_name()
   bitness <- if (grepl("64", Sys.info()["machine"])) "64" else "32"
   
   bundle_name <- gsub("\\(os\\)", os_name, placeholder)
@@ -55,10 +60,80 @@ odbc_bundle_extract <- function(url, placeholder, install_path) {
   install_path
 }
 
+odbc_bundle_check_prereqs_unixodbc <- function() {
+  identical(system2("odbcinst", stdout = FALSE), 1L)
+}
+
+odbc_bundle_check_prereqs_brew <- function() {
+  identical(system2("brew", stdout = FALSE), 1L)
+}
+
+odbc_bundle_check_prereqs_osx <- function() {
+  if (!odbc_bundle_check_prereqs_unixodbc()) {
+    if (!odbc_bundle_check_prereqs_brew()) {
+      message("Installing Brew...")
+      system2(
+        "/usr/bin/ruby",
+        args = list(
+          "-e",
+          "\"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
+        )
+      )
+    }
+    
+    message("Installing unixODBC...")
+    system2(
+      "brew",
+      args = list("install", "unixodbc")
+    )
+  }
+  
+  if (!odbc_bundle_check_prereqs_unixodbc())
+    stop("Failed to install unixODBC, please install from www.unixodbc.org")
+}
+
+odbc_bundle_check_prereqs_linux <- function() {
+  if (!odbc_bundle_check_prereqs_unixodbc())
+    stop("unixODBC is not installed, please install from www.unixodbc.org")
+}
+
+odbc_bundle_check_prereqs_windows <- function() {
+  
+}
+
+odbc_bundle_check_prereqs <- function() {
+  os_prereqs <- list(
+    osx = odbc_bundle_check_prereqs_osx,
+    windows = odbc_bundle_check_prereqs_windows,
+    linux = odbc_bundle_check_prereqs_linux
+  )
+  
+  prereqs <- os_prereqs[[odbc_bundle_os_name()]]
+  message("Checking Prerequisites...")
+  prereqs()
+}
+
+odbc_bundle_odbcini_path <- function() {
+  config <- system2("odbcinst", "-j", stdout = TRUE)
+  odbcini_entry <- config[grepl("odbcinst.ini", config)]
+  gsub("^[^/\\\\]*", "", odbcini_entry)
+}
+
+odbc_bundle_register <- function() {
+  
+}
+
 odbc_bundle_install <- function(url, placeholder, install_path) {
   install_path <- normalizePath(install_path, mustWork = FALSE)
   
+  # Check prerequisites
+  odbc_bundle_check_prereqs()
+  
+  # Download and extract
   odbc_bundle_extract(url, placeholder, install_path)
+  
+  # Register
+  odbc_bundle_register()
 }
 
 odbc_bundle_install(
