1fef71f00fe6764695f816ec69ee28fcf1f9d214
Jonathan McPherson <jonathan@rstudio.com>
2018-03-08 17:14:28 -0800

    basic job fields (WIP)

---
+++ src/cpp/session/modules/jobs/Job.cpp
@@ -0,0 +1,95 @@
+/*
+ * SessionJob.cpp
+ *
+ * Copyright (C) 2009-18 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#include "Job.hpp"
+
+#define kJobId          "id"
+#define kJobName        "name"
+#define kJobStatus      "status"
+#define kJobProgress    "progress"
+#define kJobMax         "max"
+#define kJobRunning     "running"
+#define kJobCompleted   "completed"
+
+using namespace rstudio::core;
+
+namespace rstudio {
+namespace session {
+namespace modules { 
+namespace jobs {
+
+Job::Job():
+   max_(0),
+   progress_(0),
+   running_(false),
+   completed_(false)
+{
+}
+
+std::string Job::id()
+{
+    return id_;
+}
+
+std::string Job::name()
+{
+    return name_;
+}
+
+std::string Job::status()
+{
+    return status_;
+}
+
+std::string Job::group()
+{
+    return group_;
+}
+
+int Job::progress()
+{
+    return progress_;
+}
+
+int Job::max()
+{
+    return max_;
+}
+
+bool Job::running()
+{
+    return running_;
+}
+
+bool Job::completed()
+{
+    return completed_;
+}
+
+core::json::Object Job::toJson()
+{
+
+}
+
+Job job::fromJson(const core::json::Object& src)
+{
+}
+
+
+} // namepsace jobs
+} // namespace modules
+} // namespace session
+} // namespace rstudio
+
---
+++ src/cpp/session/modules/jobs/Job.hpp
@@ -0,0 +1,76 @@
+/*
+ * Job.hpp
+ *
+ * Copyright (C) 2009-18 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#ifndef SESSION_JOBS_JOB_HPP
+#define SESSION_JOBS_JOB_HPP
+
+namespace rstudio {
+namespace session {
+namespace modules {      
+namespace jobs {
+
+class Job
+{
+public:
+   Job();
+
+   // job ID (machine-generated)
+   std::string id();
+
+   // name of job (user-defined)
+   std::string name();
+
+   // job status; updated throughout its lifetime
+   std::string status();
+
+   // group job belongs to
+   std::string group();
+
+   // the number of progress units that have been completed so far
+   int progress();
+
+   // the total number of progress units
+   int max();
+
+   // whether the job is currently executing
+   bool running();
+
+   // whether the job has completed
+   bool completed();
+
+   // convert job to/from JSON
+   core::json::Object toJson();
+   static Job fromJson(const core::json::Object& src);
+
+private:
+   std::string id_;
+   std::string name_;
+   std::string status_;
+   std::string group_;
+
+   int max_;
+   int progress_;
+
+   bool running_;
+   bool completed_;
+}
+
+
+} // namespace jobs
+} // namespace modules
+} // namespace session
+} // namespace rstudio
+
+#endif // SESSION_JOBS_JOB_HPP
