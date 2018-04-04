28fa640ef77c361a64f8eb0ebbe470d90ab19a34
Kevin Ushey <kevinushey@gmail.com>
2018-04-04 15:29:13 -0700

    remove now-unneeded base64 download code
    Closes #2031. We may still need some simple UI affordances for
    communicating download status to a user, but that will be handled in a
    separate PR.

--- src/cpp/desktop/DesktopWebPage.cpp
+++ src/cpp/desktop/DesktopWebPage.cpp
@@ -282,11 +282,7 @@ bool WebPage::acceptNavigationRequest(const QUrl &url,
    {
       bool navigated = false;
       
-      if (url.scheme() == QStringLiteral("data"))
-      {
-         handleBase64Download(url);
-      }
-      else if (allowExternalNav_)
+      if (allowExternalNav_)
       {
          // if allowing external navigation, follow this (even if a link click)
          return true;
@@ -309,99 +305,6 @@ bool WebPage::acceptNavigationRequest(const QUrl &url,
          this->view()->window()->deleteLater();
 
       return false;
-   }
-}
-
-void WebPage::handleBase64Download(QUrl url)
-{
-   // look for beginning of base64 data
-   QString base64 = QString::fromUtf8("base64,");
-   int pos = url.path().indexOf(base64);
-   if (pos == -1)
-   {
-      LOG_ERROR_MESSAGE("Base64 designator not found in data url");
-      return;
-   }
-
-   // extract the base64 data from the uri
-   QString base64Data = url.path();
-   base64Data.remove(0, pos + base64.length());
-   QByteArray base64ByteArray(base64Data.toStdString().c_str());
-   QByteArray byteArray = QByteArray::fromBase64(base64ByteArray);
-
-   // find the a tag in the page with this href
-
-   // TODO: may need to use custom JavaScript here?
-   // QWebElement aTag;
-   // QString urlString = url.toString(QUrl::None);
-   // QWebElementCollection aElements = pWebPage->findAllElements(
-   //                                           QString::fromUtf8("a"));
-   // for (int i=0; i<aElements.count(); i++)
-   // {
-   //    QWebElement a = aElements.at(i);
-   //    QString href = a.attribute(QString::fromUtf8("href"));
-   //    href.replace(QChar::fromLatin1('\n'), QString::fromUtf8(""));
-   //    if (href == urlString)
-   //    {
-   //       aTag = a;
-   //       break;
-   //    }
-   // }
-
-   // // if no a tag was found then bail
-   // if (aTag.isNull())
-   // {
-   //    LOG_ERROR_MESSAGE("Unable to finding matching a tag for data url");
-   //    return;
-   // }
-
-   // // get the download attribute (default filename)
-   // QString download = aTag.attribute(QString::fromUtf8("download"));
-
-   QString download = QString::fromUtf8("");
-   QString defaultFilename = defaultSaveDir_.absoluteFilePath(download);
-
-   // prompt for filename
-   QString filename = QFileDialog::getSaveFileName(
-                                            nullptr,
-                                            tr("Download File"),
-                                            defaultFilename,
-                                            defaultSaveDir_.absolutePath(),
-                                            nullptr,
-                                            standardFileDialogOptions());
-   if (!filename.isEmpty())
-   {
-      // see if we need to force the extension
-      FilePath defaultFilePath(defaultFilename.toUtf8().constData());
-      FilePath chosenFilePath(filename.toUtf8().constData());
-      if (chosenFilePath.extension().empty() &&
-          !defaultFilePath.extension().empty())
-      {
-         filename += QString::fromStdString(defaultFilePath.extension());
-      }
-
-      // write the file
-      QFile file(filename);
-      if (file.open(QIODevice::WriteOnly))
-      {
-         if (file.write(byteArray) == -1)
-         {
-            showFileError(QString::fromUtf8("writing"),
-                          filename,
-                          file.errorString());
-         }
-
-         file.close();
-      }
-      else
-      {
-         showFileError(QString::fromUtf8("writing"),
-                       filename,
-                       file.errorString());
-      }
-
-      // persist the defaultSaveDir
-      defaultSaveDir_ = QFileInfo(file).dir();
    }
 }
 
