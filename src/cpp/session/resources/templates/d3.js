63d1b29a8d276eeb97e15870c4ee9aa133fe1a12
jjallaire <jj@rstudio.com>
2018-04-11 12:48:14 -0400

    tweak d3 template

--- src/cpp/session/resources/templates/d3.js
+++ src/cpp/session/resources/templates/d3.js
@@ -1,6 +1,6 @@
 // !preview r2d3 data=c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
 //
-// R2D3: https://rstudio.github.io/r2d3
+// r2d3: https://rstudio.github.io/r2d3
 //
 
 var barHeight = Math.floor(height / data.length);
