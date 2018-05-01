fd02536d808ef3a50215e9be71f138c828a6c37b
Javier Luraschi <javierluraschi@hotmail.com>
2018-05-01 10:58:39 -0700

    add support to execute multiple sql statements

--- src/cpp/session/resources/templates/query.sql
+++ src/cpp/session/resources/templates/query.sql
@@ -1,3 +1,7 @@
 -- !preview dbGetQuery conn=DBI::dbConnect(RSQLite::SQLite())
 
-SELECT 1
+CREATE TABLE t(x INTEGER PRIMARY KEY ASC, y, z);
+
+INSERT INTO t(x, y, z) VALUES(1, 2, 3);
+
+SELECT * FROM t;
