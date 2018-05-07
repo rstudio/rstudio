9d02d25d024899a27b785f35c328c1e80bbb08ef
karl@rstudio.com <karl@rstudio.com>
2018-05-07 11:10:35 -0500

    Code review feedback - quitting instead of suspending at a higher level to ensure suspend-specific functionality works regardless of the new setting

--- src/cpp/session/SessionHttpMethods.cpp
+++ src/cpp/session/SessionHttpMethods.cpp
@@ -490,6 +490,17 @@ bool waitForMethod(const std::string& method,
             // note that we timed out
             suspend::setSuspendedFromTimeout(true);
 
+            if (!options().timeoutSuspend())
+            {
+               // configuration dictates that we should quit the
+               // session instead of suspending when timeout occurs
+               //
+               // the conditions for the quit must be the same as those
+               // for a regular suspend
+               rstudio::r::session::quit(false, EXIT_SUCCESS); // does not return
+               return false;
+            }
+
             // attempt to suspend (does not return if it succeeds)
             if (!suspend::suspendSession(false))
             {
