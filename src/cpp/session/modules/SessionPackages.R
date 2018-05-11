29b09c6d6b8c3c7849352cce79b479e0e22019b1
Jonathan McPherson <jonathan@rstudio.com>
2018-05-11 15:30:02 -0700

    allow S3 overrides to survive package attachment (for R Notebooks)

--- src/cpp/session/modules/SessionPackages.R
+++ src/cpp/session/modules/SessionPackages.R
@@ -50,6 +50,12 @@ if (identical(as.character(Sys.info()["sysname"]), "Darwin") &&
                                      .rs.pathPackage(pkgname, quiet=TRUE)),
                               loaded=status)
          .rs.enqueClientEvent("package_status_changed", packageStatus)
+         if (isTRUE(status)) 
+         {
+            # when a package is attached, it can register S3 methods which replace overrides we've
+            # attached manually; take this opportunity to reattach them.
+            .rs.reattachS3Overrides()
+         }
       }
    
    notifyPackageLoaded <- function(pkgname, ...)
