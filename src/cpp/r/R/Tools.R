47c83c85b70263db2c7d09070f643fc56db845ef
Javier Luraschi <javierluraschi@hotmail.com>
2018-05-14 10:23:19 -0700

    avoid implicit relative urls while downloading secondary repos file to fix #2776

--- src/cpp/r/R/Tools.R
+++ src/cpp/r/R/Tools.R
@@ -1014,3 +1014,9 @@ assign(envir = .rs.Env, ".rs.hasVar", function(name)
 .rs.addFunction("isDesktop", function() {
    identical(.Call("rs_rstudioProgramMode"), "desktop")
 })
+
+# complete url with path
+.rs.addFunction("completeUrl", function(url, path)
+{
+  .Call("rs_completeUrl", url, path)
+})
