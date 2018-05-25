5902b7deba96d78a441baf363313210d5a0e1f22
Kevin Ushey <kevinushey@gmail.com>
2018-05-25 10:15:24 -0700

    code style + indentation

--- src/cpp/session/resources/templates/plumber/plumber.R
+++ src/cpp/session/resources/templates/plumber/plumber.R
@@ -14,22 +14,22 @@ library(plumber)
 #* Echo back the input
 #* @param msg The message to echo
 #* @get /echo
-function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
+function(msg = "") {
+    list(msg = paste0("The message is: '", msg, "'"))
 }
 
 #* Plot a histogram
 #* @png
 #* @get /plot
-function(){
-  rand <- rnorm(100)
-  hist(rand)
+function() {
+    rand <- rnorm(100)
+    hist(rand)
 }
 
 #* Return the sum of two numbers
 #* @param a The first number to add
 #* @param b The second number to add
 #* @post /sum
-function(a, b){
-  as.numeric(a) + as.numeric(b)
+function(a, b) {
+    as.numeric(a) + as.numeric(b)
 }
