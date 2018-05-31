d60b46f99a3d522ab91ab4d986b9c04581f275f2
jjallaire <jj@rstudio.com>
2018-05-31 12:30:19 -0400

    install src package for r2d3

--- src/gwt/src/org/rstudio/studio/client/common/dependencies/DependencyManager.java
+++ src/gwt/src/org/rstudio/studio/client/common/dependencies/DependencyManager.java
@@ -135,7 +135,7 @@ public class DependencyManager implements InstallShinyEvent.Handler,
          new Dependency[] {
             Dependency.cranPackage("htmltools", "0.3.6"),
             Dependency.cranPackage("htmlwidgets", "1.2", true),
-            Dependency.cranPackage("r2d3", "0.2.0")
+            Dependency.cranPackage("r2d3", "0.2.0", true)
          },
          true,
          new CommandWithArg<Boolean>()
