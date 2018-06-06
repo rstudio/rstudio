03acb57e8274454d1ead8f4285d8fe5f2b249237
Gary <gary@rstudio.com>
2018-06-06 16:38:40 -0700

    Tweaks to windows-configuration script (still WIP)
    - Detect if Qt failed to install, happens with online installer on occasion (maybe a timeout, not sure). Give message, suggest user re-run the script.
    - Tweaks to avoid reinstalling already-installed components (those not installed with Chocolatey)
    - Tweaks to windows-sdk install
    - Tweaks to path to support JOM builds inside Qt Creator

--- dependencies/windows/Install-RStudio-Prereqs.ps1
+++ dependencies/windows/Install-RStudio-Prereqs.ps1
@@ -1,27 +1,12 @@
-﻿#
-# Install-RStudio-Prereqs.ps1
-#
-# Copyright (C) 2009-18 by RStudio, Inc.
-#
-# Unless you have received this program directly from RStudio pursuant
-# to the terms of a commercial license agreement with RStudio, then
-# this program is licensed to you under the terms of version 3 of the
-# GNU Affero General Public License. This program is distributed WITHOUT
-# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
-# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
-# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
-# 
-
-# ----------------------------------------------------------------------------
-# Bootstraps a clean Windows-10 system for development of RStudio.
+﻿# ----------------------------------------------------------------------------
+# Bootstrap a clean Windows-10 system for development of RStudio.
 #
 # Run this from an Administrator PowerShell prompt after enabling scripts
 # via 'Set-ExecutionPolicy Unrestricted -force'.
-#
-# After this script finishes, you must clone the rstudio repo and run
-# rstudio\dependencies\windows\install-dependencies.cmd to complete
-# setting up the build environment.
 # ----------------------------------------------------------------------------
+
+# Set to $false to keep downloads after installing; helpful for debugging script
+$DeleteDownloads = $true
 
 function Test-Administrator
 {
@@ -29,50 +14,24 @@ function Test-Administrator
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
-If (-Not (Test-Administrator)) {
-    Write-Host "Error: Must run this script as Administrator"
-    exit
+function Test-Qt-Installed([String] $QtVersion)
+{
+    $Qt1 = "C:\Qt\$QtVersion"
+    $Qt2 = "C:\Qt$QtVersion"
+    $Qt3 = "C:\Qt\Qt$QtVersion"
+
+    (Test-Path -Path $Qt1) -or (Test-Path -Path $Qt2) -or (Test-Path -Path $Qt3)
 }
 
-# install R
-Write-Host "Downloading R..."
-Invoke-WebRequest https://cran.rstudio.com/bin/windows/base/R-3.5.0-win.exe -OutFile c:\R-3.5.0-win.exe
-Write-Host "Installing R..."
-Start-Process c:\R-3.5.0-win.exe -Wait -ArgumentList '/VERYSILENT /DIR="C:\R\R-3.5.0\"'
-Remove-Item c:\R-3.5.0-win.exe -Force
-$env:path += ';C:\R\R-3.5.0\bin\i386\'
-[Environment]::SetEnvironmentVariable('Path', $env:path, [System.EnvironmentVariableTarget]::Machine);
-
-# install chocolatey
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-refreshenv
-
-# install some deps via chocolatey
-choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=""System""' --fail-on-error-output
-refreshenv
-choco install -y jdk8 ant windows-sdk-7.1 7zip git
-
-# install nsis (version on chocolatey is too new)
-Write-Host "Downloading NSIS..."
-Invoke-WebRequest https://s3.amazonaws.com/rstudio-buildtools/test-qt-windows/nsis-2.50-setup.exe -OutFile C:\nsis-2.50-setup.exe
-Write-Host "Installing NSIS..."
-Start-Process c:\nsis-2.50-setup.exe -Wait -ArgumentList '/S'
-Remove-Item c:\nsis-2.50-setup.exe
-
-# install visual c++ build tools (MSVC 2015 32 and 64-bit)
-Write-Host "Downloading Visual C++ Build Tools..."
-Invoke-WebRequest https://s3.amazonaws.com/rstudio-buildtools/visualcppbuildtools_full.exe -OutFile C:\visualcppbuildtools_full.exe
-Write-Host "Installing Visual C++ Build Tools..."
-Start-Process c:\visualcppbuildtools_full.exe -Wait -ArgumentList '/Quiet'
-Remove-Item c:\visualcppbuildtools_full.exe
-
-# cpack (an alias from chocolatey) and cmake's cpack conflict.
-Remove-Item -Force 'C:\ProgramData\chocolatey\bin\cpack.exe'
-
-# install Qt and Qt Creator
-Write-Host "Downloading Qt online installer..."
-Invoke-WebRequest https://download.qt.io/archive/online_installers/3.0/qt-unified-windows-x86-3.0.4-online.exe -OutFile c:\qt.exe
-$QtScript = @"
+function Install-Qt {
+    $QtInstaller = "C:\qt.exe"
+    if (-Not (Test-Path -Path $QtInstaller)) {
+        Write-Host "Downloading Qt online installer..."
+        Invoke-WebRequest https://download.qt.io/archive/online_installers/3.0/qt-unified-windows-x86-3.0.4-online.exe -OutFile $QtInstaller
+    } else {
+        Write-Host "Using previously downloaded Qt online installer"
+    }
+    $QtScript = @"
 
 function Controller() {
     installer.autoRejectMessageBoxes();
@@ -134,16 +93,137 @@ Controller.prototype.FinishedPageCallback = function() {
     gui.clickButton(buttons.FinishButton);
 }
 "@
-$QtScript | Out-File -FilePath C:\qt.qs -Encoding ASCII
-Write-Host "Starting Qt installation. Be patient, don't click on the buttons!"
-Start-Process c:\qt.exe -Wait -ArgumentList '--script c:\qt.qs'
-Remove-Item c:\qt.exe -Force
-Remove-Item c:\qt.qs -Force
+    $QtScript | Out-File -FilePath C:\qt.qs -Encoding ASCII
+    Write-Host "Starting Qt installation. Be patient, don't click on the buttons!"
+    Start-Process $QtInstaller -Wait -ArgumentList '--script c:\qt.qs'
+    if ($DeleteDownloads) { Remove-Item $QtInstaller -Force }
+    Remove-Item c:\qt.qs -Force
+}
 
-Write-Host "-----------------------------------------------------------"
-Write-Host "Core dependencies installed. Next steps:"
-Write-Host "(1) Start a non-adminstrator Command Prompt"
-Write-Host "(2) git clone https://github.com/rstudio/rstudio"
-Write-Host "(3) change working dir to rstudio\src\dependencies\windows"
-Write-Host "(4) install-dependencies.cmd"
-Write-Host "-----------------------------------------------------------"
+function Broadcast-SettingChange {
+
+    # broadcast WM_SETTINGCHANGE so Explorer picks up new path
+    Add-Type -TypeDefinition @"
+        using System;
+        using System.Runtime.InteropServices;
+
+        public class NativeMethods
+        {
+            [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+            public static extern IntPtr SendMessageTimeout(
+                IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
+                uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
+        }
+"@
+
+    $HWND_BROADCAST = [IntPtr] 0xffff
+    $WM_SETTINGCHANGE = 0x1a
+    $SMTO_ABORTIFHUNG = 0x2
+    $result = [UIntPtr]::Zero
+
+    [void] ([Nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [UIntPtr]::Zero, 'Environment', $SMTO_ABORTIFHUNG, 5000, [ref] $result))
+}
+
+#######################################
+# script execution starts here
+#######################################
+If (-Not (Test-Administrator)) {
+    Write-Host "Error: Must run this script as Administrator"
+    exit
+}
+
+# install R
+if (-Not (Test-Path -Path "C:\R")) {
+    $RSetupPackage = "C:\R-3.5.0-win.exe"
+    if (-Not (Test-Path -Path $RSetupPackage)) {
+        Write-Host "Downloading R..."
+        Invoke-WebRequest https://cran.rstudio.com/bin/windows/base/R-3.5.0-win.exe -OutFile $RSetupPackage
+    } else {
+        Write-Host "Using previously downloaded R installer"
+    }
+    Write-Host "Installing R..."
+    Start-Process $RSetupPackage -Wait -ArgumentList '/VERYSILENT /DIR="C:\R\R-3.5.0\"'
+    if ($DeleteDownloads) { Remove-Item $RSetupPackage -Force }
+    $env:path += ';C:\R\R-3.5.0\bin\i386\'
+    [Environment]::SetEnvironmentVariable('Path', $env:path, [System.EnvironmentVariableTarget]::Machine);
+} else {
+    Write-Host "C:\R already exists, skipping R installation"
+}
+
+# install chocolatey
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+refreshenv
+
+# install some deps via chocolatey
+choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=""System""' --fail-on-error-output
+refreshenv
+choco install -y jdk8 ant windows-sdk-10.1 7zip git
+
+# fixups for Windows SDK
+mv "C:\Program Files (x86)\Windows Kits\10\bin\x86" "C:\Program Files (x86)\Windows Kits\10\bin\x86_orig"
+mv "C:\Program Files (x86)\Windows Kits\10\bin\x64" "C:\Program Files (x86)\Windows Kits\10\bin\x64_orig"
+cmd /c mklink /J "C:\Program Files (x86)\Windows Kits\10\bin\x86" "C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x86"
+cmd /c mklink /J "C:\Program Files (x86)\Windows Kits\10\bin\x64" "C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x64"
+
+# install nsis (version on chocolatey is too new)
+if (-Not (Test-Path -Path "C:\Program Files (x86)\NSIS")) {
+    $NSISSetup = 'C:\nsis-2.50-setup.exe'
+    Write-Host "Downloading NSIS..."
+    if (-Not (Test-Path $NSISSetup)) {
+        Invoke-WebRequest https://s3.amazonaws.com/rstudio-buildtools/test-qt-windows/nsis-2.50-setup.exe -OutFile $NSISSetup
+    } else {
+        Write-Host "Using previously downloaded NSIS installer"
+    }
+    Write-Host "Installing NSIS..."
+    Start-Process $NSISSetup -Wait -ArgumentList '/S'
+    if ($DeleteDownloads) { Remove-Item $NSISSetup }
+} else {
+    Write-Host "NSIS already found, skipping"
+}
+
+# install visual c++ build tools (MSVC 2015 32 and 64-bit)
+if (-Not (Test-Path "C:\Program Files (x86)\Microsoft Visual C++ Build Tools")) {
+    $VSBuildTools = 'C:\visualcppbuildtools_full.exe'
+    if (-Not (Test-Path $VSBuildTools)) {
+        Write-Host "Downloading Visual C++ Build Tools..."
+        Invoke-WebRequest https://s3.amazonaws.com/rstudio-buildtools/visualcppbuildtools_full.exe -OutFile C:\visualcppbuildtools_full.exe
+    } else {
+        Write-Host "Using previously downloaded VC++ Build Tools installer"
+    }
+    Write-Host "Installing Visual C++ Build Tools..."
+    Start-Process c:\visualcppbuildtools_full.exe -Wait -ArgumentList '/Quiet'
+    if ($DeleteDownloads) { Remove-Item c:\visualcppbuildtools_full.exe }
+} else {
+    Write-Host "Visual C++ BUild Tools already found, skipping"
+}
+
+# cpack (an alias from chocolatey) and cmake's cpack conflict.
+Remove-Item -Force 'C:\ProgramData\chocolatey\bin\cpack.exe'
+
+# install Qt and Qt Creator
+if (Test-Qt-Installed("5.10.1")) {
+    Write-Host "Qt already installed, skipping"
+} else {
+    Install-Qt
+}
+
+# Qt installation doesn't always work (maybe a timeout?)
+if (-Not (Test-Qt-Installed("5.10.1"))) {
+    Write-Host "Qt not installed; either install yourself or re-run this script to try again"
+} else {
+
+    # Add QtCreator binaries to path, needed for building with JOM
+    $oldpath = (Get-ItemProperty -Path ‘Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment’ -Name PATH).path
+    $newpath = "$oldpath;C:\Qt\Tools\QtCreator\bin"
+    Set-ItemProperty -Path ‘Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment’ -Name PATH -Value $newpath
+    Broadcast-SettingChange
+
+    Write-Host "-----------------------------------------------------------"
+    Write-Host "Core dependencies installed. Next steps:"
+    Write-Host "(1) Start a non-adminstrator Command Prompt"
+    Write-Host "(2) git clone https://github.com/rstudio/rstudio"
+    Write-Host "(3) change working dir to rstudio\src\dependencies\windows"
+    Write-Host "(4) install-dependencies.cmd"
+    Write-Host "(5) open Qt Creator, load rstudio\src\cpp\CMakelists.txt"
+    Write-Host "-----------------------------------------------------------"
+}
