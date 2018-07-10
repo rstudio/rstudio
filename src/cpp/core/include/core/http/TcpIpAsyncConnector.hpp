ae128d002de56d613f69df38f024f9669f5d1e96
karl@rstudio.com <karl@rstudio.com>
2018-07-10 16:43:23 -0500

    Open source companion commit for https://github.com/rstudio/rstudio-pro/pull/513

--- src/cpp/core/include/core/http/TcpIpAsyncConnector.hpp
+++ src/cpp/core/include/core/http/TcpIpAsyncConnector.hpp
@@ -23,8 +23,8 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/placeholders.hpp>
 
-
 #include <core/http/TcpIpSocketUtils.hpp>
+#include <core/Thread.hpp>
 
 // special version of unexpected exception handler which makes
 // sure to call the user's ErrorHandler
@@ -55,7 +55,9 @@ public:
    TcpIpAsyncConnector(boost::asio::io_service& ioService,
                        boost::asio::ip::tcp::socket* pSocket)
      : pSocket_(pSocket),
-       resolver_(ioService)
+       resolver_(ioService),
+       isConnected_(false),
+       hasFailed_(false)
    {
    }
 
@@ -63,11 +65,23 @@ public:
    void connect(const std::string& address,
                 const std::string& port,
                 const ConnectedHandler& connectedHandler,
-                const ErrorHandler& errorHandler)
+                const ErrorHandler& errorHandler,
+                const boost::posix_time::time_duration& timeout =
+                   boost::posix_time::time_duration(boost::posix_time::pos_infin))
    {
       // save handlers
       connectedHandler_ = connectedHandler;
       errorHandler_ = errorHandler;
+
+      if (!timeout.is_special())
+      {
+         // start a timer that will cancel any outstanding asynchronous operations
+         // when it elapses if the connection operation has not succeeded
+         pConnectionTimer_.reset(new boost::asio::deadline_timer(resolver_.get_io_service(), timeout));
+         pConnectionTimer_->async_wait(boost::bind(&TcpIpAsyncConnector::onConnectionTimeout,
+                                                   TcpIpAsyncConnector::shared_from_this(),
+                                                   boost::asio::placeholders::error));
+      }
 
       // start an async resolve
       boost::asio::ip::tcp::resolver::query query(address, port);
@@ -81,65 +95,117 @@ public:
 
 private:
 
+   void onConnectionTimeout(const boost::system::error_code& ec)
+   {
+      try
+      {
+         if (ec == boost::system::errc::operation_canceled)
+            return;
+
+         LOCK_MUTEX(mutex_)
+         {
+            if (isConnected_ || hasFailed_)
+               return;
+
+            // timer has elapsed and the socket is still not connected
+            // cancel any outstanding async operations
+            resolver_.cancel();
+            pSocket_->cancel();
+
+            // invoke error handler since the connection has failed
+            handleError(systemError(boost::system::errc::timed_out, ERROR_LOCATION));
+         }
+         END_LOCK_MUTEX
+      }
+      CATCH_UNEXPECTED_ASYNC_CONNECTOR_EXCEPTION
+   }
+
    void handleResolve(
          const boost::system::error_code& ec,
          boost::asio::ip::tcp::resolver::iterator endpoint_iterator)
    {
-      try
+      if (ec == boost::asio::error::operation_aborted)
+         return;
+
+      LOCK_MUTEX(mutex_)
       {
-         if (!ec)
+         if (hasFailed_)
+            return;
+
+         try
          {
-            // try endpoints until we successfully connect with one
-            boost::asio::ip::tcp::endpoint endpoint = *endpoint_iterator;
-            pSocket_->async_connect(
-               endpoint,
-               boost::bind(&TcpIpAsyncConnector::handleConnect,
-                           TcpIpAsyncConnector::shared_from_this(),
-                           boost::asio::placeholders::error,
-                           ++endpoint_iterator));
+            if (!ec)
+            {
+               // try endpoints until we successfully connect with one
+               boost::asio::ip::tcp::endpoint endpoint = *endpoint_iterator;
+               pSocket_->async_connect(
+                  endpoint,
+                  boost::bind(&TcpIpAsyncConnector::handleConnect,
+                              TcpIpAsyncConnector::shared_from_this(),
+                              boost::asio::placeholders::error,
+                              ++endpoint_iterator));
+            }
+            else
+            {
+               handleErrorCode(ec, ERROR_LOCATION);
+            }
          }
-         else
-         {
-            handleErrorCode(ec, ERROR_LOCATION);
-         }
+         CATCH_UNEXPECTED_ASYNC_CONNECTOR_EXCEPTION
       }
-      CATCH_UNEXPECTED_ASYNC_CONNECTOR_EXCEPTION
+      END_LOCK_MUTEX
    }
 
    void handleConnect(
          const boost::system::error_code& ec,
          boost::asio::ip::tcp::resolver::iterator endpoint_iterator)
    {
-      try
+      if (ec == boost::asio::error::operation_aborted)
+         return;
+
+      LOCK_MUTEX(mutex_)
       {
-         if (!ec)
+         if (hasFailed_)
+            return;
+
+         try
          {
-            if (connectedHandler_)
-               connectedHandler_();
+            if (!ec)
+            {
+               isConnected_ = true;
+
+               if (pConnectionTimer_)
+                  pConnectionTimer_->cancel();
+
+               if (connectedHandler_)
+                  connectedHandler_();
+            }
+            else if (endpoint_iterator !=
+                     boost::asio::ip::tcp::resolver::iterator())
+            {
+               // try next endpoint
+               pSocket_->close();
+               boost::asio::ip::tcp::endpoint endpoint = *endpoint_iterator;
+               pSocket_->async_connect(
+                  endpoint,
+                  boost::bind(&TcpIpAsyncConnector::handleConnect,
+                              TcpIpAsyncConnector::shared_from_this(),
+                              boost::asio::placeholders::error,
+                              ++endpoint_iterator));
+            }
+            else
+            {
+               handleErrorCode(ec, ERROR_LOCATION);
+            }
          }
-         else if (endpoint_iterator !=
-                  boost::asio::ip::tcp::resolver::iterator())
-         {
-            // try next endpoint
-            pSocket_->close();
-            boost::asio::ip::tcp::endpoint endpoint = *endpoint_iterator;
-            pSocket_->async_connect(
-               endpoint,
-               boost::bind(&TcpIpAsyncConnector::handleConnect,
-                           TcpIpAsyncConnector::shared_from_this(),
-                           boost::asio::placeholders::error,
-                           ++endpoint_iterator));
-         }
-         else
-         {
-            handleErrorCode(ec, ERROR_LOCATION);
-         }
+         CATCH_UNEXPECTED_ASYNC_CONNECTOR_EXCEPTION
       }
-      CATCH_UNEXPECTED_ASYNC_CONNECTOR_EXCEPTION
+      END_LOCK_MUTEX
    }
 
    void handleError(const Error& error)
    {
+      hasFailed_ = true;
+
       if (errorHandler_)
          errorHandler_(error);
    }
@@ -147,12 +213,18 @@ private:
    void handleErrorCode(const boost::system::error_code& ec,
                         const ErrorLocation& location)
    {
+      if (pConnectionTimer_)
+         pConnectionTimer_->cancel();
+
       handleError(Error(ec, location));
    }
 
    void handleUnexpectedError(const std::string& description,
                               const ErrorLocation& location)
    {
+      if (pConnectionTimer_)
+         pConnectionTimer_->cancel();
+
       Error error = systemError(boost::system::errc::state_not_recoverable,
                                 description,
                                 location);
@@ -164,6 +236,11 @@ private:
    boost::asio::ip::tcp::resolver resolver_;
    ConnectedHandler connectedHandler_;
    ErrorHandler errorHandler_;
+
+   bool isConnected_;
+   bool hasFailed_;
+   boost::mutex mutex_;
+   boost::shared_ptr<boost::asio::deadline_timer> pConnectionTimer_;
 };
 
 } // namespace http
