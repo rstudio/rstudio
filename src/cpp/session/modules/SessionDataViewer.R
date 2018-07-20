d0b20a25ebedb3c5bc83c2dff28da6c4bcae1ebe
Jonathan McPherson <jonathan@rstudio.com>
2018-07-19 22:28:28 -0700

    restrict filtering to non-objects

--- src/cpp/session/modules/SessionDataViewer.R
+++ src/cpp/session/modules/SessionDataViewer.R
@@ -123,7 +123,7 @@
       # for histograms, we support only the base R numeric class and its derivatives;
       # is.numeric can return true for values that can only be manipulated using
       # packages that are currently loaded (e.g. bit64's integer64)
-      else if (is.numeric(val) && inherits(x[[idx]], "numeric"))
+      else if (is.numeric(val) && !is.object(x[[idx]]))
       {
         # ignore missing and infinite values (i.e. let any filter applied
         # implicitly remove those values); if that leaves us with nothing,
