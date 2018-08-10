c1ca22f045a527a27bd0aa8fa04c1dd0a47d3b21
Dirk Schumacher <mail@dirk-schumacher.net>
2018-08-10 14:25:25 +0200

    Remove superfluous whitespaces in shiny template

--- src/cpp/session/resources/templates/shiny/server.R
+++ src/cpp/session/resources/templates/shiny/server.R
@@ -1,9 +1,9 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
@@ -11,16 +11,16 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-     
+
     output$distPlot <- renderPlot({
-      
+
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2] 
+        x    <- faithful[, 2]
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        
+
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
-      
+
     })
-    
+
 })
