78cd01622d5db07634e23dbca41cbf83e92ae105
Gary <gary@rstudio.com>
2018-09-05 19:42:20 -0700

    install fixed-version of MSVC tools with dev bootstrapper

--- dependencies/windows/Install-RStudio-Prereqs.ps1
+++ dependencies/windows/Install-RStudio-Prereqs.ps1
@@ -29,7 +29,7 @@ if (-Not (Test-Path -Path "C:\R")) {
     $RSetupPackage = "C:\R-3.5.0-win.exe"
     if (-Not (Test-Path -Path $RSetupPackage)) {
         Write-Host "Downloading R 3.5.0..."
-        Invoke-WebRequest https://cran.rstudio.com/bin/windows/base/old/3.5.0/R-3.5.0-win.exe -OutFile $RSetupPackage
+        Invoke-WebRequest https://cran.r-project.org/bin/windows/base/old/3.5.0/R-3.5.0-win.exe -OutFile $RSetupPackage
     } else {
         Write-Host "Using previously downloaded R installer"
     }
@@ -41,21 +41,23 @@ if (-Not (Test-Path -Path "C:\R")) {
 } else {
     Write-Host "C:\R already exists, skipping R installation"
 }
+if (-Not (Test-Path -Path "C:\R"))
+{
+    Write-Host "Error: Unable to install R"
+    exit
+}
 
 # install chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 refreshenv
 
 # install some deps via chocolatey
-choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=""System""' --fail-on-error-output
+choco install -y cmake --version 3.12.1 --installargs 'ADD_CMAKE_TO_PATH=""System""' --fail-on-error-output
 refreshenv
-choco install -y jdk8 ant windows-sdk-10.1  7zip git ninja
-
-# install Visual C++ via chocolatey
-# The workload step is failing in Docker so we're installing it via Visual Studio installer 
-# later in this file. Leaving here as a reminder to try chocolatey again in the future.
-# RUN choco install -y visualstudio2017buildtools
-# RUN choco install -y visualstudio2017-workload-vctools
+choco install -y jdk8 ant 7zip git ninja
+choco install -y windows-sdk-10.1 --version 10.1.17134.12
+choco install -y visualstudio2017buildtools --version 15.8.2.0
+choco install -y visualstudio2017-workload-vctools --version 1.3.0
 
 # install nsis (version on chocolatey is too new)
 if (-Not (Test-Path -Path "C:\Program Files (x86)\NSIS")) {
@@ -71,22 +73,6 @@ if (-Not (Test-Path -Path "C:\Program Files (x86)\NSIS")) {
     if ($DeleteDownloads) { Remove-Item $NSISSetup -Force }
 } else {
     Write-Host "NSIS already found, skipping"
-}
-
-# install visual c++
-if (-Not (Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio\2017")) {
-    $VSSetup = "C:\vs_buildtools_2017.exe"
-    Write-Host "Downloading VS Buildtools setup..."
-    if (-Not (Test-Path $VSSetup)) {
-        Invoke-WebRequest https://s3.amazonaws.com/rstudio-buildtools/vs_buildtools_2017.exe -OutFile $VSSetup
-    } else {
-        Write-Host "Using previously downloaded Visual Studio installer"
-    }
-    Write-Host "Installing Visual Studio Build Tools..."
-    Start-Process $VSSetup -Wait -ArgumentList '--quiet --add Microsoft.VisualStudio.Workload.VCTools'
-    if ($DeleteDownloads) { Remove-Item $VSSetup -Force }
-} else {
-    Write-Host "Visual Studio 2017 build tools already intalled, skipping"
 }
 
 # cpack (an alias from chocolatey) and cmake's cpack conflict.
