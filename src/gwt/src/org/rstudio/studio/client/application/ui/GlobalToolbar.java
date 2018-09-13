462d701c9b2bb711fd9f6e2f4b550bafbd08e808
Kevin Ushey <kevinushey@gmail.com>
2018-09-13 11:32:51 -0700

    reorganize file menu items

--- src/gwt/src/org/rstudio/studio/client/application/ui/GlobalToolbar.java
+++ src/gwt/src/org/rstudio/studio/client/application/ui/GlobalToolbar.java
@@ -63,10 +63,10 @@ public class GlobalToolbar extends Toolbar
       newMenu_.addSeparator();
       newMenu_.addItem(commands.newTextDoc().createMenuItem(false));
       newMenu_.addItem(commands.newCppDoc().createMenuItem(false));
-      newMenu_.addItem(commands.newStanDoc().createMenuItem(false));
       newMenu_.addItem(commands.newPythonDoc().createMenuItem(false));
-      newMenu_.addItem(commands.newD3Doc().createMenuItem(false));
       newMenu_.addItem(commands.newSqlDoc().createMenuItem(false));
+      newMenu_.addItem(commands.newStanDoc().createMenuItem(false));
+      newMenu_.addItem(commands.newD3Doc().createMenuItem(false));
       newMenu_.addSeparator();
       newMenu_.addItem(commands.newSweaveDoc().createMenuItem(false));
       newMenu_.addItem(commands.newRHTMLDoc().createMenuItem(false));
