dce75ecf931ec1223f109ee5bd4758f616e79972
Javier Luraschi <javierluraschi@hotmail.com>
2018-09-21 13:44:53 -0700

    show warning when package repos modified outside preferences to fix #3523

--- src/gwt/src/org/rstudio/studio/client/server/remote/RemoteServer.java
+++ src/gwt/src/org/rstudio/studio/client/server/remote/RemoteServer.java
@@ -1248,6 +1248,12 @@ public class RemoteServer implements Server
    {
       sendRequest(RPC_SCOPE, GET_CRAN_MIRRORS, requestCallback);
    }
+
+   public void getCRANActives(
+                  ServerRequestCallback<JsArray<CRANMirror>> requestCallback)
+   {
+      sendRequest(RPC_SCOPE, GET_CRAN_ACTIVES, requestCallback);
+   }
    
    public void suggestTopics(String prefix,
                              ServerRequestCallback<JsArrayString> requestCallback)
@@ -5692,6 +5698,7 @@ public class RemoteServer implements Server
    private static final String IS_PACKAGE_LOADED = "is_package_loaded";
    private static final String SET_CRAN_MIRROR = "set_cran_mirror";
    private static final String GET_CRAN_MIRRORS = "get_cran_mirrors";
+   private static final String GET_CRAN_ACTIVES = "get_cran_actives";
    private static final String PACKAGE_SKELETON = "package_skeleton";
    private static final String DISCOVER_PACKAGE_DEPENDENCIES = "discover_package_dependencies";
 
