4100b1c8211822caa7c289220aba9442e33fc2b9
Kevin Ushey <kevinushey@gmail.com>
2018-09-25 12:51:35 -0700

    fixes for Ace preview

---
+++ src/gwt/src/org/rstudio/studio/client/workbench/prefs/views/AceEditorPreview.js
@@ -0,0 +1,21 @@
+
+// Load required Ace modules
+var event = require('ace/lib/event');
+var Editor = require('ace/editor').Editor;
+var Renderer = require('ace/virtual_renderer').VirtualRenderer;
+var container = document.getElementById('editor');
+var RMode = require('mode/r').Mode;
+
+// Initialize the Ace editor
+var editor = ace.edit('editor');
+
+// Set up options
+editor.setReadOnly(true);
+editor.setHighlightActiveLine(false);
+editor.renderer.setHScrollBarAlwaysVisible(false);
+editor.renderer.setShowGutter(false);
+editor.renderer.setDisplayIndentGuides(false);
+
+// Load R mode
+editor.getSession().setMode(new RMode(false, editor.getSession()));
+ 
