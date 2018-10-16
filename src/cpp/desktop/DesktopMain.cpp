aa467b2c4773e583c0a06c6013d343a9c37491bc
Kevin Ushey <kevinushey@gmail.com>
2018-10-16 09:52:26 -0700

    revert buggy check for mixed-DPI monitor config

--- src/cpp/desktop/DesktopMain.cpp
+++ src/cpp/desktop/DesktopMain.cpp
@@ -274,35 +274,6 @@ bool useRemoteDevtoolsDebugging()
 
 QString inferDefaultRenderingEngine()
 {
-   // prefer software rendering in mixed-dpi multi-monitor
-   // configurations
-   // https://github.com/rstudio/rstudio/issues/3479
-   core::system::ProcessResult processResult;
-   Error error = core::system::runCommand(
-            "/usr/sbin/system_profiler SPDisplaysDataType | /usr/bin/grep 'Resolution:'",
-            core::system::ProcessOptions(),
-            &processResult);
-   
-   if (error || processResult.exitStatus != EXIT_SUCCESS)
-      return QStringLiteral("auto");
-
-   std::string output = string_utils::trimWhitespace(processResult.stdOut);
-   std::vector<std::string> lines = 
-         core::algorithm::split(output, "\n");
-   
-   // TODO: we likely need to handle case where e.g. a laptop is hooked up to
-   // multiple high DPI non-retina displays
-   int retinaDisplays = 0;
-   int totalDisplays = lines.size();
-   for (const std::string& line : lines)
-   {
-      if (line.find("Retina") != std::string::npos)
-         retinaDisplays += 1;
-   }
-   
-   if (retinaDisplays > 0 && retinaDisplays != totalDisplays)
-      return QStringLiteral("software");
-   
    return QStringLiteral("auto");
 }
 
