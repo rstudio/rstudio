2630340a96f2a0500191a3ecdb67898f69d79e08
Gary <gary@rstudio.com>
2018-12-05 13:58:28 -0800

    rename jobs as "Session Jobs" in the UI
    RSP will have other types of jobs, and we want to make a clearer distinction between them in the UI.
    
    The "Clear jobs" command will be going away, so not bothering to rename it.

--- src/gwt/src/org/rstudio/studio/client/workbench/views/jobs/model/JobManager.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/jobs/model/JobManager.java
@@ -415,7 +415,7 @@ public class JobManager implements JobRefreshEvent.Handler,
    
    private void showJobLauncherDialog(FileSystemItem path)
    {
-      JobLauncherDialog dialog = new JobLauncherDialog("Run Script as Job",
+      JobLauncherDialog dialog = new JobLauncherDialog("Run Script as Session Job",
             path.getName(),
             path,
             spec ->
