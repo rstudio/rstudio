aace9420637c2a575c81f63cf762c135823d9cce
Kevin Ushey <kevinushey@gmail.com>
2019-04-04 13:02:51 -0700

    disable horizontal overflow for DataGrid

--- src/gwt/src/org/rstudio/core/client/widget/RStudioDataGrid.java
+++ src/gwt/src/org/rstudio/core/client/widget/RStudioDataGrid.java
@@ -19,6 +19,7 @@ import org.rstudio.core.client.dom.DomUtils;
 
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NodeList;
+import com.google.gwt.dom.client.Style.Overflow;
 import com.google.gwt.user.cellview.client.DataGrid;
 import com.google.gwt.view.client.ProvidesKey;
 
@@ -79,6 +80,24 @@ public class RStudioDataGrid<T> extends DataGrid<T>
          if (doesNotSparkJoy)
             style.setOverflow(com.google.gwt.dom.client.Style.Overflow.HIDDEN);
          
+      }
+      
+      // GWT's DataGrid also occasionally displays unwanted horizontal
+      // scroll bars. Since we don't ever require horizontal scrolling
+      // in the DataGrid objects we show, we just suppress horizontal
+      // overflow in any div element that does something with overflow.
+      //
+      // Again, this is something we'd normally do with CSS but because
+      // we need to perform surgery on arbitrary sub-divs which otherwise
+      // have no class we must resort to JavaScript + inline styles.
+      //
+      // https://github.com/rstudio/rstudio/issues/4529
+      children = DomUtils.querySelectorAll(parent, "div[style*='overflow:']");
+      for (int i = 0; i < children.getLength(); i++)
+      {
+         Element el = children.getItem(i);
+         com.google.gwt.dom.client.Style style = el.getStyle();
+         style.setOverflowX(Overflow.HIDDEN);
       }
       
    }
