30e7c04c94181c03d875a973e21d9e08c9abee73
Jonathan McPherson <jonathan@rstudio.com>
2019-05-01 09:59:10 -0700

    Revert "fork versioning for patch release"
    This reverts commit 90cc020cb9ba49a24ac3e3a13d4240201073c683.

--- docker/jenkins/rstudio-version.sh
+++ docker/jenkins/rstudio-version.sh
@@ -57,7 +57,7 @@ fi
 
 # get historical open source patch versions from AWS; this file is a CSV that
 # contains each open source commit and the patch version associated with it
-aws s3 cp s3://rstudio-ide-build/version/$VERSION-patch/oss-patch.csv /tmp/oss-patch.csv $EXTRA_CP_ARGS
+aws s3 cp s3://rstudio-ide-build/version/$VERSION/oss-patch.csv /tmp/oss-patch.csv $EXTRA_CP_ARGS
 
 if [[ -e "upstream/VERSION" ]]; then
     # only one upstream commit (RStudio Pro, which is downstream)
@@ -109,7 +109,7 @@ fi
 # for pro, we need to determine the version suffix as well
 if [[ $PRO = true ]]; then
     SUFFIX=0
-    aws s3 cp s3://rstudio-ide-build/version/$VERSION-patch/pro-suffix.csv /tmp/pro-suffix.csv --quiet
+    aws s3 cp s3://rstudio-ide-build/version/$VERSION/pro-suffix.csv /tmp/pro-suffix.csv --quiet
     while read PRO_SUFFIX; do
         ENTRY=(${PRO_SUFFIX//,/ })
         ENTRY_PATCH=${ENTRY[0]}
@@ -160,7 +160,7 @@ case "$ACTION" in
             echo "Push updated patch history to S3 and git"
         else
             # upload to s3
-            aws s3 cp /tmp/oss-updated.csv s3://rstudio-ide-build/version/$VERSION-patch/oss-patch.csv --quiet
+            aws s3 cp /tmp/oss-updated.csv s3://rstudio-ide-build/version/$VERSION/oss-patch.csv --quiet
 
             # tag the release on git (TODO: need Jenkins creds to do this)
             # git tag "v$OSS_VERSION"
@@ -188,7 +188,7 @@ case "$ACTION" in
         if [[ $DEBUG = true ]]; then
             echo "Push updated suffix to S3 and git"
         else
-            aws s3 cp /tmp/pro-updated.csv s3://rstudio-ide-build/version/$VERSION-patch/pro-suffix.csv $EXTRA_CP_ARGS
+            aws s3 cp /tmp/pro-updated.csv s3://rstudio-ide-build/version/$VERSION/pro-suffix.csv $EXTRA_CP_ARGS
 
             # tag the release on git (TODO: need Jenkins creds to do this)
             # git tag "v$PRO_VERSION-pro"
