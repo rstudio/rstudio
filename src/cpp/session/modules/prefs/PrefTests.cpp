0c107167b4b2ed6b61cd58dfbe86068e9f213df2
Jonathan McPherson <jonathan@rstudio.com>
2019-05-09 15:27:37 -0700

    ensure every property has a default value

--- src/cpp/session/modules/prefs/PrefTests.cpp
+++ src/cpp/session/modules/prefs/PrefTests.cpp
@@ -20,6 +20,8 @@
 
 #include <session/SessionOptions.hpp>
 
+#include <core/FileSerializer.hpp>
+
 #include <tests/TestThat.hpp>
 
 using namespace rstudio::core;
@@ -30,8 +32,59 @@ namespace modules {
 namespace prefs {
 namespace tests {
 
+std::string findMissingDefaults(const FilePath& schemaFile)
+{
+   json::Value value;
+   std::string schemaContents;
+
+   // Load schema 
+   Error error = core::readStringFromFile(schemaFile, &schemaContents);
+   if (error)
+      return error.description();
+   error = json::parse(schemaContents, ERROR_LOCATION, &value);
+   if (error)
+      return error.description();
+
+   // Find properties (prefs)
+   json::Object schema = value.get_obj();
+   json::Object::iterator objProperties = schema.find("properties");
+   if (objProperties == schema.end() ||
+       (*objProperties).value().type() != json::ObjectType)
+   {
+      return "No properties found";
+   }
+   const json::Object& properties = (*objProperties).value().get_obj();
+
+   // Ensure each property has a default value
+   for (auto prop: properties)
+   {
+      const json::Object& definition = prop.value().get_obj();
+      json::Object::iterator def = definition.find("default");
+      if (def == definition.end())
+      {
+         return "Pref named '" + prop.name() + "' does not have a default.";
+      }
+   }
+
+   return "";
+}
+
 test_context("default validation")
 {
+   test_that("all user preferences have defaults")
+   {
+      std::string err = findMissingDefaults(
+         options().rResourcesPath().complete("schema").complete(kUserPrefsSchemaFile));
+      expect_equal(err, "");
+   }
+ 
+   test_that("all user state values have defaults")
+   {
+      std::string err = findMissingDefaults(
+         options().rResourcesPath().complete("schema").complete(kUserStateSchemaFile));
+      expect_equal(err, "");
+   }
+ 
    test_that("user preference defaults are valid according to their schema")
    {
       UserPrefsDefaultLayer defaults;
