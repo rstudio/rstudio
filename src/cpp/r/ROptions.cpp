ca9aa8200d5aec35abe13c848b72215bf373291a
Jonathan McPherson <jonathan@rstudio.com>
2019-05-13 11:21:14 -0700

    add error option if necessary

--- src/cpp/r/ROptions.cpp
+++ src/cpp/r/ROptions.cpp
@@ -108,6 +108,15 @@ SEXP setErrorOption(SEXP value)
          return previous;
       }
 
+      if (CDR(option) == R_NilValue && value != R_NilValue)
+      {
+         // no error option exists at all; add it so we can set the value
+         SETCDR(option, Rf_allocList(1));
+         SETCAR(CDR(option), value);
+         SET_TAG(CDR(option), errorTag);
+         break;
+      }
+
       // next option
       option = CDR(option);
    }
