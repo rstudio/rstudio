089cb58181b94397ca233023aecbd3e15a0cd7ed
Gary Ritchie <gary@rstudio.com>
2019-05-23 12:35:05 -0400

    mark tables in Horizontal and VerticalPanels as role=presentation

--- src/gwt/src/com/google/gwt/user/updatesources.sh
+++ src/gwt/src/com/google/gwt/user/updatesources.sh
@@ -9,6 +9,8 @@ pwd
 
 # extract over previously modified sources
 jar xvf ../lib/gwt/2.8.2/gwt-user.jar \
+  com/google/gwt/user/client/ui/VerticalPanel.java \
+  com/google/gwt/user/client/ui/HorizontalPanel.java \
   com/google/gwt/user/client/ui/MenuBar.java \
   com/google/gwt/user/client/ui/SplitPanel.java \
   com/google/gwt/user/client/ui/SplitLayoutPanel.java
@@ -16,6 +18,8 @@ jar xvf ../lib/gwt/2.8.2/gwt-user.jar \
 cd com/google/gwt/user/client/ui
 
 # apply previous patches
+patch VerticalPanel.java < VerticalPanel.java.diff
+patch HorizontalPanel.java < HorizontalPanel.java.diff
 patch MenuBar.java < MenuBar.java.diff
 patch SplitPanel.java < SplitPanel.java.diff
 patch SplitLayoutPanel.java < SplitLayoutPanel.java.diff
