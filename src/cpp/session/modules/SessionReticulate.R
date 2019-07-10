d4c87f0d975747da5d5d4742e8f144b7ad3c52a7
Kevin Ushey <kevin@rstudio.com>
2019-07-10 10:05:31 -0700

    don't set hook by default for now

--- src/cpp/session/modules/SessionReticulate.R
+++ src/cpp/session/modules/SessionReticulate.R
@@ -86,9 +86,6 @@
    engine <- tolower(Sys.getenv("MPLENGINE"))
    if (engine %in% c("", "qt5agg"))
       Sys.setenv(MPLENGINE = "tkAgg")
-   
-   # set up hooks for overriding 'show()' in matplotlib
-   setHook("reticulate::matplotlib.pyplot::load", .rs.reticulate.matplotlib.pyplot.loadHook)
 })
 
 .rs.addFunction("reticulate.matplotlib.pyplot.loadHook", function(plt)
