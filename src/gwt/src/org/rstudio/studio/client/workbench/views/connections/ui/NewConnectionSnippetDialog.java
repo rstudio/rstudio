10cd95293d5620b1773f114c75e334a86f17ad90
Gary <gary@rstudio.com>
2019-09-18 09:26:31 -0700

    use LayoutGrid instead of Grid for proper accessibility annotation
    - several more instances of Grid replaced with LayoutGrid
    - associate labels with controls in Connections dialog

--- src/gwt/src/org/rstudio/studio/client/workbench/views/connections/ui/NewConnectionSnippetDialog.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/connections/ui/NewConnectionSnippetDialog.java
@@ -19,6 +19,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.google.gwt.aria.client.Roles;
+import org.rstudio.core.client.widget.FormLabel;
+import org.rstudio.core.client.widget.LayoutGrid;
 import org.rstudio.core.client.widget.ModalDialog;
 import org.rstudio.core.client.widget.OperationWithInput;
 import org.rstudio.studio.client.RStudioGinjector;
@@ -30,9 +32,7 @@ import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
-import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
@@ -85,17 +85,18 @@ public class NewConnectionSnippetDialog extends ModalDialog<HashMap<String, Stri
       SimplePanel wrapper = new SimplePanel();
       wrapper.setStyleName(RES.styles().wrapper());
       
-      Grid connGrid = new Grid(initialConfig_.size(), 2);
+      LayoutGrid connGrid = new LayoutGrid(initialConfig_.size(), 2);
       connGrid.addStyleName(RES.styles().grid());
 
       for (int idxParams = 0; idxParams < initialConfig_.size(); idxParams++) {
          final String key = initialConfig_.get(idxParams).getKey();
-         Label label = new Label(key + ":");
+         FormLabel label = new FormLabel(key + ":");
          label.addStyleName(RES.styles().label());
          connGrid.setWidget(idxParams, 0, label);
          connGrid.getRowFormatter().setVerticalAlign(idxParams, HasVerticalAlignment.ALIGN_TOP);
          
          final TextBox textbox = new TextBox();
+         label.setFor(textbox);
          textbox.setText(initialConfig_.get(idxParams).getValue());
          textbox.addStyleName(RES.styles().textbox());
          connGrid.setWidget(idxParams, 1, textbox);
