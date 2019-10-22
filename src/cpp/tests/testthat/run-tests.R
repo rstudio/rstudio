832214a489e8442c6bd885de36f230612021dd9e
Maria Semple <maria@rstudio.com>
2019-10-22 16:43:57 -0700

    wrap testthat call so we can check for R unit test failures

---
+++ src/cpp/tests/testthat/run-tests.R
@@ -0,0 +1,18 @@
+library(testthat)
+
+runAllTests <- function(sourceDir, outputDir, filter = NA)
+{
+   testThatDir <- file.path(sourceDir, "tests/testthat")
+   
+   if (!is.na(filter))
+   {
+      tests <- testthat::test_dir(testThatDir, filter = filter)
+   } else
+   {
+      tests <- testthat::test_dir(testThatDir)
+   }
+   
+   resultFile <- file(file.path(outputDir, "testthat-failures.log"), open="w")
+   cat(sum(as.data.frame(tests)$failed), file = resultFile)
+   close(resultFile)
+}
