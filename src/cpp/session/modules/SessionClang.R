1fe4e1a9448bbac009b9d7b6abecf4633ec9c685
Kevin Ushey <kevin@rstudio.com>
2019-10-24 15:56:55 -0700

    fix indentation

--- src/cpp/session/modules/SessionClang.R
+++ src/cpp/session/modules/SessionClang.R
@@ -17,72 +17,72 @@
 
 .rs.addFunction("clangPCHPath", function(pkg, clangVersion)
 {
-  paste(
-    packageVersion(pkg),
-    R.version$platform,
-    R.version$`svn rev`,
-    clangVersion,
-    sep = "-"
-  )
+   paste(
+      packageVersion(pkg),
+      R.version$platform,
+      R.version$`svn rev`,
+      clangVersion,
+      sep = "-"
+   )
 })
 
 .rs.addFunction("isClangAvailable", function() {
-  cat("Attemping to load libclang for", R.version$platform, "\n")
-  .Call("rs_isLibClangAvailable", PACKAGE = "(embedding)")
+   cat("Attemping to load libclang for", R.version$platform, "\n")
+   .Call("rs_isLibClangAvailable", PACKAGE = "(embedding)")
 })
 
 .rs.addFunction("setClangDiagnostics", function(level)
 {
-  if (!is.numeric(level) || (level < 0) || (level > 2))
-    stop("level must be 0, 1, or 2")
-  
-  if (level > 0)
-    .rs.isClangAvailable()
-  
-  .Call("rs_setClangDiagnostics", level, PACKAGE = "(embedding)")
-  
-  .rs.restartR()
-  invisible(NULL)
+   if (!is.numeric(level) || (level < 0) || (level > 2))
+      stop("level must be 0, 1, or 2")
+   
+   if (level > 0)
+      .rs.isClangAvailable()
+   
+   .Call("rs_setClangDiagnostics", level, PACKAGE = "(embedding)")
+   
+   .rs.restartR()
+   invisible(NULL)
 })
 
 .rs.addFunction("packagePCH", function(linkingTo)
 {
-  linkingTo <- .rs.parseLinkingTo(linkingTo)
-  packages <- c("RcppArmadillo", "RcppEigen", "Rcpp11", "Rcpp")
-  for (package in packages)
-    if (package %in% linkingTo)
-      return(package)
-  ""
+   linkingTo <- .rs.parseLinkingTo(linkingTo)
+   packages <- c("RcppArmadillo", "RcppEigen", "Rcpp11", "Rcpp")
+   for (package in packages)
+      if (package %in% linkingTo)
+         return(package)
+   ""
 })
 
 .rs.addFunction("includesForLinkingTo", function(linkingTo)
 {
-  includes <- character()
-  
-  linkingTo <- .rs.parseLinkingTo(linkingTo)
-  for (pkg in linkingTo) {
-    includeDir <- system.file("include", package = pkg)
-    if (file.exists(includeDir)) {
-      includes <- c(
-        includes,
-        paste("-I", .rs.asBuildPath(includeDir), sep = "")
-      )
-    }
-  }
-  
-  includes
+   includes <- character()
+   
+   linkingTo <- .rs.parseLinkingTo(linkingTo)
+   for (pkg in linkingTo) {
+      includeDir <- system.file("include", package = pkg)
+      if (file.exists(includeDir)) {
+         includes <- c(
+            includes,
+            paste("-I", .rs.asBuildPath(includeDir), sep = "")
+         )
+      }
+   }
+   
+   includes
 })
 
 .rs.addFunction("asBuildPath", function(path)
 {
-  if (.Platform$OS.type == "windows") {
-    path <- normalizePath(path)
-    if (grepl(' ', path, fixed = TRUE))
-      path <- utils::shortPathName(path)
-    path <- gsub("\\\\", "/", path)
-  }
-  
-  return(path)
+   if (.Platform$OS.type == "windows") {
+      path <- normalizePath(path)
+      if (grepl(' ', path, fixed = TRUE))
+         path <- utils::shortPathName(path)
+      path <- gsub("\\\\", "/", path)
+   }
+   
+   return(path)
 })
 
 
