8c4cfbbc1c0c0dbfad35c415369e1062362d5908
Jonathan McPherson <jonathan@rstudio.com>
2019-11-08 16:21:55 -0800

    fail when test file doesn't appear to be in a test directory

--- src/cpp/session/modules/build/SessionBuild.cpp
+++ src/cpp/session/modules/build/SessionBuild.cpp
@@ -1197,6 +1197,12 @@ private:
            // Move up from the tests folder to the app folder.
            shinyPath = shinyPath.getParent();
         }
+        else
+        {
+           // If this doesn't look like it's in a tests directory, bail out.
+           terminateWithError("Could not find Shiny app for test in " + 
+              shinyPath.getAbsolutePath());
+        }
       }
 
       // get temp path to store rds results
