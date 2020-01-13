2da1e7f23187f7e6a958d4a3eb082f8ae3d6c350
JJ Allaire <jj@rstudio.com>
2020-01-13 13:31:11 -0500

    add copyright notices

--- src/gwt/src/org/rstudio/core/client/promise/PromiseServerRequestCallback.java
+++ src/gwt/src/org/rstudio/core/client/promise/PromiseServerRequestCallback.java
@@ -1,3 +1,18 @@
+/*
+ * PromiseServerRequestCallback.java
+ *
+ * Copyright (C) 2009-20 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
 package org.rstudio.core.client.promise;
 
 import org.rstudio.studio.client.server.ServerError;
