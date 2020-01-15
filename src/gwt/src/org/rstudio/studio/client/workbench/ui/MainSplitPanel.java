b1b165a7a1d508459621b7bc009b0655edb251ea
Gary Ritchie <gary@rstudio.com>
2020-01-15 11:34:37 -0800

    add ability to resize left/right pane horizontal splitters with keyboard
    - work in progress; next step (the vertical splitter) will require a GWT tweak

--- src/gwt/src/org/rstudio/studio/client/workbench/ui/MainSplitPanel.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/ui/MainSplitPanel.java
@@ -1,7 +1,7 @@
 /*
  * MainSplitPanel.java
  *
- * Copyright (C) 2009-12 by RStudio, Inc.
+ * Copyright (C) 2009-20 by RStudio, Inc.
  *
  * Unless you have received this program directly from RStudio pursuant
  * to the terms of a commercial license agreement with RStudio, then
@@ -199,6 +199,11 @@ public class MainSplitPanel extends NotifyingSplitLayoutPanel
    {
       enforceBoundaries();
       deferredSaveWidthPercent();
+   }
+
+   public void focusSplitter()
+   {
+      // TODO (gary)
    }
 
    private void enforceBoundaries()
