ac20f6565f35a90285adb580bb913ae7f90b6127
JJ Allaire <jj@rstudio.com>
2020-01-27 12:08:01 -0500

    add copyright headers

--- src/gwt/panmirror/src/editor/src/nodes/definition_list/definition_list-inputrule.ts
+++ src/gwt/panmirror/src/editor/src/nodes/definition_list/definition_list-inputrule.ts
@@ -1,3 +1,18 @@
+/*
+ * definition_list-inputrule.ts
+ *
+ * Copyright (C) 2019-20 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
 import { InputRule } from 'prosemirror-inputrules';
 import { EditorState } from 'prosemirror-state';
 import { setTextSelection, findParentNodeOfTypeClosestToPos } from 'prosemirror-utils';
--- src/gwt/panmirror/src/workbench/store/prefs/prefs-types.ts
+++ src/gwt/panmirror/src/workbench/store/prefs/prefs-types.ts
@@ -1,3 +1,18 @@
+/*
+ * prefs-types.ts
+ *
+ * Copyright (C) 2019-20 by RStudio, Inc.
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
 export enum PrefsActionTypes {
   SET_SHOW_OUTLINE = 'PREFS/SET_SHOW_OUTLINE',
   SET_SHOW_MARKDOWN = 'PREFS/SET_SHOW_MARKDOWN',
