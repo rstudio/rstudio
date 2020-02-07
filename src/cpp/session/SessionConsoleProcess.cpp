d47face5b8eb1460ee9692e84698f48f921cb01d
Gary <gary@rstudio.com>
2020-02-07 13:52:01 -0800

    suppress macOS Catalina warning about old system bash
    - Fixes #6182

--- src/cpp/session/SessionConsoleProcess.cpp
+++ src/cpp/session/SessionConsoleProcess.cpp
@@ -1,7 +1,7 @@
 /*
  * SessionConsoleProcess.cpp
  *
- * Copyright (C) 2009-19 by RStudio, PBC
+ * Copyright (C) 2009-20 by RStudio, PBC
  *
  * Unless you have received this program directly from RStudio pursuant
  * to the terms of a commercial license agreement with RStudio, then
@@ -54,6 +54,11 @@ core::system::ProcessOptions ConsoleProcess::createTerminalProcOptions(
 
    if (shellEnv.empty())
       core::system::environment(&shellEnv);
+
+#ifdef __APPLE__
+   // suppress macOS Catalina warning suggesting switching to zsh
+   core::system::setenv(&shellEnv, "BASH_SILENCE_DEPRECATION_WARNING", "1");
+#endif
 
    *pSelectedShellType = procInfo.getShellType();
 
