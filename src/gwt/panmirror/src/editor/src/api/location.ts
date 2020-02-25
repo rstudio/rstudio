2b1ef8932446dbdbee2e3f02f28a10a5aaf7d9a0
JJ Allaire <jj@rstudio.com>
2020-02-25 09:31:56 -0500

    don't restore invalid editor locations

--- src/gwt/panmirror/src/editor/src/api/location.ts
+++ src/gwt/panmirror/src/editor/src/api/location.ts
@@ -16,6 +16,12 @@ export function getEditingLocation(view: EditorView): EditingLocation {
 }
 
 export function restoreEditingLocation(view: EditorView, location: EditingLocation, scrollIntoView = true) {
+
+  // ensure location is valid
+  if (location.pos > view.state.doc.nodeSize) {
+    return;
+  }
+
   // restore selection
   const tr = view.state.tr;
   setTextSelection(location.pos)(tr).setMeta('addToHistory', false);
