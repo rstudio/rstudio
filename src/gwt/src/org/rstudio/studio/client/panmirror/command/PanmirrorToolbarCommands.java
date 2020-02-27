f1150ccb40c6c96c55849f0b1832d5d485e91732
JJ Allaire <jj@rstudio.com>
2020-02-27 11:49:43 -0500

    give line block latching behavior on menu

--- src/gwt/src/org/rstudio/studio/client/panmirror/command/PanmirrorToolbarCommands.java
+++ src/gwt/src/org/rstudio/studio/client/panmirror/command/PanmirrorToolbarCommands.java
@@ -52,7 +52,7 @@ public class PanmirrorToolbarCommands
       add(PanmirrorCommands.Heading6, "Heading 6", Roles.getMenuitemradioRole());
       add(PanmirrorCommands.CodeBlock, "Code Block", Roles.getMenuitemradioRole());
       add(PanmirrorCommands.Blockquote, "Blockquote", Roles.getMenuitemcheckboxRole(), icons.BLOCKQUOTE);
-      add(PanmirrorCommands.LineBlock, "Line Block");
+      add(PanmirrorCommands.LineBlock, "Line Block", Roles.getMenuitemcheckboxRole());
       add(PanmirrorCommands.Div, "Section/Div...");
       add(PanmirrorCommands.AttrEdit, "Edit Attributes...");
       
