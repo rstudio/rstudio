1d174a9c90eef4057fa6a1a9551e21fc0a159f1d
karl@rstudio.com <karl@rstudio.com>
2020-03-03 16:57:36 -0600

    Code review feedback

--- src/cpp/core/include/core/Database.hpp
+++ src/cpp/core/include/core/Database.hpp
@@ -233,7 +233,8 @@ public:
    // updates the database schema to the highest version
    Error update();
 
-   // updates the database schema to the specified version
+   // updates the database schema to the specified version if it is contained
+   // within the migration schemas, or no higher than that version if it is not present
    Error updateToVersion(const std::string& maxVersion);
 
    // returns whether or not the schema is up-to-date with the latest schema version
