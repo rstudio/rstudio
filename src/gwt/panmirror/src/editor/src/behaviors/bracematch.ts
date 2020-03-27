9a5e47375687b4d25ab82249ac5fabed6162ff09
JJ Allaire <jj@rstudio.com>
2020-03-27 07:10:56 -0400

    limit brace matching to just braces (i.e. {})

--- src/gwt/panmirror/src/editor/src/behaviors/bracematch.ts
+++ src/gwt/panmirror/src/editor/src/behaviors/bracematch.ts
@@ -24,14 +24,12 @@ import { PandocExtensions } from '../api/pandoc';
 
 const braces = new Map([
   ['{', '}'],
-  ['(', ')'],
-  ['[', ']'],
 ]);
 
 const extension: Extension = {
   inputRules: (_schema: Schema) => {
     return [
-      new InputRule(/(^|[^^\\])([{([])$/, (state: EditorState, match: string[], start: number, end: number) => {
+      new InputRule(/(^|[^^\\])([{])$/, (state: EditorState, match: string[], start: number, end: number) => {
         const tr = state.tr;
         tr.insertText(match[2] + braces.get(match[2]));
         setTextSelection(start + match[1].length + 1)(tr);
