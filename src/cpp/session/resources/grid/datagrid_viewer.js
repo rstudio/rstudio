2d858ba57d75cec880721f13a01fc0f6e6a3418e
Adam Conroy <adam.conroy@gmail.com>
2020-03-31 15:20:08 -0400

    ag-Grid datagrid test

--- src/cpp/session/resources/grid/datagrid_viewer.js
+++ src/cpp/session/resources/grid/datagrid_viewer.js
@@ -1,22 +1,67 @@
 (function(){
 
 window.setData = function(data) {
-//  this.console.log("setdata");
-//  this.document.getElementById("test_div").innerHTML = data;
+   const numRows = 1000;
+   const numCols = 200;
 
-      let d = [];
-      for (let i = 0; i < 10000; i++) {
-         let row = {};
-         for (let j = 0; j < 50; j++) {
-            row[j] = "row" + i + " col " + j;
-         }
-         d.push(row);
+ //  var columnDefs = [{
+ //        headerName: "Make",
+ //        field: "0"
+ //     },
+ //     {
+ //        headerName: "Model",
+ //        field: "1"
+ //     },
+ //     {
+ //        headerName: "Price",
+ //        field: "2"
+ //     }
+ //  ];
+
+   let columnDefs = [];
+   for (let i = 0; i < numCols; i++) {
+      columnDefs.push({
+         headerName: "" + i,
+         field: "" + i
+      });
+   }
+
+   let rowData = [];
+   for (let i = 0; i < numRows; i++) {
+      let row = {};
+      for (let j = 0; j < numCols; j++) {
+         row[j] = i + " " + j;
       }
+      rowData.push(row);
+   }
 
-      const myGrid = new fin.Hypergrid(null, {});
-      myGrid.setData(d);
-};
+//   // specify the data
+//   var rowData = [{
+//         0: "Toyota",
+//         1: "Celica",
+//         2: 35000
+//      },
+//      {
+//         0: "Ford",
+//         1: "Mondeo",
+//         2: 32000
+//      },
+//      {
+//         0: "Porsche",
+//         1: "Boxter",
+//         2: 72000
+//      }
+//   ];
 
-window.setOption = function(option, value) {
-};
+   // let the grid know which columns and what data to use
+   var gridOptions = {
+      columnDefs: columnDefs,
+      rowData: rowData
+   };
+
+   let gridDiv = document.querySelector("#grid");
+   new agGrid.Grid(gridDiv, gridOptions);
+   };
+
+   window.setOption = function (option, value) {};
 })();
