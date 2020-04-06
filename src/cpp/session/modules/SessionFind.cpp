ff6168fc4d48fc31bdb66aacc01f9718453e50f6
Gary Ritchie <gary@rstudio.com>
2020-04-06 11:29:21 -0700

    more tweaks

--- src/cpp/session/modules/SessionFind.cpp
+++ src/cpp/session/modules/SessionFind.cpp
@@ -436,7 +436,7 @@ private:
       std::string encodedContents;
    };
 
-   bool onContinue(const core::system::ProcessOperations& ops) const
+   bool onContinue(const core::system::ProcessOperations& /*ops*/) const
    {
       return findResults().isRunning() && findResults().handle() == handle();
    }
@@ -538,7 +538,7 @@ private:
          {
             *contents = contents->erase(0, firstMatchOn - 30);
             contents->insert(0, "...");
-            size_t leadingCharactersErased = firstMatchOn - 33;
+            int leadingCharactersErased = firstMatchOn - 33;
             json::Array newMatchOnArray;
             json::Array newMatchOffArray;
             for (size_t i = 0; i < pMatchOn->getSize(); i++)
@@ -875,7 +875,7 @@ private:
               file.find("/.Rhistory") != std::string::npos);
    }
 
-   void onStdout(const core::system::ProcessOperations& ops, const std::string& data)
+   void onStdout(const core::system::ProcessOperations& /*ops*/, const std::string& data)
    {
       json::Array files;
       json::Array lineNums;
@@ -1068,14 +1068,14 @@ private:
       }
    }
 
-   void onStderr(const core::system::ProcessOperations& ops, const std::string& data)
+   void onStderr(const core::system::ProcessOperations& /*ops*/, const std::string& data)
    {
       LOG_ERROR_MESSAGE("grep: " + data);
       if (boost::algorithm::icontains(data, "not a git repository"))
          module_context::showErrorMessage("Not a Git Repository", data);
    }
 
-   void onExit(int exitCode)
+   void onExit(int /*exitCode*/)
    {
       findResults().onFindEnd(handle());
       module_context::enqueClientEvent(
@@ -1451,7 +1451,7 @@ core::Error beginFind(const json::JsonRpcRequest& request,
 }
 
 core::Error stopFind(const json::JsonRpcRequest& request,
-                     json::JsonRpcResponse* pResponse)
+                     json::JsonRpcResponse* /*pResponse*/)
 {
    std::string handle;
    Error error = json::readParams(request.params, &handle);
@@ -1463,8 +1463,8 @@ core::Error stopFind(const json::JsonRpcRequest& request,
    return Success();
 }
 
-core::Error clearFindResults(const json::JsonRpcRequest& request,
-                             json::JsonRpcResponse* pResponse)
+core::Error clearFindResults(const json::JsonRpcRequest& /*request*/,
+                             json::JsonRpcResponse* /*pResponse*/)
 {
    findResults().clear();
    return Success();
@@ -1538,7 +1538,7 @@ core::Error completeReplace(const json::JsonRpcRequest& request,
 }
 
 core::Error stopReplace(const json::JsonRpcRequest& request,
-                        json::JsonRpcResponse* pResponse)
+                        json::JsonRpcResponse* /*pResponse*/)
 {
    std::string handle;
    Error error = json::readParams(request.params, &handle);
@@ -1636,7 +1636,7 @@ core::Error Replacer::completeReplace(const boost::regex& searchRegex,
          "A regex error occurred during replace operation: " + std::string(e.what()),
          ERROR_LOCATION);
 
-      error.addProperty("position", e.position());
+      error.addProperty("position", gsl::narrow_cast<int>(e.position()));
       return error;
    }
 
