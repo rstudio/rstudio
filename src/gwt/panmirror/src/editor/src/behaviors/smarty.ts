7826f3cb69104019ed3472837e08cf8934131f0e
JJ Allaire <jj@rstudio.com>
2020-04-06 06:44:57 -0400

    more targeted enDash/emDash input rules

--- src/gwt/panmirror/src/editor/src/behaviors/smarty.ts
+++ src/gwt/panmirror/src/editor/src/behaviors/smarty.ts
@@ -22,8 +22,10 @@ import { Extension, extensionIfEnabled } from '../api/extension';
 const plugin = new PluginKey('smartypaste');
 
 // match enDash but only for lines that aren't an html comment
-const enDash = new InputRule(/^(?:[^-]*)--$/, (state: EditorState, match: string[], start: number, end: number) => {
-  if (!state.doc.textBetween(start, end).startsWith('<!--')) {
+const enDash = new InputRule(/[^!-]--$/, (state: EditorState, match: string[], start: number, end: number) => {
+  const { parent, parentOffset } = state.selection.$head;
+  const precedingText = parent.textBetween(0, parentOffset);
+  if (precedingText.indexOf('<!--') === -1) {
     const tr = state.tr;
     tr.insertText('–', end - 1, end);
     return tr;
@@ -32,7 +34,7 @@ const enDash = new InputRule(/^(?:[^-]*)--$/, (state: EditorState, match: string
   }
 });
 
-const emDash = new InputRule(/^(?:.*)–-$/, (state: EditorState, match: string[], start: number, end: number) => {
+const emDash = new InputRule(/–-$/, (state: EditorState, match: string[], start: number, end: number) => {
   const tr = state.tr;
   tr.insertText('—', end - 1, end);
   return tr;
