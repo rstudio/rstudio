abd2862a2cdf66865545e4ff776e4ab5f9a78e43
JJ Allaire <jj@rstudio.com>
2020-04-24 06:13:39 -0400

    Revert "ensure that scope icon isn't visible in visual mode"
    This reverts commit c2ef47f099cbebb025a010c395e3621c785cdf03.

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/status/StatusBarWidget.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/status/StatusBarWidget.java
@@ -138,7 +138,7 @@ public class StatusBarWidget extends Composite
       if (type == StatusBar.SCOPE_TOP_LEVEL || message_.isVisible())
          scopeIcon_.setVisible(false);
       else
-         scopeIcon_.setVisible(scope_.getContentsVisible());
+         scopeIcon_.setVisible(true);
 
       if (type == StatusBar.SCOPE_CLASS)
       {
