e3513d57931217904d63f4da0c6c8117f23b1301
JJ Allaire <jj@rstudio.com>
2020-04-26 11:38:32 -0400

    new insert comment command

--- src/gwt/panmirror/src/editor/src/marks/raw_inline/raw_html-comment.ts
+++ src/gwt/panmirror/src/editor/src/marks/raw_inline/raw_html-comment.ts
@@ -15,23 +15,65 @@
  */
 
 import { Schema } from "prosemirror-model";
-import { Transaction, TextSelection } from "prosemirror-state";
+import { Transaction, TextSelection, EditorState } from "prosemirror-state";
+import { toggleMark } from "prosemirror-commands";
 
-import { EditorCommandId } from "../../api/command";
+import { setTextSelection } from "prosemirror-utils";
 
-import { RawInlineInsertCommand } from "./raw_inline";
+import { EditorCommandId, ProsemirrorCommand } from "../../api/command";
+
+import { canInsertNode } from "../../api/node";
 
 
-export class InsertHTMLCommentCommand extends RawInlineInsertCommand {
+export class InsertHTMLCommentCommand extends ProsemirrorCommand {
   constructor(schema: Schema) {
-    super(EditorCommandId.HTMLComment, ['Shift-Mod-c'], schema.marks.raw_html, (tr: Transaction) => {
-      const mark = schema.marks.raw_html.create({ comment: true });
-      const comment = '<!--  -->';
-      const node = schema.text(comment, [mark]);
-      tr.replaceSelectionWith(node, false);
-      tr.setSelection(
-        new TextSelection(tr.doc.resolve(tr.selection.from - (comment.length/2 - 1))),
-      );
+    super(EditorCommandId.HTMLComment, ['Shift-Mod-c'], (state: EditorState, dispatch?: (tr: Transaction) => void) => {
+      
+      // make sure we can insert a text node here
+      if (!canInsertNode(state, schema.nodes.text)) {
+        return false;
+      }
+
+      // make sure we can apply this mark here
+      if (!toggleMark(schema.marks.raw_html)(state)) {
+        return false;
+      }
+
+      // make sure the end of the selection (where we will insert the comment) 
+      // isn't already in a mark of this type
+      if (state.doc.rangeHasMark(state.selection.to, state.selection.to+1, schema.marks.raw_html)) {
+        return false;
+      }
+
+      if (dispatch) {
+        const tr = state.tr;
+
+        // set the selection to the end of the current selection (comment 'on' the selection)
+        setTextSelection(tr.selection.to)(tr);
+
+        // if we have a character right before us then insert a space
+        const { parent, parentOffset } = tr.selection.$to;
+        const charBefore = parent.textContent.slice(parentOffset - 1, parentOffset);
+        if (charBefore.length && charBefore !== ' ') {
+          tr.insertText(' ');
+        }
+
+        // insert the comment
+        const mark = schema.marks.raw_html.create({ comment: true });
+        const comment = '<!--  -->';
+        const node = schema.text(comment, [mark]);
+        tr.insert(tr.selection.from, node);
+
+        // set the selection to the middle of the comment
+        tr.setSelection(
+          new TextSelection(tr.doc.resolve(tr.selection.from - (comment.length/2 - 1))),
+        );
+
+        // dispatch
+        dispatch(tr);
+      }
+
+      return true;
     });
   }
 }
