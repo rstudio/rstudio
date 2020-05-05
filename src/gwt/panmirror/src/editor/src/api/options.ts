31df7d30aa5fab012ed44d3b1d1a02bb65d3aa62
JJ Allaire <jj@rstudio.com>
2020-05-05 11:16:42 -0400

    fix spelling error in chunk execution option

--- src/gwt/panmirror/src/editor/src/api/options.ts
+++ src/gwt/panmirror/src/editor/src/api/options.ts
@@ -18,7 +18,7 @@ export interface EditorOptions {
   readonly spellCheck?: boolean;
   readonly codemirror?: boolean;
   readonly rmdImagePreview?: boolean;
-  readonly rmdChunkExcution?: string[];
+  readonly rmdChunkExecution?: string[];
   readonly hideFormatComment?: boolean;
   readonly className?: string;
 }
