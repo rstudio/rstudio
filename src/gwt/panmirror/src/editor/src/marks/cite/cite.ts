64ac984e511b5d8f216f7d4c3164e78cb7d190ef
JJ Allaire <jj@rstudio.com>
2020-05-06 11:04:45 -0400

    correct escaping for cite id regex

--- src/gwt/panmirror/src/editor/src/marks/cite/cite.ts
+++ src/gwt/panmirror/src/editor/src/marks/cite/cite.ts
@@ -30,7 +30,7 @@ import { PandocCapabilities } from '../../api/pandoc_capabilities';
 const CITE_CITATIONS = 0;
 
 const kCiteIdPrefixPattern = '-?@';
-const kCiteIdCharsPattern = '\\w[\\w:.#$%&-+?<>~/]*';
+const kCiteIdCharsPattern = '\\w[\\w:\\.#\\$%&\\-\\+\\?<>~/]*';
 const kCiteIdPattern = `^${kCiteIdPrefixPattern}${kCiteIdCharsPattern}$`;
 const kBeginCitePattern = `(.* ${kCiteIdPrefixPattern}|${kCiteIdPrefixPattern})`;
 
