5e8ebde65748cad8d4cf0534ac8edf2734a7fe1b
JJ Allaire <jj@rstudio.com>
2020-05-14 06:41:01 -0400

    embed prosemirror-view types to manage incompatibity w/ embeeded prosemirror-view types in prosemirror-inputrules

--- src/gwt/panmirror/src/editor/src/editor/editor.ts
+++ src/gwt/panmirror/src/editor/src/editor/editor.ts
@@ -675,9 +675,7 @@ export class Editor {
 
     // override to disable input rules as requested
     // https://github.com/ProseMirror/prosemirror-inputrules/commit/b4bf67623aa4c4c1e096c20aa649c0e63751f337
-    // (use any as type for view to workaround incompatibility of View typescript declarations
-    // between @types/prosemirror-view and prosemirror-inputrules
-    plugin.props.handleTextInput = (view: any, from: number, to: number, text: string) => {
+    plugin.props.handleTextInput = (view: EditorView, from: number, to: number, text: string) => {
       if (!markFilter(view.state)) {
         return false;
       }
