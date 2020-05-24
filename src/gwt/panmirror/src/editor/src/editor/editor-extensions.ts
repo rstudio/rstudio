e7de8a85913ee4c66e8a479e2a68166fe668c1be
JJ Allaire <jj@rstudio.com>
2020-05-23 21:20:52 -0400

    preserve marks when pasting plain text

--- src/gwt/panmirror/src/editor/src/editor/editor-extensions.ts
+++ src/gwt/panmirror/src/editor/src/editor/editor-extensions.ts
@@ -72,6 +72,7 @@ import behaviorTrailingP from '../behaviors/trailing_p';
 import behaviorEmptyMark from '../behaviors/empty_mark';
 import behaviorOutline from '../behaviors/outline';
 import beahviorCodeBlockInput from '../behaviors/code_block_input';
+import behaviorPasteText from '../behaviors/paste_text';
 
 // marks
 import markStrikeout from '../marks/strikeout';
@@ -150,6 +151,7 @@ export function initExtensions(
     behaviorEmptyMark,
     behaviorOutline,
     beahviorCodeBlockInput,
+    behaviorPasteText,
 
     // nodes
     nodeDiv,
