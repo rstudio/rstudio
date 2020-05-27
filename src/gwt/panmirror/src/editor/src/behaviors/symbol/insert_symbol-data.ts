d1dde98c40602c8544938dc940b9865170bc7339
Charles Teague <cteague@gmail.com>
2020-05-27 17:08:34 -0400

    Add source code header comment

--- src/gwt/panmirror/src/editor/src/behaviors/symbol/insert_symbol-data.ts
+++ src/gwt/panmirror/src/editor/src/behaviors/symbol/insert_symbol-data.ts
@@ -1,3 +1,18 @@
+/*
+ * insert_symbol-data.ts
+ *
+ * Copyright (C) 2019-20 by RStudio, PBC
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
 import untypedSymbolData from './insert_symbol-data.json';
 import { parseCodepoint } from '../../api/unicode';
 
