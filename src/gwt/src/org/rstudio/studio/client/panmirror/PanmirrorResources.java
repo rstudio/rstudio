d514a396198e4b257aefcb18069fad2ae7b6c360
Kevin Ushey <kevinushey@gmail.com>
2020-05-28 16:42:59 -0700

    pacify Eclipse

--- src/gwt/src/org/rstudio/studio/client/panmirror/PanmirrorResources.java
+++ src/gwt/src/org/rstudio/studio/client/panmirror/PanmirrorResources.java
@@ -29,11 +29,5 @@ public interface PanmirrorResources extends ClientBundle
    @Source("PanmirrorStyles.css")
    Styles styles();
    
-   public static PanmirrorResources INSTANCE = (PanmirrorResources)GWT.create(PanmirrorResources.class);
-   
-
-   public static void ensureStylesInjected()
-   {
-      INSTANCE.styles().ensureInjected();
-   }
+   public static PanmirrorResources INSTANCE = GWT.create(PanmirrorResources.class);
 }
