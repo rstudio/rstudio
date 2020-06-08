2af9a14d472cb7eb6290c44e78c03cd65a377584
Charles Teague <cteague@gmail.com>
2020-06-08 17:02:26 -0400

    Improce comment + remove console.log

--- src/gwt/panmirror/src/editor/src/behaviors/insert_symbol/insert_symbol-popup.tsx
+++ src/gwt/panmirror/src/editor/src/behaviors/insert_symbol/insert_symbol-popup.tsx
@@ -130,6 +130,10 @@ export const InsertSymbolPopup: React.FC<InsertSymbolPopupProps> = props => {
 
     if (textRef.current && isElementFocused(textRef.current)) {
       if (textRef.current?.value.length > 0) {
+
+        // If the textbox is focused, figure out whether the grid can
+        // still take arrow / page events. If there is no text or the cursor
+        // is at the end of the input, we can handle arrows (see conditions).
         const numberOfCharacters = textRef.current?.value.length;
         const cursorPosition = textRef.current?.selectionStart;
         if (cursorPosition === 0 && event.key === 'ArrowLeft') { 
@@ -147,7 +151,6 @@ export const InsertSymbolPopup: React.FC<InsertSymbolPopupProps> = props => {
 
   function processGridKeyboardEvent(event: React.KeyboardEvent) {
     const newIndex = newIndexForKeyboardEvent(event, selectedSymbolIndex, kNumberOfcolumns, filteredSymbols.length);
-    console.log(newIndex);
     if (newIndex !== undefined && newIndex >= 0) {
       setSelectedSymbolIndex(newIndex);
       event.preventDefault();
