1681a8c98a9f83a8170d4d4dcc73cb5bc461072b
Charles Teague <cteague@gmail.com>
2020-07-06 12:20:21 -0400

    Cleanup tabs (use spaces)

--- src/gwt/src/org/rstudio/studio/client/panmirror/dialogs/model/PanmirrorInsertCiteProps.java
+++ src/gwt/src/org/rstudio/studio/client/panmirror/dialogs/model/PanmirrorInsertCiteProps.java
@@ -1,16 +1,14 @@
 package org.rstudio.studio.client.panmirror.dialogs.model;
 
-
-// JJA: tabs for spaces
-
 import elemental2.core.JsObject;
 import jsinterop.annotations.JsType;
 
 @JsType
-public class PanmirrorInsertCiteProps {
-	public String doi;
-	public String[] existingIds;
-	public String[] bibliographyFiles;
-	public JsObject csl;
-	public PanmirrorInsertCiteUI citeUI;
+public class PanmirrorInsertCiteProps
+{
+   public String doi;
+   public String[] existingIds;
+   public String[] bibliographyFiles;
+   public JsObject csl;
+   public PanmirrorInsertCiteUI citeUI;
 }
--- src/gwt/src/org/rstudio/studio/client/panmirror/dialogs/model/PanmirrorInsertCiteResult.java
+++ src/gwt/src/org/rstudio/studio/client/panmirror/dialogs/model/PanmirrorInsertCiteResult.java
@@ -1,13 +1,12 @@
 package org.rstudio.studio.client.panmirror.dialogs.model;
 
-//JJA: tabs for spaces
-
 import elemental2.core.JsObject;
 import jsinterop.annotations.JsType;
 
 @JsType
-public class PanmirrorInsertCiteResult {
-	public String id;
-	public String bibliographyFile;
-	public JsObject csl;
+public class PanmirrorInsertCiteResult
+{
+   public String id;
+   public String bibliographyFile;
+   public JsObject csl;
 }
