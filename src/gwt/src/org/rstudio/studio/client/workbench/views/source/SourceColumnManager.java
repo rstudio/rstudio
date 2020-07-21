d09a47892c154ad1bc12b0978259f4e29a4a97c3
melissa-barca <melissa@rstudio.com>
2020-07-21 15:29:44 -0700

    fix issue where selected tab on startup was being overwritten, persist active column

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/SourceColumnManager.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/SourceColumnManager.java
@@ -99,9 +99,10 @@ public class SourceColumnManager implements CommandPaletteEntrySource,
 
    public static class State extends JavaScriptObject
    {
-      public static native State createState(JsArrayString names) /*-{
+      public static native State createState(JsArrayString names, String activeColumn) /*-{
          return {
-            names: names
+            names: names,
+            activeColumn: activeColumn
          }
       }-*/;
 
@@ -112,6 +113,13 @@ public class SourceColumnManager implements CommandPaletteEntrySource,
       {
          return JsUtil.toStringArray(getNamesNative());
       }
+
+      public final native String getActiveColumn() /*-{
+         if (this.activeColumn)
+            return this.activeColumn;
+         else
+            return "";
+      }-*/;
 
       private native JsArrayString getNamesNative() /*-{
           return this.names;
@@ -236,7 +244,9 @@ public class SourceColumnManager implements CommandPaletteEntrySource,
 
             if (value == null)
             {
-               columnState_ = State.createState(JsUtil.toJsArrayString(getNames(false)));
+               columnState_ =
+                  State.createState(JsUtil.toJsArrayString(getNames(false)),
+                                    getActive().getName());
                return;
             }
 
@@ -310,12 +320,16 @@ public class SourceColumnManager implements CommandPaletteEntrySource,
          setActive(column);
 
       if (updateState)
-         columnState_ = State.createState(JsUtil.toJsArrayString(getNames(false)));
+         columnState_ = State.createState(JsUtil.toJsArrayString(getNames(false)),
+                                          getActive().getName());
       return column.getName();
    }
 
    public void initialSelect(int index)
    {
+      SourceColumn lastActive = getByName(columnState_.getActiveColumn());
+      if (lastActive != null)
+         setActive(getByName(columnState_.getActiveColumn()));
       getActive().initialSelect(index);
    }
 
@@ -364,6 +378,9 @@ public class SourceColumnManager implements CommandPaletteEntrySource,
             activeColumn_.setActiveEditor();
          manageCommands(true);
       }
+
+      columnState_ = State.createState(JsUtil.toJsArrayString(getNames(false)),
+                                       activeColumn_ == null ? "" : activeColumn_.getName());
    }
 
    private void setActiveDocId(String docId)
@@ -1444,7 +1461,8 @@ public class SourceColumnManager implements CommandPaletteEntrySource,
          }
       }
 
-      columnState_ = State.createState(JsUtil.toJsArrayString(getNames(false)));
+      columnState_ = State.createState(JsUtil.toJsArrayString(getNames(false)),
+                                       getActive().getName());
    }
 
    public void closeColumn(String name)
@@ -1456,7 +1474,8 @@ public class SourceColumnManager implements CommandPaletteEntrySource,
          setActive(MAIN_SOURCE_NAME);
 
       columnList_.remove(column);
-      columnState_ = State.createState(JsUtil.toJsArrayString(getNames(false)));
+      columnState_ = State.createState(JsUtil.toJsArrayString(getNames(false)),
+                                       getActive().getName());
    }
 
    public void closeColumn(SourceColumn column, boolean force)
@@ -1475,7 +1494,8 @@ public class SourceColumnManager implements CommandPaletteEntrySource,
       if (column == activeColumn_)
          setActive("");
       columnList_.remove(column);
-      columnState_ = State.createState(JsUtil.toJsArrayString(getNames(false)));
+      columnState_ = State.createState(JsUtil.toJsArrayString(getNames(false)),
+                                       getActive().getName());
    }
 
    public void ensureVisible(boolean newTabPending)
