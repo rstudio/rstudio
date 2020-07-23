cbb9b3817c74271e0868d6850da4badf583d5a90
melissa-barca <melissa@rstudio.com>
2020-07-22 17:15:13 -0700

    fix issues with source navigation buttons

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/SourceColumnManager.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/SourceColumnManager.java
@@ -616,8 +616,8 @@ public class SourceColumnManager implements CommandPaletteEntrySource,
    {
       commands_.sourceNavigateBack().setEnabled(
          sourceNavigationHistory_.isBackEnabled());
-      commands_.sourceNavigateBack().setEnabled(
-         sourceNavigationHistory_.isBackEnabled());
+      commands_.sourceNavigateForward().setEnabled(
+         sourceNavigationHistory_.isForwardEnabled());
    }
 
    public EditingTarget addTab(SourceDocument doc, int mode, SourceColumn column)
