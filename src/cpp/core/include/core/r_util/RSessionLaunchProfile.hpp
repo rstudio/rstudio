ce18bb7cf4d8a860ed35b7ab8c62a34cb94f5d5e
Ricardo Andrade <ricardo@rstudio.com>
2020-08-25 22:22:45 +0000

    Remove session profile password encryption from open source

--- src/cpp/core/include/core/r_util/RSessionLaunchProfile.hpp
+++ src/cpp/core/include/core/r_util/RSessionLaunchProfile.hpp
@@ -41,9 +41,6 @@ json::Object sessionLaunchProfileToJson(const SessionLaunchProfile& profile);
 
 SessionLaunchProfile sessionLaunchProfileFromJson(const json::Object& jsonProfile);
 
-Error encryptProfilePassword(SessionLaunchProfile& profile, std::string* encryptedPassword);
-Error decryptProfilePassword(SessionLaunchProfile& profile, const std::string& encryptedPassword);
-
 } // namespace r_util
 } // namespace core 
 } // namespace rstudio
