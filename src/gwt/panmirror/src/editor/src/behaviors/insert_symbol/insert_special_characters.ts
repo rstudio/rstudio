2b81ded444142ad12257b73b3715e9d3414b29bc
JJ Allaire <jj@rstudio.com>
2020-09-07 14:34:01 -0400

    remove extra insert soft break command

--- src/gwt/panmirror/src/editor/src/behaviors/insert_symbol/insert_special_characters.ts
+++ src/gwt/panmirror/src/editor/src/behaviors/insert_symbol/insert_special_characters.ts
@@ -22,7 +22,6 @@ const extension = {
     return [
       new InsertCharacterCommand(EditorCommandId.EmDash, '—', []),
       new InsertCharacterCommand(EditorCommandId.EnDash, '–', []),
-      new InsertCharacterCommand(EditorCommandId.NonBreakingSpace, '\u00A0', ['Ctrl-Space', 'Ctrl-Shift-Space'])
     ];
   },
 };
