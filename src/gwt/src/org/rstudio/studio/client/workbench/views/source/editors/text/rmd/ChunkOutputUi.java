9a2f51562ffa91b468f127f3ed6347ee05f48987
melissa-barca <melissa@rstudio.com>
2020-09-11 14:11:48 -0400

    increase chunk output height

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/rmd/ChunkOutputUi.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/rmd/ChunkOutputUi.java
@@ -278,7 +278,7 @@ public class ChunkOutputUi
 
    public final static int MIN_CHUNK_HEIGHT = 25;
    public final static int CHUNK_COLLAPSED_HEIGHT = 15;
-   public final static int MAX_CHUNK_HEIGHT = 650;
+   public final static int MAX_CHUNK_HEIGHT = 1000;
    
    public final static int MIN_PLOT_WIDTH = 400;
    public final static int MAX_PLOT_WIDTH = 700;
