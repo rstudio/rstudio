rstudio/rstudio

commit 4fba2a0615af2cb9d7aed0fa0b6a7459a491be60

fix traceback showing while VirtaulScroller is turned on (#7942)

src/gwt/src/org/rstudio/studio/client/common/CommandLineHistory.java
@@ -83,7 +83,7 @@ public class CommandLineHistory
    private int getPositionAtOffset(int offset)
    {
       int pos = historyPos_ + offset;
-      return Math.max(0, Math.min(pos, history_.size()));
+      return Math.max(0, Math.min(pos, history_.size()) - 1);
    }
 
    private final ArrayList<String> history_ = new ArrayList<String>();

src/gwt/src/org/rstudio/core/client/VirtualConsole.java
@@ -488,7 +488,7 @@ public class VirtualConsole
    public void submit(String data, String clazz, boolean forceNewRange, boolean ariaLiveAnnounce)
    {
       // Only capture new elements when dealing with error output, which
-      // is only place that sets forceNewRange to true. This is just an
+      // is the only place that sets forceNewRange to true. This is just an
       // optimization to avoid unnecessary overhead for large (non-error)
       // output.
       captureNewElements_ = forceNewRange;
@@ -686,9 +686,7 @@ public class VirtualConsole
          element.setInnerText(text);
 
          if (captureNewElements_)
-         {
             newElements_.add(element);
-         }
       }
 
       public void trimLeft(int delta)

src/gwt/src/org/rstudio/studio/client/common/debugging/ui/ConsoleError.java
@@ -58,6 +58,7 @@ public class ConsoleError extends Composite
       initWidget(uiBinder.createAndBindUi(this));
       
       VirtualConsole vc = RStudioGinjector.INSTANCE.getVirtualConsoleFactory().create(errorMessage.getElement());
+      vc.setVirtualizedDisableOverride(true);
       vc.submit(err.getErrorMessage().trim());
       errorMessage.addStyleName(errorClass);
       

commit 32083ddaa1c4fe14ab13f9736680e7db2c5e6874

fix navigation of console history with arrow keys
- Fixes #7945
- Caused by #7942

src/gwt/src/org/rstudio/studio/client/common/CommandLineHistory.java
@@ -67,7 +67,7 @@ public class CommandLineHistory
             "");
       historyPos_ = newPos;
    }
-   
+
    public String getHistoryEntry(int offset)
    {
       int pos = getPositionAtOffset(offset);
@@ -79,11 +79,11 @@ public class CommandLineHistory
       historyPos_ = history_.size();
       historyTail_ = "";
    }
-   
+
    private int getPositionAtOffset(int offset)
    {
       int pos = historyPos_ + offset;
-      return Math.max(0, Math.min(pos, history_.size()) - 1);
+      return Math.max(0, Math.min(pos, history_.size()));
    }
 
    private final ArrayList<String> history_ = new ArrayList<String>();