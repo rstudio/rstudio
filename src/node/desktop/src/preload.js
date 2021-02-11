a67f62c81714886b07bc92663496e5adad9cc655
Gary Ritchie <gary@rstudio.com>
2021-02-11 15:43:11 -0800

    add context bridge and more stubs

--- src/node/desktop/src/preload.js
+++ src/node/desktop/src/preload.js
@@ -12,5 +12,30 @@
  * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
  *
  */
+const { contextBridge } = require('electron')
 
-window.desktop = true;
+// see DesktopGwtCallback in QtWebEngine version, attached in DesktopMainWindow constructor
+contextBridge.exposeInMainWorld('desktop', {
+});
+
+// RDP-only
+contextBridge.exposeInMainWorld('remoteDesktop', {
+});
+
+contextBridge.exposeInMainWorld('desktopInfo', {
+  chromiumDevtoolsPort: () => {console.log('chromiumDevToolsPort'); return 123; }
+});
+
+contextBridge.exposeInMainWorld('desktopMenuCallback', {
+  beginMainMenu: () => {console.log('beginMainMenu')},
+  beginMenu: (label) => {},
+  addCommand: (cmdId, label, tooltip, shortcut, isChecked) => {},
+  addSeparator: () => {},
+  endMenu: () => {},
+  endMainMenu: () => {console.log('endMainMenu')},
+  setCommandVisible: (commandId, visible) => {},
+  setCommandEnabled: (commandId, enabled) => {},
+  setCommandChecked: (commandId, checked) => {},
+  setMainMenuEnabled: (enabled) => {},
+  setCommandLabel: (commandId, label) => {},
+});
