41b352bb0fb6e11c3b0d4be29b0838fa4251ec0f
Gary Ritchie <gary@rstudio.com>
2021-02-13 19:54:27 -0800

    UI now bootable on Mac

--- src/node/desktop/src/preload.js
+++ src/node/desktop/src/preload.js
@@ -12,18 +12,43 @@
  * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
  *
  */
-const { contextBridge } = require('electron')
+const { contextBridge } = require('electron');
 
-// see DesktopGwtCallback in QtWebEngine version, attached in DesktopMainWindow constructor
-contextBridge.exposeInMainWorld('desktop', {
+/**
+ * The preload script is run in the renderer before our GWT code and enables
+ * setting up a bridge between the main process and the renderer process via
+ * the contextBridge mechanism.
+ * 
+ * Code in here has access to powerful node.js and Electron APIs even though
+ * the renderer itself is configured with node disabled and context isolation.
+ * 
+ * Be careful to only expose the exact APIs desired; DO NOT expose general-purpose
+ * IPC objects, etc.
+ */
+
+contextBridge.exposeInMainWorld('desktop',  {
+  getSessionServers: () => [], // only meaningful for RDP but invoked in open-source
+  setBusy: (busy) => {},
+  onWorkbenchInitialized: (scratchPath) => {},
+  getInitMessages: () => { return ""; },
+  setBackgroundColor: (rgbColor) => {},
+  syncToEditorTheme: (isDark) => {},
+  changeTitleBarColor: (red, green, blue)  => {},
+  reloadZoomWindow: () => {},
+
+  showMessageBox: (type, caption, message, buttons, defaultButton, cancelButton) => {
+    window.alert(message);
+    return 1;
+  },
+
+  setPendingQuit: (pendingQuit) => {},
 });
 
 // RDP-only
-//contextBridge.exposeInMainWorld('remoteDesktop', {
-//});
+//contextBridge.exposeInMainWorld('remoteDesktop', {});
 
 contextBridge.exposeInMainWorld('desktopInfo', {
-  chromiumDevtoolsPort: () => {console.log('chromiumDevToolsPort'); return 123; }
+  chromiumDevtoolsPort: () => { return 0; }
 });
 
 contextBridge.exposeInMainWorld('desktopMenuCallback', {
