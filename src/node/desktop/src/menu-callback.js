bb422688781fc07f0adf7e15a6f1b1e43447d56f
Gary Ritchie <gary@rstudio.com>
2021-02-18 18:10:02 -0800

    create menu skeleton

--- src/node/desktop/src/menu-callback.js
+++ src/node/desktop/src/menu-callback.js
@@ -12,51 +12,56 @@
  * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
  *
  */
-const { ipcMain } = require('electron');
+const { ipcMain, Menu, MenuItem } = require('electron');
 
 module.exports = class MenuCallback {
   constructor() {
 
+    this.mainMenu = null;
+    this.menuStack = [];
+
     ipcMain.on('menu_begin_main', (event) => {
+      this.mainMenu = new Menu();
     });
 
     ipcMain.on('menu_begin', (event, label) => {
+      let subMenu = new Menu();
+      let menuItem = new MenuItem({submenu: subMenu, label: label});
+      if (this.menuStack.length == 0) {
+        this.mainMenu.append(menuItem);
+      } else {
+        this.menuStack[this.menuStack.length - 1].append(menuItem);
+      }
+      this.menuStack.push(subMenu);
     });
 
     ipcMain.on('menu_add_command', (event, cmdId, label, tooltip, shortcut, isChecked) => {
-
     });
 
     ipcMain.on('menu_add_separator', (event) => {
-
     });
 
     ipcMain.on('menu_end', (event) => {
-
+      this.menuStack.pop();
     });
 
     ipcMain.on('menu_end_main', (event) => {
-
+      Menu.setApplicationMenu(this.mainMenu);
     });
 
     ipcMain.on('menu_set_command_visible', (event, commandId, visible) => {
-
     });
 
     ipcMain.on('menu_set_command_enabled', (event, commandId, enabled) => {
-
     });
 
     ipcMain.on('menu_set_command_checked', (event, commandId, checked) => {
-
     });
 
     ipcMain.on('menu_set_main_menu_enabled', (event, enabled) => {
-
     });
 
     ipcMain.on('menu_set_command_label', (event, commandId, label) => {
-
     });
   }
 }
