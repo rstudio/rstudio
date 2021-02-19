65da1d896a622605af29ba17f9e48f31d28be315
Jonathan McPherson <jonathan@rstudio.com>
2021-02-18 17:03:51 -0800

    add a client event for synchronizing set of subscribed commands

--- src/gwt/src/org/rstudio/studio/client/server/remote/ClientEventDispatcher.java
+++ src/gwt/src/org/rstudio/studio/client/server/remote/ClientEventDispatcher.java
@@ -18,9 +18,12 @@ package org.rstudio.studio.client.server.remote;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArray;
+import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.RepeatingCommand;
 
+import org.rstudio.core.client.Debug;
+import org.rstudio.core.client.command.CommandCallbacksChangedEvent;
 import org.rstudio.core.client.events.ExecuteAppCommandEvent;
 import org.rstudio.core.client.events.HighlightEvent;
 import org.rstudio.core.client.files.FileSystemItem;
@@ -1074,6 +1077,11 @@ public class ClientEventDispatcher
          {
             MemoryUsage data = event.getData();
             eventBus_.dispatchEvent(new MemoryUsageChangedEvent(data));
+         }
+         else if (type == ClientEvent.CommandCallbacksChanged)
+         {
+            JsArrayString commands = event.getData();
+            eventBus_.dispatchEvent(new CommandCallbacksChangedEvent(commands));
          }
          else
          {
