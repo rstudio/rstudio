475fd8dc0d4b6be067ee3cfcd2f15f412938cc39
Gary Ritchie <gary@rstudio.com>
2021-05-28 16:52:33 -0700

    placeholder test spec for app.ts

---
+++ src/node/desktop/test/main/app.spec.ts
@@ -0,0 +1,25 @@
+/*
+ * app.spec.ts
+ *
+ * Copyright (C) 2021 by RStudio, PBC
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+import { describe } from 'mocha';
+import { expect } from 'chai';
+
+import Main from '../../src/main/main';
+
+// IMPORTANT: Cannot unit-test app.ts, because it will cause app.WhenReady() to
+// start up RStudio, instead of the tests!
+describe('App', () => {
+  describe('No tests (by design)', () => {});
+});
