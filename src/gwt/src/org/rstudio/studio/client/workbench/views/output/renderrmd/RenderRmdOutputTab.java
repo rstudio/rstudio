d2c6aa171dcb0818f187b9a4e6b01bae216de3ef
JJ Allaire <jj@rstudio.com>
2021-06-06 16:22:08 -0400

    use render as tab name

--- src/gwt/src/org/rstudio/studio/client/workbench/views/output/renderrmd/RenderRmdOutputTab.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/output/renderrmd/RenderRmdOutputTab.java
@@ -58,7 +58,7 @@ public class RenderRmdOutputTab
                              Commands commands,
                              final Session session)
    {
-      super("R Markdown", shim);
+      super("Render", shim);
       shim_ = shim;
 
       GWT.<Binder>create(Binder.class).bind(commands, shim_);
