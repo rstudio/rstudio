7190c873177e71acaf8c31f638e08929ac3239a9
Kevin Ushey <kevin@rstudio.com>
2021-06-22 11:51:14 -0700

    automatically activate .condaenv environments; fixes for reticulate binding to Anaconda (#9505)
    * automatically activate project .condaenv
    
    * use RSTUDIO_FALLBACK_LIBRARY_PATH for anaconda
    
    * remove dupe
    
    * cleanup on exit
    
    * PR feedback
    
    * don't need to recompute suffixes
    
    * use user-specific library path
    
    * merge fixup

--- src/cpp/session/SessionMain.cpp
+++ src/cpp/session/SessionMain.cpp
@@ -239,6 +239,12 @@ using namespace rsession::client_events;
 namespace rstudio {
 namespace session {
 
+namespace {
+
+std::string s_fallbackLibraryPath;
+
+} // end anonymous namespace
+
 bool disableExecuteRprofile()
 {
    // check for session-specific override
@@ -459,6 +465,7 @@ void exitEarly(int status)
 {
    stopMonitorWorkerThread();
    FileLock::cleanUp();
+   FilePath(s_fallbackLibraryPath).removeIfExists();
    ::exit(status);
 }
 
@@ -1748,10 +1755,13 @@ void initMonitorClient()
 } // anonymous namespace
 
 // run session
-int main (int argc, char * const argv[])
+int main(int argc, char * const argv[])
 {
    try
    {
+      // save fallback library path
+      s_fallbackLibraryPath = core::system::getenv("RSTUDIO_FALLBACK_LIBRARY_PATH");
+      
       // sleep on startup if requested (mainly for debugging)
       std::string sleepOnStartup = core::system::getenv("RSTUDIO_SESSION_SLEEP_ON_STARTUP");
       if (!sleepOnStartup.empty())
@@ -1903,7 +1913,7 @@ int main (int argc, char * const argv[])
       // Mirror the R getOptions("width") value in an environment variable
       core::system::setenv("RSTUDIO_CONSOLE_WIDTH",
                safe_convert::numberToString(rstudio::r::options::kDefaultWidth));
-
+ 
       // set the rstudio user identity environment variable (can differ from
       // username in debug configurations). this is provided so that
       // rpostback knows what local stream to connect back to
