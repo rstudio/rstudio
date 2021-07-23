a565323adc19e617fe2aea4da83bb1841a1c3f52
Kevin Ushey <kevinushey@gmail.com>
2021-07-23 15:07:32 -0700

    support automatic R discovery on Windows

--- src/node/desktop/src/main/detect_r.ts
+++ src/node/desktop/src/main/detect_r.ts
@@ -15,20 +15,27 @@
 
 import { app, dialog } from 'electron';
 import { exec, execSync } from 'child_process';
-import { promisify } from 'util';
 import { existsSync } from 'fs';
-import { assert } from 'console';
 
 import { logger } from '../core/logger';
-import { Environment, getenv, setVars } from '../core/environment';
+import { Environment, getenv, setenv, setVars } from '../core/environment';
 import { FilePath } from '../core/file-path';
 
 import { appState } from './app-state';
+import path, { dirname } from 'path';
 
-const asyncExec = promisify(exec);
+interface REnvironment {
+  rScriptPath: string,
+  version: string,
+  envVars: Environment,
+}
+
+function rNotFoundErrorMessage(): string {
+  return "Could not locate an R installation on the system.";
+}
 
 function showRNotFoundError(msg: string): void {
-  dialog.showErrorBox('R Not Found', msg);
+  dialog.showErrorBox('R not found', msg);
 }
 
 /**
@@ -37,123 +44,98 @@ function showRNotFoundError(msg: string): void {
  * @returns true if startup should continue, false on fatal error
  */
 export async function prepareEnvironment(): Promise<boolean> {
-  if (process.platform === 'win32') {
-    return await prepareEnvironmentWin32();
-  } else {
-    return await prepareEnvironmentPosix();
+
+  try {
+    return prepareEnvironmentImpl();
+  } catch (error) {
+    logger().logError(error);
+    return false;
   }
+
 }
 
-async function prepareEnvironmentPosix(): Promise<boolean> {
-
-  assert((process.platform !== 'win32'));
-
-  // check for which R override
-  let rWhichRPath = new FilePath();
-  const whichROverride = getenv('RSTUDIO_WHICH_R');
-  if (whichROverride) {
-    rWhichRPath = new FilePath(whichROverride);
-  }
-
-  // determine rLdPaths script location
-  let rLdScriptPath = new FilePath();
-  if (process.platform === 'darwin') {
-    if (app.isPackaged) {
-      rLdScriptPath = appState().scriptsPath?.completePath('session/r-ldpath') ?? new FilePath();
-      if (!rLdScriptPath.existsSync()) {
-        rLdScriptPath = new FilePath(app.getAppPath()).completePath('r-ldpath');
-      }
-    } else {
-      rLdScriptPath = appState().sessionPath?.completePath('../r-ldpath') ?? new FilePath();
-    }
-  } else {
-    rLdScriptPath = appState().supportingFilePath().completePath('bin/r-ldpath');
-    if (!rLdScriptPath.existsSync()) {
-      rLdScriptPath = appState().supportingFilePath().completePath('session/r-ldpath');
-    }
-  }
+async function prepareEnvironmentImpl(): Promise<boolean> {
 
   // attempt to detect R environment
-  const detectResult = await detectREnvironment(rWhichRPath, rLdScriptPath, '');
-  if (!detectResult.success) {
-    showRNotFoundError(detectResult.errMsg ?? 'Unknown error');
+  var detectResult : REnvironment;
+  try {
+    detectResult = await detectREnvironment();
+  } catch (error) {
+    showRNotFoundError(error ?? 'An unknown error occurred.');
     return false;
   }
 
-  logger().logDiagnostic(`Using R script: ${detectResult.rScriptPath}`);
+  // set environment variables from R
+  setVars(detectResult.envVars);
 
-  setREnvironmentVars(detectResult.envVars ?? {});
-
-  if (process.platform === 'darwin') {
-    // TODO: deal with selection of x86_64 or arm64 build of R
-  }
-
-  return true;
-}
-
-async function prepareEnvironmentWin32(): Promise<boolean> {
-  assert((process.platform === 'win32'));
-
-  // TODO: this is just a hacked-together placeholder for dev purposes
-
-  // check for which R override
-  let rWhichRPath = new FilePath();
-  const whichROverride = getenv('RSTUDIO_WHICH_R');
-  if (!whichROverride) {
-    dialog.showErrorBox(
-      'RSTUDIO_WHICH_R Not Set',
-      'Temporary: RSTUDIO_WHICH_R environment variable must point to R installation folder. Cannot continue.');
-    return false;
-  }
-
-  rWhichRPath = new FilePath(whichROverride);
-  if (!rWhichRPath.existsSync()) {
-    dialog.showErrorBox(
-      'RSTUDIO_WHICH_R Incorrect',
-      `RSTUDIO_WHICH_R environment variable set incorrectly; ${rWhichRPath.getAbsolutePathNative()} not found. Cannot continue.`);
-    return false;
-  }
-
-
-  process.env.R_HOME = rWhichRPath.getAbsolutePathNative();
-  const rPath = rWhichRPath.completeChildPath('bin\\x64');
-  process.env.PATH = `${rPath};${process.env.PATH}`;
-
-  return true;
-}
-
-async function scanForR(rsessionWhichR: FilePath): Promise<FilePath> {
+  // on Windows, ensure R is on the PATH so that companion DLLs
+  // in the same directory can be resolved
+  const scriptPath = detectResult.rScriptPath || '';
   if (process.platform === 'win32') {
-    return await scanForRWin32(rsessionWhichR);
-  } else {
-    return await scanForRPosix(rsessionWhichR);
+    const binDir = dirname(scriptPath);
+    process.env.PATH = `${binDir};${process.env.PATH}`;
   }
+
+  return true;
+
 }
 
-async function scanForRPosix(rsessionWhichR: FilePath): Promise<FilePath> {
-  assert((process.platform !== 'win32'));
+async function detectREnvironment(): Promise<REnvironment> {
 
-  // if an override has been supplied (typically from rsession-which-r)
-  // then prefer that
-  if (!rsessionWhichR.isEmpty()) {
-    return rsessionWhichR;
+  // scan for R
+  const R = await scanForR();
+  if (!R) {
+    throw rNotFoundErrorMessage();
   }
+
+  // get R_HOME + other related R environment variables
+  const stdout = execSync(`${R} RHOME`, { encoding: 'utf-8'});
+  const home = stdout.trim();
+  const envvars = {
+    R_HOME:        `${home}`,
+    R_SHARE_DIR:   `${home}/share`,
+    R_INCLUDE_DIR: `${home}/include`,
+    R_DOC_DIR:     `${home}/doc`
+  };
+
+  // get R version string
+  const rVersion = execSync(
+    `${R} --vanilla -s -e "cat(format(getRversion()))"`,
+    { encoding: 'utf-8' }
+  );
+
+  return {
+    rScriptPath: R,
+    version:     rVersion,
+    envVars:     envvars
+  };
+
+}
+
+async function scanForR(): Promise<string> {
 
   // if the RSTUDIO_WHICH_R environment variable is set, use that
   const rstudioWhichR = getenv('RSTUDIO_WHICH_R');
   if (rstudioWhichR) {
-    return new FilePath(rstudioWhichR);
+    logger().logDiagnostic(`Using RSTUDIO_WHICH_R: ${rstudioWhichR}`)
+    return rstudioWhichR;
   }
 
-  // first look for R on the PATH
-  try {
-    const { stdout } = await asyncExec('/usr/bin/which R', { encoding: 'utf-8' });
-    const R = stdout.trim();
-    if (R) {
-      return new FilePath(R);
-    }
-  } catch (error) {
-    logger().logError(error);
+  // otherwise, use platform-specific lookup strategies
+  if (process.platform === 'win32') {
+    return await scanForRWin32();
+  } else {
+    return await scanForRPosix();
+  }
+}
+
+async function scanForRPosix(): Promise<string> {
+
+  // first, look for R on the PATH
+  const stdout = execSync('/usr/bin/which R', { encoding: 'utf-8' });
+  const R = stdout.trim();
+  if (R) {
+    return R;
   }
 
   // otherwise, look in some hard-coded locations
@@ -170,83 +152,55 @@ async function scanForRPosix(rsessionWhichR: FilePath): Promise<FilePath> {
 
   for (const location of defaultLocations) {
     if (existsSync(location)) {
-      return new FilePath(location);
+      return location;
     }
   }
 
   // nothing found
-  return new FilePath();
+  return '';
+
 }
 
-export async function scanForRWin32(rsessionWhichR: FilePath): Promise<FilePath> {
+function findDefaultInstallPathWin32(version: string): string {
 
-  assert((process.platform === 'win32'));
+  const keyName = `HKEY_LOCAL_MACHINE\\SOFTWARE\\R-Core\\${version}`;
+  const regOutput = execSync(`reg query ${keyName} /v InstallPath`, { encoding: 'utf-8'});
 
-  // if an override has been supplied (typically from rsession-which-r)
-  // then prefer that
-  if (!rsessionWhichR.isEmpty()) {
-    return rsessionWhichR;
+  const lines = regOutput.split('\r\n');
+  for (const line of lines) {
+    const match = /^\s*InstallPath\s*REG_SZ\s*(.*)$/.exec(line);
+    if (match != null) {
+      return match[1];
+    }
   }
+
+  return '';
+
+}
+
+export async function scanForRWin32(): Promise<string> {
 
   // if the RSTUDIO_WHICH_R environment variable is set, use that
   const rstudioWhichR = getenv('RSTUDIO_WHICH_R');
   if (rstudioWhichR) {
-    return new FilePath(rstudioWhichR);
+    return rstudioWhichR;
   }
 
-  // read default version information from registry
-  const keyName = 'HKEY_LOCAL_MACHINE\\SOFTWARE\\R-Core\\R64';
-  const regOutput = execSync(`reg query ${keyName} /v InstallPath`, { encoding: 'utf-8'});
-
-  const lines = regOutput.split('\n');
-  for (const line of lines) {
-    const match = /^\s*InstallPath\s*REG_SZ\s*(.*)$/.exec(line);
-    if (match != null) {
-      const installPath = match[1];
-      return new FilePath(installPath);
+  // look for a 64-bit version of R
+  if (process.arch !== 'x32') {
+    const x64InstallPath = findDefaultInstallPathWin32("R64");
+    if (x64InstallPath) {
+      return `${x64InstallPath}/bin/x64/R.exe`;
     }
   }
 
-  // For now must set env var to run on Windows
-  return new FilePath();
-}
-
-interface REnvironment {
-  success: boolean,
-  rScriptPath?: string,
-  version?: string,
-  envVars?: Environment,
-  errMsg?: string,
-}
-
-async function detectREnvironment(
-  whichRScript: FilePath,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  ldPathsScript: FilePath,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  ldLibraryPath: string
-): Promise<REnvironment> {
-
-  const scanResult = await scanForR(whichRScript);
-  if (scanResult.isEmpty()) {
-    return { success: false, errMsg: 'Did not find R' };
+  // look for a 32-bit version of R
+  const i386InstallPath = findDefaultInstallPathWin32("R");
+  if (i386InstallPath) {
+    return `${i386InstallPath}/bin/i386/R.exe`;
   }
-  const R = scanResult.getAbsolutePath();
 
-  // read some important environment variables
-  const { stdout } = await asyncExec(`${R} --no-save --no-restore RHOME`);
-  const rHome = stdout.trim();
-  const rEnv = {
-    R_HOME: `${rHome}`,
-    R_SHARE_DIR: `${rHome}/share`,
-    R_DOC_DIR: `${rHome}/doc`
-  };
+  // nothing found; return empty filepath
+  return '';
 
-  // TODO: detect and return R version 
-
-  return { success: true, rScriptPath: scanResult.getAbsolutePath(), envVars: rEnv };
-}
-
-function setREnvironmentVars(vars: Environment) {
-  setVars(vars);
 }
