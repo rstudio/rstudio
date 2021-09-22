332ef3d64eb3508b13b28eea2d68f3b4acc6233f
JJ Allaire <jj@rstudio.com>
2021-09-22 17:28:04 -0400

    ensrue saved document before executing quarto xref rpc calls from editor

--- src/gwt/panmirror/src/editor/src/marks/xref/xref-completion.tsx
+++ src/gwt/panmirror/src/editor/src/marks/xref/xref-completion.tsx
@@ -150,7 +150,6 @@ function xrefCompletions(ui: EditorUI, server: XRefServer, index: FuseIndex) {
         completions: async () => {
           const docPath = ui.context.getDocumentPath();
           if (docPath) {
-            // NOTE: don't need the withSavedDocument for quarto
             await ui.context.withSavedDocument();
             const xrefs = await server.indexForFile(docPath);
             index.update(xrefs.refs);
