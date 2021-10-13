caace054080ca3d1228ae8e8bcb0cee7aefe06ec
Jonathan McPherson <jonathan@rstudio.com>
2021-10-13 16:03:08 -0700

    working github push

--- docker/jenkins/publish-build.sh
+++ docker/jenkins/publish-build.sh
@@ -8,7 +8,7 @@ if [ $# -eq 0 ]; then
     echo "Arguments:"
     echo ""
     echo "--build   What kind of build is being added, as a path. Example: "
-    echo "          rstudio/flower-name/desktop-pro/windows"
+    echo "          desktop-pro/windows"
     echo ""
     echo "--url     A URL to a location where the build can be downloaded. Example:"
     echo "          https://s3.amazonaws.com/rstudio-ide-build/desktop/windows/RStudio-pro.exe"
@@ -16,14 +16,15 @@ if [ $# -eq 0 ]; then
     echo "--file    A path to a local copy of the build file. Example:"
     echo "          /tmp/RStudio-pro.exe"
     echo ""
-    echo "--commit  The long SHA1 hash of the Git commit on which the build is based. Example:"
-    echo "          17cb842363757497c5901c9482ace78ae16c2cbc"
-    echo ""
-    echo "--channel The build channel. One of Daily, Preview, or Release."
-    echo ""
     echo "--version The full build version. Example: 2025.09.0-daily+123"
+    echo ""
+    echo "--pat     The Github Personal Access Token (PAT) to be used to authorize the commit."
+    echo "          May be specified in the environment variable GITHUB_PAT instead."
     exit 0
 fi
+
+# abort on error
+set -e
 
 
 # List of arguments
@@ -31,9 +32,8 @@ ARGUMENT_LIST=(
     "build"
     "url"
     "file"
-    "commit"
-    "channel"
     "version"
+    "pat"
 )
 
 # Parse arguments with getopt
@@ -62,20 +62,16 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
 
-        --commit)
-            commit=$2
-            shift 2
-            ;;
-
-        --channel)
-            channel=$2
-            shift 2
-            ;;
-
         --version)
             version=$2
             shift 2
             ;;
+
+        --pat)
+            pat=$2
+            shift 2
+            ;;
+
         *)
             break
             ;;
@@ -84,12 +80,12 @@ done
 
 # Check args
 if [ -z "$build" ]; then
-    echo "Build not set; specify a build with --build. Example: --build rstudio/flower-name/desktop/windows"
+    echo "Build not set; specify a build with --build. Example: --build desktop/windows"
     exit 1
 fi
 
 if [ -z "$url" ]; then
-    echo "URL not set; specify a build with --url. Example: --url https://s3.amazonaws.com/rstudio-ide-build/desktop/windows/RStudio-pro.exe"
+    echo "URL not set; specify a build with --url. Example: --url https:/s3.amazonaws.com/rstudio-ide-build/desktop/windows/RStudio-pro.exe"
     exit 1
 fi
 
@@ -98,19 +94,19 @@ if [ -z "$file" ]; then
     exit 1
 fi
 
-if [ -z "$commit" ]; then
-    echo "Commit not set; specify a Git commit with --commit. Example: --commit 17cb842363757497c5901c9482ace78ae16c2cbc"
-    exit 1
-fi
-
-if [ -z "$channel" ]; then
-    echo "Channel not set; specify a channel with --channel. Example: --channel Daily"
-    exit 1
-fi
-
 if [ -z "$version" ]; then
     echo "Version not set; specify a version with --version. Example: --version 2025.09.0-daily+123"
     exit 1
+fi
+
+if [ -z "$pat" ]; then
+    if [ -z "$GITHUB_PAT" ]; then
+        echo "Github Personal Access Token (PAT) not set; specify a PAT with --pat or the GITHUB_PAT environment variable."
+        exit 1
+    else
+        # PAT supplied in environment variable; promote to local
+        pat="$GITHUB_PAT"
+    fi
 fi
 
 # Determine file size
@@ -122,6 +118,16 @@ sha256=$(sha256sum $file| awk '{print $1}')
 # Form ISO 8601 timestamp
 timestamp=$(date +"%Y-%m-%dT%H:%M:%S%z")
 
+# Determine release channel (build type) and flower
+RSTUDIO_ROOT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd ../.. && pwd )"
+channel="$(cat "$RSTUDIO_ROOT_DIR/version/BUILDTYPE" | tr '[ ]' '-' | tr -d '[:space:]')"
+flower="$(cat "$RSTUDIO_ROOT_DIR/version/RELEASE" | tr '[ ]' '-' | tr -d '[:space:]' | tr '[:upper:]' '[:lower:]')"
+
+# Determine commit (use local hash)
+pushd $RSTUDIO_ROOT_DIR
+commit=$(git rev-parse HEAD)
+popd
+
 # Create version stem. This is a very file-safe version of the version: first
 # we replace non-alphanumerics with dashes, then collapse multiple dashes to a
 # single dash.
@@ -129,17 +135,27 @@ version_stem=$(echo $version | sed -e 's/[^a-zA-Z0-9-]/-/g' | sed -e 's/--*/-/g'
 
 filename=$(basename $file)
 
-echo "$platform/$version_stem.md"
-echo "---"
-echo "type: build"
-echo "link: \"$url\""
-echo "filename: \"$filename\""
-echo "sha256: \"$sha256\""
-echo "channel: \"$channel\""
-echo "version: \"$version\""
-echo "commit: \"$commit\""
-echo "size: \"$size\""
-echo "date: \"$timestamp\""
-echo "---"
+md_contents="---
+type: build
+date: $timestamp
+link: \"$url\"
+filename: \"$filename\"
+sha256: \"$sha256\"
+channel: \"$channel\"
+version: \"$version\"
+commit: \"$commit\"
+size: $size
+---
+"
 
+echo "Creating $flower/$build/$version_stem.md..."
+echo "$md_contents"
+base64_contents=$(echo "$md_contents" | base64)
+
+curl \
+  -X PUT \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $pat" \
+  "https://api.github.com/repos/rstudio/latest-builds/contents/content/rstudio/$flower/$build/$version_stem.md" \
+  -d "{\"message\":\"Add $flower build $version in $build\",\"content\":\"$base64_contents\"}"
 
