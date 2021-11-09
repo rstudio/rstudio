02e8df1dfaa4ab577ee9dad975ea3b24d2536eb2
JJ Allaire <jj@rstudio.com>
2021-11-09 09:51:21 -0500

    visual editor: improve detection of markdown content that requires serialization as a grid table

--- src/gwt/panmirror/src/editor/src/api/paragraph.ts
+++ src/gwt/panmirror/src/editor/src/api/paragraph.ts
@@ -14,6 +14,7 @@
  */
 
 import { EditorState, Transaction } from 'prosemirror-state';
+import { Node as ProsemirrorNode } from 'prosemirror-model';
 
 import { setTextSelection } from 'prosemirror-utils';
 
@@ -34,4 +35,13 @@ export function insertParagraph(state: EditorState, dispatch?: (tr: Transaction)
   }
 
   return true;
+}
+
+export function isParagraphNode(node: ProsemirrorNode | null | undefined) {
+  if (node) {
+    const schema = node.type.schema;
+    return node.type === schema.nodes.paragraph;
+  } else {
+    return false;
+  }
 }
