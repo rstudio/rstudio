c85c16e2a6707a961f2bc56d8e465279037f9b56
JJ Allaire <jj@rstudio.com>
2021-11-15 12:43:33 -0500

    quarto: update to 0.2.279

--- src/cpp/session/modules/quarto/SessionQuarto.cpp
+++ src/cpp/session/modules/quarto/SessionQuarto.cpp
@@ -151,10 +151,10 @@ std::tuple<FilePath,Version> userInstalledQuarto()
 void detectQuartoInstallation()
 {
    // required quarto version (quarto features don't work w/o it)
-   const Version kQuartoRequiredVersion("0.2.275");
+   const Version kQuartoRequiredVersion("0.2.279");
 
    // recommended quarto version (a bit more pestery than required)
-   const Version kQuartoRecommendedVersion("0.2.275");
+   const Version kQuartoRecommendedVersion("0.2.279");
 
    // reset
    s_userInstalledPath = FilePath();
