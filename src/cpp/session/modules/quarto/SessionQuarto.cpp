7be1ebcd6c4f3ace17a1eb68dce805bd4b8363e2
JJ Allaire <jj@rstudio.com>
2021-11-15 14:02:52 -0500

    quarto: v02.280

--- src/cpp/session/modules/quarto/SessionQuarto.cpp
+++ src/cpp/session/modules/quarto/SessionQuarto.cpp
@@ -151,10 +151,10 @@ std::tuple<FilePath,Version> userInstalledQuarto()
 void detectQuartoInstallation()
 {
    // required quarto version (quarto features don't work w/o it)
-   const Version kQuartoRequiredVersion("0.2.279");
+   const Version kQuartoRequiredVersion("0.2.280");
 
    // recommended quarto version (a bit more pestery than required)
-   const Version kQuartoRecommendedVersion("0.2.279");
+   const Version kQuartoRecommendedVersion("0.2.280");
 
    // reset
    s_userInstalledPath = FilePath();
