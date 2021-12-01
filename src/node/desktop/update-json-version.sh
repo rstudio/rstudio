c12534b115341a08214a3205ac7a54753d32f05e
Matheus Tavares <matheustavaresdev@gmail.com>
2021-12-01 17:46:30 -0300

    Renamed update-json-version script

--- src/node/desktop/update-json-version.sh
+++ src/node/desktop/update-json-version.sh
@@ -4,7 +4,7 @@ if [ "$1" = "" ]; then
     RED=$(tput setaf 1)
     echo "${RED}Error when trying to change Electron's version"
     echo "${RED}Please provide a version. Usage example:"
-    echo "${RED}./update_json_version.sh 1.2.3"
+    echo "${RED}./update-json-version.sh 1.2.3"
 else
     GREEN=$(tput setaf 2)
     json -I -f package.json -e "this.version=\"$1\""
