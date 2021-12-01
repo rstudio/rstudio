bfa52f2b2e1b6b4cd2ecb970882a63e82df33b54
Matheus Tavares <matheustavaresdev@gmail.com>
2021-12-01 18:00:00 -0300

    Added support for update-json-version.sh to be called from any folder JSON package is now being called from inside the project's node_modules instead of the global variable

--- src/node/desktop/scripts/update-json-version.sh
+++ src/node/desktop/scripts/update-json-version.sh
@@ -7,6 +7,10 @@ if [ "$1" = "" ]; then
     echo "${RED}./update-json-version.sh 1.2.3"
 else
     GREEN=$(tput setaf 2)
-    json -I -f package.json -e "this.version=\"$1\""
+
+    SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+    cd "${SCRIPT_DIR}/../";
+    
+    yarn json -I -f package.json -e "this.version=\"$1\""
     echo "${GREEN}Electron's Version successfully updated to $1"
 fi
