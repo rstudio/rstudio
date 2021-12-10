07ec309cdf9f1f69d0c6b83b78747ccff27c4d9b
JJ Allaire <jj@rstudio.com>
2021-12-09 19:43:25 -0500

    default presentation preview pane to tabset 2 (bottom)

--- src/gwt/src/org/rstudio/studio/client/workbench/prefs/model/UserPrefsAccessor.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/prefs/model/UserPrefsAccessor.java
@@ -381,11 +381,11 @@ public class UserPrefsAccessor extends Prefs
       }-*/;
 
       public final native JsArrayString getTabSet1() /*-{
-         return this && this.tabSet1 || ["Environment","History","Connections","Build","VCS","Tutorial","Presentations","Presentation"];
+         return this && this.tabSet1 || ["Environment","History","Connections","Build","VCS","Tutorial","Presentation"];
       }-*/;
 
       public final native JsArrayString getTabSet2() /*-{
-         return this && this.tabSet2 || ["Files","Plots","Packages","Help","Viewer"];
+         return this && this.tabSet2 || ["Files","Plots","Packages","Help","Viewer","Presentations"];
       }-*/;
 
       public final native JsArrayString getHiddenTabSet() /*-{
