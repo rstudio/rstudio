1d2dde44493c01a1fbbef3bb0050435c6e18bac2
Romain Francois <romain@rstudio.com>
2022-01-11 14:22:25 +0100

    Add cpp11 template

---
+++ src/cpp/session/resources/templates/cpp11.cpp
@@ -0,0 +1,16 @@
+#include <cpp11.hpp>
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using cpp11::cpp_source()
+// Learn more about cpp11 at: 
+//
+//   https://cpp11.r-lib.org/index.html
+
+[[cpp11::register]]
+double sum(cpp11::doubles x) {
+    double total = 0.0;
+    for (double value: x) {
+        total += x;
+    }
+    return total;
+}
