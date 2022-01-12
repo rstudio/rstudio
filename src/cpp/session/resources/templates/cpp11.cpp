cf983444513aceb2edda7fe784b0a0d747de0963
Romain François <romain@rstudio.com>
2022-01-12 10:02:25 +0100

    Update src/cpp/session/resources/templates/cpp11.cpp
    Co-authored-by: Kevin Ushey <kevin@rstudio.com>

--- src/cpp/session/resources/templates/cpp11.cpp
+++ src/cpp/session/resources/templates/cpp11.cpp
@@ -1,7 +1,8 @@
 #include <cpp11.hpp>
 
 // This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using cpp11::cpp_source()
+// source this function into an R session using `cpp11::cpp_source()`.
+
 // Learn more about cpp11 at: 
 //
 //   https://cpp11.r-lib.org/index.html
