2b7ea6559ed10287c525aa365a14320c853faf50
Romain Francois <romain@rstudio.com>
2022-01-21 23:45:44 +0100

    Fix tests

--- src/cpp/tests/testthat/test-cpp.R
+++ src/cpp/tests/testthat/test-cpp.R
@@ -15,44 +15,44 @@
 
 context("cpp")
 
-test_that("cpp_source_file() uses correct package", {
+test_that("cppSourceFile() uses correct package", {
     tf <- tempfile(fileext = ".cpp")
     
     writeLines('#include <Rcpp.h>\n//[[Rcpp::export]]\nvoid fun(){}', tf)
-    call <- .rs.cpp_source_file(tf)
+    call <- .rs.cppSourceFile(tf)
     expect_match(call, "Rcpp::sourceCpp")
 
     writeLines('void fun(){}', tf)
-    call <- .rs.cpp_source_file(tf)
+    call <- .rs.cppSourceFile(tf)
     expect_match(call, "Rcpp::sourceCpp")
 
     writeLines('#include <cpp11.hpp>\n[[cpp11::register]]\nvoid fun(){}', tf)
-    call <- .rs.cpp_source_file(tf)
+    call <- .rs.cppSourceFile(tf)
     expect_match(call, "cpp11::cpp_source")
     
     unlink(tf)
-    call <- .rs.cpp_source_file(tf)
+    call <- .rs.cppSourceFile(tf)
     expect_match(call, "Rcpp::sourceCpp")
 })
 
-test_that("cpp_project_style() correctly guesses cpp flavor", {
+test_that("cppProjectStyle() correctly guesses cpp flavor", {
     path <- tempfile()
     on.exit(unlink(path, recursive = TRUE))
 
     dir.create(path)
     writeLines("LinkingTo: Rcpp", file.path(path, "DESCRIPTION"))
-    expect_equal(.rs.cpp_project_style(path), "Rcpp")
+    expect_equal(.rs.cppProjectStyle(path), "Rcpp")
 
     writeLines("LinkingTo: cpp11", file.path(path, "DESCRIPTION"))
-    expect_equal(.rs.cpp_project_style(path), "cpp11")
+    expect_equal(.rs.cppProjectStyle(path), "cpp11")
 
     writeLines("Type: Package", file.path(path, "DESCRIPTION"))
-    expect_equal(.rs.cpp_project_style(path), "")
+    expect_equal(.rs.cppProjectStyle(path), "")
 
     writeLines("Type/Package", file.path(path, "DESCRIPTION"))
-    expect_equal(.rs.cpp_project_style(path), "")
+    expect_equal(.rs.cppProjectStyle(path), "")
 
     dir.create(file.path(path, "inst", "include"), recursive = TRUE)
     writeLines("", file.path(path, "inst", "include", "cpp11.hpp"))
-    expect_equal(.rs.cpp_project_style(path), "cpp11")
+    expect_equal(.rs.cppProjectStyle(path), "cpp11")
 })
