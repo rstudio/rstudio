c983bfc8a98c6230f807af8f3208a29976ebd1c6
Jackie Gutman <jacqueline.gutman@rstudio.com>
2022-01-26 14:38:19 -0500

    just use perl

--- src/cpp/session/modules/SessionFind.cpp
+++ src/cpp/session/modules/SessionFind.cpp
@@ -1757,10 +1757,7 @@ core::Error Replacer::replaceRegexIgnoreCase(size_t matchOn, size_t matchOff,
 {
    try
    {
-
-      // egrep (grep -E) flavor regex includes POSIX ERE
-      boost::regex_constants::syntax_option_type flags = boost::regex::egrep & boost::regex::bk_vbar;
-      flags = flags | boost::regex::icase;
+      boost::regex_constants::syntax_option_type flags = boost::regex::perl | boost::regex::icase;
       boost::regex find(findRegex, flags);
       core::Error error = completeReplace(find, replaceRegex, matchOn, matchOff, pLine,
          pReplaceMatchOff);
@@ -1786,7 +1783,7 @@ core::Error Replacer::replaceRegexWithCase(size_t matchOn, size_t matchOff,
 {
    try
    {
-      boost::regex_constants::syntax_option_type flags = boost::regex::egrep & boost::regex::bk_vbar;
+      boost::regex_constants::syntax_option_type flags = boost::regex::perl;
       boost::regex find(findRegex, flags);
       core::Error error = completeReplace(find, replaceRegex, matchOn, matchOff, pLine,
          pReplaceMatchOff);
