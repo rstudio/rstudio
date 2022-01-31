4e0ed0d470efe405575ed72c40b70dfb51325d2a
Kevin Ushey <kevinushey@gmail.com>
2022-01-31 13:50:44 -0800

    support single-arch package builds on Intel macOS

--- package/osx/scripts/merge-electron.mjs
+++ package/osx/scripts/merge-electron.mjs
@@ -6,20 +6,33 @@ import { rm } from 'fs/promises';
 
 const [ node, script, x64AppPath, arm64AppPath, outPath ] = process.argv;
 
-// build universal application in temporary directory,
-// then merge it all together when we're done
-const tmpPath = `${outPath}.tmp`;
+// for x64, we don't build a universal application; instead,
+// we just copy the x64 Electron bits directly into the package
+let tmpPath = "";
+if (process.arch === 'arm64') {
 
-// merge the two builds together
-console.log("# Building universal Desktop application.")
-console.log(`- [i] x64AppPath: ${x64AppPath}`)
-console.log(`- [i] arm64AppPath: ${arm64AppPath}`)
-await makeUniversalApp({
-  x64AppPath: x64AppPath,
-  arm64AppPath: arm64AppPath,
-  outAppPath: tmpPath,
-  force: true,
-});
+  // build universal application in temporary directory,
+  // then merge it all together when we're done
+  tmpPath = `${outPath}.tmp`;
+
+  // merge the two builds together
+  console.log("# Building universal Desktop application.")
+  console.log(`- [i] x64AppPath: ${x64AppPath}`)
+  console.log(`- [i] arm64AppPath: ${arm64AppPath}`)
+  await makeUniversalApp({
+    x64AppPath: x64AppPath,
+    arm64AppPath: arm64AppPath,
+    outAppPath: tmpPath,
+    force: true,
+  });
+
+} else {
+
+  console.log("# Building x86_64 Electron application");
+  console.log(`- [i] x64AppPath: ${x64AppPath}`)
+  tmpPath = x64AppPath;
+  
+}
 
 // use rsync to move them into the final install path
 console.log("- [i] Merging desktop and session packages ...")
@@ -27,5 +40,7 @@ execSync(`rsync -a "${tmpPath}/" "${outPath}/"`, { stdio: 'inherit' });
 console.log("- [i] Done!")
 
 // clean up
-await rm(tmpPath, { recursive: true });
+if (process.arch === 'arm64') {
+  await rm(tmpPath, { recursive: true });
+}
 
