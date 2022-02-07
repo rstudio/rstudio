f88aad7fce9971f223bdc1c8b59babb784c2de97
JJ Allaire <jj@rstudio.com>
2022-02-06 22:06:22 -0500

    Update Quarto to v0.3.105

--- src/cpp/session/modules/quarto/SessionQuarto.cpp
+++ src/cpp/session/modules/quarto/SessionQuarto.cpp
@@ -207,10 +207,10 @@ void detectQuartoInstallation()
 {
 #ifdef QUARTO_ENABLED
    // required quarto version (quarto features don't work w/o it)
-   const Version kQuartoRequiredVersion("0.3.96");
+   const Version kQuartoRequiredVersion("0.3.105");
 
    // recommended quarto version (a bit more pestery than required)
-   const Version kQuartoRecommendedVersion("0.3.96");
+   const Version kQuartoRecommendedVersion("0.3.105");
 
    // reset
    s_userInstalledPath = FilePath();
