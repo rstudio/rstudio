40fd4959b302804207d5d064878251e744a1397c
Tracy Teal <tracyteal@tamarack.local>
2022-02-15 18:17:09 -0800

    changing double quotes to single quotes

--- src/cpp/session/resources/templates/shiny/app.R
+++ src/cpp/session/resources/templates/shiny/app.R
@@ -42,8 +42,8 @@ server <- function(input, output) {
 
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = "Waiting time to next eruption (in mins)",
-             main = "Histogram of waiting times")
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
     })
 }
 
