d0462dd897123f735297e43f330d150acce1a401
Michael Chirico <michaelchirico4@gmail.com>
2022-03-22 23:53:31 -0700

    check is.double to skip always as.integer() integer input

--- src/cpp/r/R/Api.R
+++ src/cpp/r/R/Api.R
@@ -674,10 +674,10 @@
    }
 
    # transform numeric line, column values to integer
-   if (is.numeric(line))
+   if (is.double(line))
       line <- as.integer(line)
    
-   if (is.numeric(col))
+   if (is.double(col))
       col <- as.integer(col)
 
    # validate line/col arguments
