14e00c236f6db13910a2138325b781bcae577e8b
Gary Ritchie <gary@rstudio.com>
2022-03-31 16:53:25 -0600

    don't use yargs

--- src/node/desktop/src/main/args-manager.ts
+++ src/node/desktop/src/main/args-manager.ts
@@ -1,22 +1,26 @@
+/*
+ * args-manager.ts
+ *
+ * Copyright (C) 2022 by RStudio, PBC
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
 import { app } from 'electron';
 import { WinstonLogger } from '../core/winston-logger';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
 
 import { enableDiagnosticsOutput, parseCommandLineLogLevel, setLogger } from '../core/logger';
 import { Application } from './application';
 import { exitSuccess, ProgramStatus, run } from './program-status';
 import { getComponentVersions, userLogPath } from './utils';
 
-// How to use:
-// [MAC] RStudio.app/Contents/MacOS/RStudio --session-exit
-// [MAC] RStudio.app/Contents/MacOS/RStudio --help
-// [Windows] C:\Program Files\RStudio\rstudio.exe --help
-// [Linux] C:\Program Files\RStudio\rstudio.exe --help
-// npm run start -- -- --session-exit
-// npm run start -- -- --log-level=err
-// npm run start -- -- --help
-// npm run start -- -- --version
 // RStudio command-line switches
 export const kRunDiagnosticsOption = '--run-diagnostics';
 export const kVersion = '--version';
@@ -24,6 +28,7 @@ export const kVersionJson = '--version-json';
 export const kLogLevel = 'log-level';
 export const kDelaySession = 'session-delay';
 export const kSessionExit = 'session-exit';
+export const kHelp = '--help';
 
 // RStudio Pro Only
 // export const kSessionServerOption = '--session-server';
@@ -32,41 +37,22 @@ export const kSessionExit = 'session-exit';
 
 // !IMPORTANT: If some args should early exit the application, add them to `webpack.plugins.js`
 export class ArgsManager {
-  argsList = [
-    {
-      name: kVersionJson,
-      describe: 'Display versions of major components in JSON format',
-    },
-    {
-      name: kRunDiagnosticsOption,
-      describe: 'Run diagnostics and save in a file',
-    },
-    {
-      name: kLogLevel,
-      describe: 'Sets the verbosity of the logging --log-level=ERR|WARN|INFO|DEBUG',
-    },
-    {
-      name: kDelaySession,
-      describe: 'Causes the rsession to pause so the user can see the "Loading R" screen longer',
-    },
-    {
-      name: kSessionExit,
-      describe: 'Causes the rsession to terminate immediately so the user can see the error page',
-    },
-  ];
-
-  handleHelp(argv: string[]) {
-    const yargsHelper = yargs(hideBin(argv)); //.argv;
-
-    this.argsList.forEach((arg) => {
-      yargsHelper.option(arg.name.replace('--', ''), { describe: arg.describe });
-    });
-
-    yargsHelper.help().parseSync();
-  }
 
   initCommandLine(application: Application, argv: string[] = process.argv): ProgramStatus {
-    this.handleHelp(argv);
+
+    // display usage help
+    if (argv.indexOf(kHelp) > -1) {
+      console.log('Options:');
+      console.log('  --version          Display RStudio version');
+      console.log('  --version-json     Display versions of major components in JSON format');
+      console.log('  --run-diagnostics  Run diagnostics and save in a file');
+      console.log('  --log-level        Sets the verbosity of the logging');
+      console.log('                     --log-level=ERR|WARN|INFO|DEBUG');
+      console.log('  --session-delay    Pause the rsession so the "Loading R" screen displays longer');
+      console.log('  --session-exit     Terminate the rsession immediately forcing error page to display');
+      console.log('  --help             Show this help');
+      return exitSuccess();
+    }
 
     // look for a version check request; if we have one, just do that and exit
     if (argv.indexOf(kVersion) > -1) {
