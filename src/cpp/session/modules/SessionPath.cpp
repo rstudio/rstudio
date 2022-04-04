0905b16fcaead3997ea2ea0e2bb3e11906f290ca
Kevin Ushey <kevinushey@gmail.com>
2022-04-04 11:43:11 -0700

    add libfmt dependency; use for log macros

--- src/cpp/session/modules/SessionPath.cpp
+++ src/cpp/session/modules/SessionPath.cpp
@@ -20,10 +20,11 @@
 
 #include <boost/bind/bind.hpp>
 
-#include <core/Algorithm.hpp>
 #include <shared_core/Error.hpp>
-#include <core/Log.hpp>
 #include <shared_core/FilePath.hpp>
+
+#include <core/Algorithm.hpp>
+#include <core/Log.hpp>
 #include <core/FileSerializer.hpp>
 
 #include <core/system/System.hpp>
