a5c0bddd022013836a6fa33195ef2315a8995436
Gary Ritchie <gary@rstudio.com>
2022-04-13 17:22:28 -0600

    use cookie instead of local storage

---
+++ src/gwt/src/org/rstudio/studio/client/workbench/prefs/model/LocaleCookie.java
@@ -0,0 +1,50 @@
+/*
+ * LocaleCookie.java
+ *
+ * Copyright (C) 2022 by RStudio, PBC
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+package org.rstudio.studio.client.workbench.prefs.model;
+
+import com.google.gwt.user.client.Cookies;
+import org.rstudio.core.client.StringUtil;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Manage cookie used for setting the UI Language. This cookie's name corresponds to the
+ * value specified for locale.cookie property in RStudio.gwt.xml.
+ */
+public class LocaleCookie
+{
+   /**
+    * @return Current UI language setting, or the default (en) if none set
+    */
+   public static String getUiLanguage()
+   {
+      String cookieValue = Cookies.getCookie(UI_LANG_COOKIE);
+      if (StringUtil.isNullOrEmpty(cookieValue))
+         cookieValue = "en";
+      return cookieValue;
+   }
+
+   /**
+    * Set the cookie for UI language.
+    * @param uiLanguage
+    */
+   public static void setUiLanguage(String uiLanguage)
+   {
+      Cookies.setCookie(UI_LANG_COOKIE, uiLanguage, new Date(9999, Calendar.DECEMBER, 31, 18, 0));
+   }
+
+   private static final String UI_LANG_COOKIE = "LOCALE";
+}
