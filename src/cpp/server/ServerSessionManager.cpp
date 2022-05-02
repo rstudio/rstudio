9b61828876462dcd589be28c5d334a5ca3b9cfee
Maria Semple <maria@rstudio.com>
2022-05-02 13:23:53 -0700

    forward the session storage option to the session

--- src/cpp/server/ServerSessionManager.cpp
+++ src/cpp/server/ServerSessionManager.cpp
@@ -118,6 +118,8 @@ core::system::ProcessConfig sessionProcessConfig(
    args.push_back(std::make_pair("--" kSameSiteSessionOption,
                                  safe_convert::numberToString(static_cast<int>(options.wwwSameSite()))));
 
+   args.push_back({ "--" kSessionUseFileStorage, options.rsessionUseFileStorage() ? "1" : "0"});
+
    // create launch token if we haven't already
    if (s_launcherToken.empty())
       s_launcherToken = core::system::generateShortenedUuid();
