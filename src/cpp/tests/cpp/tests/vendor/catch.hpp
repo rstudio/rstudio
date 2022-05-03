4033ae9dde18a451d58db1e2787464c9ec551339
Gary Ritchie <gary@rstudio.com>
2022-05-03 16:12:15 -0600

    open-source merge for pro PR 3419

--- src/cpp/tests/cpp/tests/vendor/catch.hpp
+++ src/cpp/tests/cpp/tests/vendor/catch.hpp
@@ -10824,7 +10824,7 @@ namespace Catch {
 
     // 32kb for the alternate stack seems to be sufficient. However, this value
     // is experimentally determined, so that's not guaranteed.
-    static constexpr std::size_t sigStackSize = 32768 >= MINSIGSTKSZ ? 32768 : MINSIGSTKSZ;
+    static constexpr std::size_t sigStackSize = 32768;
 
     static SignalDefs signalDefs[] = {
         { SIGINT,  "SIGINT - Terminal interrupt signal" },
