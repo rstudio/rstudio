25de4cbc0fdc4e8eaef82bb1c64ac8f9e77df37b
Romain François <romain@rstudio.com>
2022-05-06 22:02:57 +0200

    show test_that() calls in document outline (#11108)
    * show test_that() calls in document outline
    
    * use test icon
    
    * style test items in the outline
    
    * +NEWS
    
    * Make function assignments secundary in test files
    
    * nodeLabelSecundary not italic
    
    * hyperlink get no default color (#11147)
    
    * update rstheme
    
    * update expected rstheme
    
    * :fr: for "plot" is "graphique" (#11148)
    
    * update plot french text
    
    * more
    
    * tracé -> graphique
    
    * plural
    
    * `uiLanguage*` properties in fr (#11119)
    
    * uiLanguage* properties in french
    
    * add trainling : in fr and en
    
    * visual editor: auto-prefix footnotes in included files
    
    support for 'editor' metadata options in files and projects
    
    * index `test_that()` calls for fuzzy finder (#11102)
    
    * index test_that calls for fuzzy finder
    
    * use moveToNextSignificantToken()
    
    * Don't unclude test_that() in the string
    
    * rm quote from test desc
    
    * make removeQuoteRegex static const
    
    * testThatCAllIndexer adds "t " prefix
    
    * only show test items if the search term starts with "t "
    
    * fuzzy finder removes artifical "t " prefix
    
    * remove dead code
    
    * When search term starts with "t ", only include test items
    
    * not looking for files and C++ items when only looking for test items
    
    * NEWS
    
    * factor out includeTestItems
    
    * handle raw string in test_that(<raw string>)
    
    * Update src/gwt/acesupport/acemode/r_scope_tree.js
    
    Co-authored-by: Kevin Ushey <kevin@rstudio.com>
    
    * show test_that() calls in document outline
    
    * use test icon
    
    * style test items in the outline
    
    * +NEWS
    
    * Make function assignments secundary in test files
    
    * nodeLabelSecundary not italic
    
    * use opacity 0.8
    
    * opacity: .7
    
    * .nodeLabelSecundary:hover
    
    Co-authored-by: JJ Allaire <jj@rstudio.com>
    Co-authored-by: Kevin Ushey <kevin@rstudio.com>

---
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/ScopeTest.java
@@ -0,0 +1,26 @@
+/*
+ * ScopeTest.java
+ *
+ * Copyright (C) 2022 by RStudio, PBC
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+package org.rstudio.studio.client.workbench.views.source.editors.text;
+
+public class ScopeTest extends Scope
+{
+   protected ScopeTest()
+   {}
+   
+   public final native String getTestName() /*-{
+      return this.attributes.name;
+   }-*/;
+
+}
