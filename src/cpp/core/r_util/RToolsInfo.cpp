7cfc5c0b0cf9556cbf3d6afec9b936ec34a6067d
Kevin Ushey <kevin@rstudio.com>
2022-05-17 17:50:45 -0700

    tweak how compiler definitions are passed along to libclang (#11211)
    * tweak how compiler definitions are passed along to libclang
    
    * fixup for RcppArmadillo
    
    * force rebuild of database if '-include' no longer exists
    
    * tweaks
    
    * further refine
    
    * log pch package name
    
    * [WIP]
    
    * add motivation
    
    * separate win32 implementation
    
    * suppress compiler warnings

--- src/cpp/core/r_util/RToolsInfo.cpp
+++ src/cpp/core/r_util/RToolsInfo.cpp
@@ -242,6 +242,12 @@ RToolsInfo::RToolsInfo(const std::string& name,
       std::replace(rtoolsPath.begin(), rtoolsPath.end(), '/', '\\');
       environmentVars.push_back({"RTOOLS42_HOME", rtoolsPath});
 
+      // undefine _MSC_VER, so that we can "pretend" to be gcc
+      // this is important for C++ libraries which might try to use
+      // MSVC-specific tools when _MSC_VER is defined (e.g. Eigen), which might
+      // not actually be defined or available in Rtools
+      clangArgs.push_back("-U_MSC_VER");
+
       // set GNUC levels
       // (required for _mingw.h, which otherwise tries to use incompatible MSVC defines)
       clangArgs.push_back("-D__GNUC__=9");
