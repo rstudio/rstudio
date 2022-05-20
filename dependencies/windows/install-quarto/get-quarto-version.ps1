a349847bdfc3c601a6e3ab107b98391879f0d4b4
Tim Mok <tim.mok@rstudio.com>
2022-05-20 09:00:16 -0400

    Fix windows dependency installer for Quarto

--- dependencies/windows/install-quarto/get-quarto-version.ps1
+++ dependencies/windows/install-quarto/get-quarto-version.ps1
@@ -1,6 +1,6 @@
 # Retrieve latest Quarto version from GitHub
 
-$response = Invoke-WebRequest -Uri https://api.github.com/repos/quarto-dev/quarto-cli/releases
+$response = Invoke-WebRequest -UseBasicParsing -Uri https://api.github.com/repos/quarto-dev/quarto-cli/releases
 $releases = ConvertFrom-Json $response.content
 $tag = $releases.tag_name[0]
 $quartoVersion = $tag | Select-String -Pattern '[0-9]+\.[0-9]+\.[0-9]+'
