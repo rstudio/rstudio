cc5d033fa86ebf7bea1fcbe900be72e438b4fbbf
Romain François <romain@rstudio.com>
2022-07-20 09:22:34 +0200

    more updates, tone down bright blue in vibtant ink

--- src/gwt/tools/xrdb/download_theme_xrdb.R
+++ src/gwt/tools/xrdb/download_theme_xrdb.R
@@ -19,11 +19,11 @@ patches <- list(
     solarized_light = c(Ansi_7_Color = "#d3d7cf", Ansi_15_Color = "#d3d7cf"),
     tomorrow = c(Ansi_7_Color = "#d3d7cf", Ansi_15_Color = "#eeeeec"),
     tomorrow_night = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c"), 
-    tomorrow_night_blue = c(Ansi_0_Color = "#4f4f4f"), 
+    tomorrow_night_blue = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c"), 
     tomorrow_night_bright = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c"),
-    tomorrow_night_eighties = c(Ansi_0_Color = "#4f4f4f"),
+    tomorrow_night_eighties = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c"),
     twilight = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c"), 
-    vibrant_ink = c(Ansi_8_Color = "#7c7c7c")
+    vibrant_ink = c(Ansi_8_Color = "#7c7c7c", Ansi_12_Color = "#5959ff")
 )
 
 for (i in seq_along(patches)) {
