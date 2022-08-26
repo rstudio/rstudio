6cf592ec21df6a89ef1815e5e027e336470f47ab
Tim Mok <timtmok@users.noreply.github.com>
2022-08-26 13:13:09 -0400

    Revert "Pin Quarto to 1.0.38"
    This reverts commit 5683531530d5a6f691ec47977efe193e4ade5d23.

--- dependencies/tools/upload-quarto.sh
+++ dependencies/tools/upload-quarto.sh
@@ -5,7 +5,7 @@
 # tools (awscli) installed, and configured with a valid AWS account.
 
 # Modify to set the Quarto version to upload
-QUARTO_VERSION=1.0.38
+QUARTO_VERSION=0.9.230
 
 BASEURL="https://github.com/quarto-dev/quarto-cli/releases/download/"
 AWS_BUCKET="s3://rstudio-buildtools"
