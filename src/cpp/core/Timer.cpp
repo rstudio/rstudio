19b4c0325e944e9d35113874a14fa17efd66fa39
kfeinauer <karl@rstudio.com>
2022-09-15 09:41:29 -0500

    Add Timer class and ITimer interface to core library.
    This allows code that needs to test timeout functionality the ability
    to mock the timer to allow you to have control over when the timer
    is actually invoked, making this very useful for unit testing.
    
    This has already been proven out in the Launcher codebase.

---
+++ src/cpp/core/Timer.cpp
@@ -0,0 +1,42 @@
+/*
+ * Timer.cpp
+ *
+ * Copyright (C) 2022 by RStudio, PBC
+ *
+ * Unless you have received this program directly from RStudio pursuant
+ * to the terms of a commercial license agreement with RStudio, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
+#include <core/Timer.hpp>
+
+namespace rstudio {
+namespace core {
+
+Timer::Timer(boost::asio::io_service& ioService) :
+   timer_(ioService)
+{
+}
+
+void Timer::setExpiration(const boost::posix_time::time_duration& timeDuration)
+{
+   timer_.expires_from_now(timeDuration);
+}
+
+void Timer::cancel()
+{
+   timer_.cancel();
+}
+
+void Timer::wait(const std::function<void(const boost::system::error_code& ec)>& callback)
+{
+   timer_.async_wait(callback);
+}
+
+} // namespace core
+} // namespace rstudio
