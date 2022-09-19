d3ca44b84b936d023067e43d3b6b67c1a21b768b
Tim Mok <tim.mok@rstudio.com>
2022-09-19 15:31:31 -0400

    node-gyp cache clear debugging

--- package/win32/clean-build.bat
+++ package/win32/clean-build.bat
@@ -1,5 +1,8 @@
+echo node-gyp cache
 dir C:\Users\ContainerAdministrator\AppData\Local\node-gyp
 rmdir /s /q C:\Users\ContainerAdministrator\AppData\Local\node-gyp\Cache
+dir C:\Users\ContainerAdministrator\AppData\Local\node-gyp
+echo node-gyp cache deleted
 @echo off
 
 if exist build rmdir /s /q build 
