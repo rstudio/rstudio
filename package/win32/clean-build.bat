27942da42f065db96bde35508ddc1d509657c074
Tim Mok <tim.mok@rstudio.com>
2022-09-20 10:28:41 -0400

    npm set msvs_version and ensure node-gyp install

--- package/win32/clean-build.bat
+++ package/win32/clean-build.bat
@@ -1,8 +1,9 @@
-echo node-gyp cache
-dir C:\Users\ContainerAdministrator\AppData\Local\node-gyp
-rmdir /s /q C:\Users\ContainerAdministrator\AppData\Local\node-gyp\Cache
-dir C:\Users\ContainerAdministrator\AppData\Local\node-gyp
-echo node-gyp cache deleted
+echo npm set msvs_version 2019
+npm set msvs_version 2019
+dir C:\Users\ContainerAdministrator
+type C:\Users\ContainerAdministrator\.npmrc
+npx node-gyp install 16.14.0
+dir C:\Users\ContainerAdministrator\AppData\Local\node-gyp\Cache\16.14.0\include\node
 @echo off
 
 if exist build rmdir /s /q build 
