2e032869f27f82761eb540775086dfb8e2e12244
Romain François <romain@rstudio.com>
2022-09-30 16:33:02 +0200

    tweaks to twilight, and monokai themes

--- src/gwt/tools/xrdb/download_theme_xrdb.R
+++ src/gwt/tools/xrdb/download_theme_xrdb.R
@@ -13,7 +13,7 @@ patches <- list(
     dracula = c(Ansi_0_Color = "#4f4f4f"),
     # just here for the record, but material needs manual updating
     material = c(Ansi_0_Color = "#4f4f4f"), 
-    monokai = c(Ansi_0_Color = "#4f4f4f"), 
+    monokai = c(Ansi_0_Color = "#4f4f4f", Ansi_5_Color = "#ff73fd", Ansi_13_Color = "#ff9cfe"), 
     pastel_on_dark = c(Ansi_8_Color = "#7c7c7c"),
     solarized_dark = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c"), 
     solarized_light = c(Ansi_7_Color = "#d3d7cf", Ansi_8_Color = "#acacac", Ansi_15_Color = "#d3d7cf"),
@@ -22,7 +22,7 @@ patches <- list(
     tomorrow_night_blue = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c"), 
     tomorrow_night_bright = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c"),
     tomorrow_night_eighties = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c"),
-    twilight = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c"), 
+    twilight = c(Ansi_0_Color = "#4f4f4f", Ansi_8_Color = "#7c7c7c", Ansi_4_Color = "#0b44a4", Ansi_12_Color = "#0058df", Ansi_5_Color = "#9b309b", Ansi_13_Color = "#d93bd9", Ansi_6_Color = "#219899", Ansi_14_Color = "#19d6d8"), 
     vibrant_ink = c(Ansi_8_Color = "#7c7c7c", Ansi_12_Color = "#5959ff")
 )
 
