9440d48bddecc360538002b5db9127b6c73ba158
JJ Allaire <jj@rstudio.com>
2022-10-03 04:29:31 -0700

    more resilient detection of job still not running

--- src/gwt/src/org/rstudio/studio/client/workbench/views/jobs/events/JobsPresenterEventHandlersImpl.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/jobs/events/JobsPresenterEventHandlersImpl.java
@@ -193,6 +193,8 @@ public class JobsPresenterEventHandlersImpl implements JobsPresenterEventHandler
    private void selectJob(final String id, boolean animate, boolean isLauncherJob)
    {
       boolean bypassLauncherCall = (isLauncherJob && getSessionServer() != null);
+      
+      Job job = pJobManager_.get().getJob(id);
 
       server_.setJobListening(id, true, bypassLauncherCall, new ServerRequestCallback<JsArray<JobOutput>>()
       {
@@ -212,8 +214,7 @@ public class JobsPresenterEventHandlersImpl implements JobsPresenterEventHandler
          {
             // ignore failure to retreive status from quarto as it kills and
             // restarts preview jobs in quick succession
-            Job job = pJobManager_.get().getJob(id);
-            if (job != null && JsArrayUtil.jsArrayStringContains(job.tags,  "quarto"))
+            if (job == null || JsArrayUtil.jsArrayStringContains(job.tags,  "quarto"))
                return;
             
             // CONSIDER: this error is unlikely, but it'd be nicer to show the
