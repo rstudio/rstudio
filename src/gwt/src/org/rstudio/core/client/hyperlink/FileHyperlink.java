72ba9da62a47c97cec8e79e5f43956ab9f1ed118
Romain François <romain@rstudio.com>
2022-10-04 15:19:29 +0200

    `file://` hyperlinks can handle `#123` and `#123:4` fragments

--- src/gwt/src/org/rstudio/core/client/hyperlink/FileHyperlink.java
+++ src/gwt/src/org/rstudio/core/client/hyperlink/FileHyperlink.java
@@ -22,6 +22,8 @@ import com.google.gwt.core.client.Scheduler;
 import org.rstudio.core.client.FilePosition;
 import org.rstudio.core.client.ResultCallback;
 import org.rstudio.core.client.StringUtil;
+import org.rstudio.core.client.regex.Match;
+import org.rstudio.core.client.regex.Pattern;
 import org.rstudio.studio.client.RStudioGinjector;
 import org.rstudio.studio.client.common.SimpleRequestCallback;
 import org.rstudio.studio.client.server.ServerError;
@@ -34,25 +36,34 @@ public class FileHyperlink extends Hyperlink
     public FileHyperlink(String url, Map<String, String> params, String text, String clazz)
     {
         super(url, params, text, clazz);
-
-        if (url.matches("^file:///[a-zA-Z]:"))
-        {
-            filename = url.replaceFirst("^file:///", "");
-        }
-        else
-        {
-            filename = url.replaceFirst("file://", "");
-        }
-        
         line = -1;
         col = -1;
 
-        if (params.containsKey("line"))
-            line = StringUtil.parseInt(params.get("line"), -1);
-        
-        if (params.containsKey("col"))
-            col = StringUtil.parseInt(params.get("col"), -1);    
+        Match match = POSITION_FRAGMENT_PATTERN.match(url, 0);
+        if (match != null)
+        {
+            url = url.replaceFirst("#.*$", "");
+            
+            line = StringUtil.parseInt(match.getGroup(1), -1);
 
+            String second = match.getGroup(2);
+            if (second != null)
+                col = StringUtil.parseInt(match.getGroup(2).substring(1), -1);
+        }
+        else 
+        {
+            if (params.containsKey("line"))
+                line = StringUtil.parseInt(params.get("line"), -1);
+        
+            if (params.containsKey("col"))
+                col = StringUtil.parseInt(params.get("col"), -1);    
+        }
+
+        if (url.matches("^file:///[a-zA-Z]:"))
+            filename = url.replaceFirst("file:///", "");
+        else
+            filename = url.replaceFirst("file://", "");
+        
         server_ = RStudioGinjector.INSTANCE.getServer();
     }
 
@@ -66,24 +77,7 @@ public class FileHyperlink extends Hyperlink
             {
                 if (response.length() > 0)
                 {
-                    final SourceColumnManager columnManager = RStudioGinjector.INSTANCE.getSourceColumnManager(); 
-        
-                    columnManager.editFile(response, new ResultCallback<EditingTarget, ServerError>()
-                    {
-                        @Override
-                        public void onSuccess(final EditingTarget result)
-                        {
-                            if (line != -1) 
-                            {
-                                // give ace time to render before scrolling to position
-                                Scheduler.get().scheduleDeferred(() ->
-                                {
-                                    FilePosition position = FilePosition.create(line, Math.max(col, 1));
-                                    columnManager.scrollToPosition(position, true, () -> {});
-                                });
-                            }
-                        }
-                    });
+                    navigateToAliased(response);
                 }
                 else 
                 {
@@ -91,6 +85,28 @@ public class FileHyperlink extends Hyperlink
                         constants_.noSuchFile(),
                         constants_.doesNotExist(filename)
                         );
+                }
+            }
+        });
+    }
+
+    private void navigateToAliased(String file) 
+    {
+        final SourceColumnManager columnManager = RStudioGinjector.INSTANCE.getSourceColumnManager(); 
+        
+        columnManager.editFile(file, new ResultCallback<EditingTarget, ServerError>()
+        {
+            @Override
+            public void onSuccess(final EditingTarget result)
+            {
+                if (line != -1) 
+                {
+                    // give ace time to render before scrolling to position
+                    Scheduler.get().scheduleDeferred(() ->
+                    {
+                        FilePosition position = FilePosition.create(line, Math.max(col, 1));
+                        columnManager.scrollToPosition(position, true, () -> {});
+                    });
                 }
             }
         });
@@ -104,6 +120,8 @@ public class FileHyperlink extends Hyperlink
     private String filename;
     private int line;
     private int col;
+
+    public static final Pattern POSITION_FRAGMENT_PATTERN = Pattern.create("#([0-9]+)(:[0-9]+)?$");
 
     private SourceServerOperations server_;
     private static final HyperlinkConstants constants_ = GWT.create(HyperlinkConstants.class);
