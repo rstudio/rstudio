60b387dfd21717c0492e820f49eddb3744dab15b
Romain François <romain@rstudio.com>
2022-10-04 11:17:20 +0200

    maximise replace by fit in console_viewer option

--- src/cpp/r/R/Options.R
+++ src/cpp/r/R/Options.R
@@ -46,11 +46,11 @@
    if (!is.character(url) || (length(url) != 1))
       stop("url must be a single element character vector.", call. = FALSE)
    
-   if (identical(height, "maximize"))
+   if (identical(height, "fit"))
       height <- -1
    
    if (!is.null(height) && (!is.numeric(height) || (length(height) != 1)))
-      stop("height must be a single element numeric vector or 'maximize'.", call. = FALSE)
+      stop("height must be a single element numeric vector or 'fit'.", call. = FALSE)
    
    invisible(.Call("rs_consoleViewer", url, height, PACKAGE = "(embedding)"))
 })
