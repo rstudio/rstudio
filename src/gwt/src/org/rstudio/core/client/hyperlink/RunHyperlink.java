b62e13a53d4c2e666d9630bebfb748535ee096e7
Romain François <romain@rstudio.com>
2022-10-04 20:17:03 +0200

    `x-r-help:`, `x-r-run:`, and `x-r-vignette:` schemes (#12096)
    * handle `x-r-help:` in addition to `ide:help` and `rstudio:help`
    
    * `x-r-vignette:`
    
    * x-r-vignette
    
    * x-r-run:
    
    * another isOneOf() use
    
    * nit

--- src/gwt/src/org/rstudio/core/client/hyperlink/RunHyperlink.java
+++ src/gwt/src/org/rstudio/core/client/hyperlink/RunHyperlink.java
@@ -39,7 +39,7 @@ public class RunHyperlink extends Hyperlink
         Match match = HYPERLINK_PATTERN.match(url, 0);
         package_ = match.getGroup(2);
         fun_ = match.getGroup(3);
-        code_ = url.replaceFirst("^(ide|rstudio):run:", "");
+        code_ = url.replaceFirst("^(x-r-|ide:|rstudio:)run:", "");
         
         server_ = RStudioGinjector.INSTANCE.getServer();
     }
@@ -77,13 +77,14 @@ public class RunHyperlink extends Hyperlink
             return false;
 
         String pkg = match.getGroup(2);
-        if (StringUtil.equals(pkg, "base") || StringUtil.equals(pkg, "utils") || StringUtil.equals(pkg, "stats"))
+        if (StringUtil.isOneOf(pkg, "base", "utils", "stats"))
             return false;
         
         return true;
     }
 
-    public void showHelp(){
+    public void showHelp()
+    {
         server_.showHelpTopic(fun_, package_, RCompletionType.FUNCTION);
     }
 
@@ -94,5 +95,5 @@ public class RunHyperlink extends Hyperlink
     private Server server_;
 
     // allow code of the form pkg::fn(<args>) where args does not have ;()
-    private static final Pattern HYPERLINK_PATTERN = Pattern.create("^(rstudio|ide):run:(\\w+)::(\\w+)[(][^();]*[)]$", "");
+    private static final Pattern HYPERLINK_PATTERN = Pattern.create("^(x-r-|rstudio:|ide:)run:(\\w+)::(\\w+)[(][^();]*[)]$", "");
 }
