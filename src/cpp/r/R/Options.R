9a971ad8f7f2eaa122de3ef2dc897e04c1100436
Romain François <romain@rstudio.com>
2022-10-04 11:15:27 +0200

    set the `console_viewer` option, related to https://github.com/rstudio/htmltools/pull/340

--- src/cpp/r/R/Options.R
+++ src/cpp/r/R/Options.R
@@ -40,6 +40,21 @@
    invisible(.Call("rs_viewer", url, height, PACKAGE = "(embedding)"))
 })
 
+# default console_viewer option if not already set
+.rs.setOptionDefault("console_viewer", function(url, height = NULL)
+{
+   if (!is.character(url) || (length(url) != 1))
+      stop("url must be a single element character vector.", call. = FALSE)
+   
+   if (identical(height, "maximize"))
+      height <- -1
+   
+   if (!is.null(height) && (!is.numeric(height) || (length(height) != 1)))
+      stop("height must be a single element numeric vector or 'maximize'.", call. = FALSE)
+   
+   invisible(.Call("rs_consoleViewer", url, height, PACKAGE = "(embedding)"))
+})
+
 # default page_viewer option if not already set
 .rs.setOptionDefault("page_viewer", function(url,
                                              title = "RStudio Viewer",
