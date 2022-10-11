4d4b7eabeac6d059df90f4a5ad3feecf6981dc60
Kevin Ushey <kevin@rstudio.com>
2022-10-11 11:43:00 -0700

    Update src/gwt/src/org/rstudio/studio/client/workbench/model/helper/ClientStateValue.java

--- src/gwt/src/org/rstudio/studio/client/workbench/model/helper/ClientStateValue.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/model/helper/ClientStateValue.java
@@ -84,7 +84,7 @@ public abstract class ClientStateValue<T> implements SaveClientStateEvent.Handle
    protected abstract void onInit(T value);
    protected abstract T getValue();
 
-   public void onSaveClientState(SaveClientStateEvent event)
+   public final void onSaveClientState(SaveClientStateEvent event)
    {
       try
       {
