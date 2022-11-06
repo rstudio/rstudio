f3eb68a11735d56130ea51cc72f96fb6d2bb7852
Jeff Vroom <jeff.vroom@rstudio.com>
2022-11-05 22:02:20 -0700

    update latest daily links for preview builds

--- docker/jenkins/publish-daily-binary.sh
+++ docker/jenkins/publish-daily-binary.sh
@@ -56,11 +56,10 @@ fi
 
 # figure out the "latest" package name by replacing the version number with "latest"; for example
 # for "rstudio-workbench-2021.09.0-daily-123.pro1.deb", we want "rstudio-workbench-latest.deb"
-LATEST=$(echo "$FILENAME" | sed -e 's/[[:digit:]][[:digit:]][[:digit:]][[:digit:]]\.[[:digit:]][[:digit:]]\.[[:digit:]][[:digit:]]*-daily-[[:digit:]][[:digit:]]*\(\.pro[[:digit:]][[:digit:]]*\)*/latest/')
-
-if [[ $LATEST =~ "preview" ]] ; then
-   echo "Not publishing link for preview release: $FILENAME"
-   exit 0
+if [[ $FILENAME =~ "preview" ]] ; then
+   LATEST=$(echo "$FILENAME" | sed -e 's/[[:digit:]][[:digit:]][[:digit:]][[:digit:]]\.[[:digit:]][[:digit:]]\.[[:digit:]][[:digit:]]*-preview-[[:digit:]][[:digit:]]*\(\.pro[[:digit:]][[:digit:]]*\)*/latest/')
+else
+   LATEST=$(echo "$FILENAME" | sed -e 's/[[:digit:]][[:digit:]][[:digit:]][[:digit:]]\.[[:digit:]][[:digit:]]\.[[:digit:]][[:digit:]]*-daily-[[:digit:]][[:digit:]]*\(\.pro[[:digit:]][[:digit:]]*\)*/latest/')
 fi
 
 echo "Publishing $FILENAME as daily $FLAVOR build for $OS ($PLATFORM): $LATEST..."
