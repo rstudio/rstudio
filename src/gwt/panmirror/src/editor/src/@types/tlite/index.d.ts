d1a99c3c6f7c465ec756d3cb6a94849edd167124
J.J. Allaire <jj.allaire@gmail.com>
2022-12-11 19:02:20 -0500

    remove old panmirror src

--- src/gwt/panmirror/src/editor/src/@types/tlite/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'tlite' {
-  export function show(el: Element, opts?: {}, isAuto?: boolean);
-  export function hide(el: Element, isAuto?: boolean);
-}
--- src/gwt/panmirror/src/editor/src/index.ts
+++
@@ -1,6 +0,0 @@
-// apply polyfills
-import polyfill from './polyfill/index';
-polyfill();
-
-// objects instantiated by the IDE
-export { Editor, UITools } from './editor/editor';
