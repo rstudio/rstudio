8557fe04cbb13c26bc44444f8186f5063bb0de69
Zach Hannum <zach.hannum@rstudio.com>
2022-12-14 15:53:29 -0500

    Add Jenkins pipeline for pull requests

---
+++ utils.groovy
@@ -0,0 +1,22 @@
+// Utility methods for Jenkins pipelines
+
+/**
+  * Returns true if branch has changes in the specified path with the target branch.
+  */
+boolean hasChangesIn(String module) {
+  sh "echo 'Comparing changes in ${module} with ${env.CHANGE_TARGET}..${env.BRANCH_NAME}'"
+  return !env.CHANGE_TARGET ||
+  sh(
+    returnStatus: true,
+    script: "git diff --name-only --quiet origin/${env.CHANGE_TARGET}..origin/${env.BRANCH_NAME} -- ${module}") == 1
+}
+
+/**
+  * Adds a remote reference to the specified branch.
+  */
+void addRemoteRef(String branchName) {
+  sh "git config --add remote.origin.fetch +refs/heads/${branchName}:refs/remotes/origin/${branchName}"
+  sh "git fetch --no-tags origin ${branchName}"
+}
+
+return this
