1a73dc71ef61fcaf1fde4c00f6655d48f99f5d0b
J.J. Allaire <jj.allaire@gmail.com>
2022-12-14 20:07:16 -0500

    sync to the latest patch release of quarto v1.2
    this fixes an important issue that prevents tinytex package auto-installation from working on osx.

--- dependencies/windows/install-dependencies.cmd
+++ dependencies/windows/install-dependencies.cmd
@@ -44,7 +44,7 @@ set PANDOC_NAME=pandoc-%PANDOC_VERSION%
 set PANDOC_FILE=%PANDOC_NAME%-windows-x86_64.zip
 
 REM Pin to specific Quarto version for releases
-set QUARTO_VERSION=1.2.269
+set QUARTO_VERSION=1.2.280
 
 REM Get latest Quarto release version
 REM cd install-quarto
