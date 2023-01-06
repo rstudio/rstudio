09e9c2afe3ef0bdb264daced3e4cc83ef1642ae9
Tim Mok <timtmok@users.noreply.github.com>
2023-01-06 16:35:00 -0500

    Fix yarn and panmirror for GWT

--- dependencies/windows/install-panmirror/clone-quarto-repo.cmd
+++ dependencies/windows/install-panmirror/clone-quarto-repo.cmd
@@ -4,7 +4,7 @@ pushd ..\..\..\src\gwt\lib
 
 if not exist quarto (
   echo "Cloning quarto repo"
-  git clone https://github.com/quarto-dev/quarto.git
+  git clone https://github.com/quarto-dev/quarto.git ..\..\..\src\gwt\lib\quarto
 ) else (
   echo "quarto repo already cloned"
 
