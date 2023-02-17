cc7381ff57dd34d396499c94ef6534fc56179bf2
Romain François <romain@posit.co>
2023-02-17 10:42:52 +0100

    handle "context" in .rs.sortCompletions() and .rs.appendCompletions()

--- src/cpp/session/modules/SessionRCompletions.R
+++ src/cpp/session/modules/SessionRCompletions.R
@@ -1291,7 +1291,7 @@ assign(x = ".rs.acCompletionTypes",
 
 .rs.addFunction("subsetCompletions", function(completions, indices)
 {
-   for (name in c("results", "packages", "quote", "type"))
+   for (name in c("results", "packages", "quote", "type", "context"))
       completions[[name]] <- completions[[name]][indices]
    
    if (!is.null(completions[["suggestOnAccept"]]))
@@ -1328,7 +1328,7 @@ assign(x = ".rs.acCompletionTypes",
 {
    old[["suggestOnAccept"]] <- .rs.appendCompletionsOptionalElement(old, new, "suggestOnAccept", FALSE)
    
-   for (name in c("results", "packages", "quote", "type", "meta"))
+   for (name in c("results", "packages", "quote", "type", "meta", "context"))
       old[[name]] <- c(old[[name]], new[[name]])
 
    # resolve duplicates -- a completion is duplicated if its result
@@ -1340,7 +1340,7 @@ assign(x = ".rs.acCompletionTypes",
    
    if (length(drop))
    {
-      for (name in c("results", "packages", "quote", "type", "meta"))
+      for (name in c("results", "packages", "quote", "type", "meta", "context"))
          old[[name]] <- old[[name]][-c(drop)]
 
       if (!is.null(old[["suggestOnAccept"]])) {
