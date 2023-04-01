f067a3bfbeef47fba748c831bf3cbb6880f6f861
Kevin Ushey <kevinushey@gmail.com>
2023-03-31 19:48:29 -0700

    avoid where.exe (closes #12911)

--- src/cpp/session/modules/clang/CodeCompletion.cpp
+++ src/cpp/session/modules/clang/CodeCompletion.cpp
@@ -222,14 +222,15 @@ void discoverSystemIncludePaths(std::vector<std::string>* pIncludePaths)
 
 #ifdef _WIN32
    {
-      core::system::ProcessResult result;
-      Error error = core::system::runCommand("where.exe gcc.exe", processOptions, &result);
+      Error error = r::exec::RFunction("base:::Sys.which")
+            .addParam("gcc.exe")
+            .call(&compilerPath);
+      
       if (error)
+      {
          LOG_ERROR(error);
-      else if (result.exitStatus != EXIT_SUCCESS)
-         LOG_ERROR_MESSAGE("Error querying CXX compiler: " + result.stdOut);
-      else
-         compilerPath = string_utils::trimWhitespace(result.stdOut);
+         return;
+      }
    }
 #else
    {
