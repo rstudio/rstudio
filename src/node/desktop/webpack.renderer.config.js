14ddfe8daed4f6a527cebcda4a80ca17fedb7de9
sharon wang <sharon-wang@users.noreply.github.com>
2023-04-26 13:09:22 -0400

    open source companion to rstudio/rstudio-pro#4570 (#13054)

--- src/node/desktop/webpack.renderer.config.js
+++ src/node/desktop/webpack.renderer.config.js
@@ -1,10 +1,23 @@
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 rules.push({
   test: /\.css$/,
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
+
+const copyWebpackPlugin = new CopyWebpackPlugin({
+  patterns: [
+    {
+      from: path.resolve(__dirname, 'src', 'assets'),
+      to: path.resolve(__dirname, '.webpack/renderer', 'assets'),
+    },
+  ],
+});
+
+plugins.push(copyWebpackPlugin);
 
 module.exports = {
   module: {
