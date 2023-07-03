56510771e8a286ee18ed444a4552339364250ad3
Jeff Vroom <jeff.vroom@rstudio.com>
2023-07-03 16:37:50 -0700

    Speedup and bugfix for rsession metadata
    * Don't create the session directory as part of creating the storage object
    as this implicitly creates a session that's not valid just by looking to see if
    a session exists with a given id.
    
    * Don't use ensureDirectory for the properties directory on each property access.
    This adds overhead and improperly creates a session just by trying to read a
    property from it. Instead, catch the "no such file or directory" error when trying
    to write a property and create the directory at that point.
    
    * Bonus - speedup of test for web-socket upgrade (don't create the regex on every
    request) - this showed up in the flame-graph I did of the rserver under load.

--- src/cpp/core/include/core/r_util/RActiveSessions.hpp
+++ src/cpp/core/include/core/r_util/RActiveSessions.hpp
@@ -554,7 +554,7 @@ public:
       Error storageError = storage_->isValid(&validStorage);
       if (storageError || !validStorage)
       {
-         LOG_DEBUG_MESSAGE("ActiveSession validation failed: properties storage not valid");
+         LOG_DEBUG_MESSAGE("ActiveSession validation failed - no session metadata for: " + id());
          if(storageError)
             LOG_ERROR(storageError);
          return false;
