27ed27918d776372ed2aa3c887d99d1bcc5f1263
sharon wang <sharon-wang@users.noreply.github.com>
2023-07-25 10:10:01 -0400

    handle ENTER keydown on verification URI

--- src/gwt/src/org/rstudio/studio/client/workbench/copilot/ui/CopilotSignInDialog.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/copilot/ui/CopilotSignInDialog.java
@@ -4,14 +4,15 @@ import org.rstudio.core.client.resources.ImageResource2x;
 import org.rstudio.core.client.widget.ElementPanel;
 import org.rstudio.core.client.widget.ModalDialogBase;
 import org.rstudio.core.client.widget.ProgressIndicator;
-import org.rstudio.core.client.widget.ThemedButton;
 import org.rstudio.core.client.widget.images.MessageDialogImages;
 
 import com.google.gwt.aria.client.Roles;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.AnchorElement;
+import com.google.gwt.dom.client.BrowserEvents;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Event;
@@ -34,7 +35,7 @@ public class CopilotSignInDialog extends ModalDialogBase
    {
       super(Roles.getDialogRole());
       setText("GitHub Copilot: Sign in");
-      
+
       ui_ = uiBinder.createAndBindUi(this);
       progress_ = addProgressIndicator();
       verificationUri_.setInnerText(verificationUri);
@@ -42,10 +43,11 @@ public class CopilotSignInDialog extends ModalDialogBase
       verificationCode_.setInnerText(verificationCode);
       verificationCode_.getStyle().setProperty("userSelect", "all");
       
-      Event.sinkEvents(verificationUri_, Event.ONCLICK);
-      Event.sinkEvents(verificationUri_, Event.ONMOUSEUP);
+      Event.sinkEvents(verificationUri_, Event.ONCLICK | Event.ONMOUSEUP | Event.ONKEYDOWN);
       Event.setEventListener(verificationUri_, (event) ->
       {
+         if (BrowserEvents.KEYDOWN.equals(event.getType()) && event.getKeyCode() != KeyCodes.KEY_ENTER)
+            return;
          progress_.onProgress("Authenticating...", () -> progress_.clearProgress());
       });
 
