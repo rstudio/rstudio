1972794d1c260724524dcab9d8d961080ef9e8a8
Kevin Ushey <kevin@posit.co>
2023-08-09 19:42:41 -0700

    improve handling of nested data frames in Data Viewer (#13430)
    * improve flattening of data.frame in View
    
    * update NEWS
    
    * skip if all data is non-rectangular
    
    * handle missing names
    
    * fixup for tests

--- src/cpp/session/modules/SessionCodeTools.R
+++ src/cpp/session/modules/SessionCodeTools.R
@@ -2618,3 +2618,58 @@
    getNamespace(envir)
 
 })
+
+.rs.addFunction("stack", function(mode = "list")
+{
+   .data <- list()
+   storage.mode(.data) <- mode
+   
+   list(
+      
+      push = function(...) {
+         dots <- list(...)
+         for (data in dots) {
+            if (is.null(data))
+               .data[length(.data) + 1] <<- list(NULL)
+            else
+               .data[[length(.data) + 1]] <<- data
+         }
+      },
+      
+      pop = function() {
+         item <- .data[[length(.data)]]
+         length(.data) <<- length(.data) - 1
+         item
+      },
+      
+      peek = function() {
+         .data[[length(.data)]]
+      },
+      
+      contains = function(data) {
+         data %in% .data
+      },
+      
+      empty = function() {
+         length(.data) == 0
+      },
+      
+      get = function(index) {
+         if (index <= length(.data)) .data[[index]]
+      },
+      
+      set = function(index, value) {
+         .data[[index]] <<- value
+      },
+      
+      clear = function() {
+         .data <<- list()
+      },
+      
+      data = function() {
+         .data
+      }
+      
+   )
+   
+})
