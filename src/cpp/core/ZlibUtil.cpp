b746d3297a14900aa557061a2fe4827425d1d54c
Kevin Ushey <kevin@posit.co>
2023-09-27 13:40:24 -0700

    move zlib.cpp to ZlibUtil.cpp; update zlib.h include (#13702)
    * move zlib.cpp to ZlibUtil.cpp; update zlib.h include
    
    * move header file as well
    
    * whitespace
    
    * move test file

--- src/cpp/core/ZlibUtil.cpp
+++ src/cpp/core/ZlibUtil.cpp
@@ -1,25 +1,29 @@
 /*
-* zlib.cpp
-*
-* Copyright (C) 2022 by Posit Software, PBC
-*
-* Unless you have received this program directly from Posit Software pursuant
-* to the terms of a commercial license agreement with Posit Software, then
-* this program is licensed to you under the terms of version 3 of the
-* GNU Affero General Public License. This program is distributed WITHOUT
-* ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
-* MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
-* AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
-*
-*/
+ * ZlibUtil.cpp
+ *
+ * Copyright (C) 2022 by Posit Software, PBC
+ *
+ * Unless you have received this program directly from Posit Software pursuant
+ * to the terms of a commercial license agreement with Posit Software, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
 
-#include <core/zlib/zlib.hpp>
+#include <core/ZlibUtil.hpp>
 
 #include <cstring> // for memcpy
 
 #include <core/Log.hpp>
 
-#include <zlib.h>
+#ifdef _WIN32
+# include "zlib/zlib.h"
+#else
+# include <zlib.h>
+#endif
 
 namespace rstudio {
 namespace core {
