dee73410cf443cc7196ee20a509a08caf7599383
Jackie Gutman <jacqueline.gutman@rstudio.com>
2023-10-04 16:09:02 -0400

    convert dashes to dots in yaml style chunk options

--- src/cpp/session/modules/SessionRmdNotebook.R
+++ src/cpp/session/modules/SessionRmdNotebook.R
@@ -620,7 +620,14 @@ assign(".rs.notebookVersion", envir = .rs.toolsEnv(), "1.0")
 {
    # read as YAML
    opts <- .rs.fromYAML(opt)
-   
+
+   # knitr version 1.44 introduced a function `knitr:::dot_names`
+   # which converts dashes to dots in chunk options
+   if (packageVersion("knitr") >= "1.44") {
+      opts <- knitr:::dot_names(opts)
+   } else {
+      names(opts) <- gsub("-", ".", names(opts))
+   }
    # parse truthy and falsy style yaml options
    opts <- lapply(opts, function(value)
    {
