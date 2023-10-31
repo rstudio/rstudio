9cbfe4b582ce26b76d1044247502c4c1fc680fe0
Kevin Ushey <kevinushey@gmail.com>
2023-10-31 14:26:59 -0700

    allow copy of code from History pane

--- src/gwt/src/org/rstudio/core/client/dom/DomUtils.java
+++ src/gwt/src/org/rstudio/core/client/dom/DomUtils.java
@@ -401,17 +401,14 @@ public class DomUtils
       return impl.selectionExists();
    }
 
-   public static boolean contains(Element container, Node descendant)
-   {
-      while (descendant != null)
-      {
-         if (descendant == container)
-            return true;
-
-         descendant = descendant.getParentNode();
-      }
-      return false;
-   }
+   public static final native boolean contains(Element container, Node descendant) /*-{
+      return container.contains(descendant);
+   }-*/;
+   
+   public static final native boolean containsElement(Element container, Element descendant)
+   /*-{
+      return container.contains(descendant);
+   }-*/;
 
    /**
     * CharacterData.deleteData(node, index, offset)
