68a2b9bc237675e62b190891f1008f7e32361941
Kevin Ushey <kevinushey@gmail.com>
2023-11-13 13:39:17 -0800

    stop using pillar for formatting (#13097)

--- src/cpp/session/modules/SessionHelp.R
+++ src/cpp/session/modules/SessionHelp.R
@@ -485,52 +485,10 @@ options(help_type = "html")
    if (is.null(data))
       return(NULL)
    
-   column <- data[[name]]
-   
-   canUsePillar <- FALSE
-   if ("pillar" %in% loadedNamespaces())
-   {
-      pillar <- asNamespace("pillar")
-      canUsePillar <-
-         is.function(pillar$format_glimpse) &&
-         is.function(pillar$str_trunc) &&
-         is.function(pillar$get_pillar_type)
-   }
-   
-   if (canUsePillar)
-   {
-      formatted <- pillar$format_glimpse(column)
-      
-      bits <- c()
-      nchars <- 55
-      i <- 1
-      while (nchars > 1 && i <= length(column)) {
-         current <- formatted[i]
-         currentNChars <- nchar(current, keepNA = FALSE)
-         if (currentNChars > nchars) {
-            current <- pillar$str_trunc(current, nchars)
-         }
-         bits <- c(bits, current)
-         nchars <- nchars - currentNChars - 2
-         i <- i + 1
-      }
-      
-      if (length(bits) < length(column))
-      {
-         bits <- c(bits, paste0("<i>", pillar:::get_ellipsis(), "</i>"))
-      }
-      
-      description <- paste("<ul>", paste(paste0("<li>", bits, "</li>"), collapse = " "), "</ul>")
-      type <- pillar$get_pillar_type(column)
-      size <- length(formatted)
-   }
-   else 
-   {
-      described <- .rs.describeObject(data, name)
-      description <- described$description
-      type <- described$type
-      size <- described$length
-   }
+   described <- .rs.describeObject(data, name)
+   description <- described$description
+   type <- described$type
+   size <- described$length
    
    list(
       html = paste0("<h2></h2><h3>Description</h3><p>", description, "</p>"),
