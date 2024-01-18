caf4e324c0587a24e55c6795212490f0a0a8662c
Kevin Ushey <kevinushey@gmail.com>
2024-01-18 11:21:13 -0800

    guard pragma usages

--- src/cpp/session/include/session/SessionConsoleProcessSocket.hpp
+++ src/cpp/session/include/session/SessionConsoleProcessSocket.hpp
@@ -29,14 +29,18 @@
 #include <shared_core/Error.hpp>
 #include <core/Thread.hpp>
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#ifdef __clang__
+# pragma clang diagnostic push
+# pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
 
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 #include <websocketpp/frame.hpp>
 
-#pragma clang diagnostic pop
+#ifdef __clang__
+# pragma clang diagnostic pop
+#endif
 
 #include "SessionConsoleProcessConnectionCallbacks.hpp"
 
