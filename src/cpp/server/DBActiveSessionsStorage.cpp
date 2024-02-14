52a9ac78cf61cdbef2d304ed67b69b040328f174
Craig Manning <craig.manning@rstudio.com>
2024-02-14 15:27:17 -0700

    code change 3

--- src/cpp/server/DBActiveSessionsStorage.cpp
+++ src/cpp/server/DBActiveSessionsStorage.cpp
@@ -11,7 +11,7 @@
  * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
  * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
  *
- */   
+ */       
 
 #include <server/DBActiveSessionsStorage.hpp>
 
