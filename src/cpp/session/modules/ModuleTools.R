8141c8354abd80d051ed1a7137e60c251a9b2b50
olivroy <52606734+olivroy@users.noreply.github.com>
2024-02-27 10:14:38 -0500

    Use non-deprecated pandoc options

--- src/cpp/session/modules/ModuleTools.R
+++ src/cpp/session/modules/ModuleTools.R
@@ -410,7 +410,13 @@
    args <- c(c("+RTS", paste0("-K", stack_size), "-RTS"), args)
    
    # additional options
-   args <- c(args, "--self-contained")
+   if (requireNamespace("rmarkdown", quietly = TRUE) && rmarkdown::pandoc_available("2.19")) {
+     args <- c(args, "--embed-resources", "--standalone")
+   } else {
+     # --self-contained deprecated in pandoc 2.19
+     # https://github.com/rstudio/rstudio/issues/14332
+     args <- c(args, "--self-contained")
+   }
    args <- c(args, "--template", template)
    
    # build the conversion command
