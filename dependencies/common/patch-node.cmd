1cbf04d85ad33270d93081c7b7e6f9ac2bdf80c5
Gary Ritchie <gary@posit.co>
2024-03-14 11:13:50 -0600

    Windows implementation

---
+++ dependencies/common/patch-node.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+setlocal
+
+call ..\tools\rstudio-tools.cmd
+
+if "%NODE_VERSION%" == "18.19.1" (
+	echo Applying patches to node %NODE_VERSION%
+
+	:: remove npm from the node installation
+	rd /s /q "%NODE_SUBDIR%\node_modules\npm"
+	del "%NODE_SUBDIR%\npm"
+	del "%NODE_SUBDIR%\npm.cmd"
+)
+
+exit /b 0
