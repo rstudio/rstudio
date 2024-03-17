e0e7d4fdfa5613c79d24666d7a159ac706eeef8b
Kevin Ushey <kevin@posit.co>
2024-03-17 16:50:01 -0700

    stop offline thread when suspending sessions (#14434)

--- src/cpp/session/include/session/SessionConstants.hpp
+++ src/cpp/session/include/session/SessionConstants.hpp
@@ -175,6 +175,7 @@ const char * const kQuitSession = "quit_session";
 const char * const kSuspendSession = "suspend_session";
 const char * const kInterrupt = "interrupt";
 const char * const kConsoleInput = "console_input";
+const char * const kSuspendForRestart = "suspend_for_restart";
 const char * const kRStudioAPIShowDialogMethod = "rstudio_api_show_dialog";
 const char * const kPing = "ping";
 
