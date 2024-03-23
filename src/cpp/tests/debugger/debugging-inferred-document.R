0b8d67fe35fcc72bc2646a01c2222d6e81927e25
Kevin Ushey <kevinushey@gmail.com>
2024-03-22 17:30:18 -0700

    update

--- src/cpp/tests/debugger/debugging-inferred-document.R
+++ src/cpp/tests/debugger/debugging-inferred-document.R
@@ -3,16 +3,14 @@
 # Execute the function definition below with Cmd + Enter.
 # Then, run foo() to run the code.
 # The debugger should open into a view of the document.
+# Try editing the function definition while stepping through.
+# The debug highlight should be removed, and a warning should be displayed.
 #
 
 foo <- function() {
    1 + 1
    browser()
    2 + 2
-   6 + 6
-   10000
-   10000
-   7 + 7
-   8 + 8
-   80000
+   3 + 3
+   4 + 4
 }
