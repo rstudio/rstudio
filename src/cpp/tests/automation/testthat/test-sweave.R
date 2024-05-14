0dae14ec51c9c1834b4d37f02e611d60f153085c
Kevin Ushey <kevinushey@gmail.com>
2024-05-13 17:01:14 -0700

    finish up automation test

--- src/cpp/tests/automation/testthat/test-sweave.R
+++ src/cpp/tests/automation/testthat/test-sweave.R
@@ -7,20 +7,21 @@ client <- remote$client
 test_that("Braces are inserted and highlighted correctly in Sweave documents", {
    
    documentContents <- .rs.heredoc('
-      
       This is a Sweave document.
       
       <<>>=
       
       @
-      
    ')
       
    remote$documentExecute(".Rnw", documentContents, {
+      Sys.sleep(0.1)
       remote$aceSetCursorPosition(3, 0)
       client$Input.insertText(text = "{ 1 + 1 }")
       tokens <- remote$aceLineTokens(row = 3L)
-      expect_length(tokens, 5L)
+      values <- vapply(tokens, `[[`, "value", FUN.VALUE = character(1))
+      expected <- c("{", " ", "1", " ", "+", " ", "1", " ", "}")
+      expect_equal(values, expected)
    })
    
 })
