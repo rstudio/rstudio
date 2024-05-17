398170a04711fe29e2aa3862b281292bb6454783
Kevin Ushey <kevinushey@gmail.com>
2024-05-16 18:17:20 -0700

    fixups

--- src/cpp/tests/automation/testthat/test-automation-sweave.R
+++ src/cpp/tests/automation/testthat/test-automation-sweave.R
@@ -20,7 +20,7 @@ test_that("Braces are inserted and highlighted correctly in Sweave documents", {
       remote$editorSetCursorPosition(3, 0)
       Sys.sleep(0.1)
       client$Input.insertText(text = "{ 1 + 1 }")
-      tokens <- remote$editorLineTokens(row = 3L)
+      tokens <- remote$editorGetTokens(row = 3L)
       values <- vapply(tokens, `[[`, "value", FUN.VALUE = character(1))
       expected <- c("{", " ", "1", " ", "+", " ", "1", " ", "}")
       expect_equal(values, expected)
--- src/cpp/tests/automation/testthat/test-automation-syntax-highlighting.R
+++ src/cpp/tests/automation/testthat/test-automation-syntax-highlighting.R
@@ -21,7 +21,7 @@ test_that("Quarto Documents are highlighted as expected", {
    remote$documentExecute(".Rmd", documentContents, {
       # TODO: Better wait for document ready when checking Ace tokens.
       Sys.sleep(0.1)
-      tokens <- remote$editorLineTokens(8L)
+      tokens <- remote$editorGetTokens(8L)
       expect_length(tokens, 1L)
       expect_equal(tokens[[1]]$type,  "text")
       expect_equal(tokens[[1]]$value, "1 + 1")
