d61625d9ec64e283b4cd8cec6a5036fed3bb1ac9
Kevin Ushey <kevinushey@gmail.com>
2024-06-08 16:16:15 -0700

    fixup for viewer automation test

--- src/cpp/tests/automation/testthat/test-automation-data-viewer.R
+++ src/cpp/tests/automation/testthat/test-automation-data-viewer.R
@@ -7,8 +7,7 @@ on.exit(.rs.automation.deleteRemote(), add = TRUE)
 # https://github.com/rstudio/rstudio/pull/14657
 test_that("we can use the data viewer with temporary R expressions", {
    remote$consoleExecute("View(subset(mtcars, mpg >= 30))")
-   objectId <- remote$domGetObjectId("iframe[class=\"gwt-Frame\"]")
-   viewerSrc <- remote$jsCall("function() { return this.src; }", objectId = objectId)
-   expect_true(grepl("gridviewer.html", viewerSrc$result$value))
+   viewerFrame <- remote$jsObjectViaSelector("iframe[class=\"gwt-Frame\"]")
+   expect_true(grepl("gridviewer.html", viewerFrame$src))
    remote$commandExecute("closeSourceDoc")
 })
