cc08cee71e1156b0d4d87c469e03a283aff002f9
Gary Ritchie <gary@rstudio.com>
2024-06-08 11:25:34 -0600

    install usethis for automation

--- src/cpp/session/modules/automation/SessionAutomation.R
+++ src/cpp/session/modules/automation/SessionAutomation.R
@@ -29,7 +29,7 @@
 
 .rs.addFunction("automation.installRequiredPackages", function()
 {
-   packages <- c("here", "httr", "later", "processx", "ps", "websocket", "withr", "xml2")
+   packages <- c("here", "httr", "later", "processx", "ps", "usethis", "websocket", "withr", "xml2")
    pkgLocs <- find.package(packages, quiet = TRUE)
    if (length(packages) == length(pkgLocs))
       return()
