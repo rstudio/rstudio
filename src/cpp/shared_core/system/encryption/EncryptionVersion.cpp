5e23876990bdf0ce72d02d0ce4e6789a92edcbba
Craig <56326543+cm421@users.noreply.github.com>
2024-06-18 12:49:35 -0600

    Feature/decryption versioning (#14838)
    Add version handling to Crypto.cpp

--- src/cpp/shared_core/system/encryption/EncryptionVersion.cpp
+++ src/cpp/shared_core/system/encryption/EncryptionVersion.cpp
@@ -70,18 +70,44 @@ Error getLastCryptoError(const ErrorLocation& in_location)
        in_location);
 }
 
+// Forward declare v1 low-level functions for use by v0
+namespace v1 {
+Error aesDecrypt(
+    const unsigned char* const in_data, const int data_len,
+    const std::vector<unsigned char>& in_key,
+    const std::vector<unsigned char>& in_iv,
+    std::vector<unsigned char>& out_decrypted);
+
+Error aesEncrypt(
+    const unsigned char* const data, const int data_length,
+    const std::vector<unsigned char>& key,
+    const std::vector<unsigned char>& iv,
+    int& out_bytes_encrypted,
+    unsigned char* out_encrypted);
+}
+
 namespace v0 {
 
 /**
  * v0 decryption is the same as v1. Enforce compatability by calling v1 decryption
  */
 Error aesDecrypt(
-    const std::vector<unsigned char>& in_data,
+    const std::vector<unsigned char>& in_v0_data,
     const std::vector<unsigned char>& in_key,
     const std::vector<unsigned char>& in_iv,
     std::vector<unsigned char>& out_decrypted)
 {
-   return v1::aesDecrypt(in_data, in_key, in_iv, out_decrypted);
+   // Key size mismatches can silently read more data than intended. Prevent that upfront.
+   if (in_key.size() < KEY_LENGTH_BYTES )
+      throw EncryptionVersionMismatchException();
+
+   out_decrypted.resize(gsl::narrow_cast<int>(in_v0_data.size()));
+
+   return v1::aesDecrypt(in_v0_data.data(),
+                         gsl::narrow_cast<int>(in_v0_data.size()),
+                         in_key,
+                         in_iv,
+                         out_decrypted);
 }
 
 /**
@@ -91,31 +117,45 @@ Error aesEncrypt(
     const std::vector<unsigned char>& data,
     const std::vector<unsigned char>& key,
     const std::vector<unsigned char>& iv,
-    std::vector<unsigned char>& out_encrypted)
+    std::vector<unsigned char>& out_v0_encrypted)
 {
-   return v1::aesEncrypt(data, key, iv, out_encrypted);
+   // Key size mismatches can silently read more data than intended. Prevent that upfront.
+   if (key.size() < KEY_LENGTH_BYTES )
+      throw EncryptionVersionMismatchException();
+
+   int encryptedBytes = 0;
+   // allow enough space in output buffer for version byte, data, and potential additional block
+   out_v0_encrypted.resize(data.size() + EVP_MAX_BLOCK_LENGTH);
+
+   Error result = v1::aesEncrypt(data.data(),
+                                 gsl::narrow_cast<int>(data.size()),
+                                 key,
+                                 iv,
+                                 encryptedBytes,
+                                 out_v0_encrypted.data());
+
+   out_v0_encrypted.resize(encryptedBytes);
+   return result;
 }
 
 } // namespace v0
 
 namespace v1 {
-
 Error aesDecrypt(
-   const std::vector<unsigned char>& in_data,
+   const unsigned char* const in_data, const int data_len,
    const std::vector<unsigned char>& in_key,
    const std::vector<unsigned char>& in_iv,
    std::vector<unsigned char>& out_decrypted)
 {
-   out_decrypted.resize(in_data.size());
    int outLen = 0;
    int bytesDecrypted = 0;
 
    EVP_CIPHER_CTX *ctx;
    ctx = EVP_CIPHER_CTX_new();
-   EVP_CipherInit_ex(ctx, EVP_aes_128_cbc(), nullptr, &in_key[0], &in_iv[0], s_decrypt);
+   EVP_CipherInit_ex(ctx, EVP_aes_128_cbc(), nullptr, in_key.data(), in_iv.data(), s_decrypt);
 
    // perform the decryption
-   if(!EVP_CipherUpdate(ctx, &out_decrypted[0], &outLen, &in_data[0], gsl::narrow_cast<int>(in_data.size())))
+   if(!EVP_CipherUpdate(ctx, out_decrypted.data(), &outLen, in_data, data_len))
    {
       EVP_CIPHER_CTX_free(ctx);
       return getLastCryptoError(ERROR_LOCATION);
@@ -139,42 +179,91 @@ Error aesDecrypt(
 }
 
 Error aesEncrypt(
-   const std::vector<unsigned char>& data,
-   const std::vector<unsigned char>& key,
-   const std::vector<unsigned char>& iv,
-   std::vector<unsigned char>& out_encrypted)
+    const unsigned char* const data, const int data_length,
+    const std::vector<unsigned char>& key,
+    const std::vector<unsigned char>& iv,
+    int& out_bytes_encrypted,
+    unsigned char* out_encrypted)
 {
-   // allow enough space in output buffer for additional block
-   out_encrypted.resize(data.size() + EVP_MAX_BLOCK_LENGTH);
    int outlen = 0;
-   int bytesEncrypted = 0;
 
    EVP_CIPHER_CTX *ctx;
    ctx = EVP_CIPHER_CTX_new();
-   EVP_CipherInit_ex(ctx, EVP_aes_128_cbc(), nullptr, &key[0], iv.empty() ? nullptr : &iv[0], s_encrypt);
+   EVP_CipherInit_ex(ctx, EVP_aes_128_cbc(), nullptr, key.data(), iv.empty() ? nullptr : iv.data(), s_encrypt);
 
    // perform the encryption
-   if(!EVP_CipherUpdate(ctx, &out_encrypted[0], &outlen, &data[0], gsl::narrow_cast<int>(data.size())))
+   if(!EVP_CipherUpdate(ctx, out_encrypted, &outlen, data, data_length))
    {
       EVP_CIPHER_CTX_free(ctx);
+      out_bytes_encrypted = 0;
       return getLastCryptoError(ERROR_LOCATION);
    }
-   bytesEncrypted += outlen;
+   out_bytes_encrypted += outlen;
 
    // perform final flush including left-over padding
    if(!EVP_CipherFinal_ex(ctx, &out_encrypted[outlen], &outlen))
    {
       EVP_CIPHER_CTX_free(ctx);
+      out_bytes_encrypted = 0;
       return getLastCryptoError(ERROR_LOCATION);
    }
-   bytesEncrypted += outlen;
+   out_bytes_encrypted += outlen;
 
    EVP_CIPHER_CTX_free(ctx);
 
-   // resize the container to the amount of actual bytes encrypted (including padding)
-   out_encrypted.resize(bytesEncrypted);
-
    return Success();
+}
+
+Error aesDecrypt(
+   const std::vector<unsigned char>& in_v1_data,
+   const std::vector<unsigned char>& in_key,
+   const std::vector<unsigned char>& in_iv,
+   std::vector<unsigned char>& out_decrypted)
+{
+   // Key size mismatches can silently read more data than intended. Prevent that upfront.
+   if (in_key.size() < KEY_LENGTH_BYTES )
+      throw EncryptionVersionMismatchException();
+
+   // Extract v1 byte info and only decrypt actual encrypted data:
+   // [ version byte ][ v1 encrypted data ]
+   // Use std::max to prevent index math going negative.
+   out_decrypted.resize(std::max(in_v1_data.size() - ENCRYPTION_VERSION_SIZE_BYTES, (size_t)0));
+
+   return aesDecrypt(&in_v1_data[ENCRYPTION_VERSION_SIZE_BYTES],
+                     gsl::narrow_cast<int>(std::max(in_v1_data.size() - ENCRYPTION_VERSION_SIZE_BYTES, (size_t)0)),
+                     in_key,
+                     in_iv,
+                     out_decrypted);
+}
+
+Error aesEncrypt(
+    const std::vector<unsigned char>& data,
+    const std::vector<unsigned char>& key,
+    const std::vector<unsigned char>& iv,
+    std::vector<unsigned char>& out_v1_encrypted)
+{
+   // Key size mismatches can silently read more data than intended. Prevent that upfront.
+   if (key.size() < KEY_LENGTH_BYTES )
+      throw EncryptionVersionMismatchException();
+
+   int bytesEncrypted = 0;
+
+   // allow enough space in output buffer for version byte, data, and potential additional block
+   out_v1_encrypted.resize(data.size() + EVP_MAX_BLOCK_LENGTH + ENCRYPTION_VERSION_SIZE_BYTES);
+
+   Error result = aesEncrypt(
+       data.data(),
+       gsl::narrow_cast<int>(data.size()),
+       key,
+       iv,
+       bytesEncrypted,
+       &out_v1_encrypted[ENCRYPTION_VERSION_SIZE_BYTES]);
+
+   // resize the container to the version byte and amount of actual bytes encrypted (including padding)
+   out_v1_encrypted.resize(bytesEncrypted + ENCRYPTION_VERSION_SIZE_BYTES);
+   out_v1_encrypted[VERSION_BYTE_INDEX] = VERSION_BYTE;
+
+   return result;
 }
 
 } // namespace v1
@@ -182,30 +271,24 @@ Error aesEncrypt(
 namespace v2 {
 
 Error aesDecrypt(
-    const std::vector<unsigned char>& in_data,
+    const unsigned char* const in_data, const int data_length,
     const std::vector<unsigned char>& in_key,
     const std::vector<unsigned char>& in_iv,
-    const std::vector<unsigned char>& in_aad,
-    std::vector<unsigned char>& in_mac,
+    const unsigned char* const in_aad, const int aad_length,
+    unsigned char* const in_mac, const int mac_length,
     std::vector<unsigned char>& out_decrypted)
 {
-   out_decrypted.resize(in_data.size());
-
-   // if in_mac is wrong size, we'll fail below. Resize here to prevent other problems
-   if (in_mac.size() != 16)
-      in_mac.resize(16);
-
    int outLen = 0;
    int bytesDecrypted = 0;
 
    EVP_CIPHER_CTX *ctx;
    ctx = EVP_CIPHER_CTX_new();
-   EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, &in_key[0], &in_iv[0]);
+   EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, in_key.data(), in_iv.data());
 
    // provide any AAD data
-   if(!in_aad.empty())
+   if(aad_length > 0)
    {
-      if(!EVP_DecryptUpdate(ctx, nullptr, &outLen, &in_aad[0], gsl::narrow_cast<int>(in_aad.size())))
+      if(!EVP_DecryptUpdate(ctx, nullptr, &outLen, in_aad, aad_length))
       {
          EVP_CIPHER_CTX_free(ctx);
          return getLastCryptoError(ERROR_LOCATION);
@@ -213,7 +296,7 @@ Error aesDecrypt(
    }
 
    // perform the decryption
-   if(!EVP_DecryptUpdate(ctx, &out_decrypted[0], &outLen, &in_data[0], gsl::narrow_cast<int>(in_data.size())))
+   if(!EVP_DecryptUpdate(ctx, out_decrypted.data(), &outLen, in_data, data_length))
    {
       EVP_CIPHER_CTX_free(ctx);
       return getLastCryptoError(ERROR_LOCATION);
@@ -221,17 +304,16 @@ Error aesDecrypt(
    bytesDecrypted += outLen;
 
    // Set expected tag value
-   if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, &in_mac[0]))
+   if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, mac_length, in_mac))
    {
       EVP_CIPHER_CTX_free(ctx);
       return getLastCryptoError(ERROR_LOCATION);
    }
 
-   // Finalize encryption
+   // Finalize encryption. Additional encryption data is not written here for GCM
    // A positive return value indicates success,
    // anything else is a failure - the plaintext is not trustworthy.
-   int finalLen = gsl::narrow_cast<int>(out_decrypted.size()) == outLen ? outLen - 1 : outLen;
-   if(EVP_DecryptFinal_ex(ctx, &out_decrypted[finalLen], &outLen) <= 0)
+   if(EVP_DecryptFinal_ex(ctx, &out_decrypted[outLen - 1], &outLen) <= 0)
    {
       EVP_CIPHER_CTX_free(ctx);
       out_decrypted.resize(0);
@@ -248,62 +330,143 @@ Error aesDecrypt(
 }
 
 Error aesEncrypt(
-    const std::vector<unsigned char>& data,
+    const unsigned char* const data, const int data_length,
     const std::vector<unsigned char>& key,
     const std::vector<unsigned char>& iv,
-    const std::vector<unsigned char>& aad,
+    const unsigned char* const aad, const int aad_length,
     std::vector<unsigned char>& out_mac,
-    std::vector<unsigned char>& out_encrypted)
+    int& out_bytesEncrypted,
+    unsigned char* out_encrypted)
 {
-   // allow enough space in output buffer for additional block
-   out_encrypted.resize(data.size() + EVP_MAX_BLOCK_LENGTH);
-
-   // ensure enough room in mac buffer
-   if (out_mac.size() != 16)
-      out_mac.resize(16);
-
    int outlen = 0;
-   int bytesEncrypted = 0;
 
    EVP_CIPHER_CTX *ctx;
    ctx = EVP_CIPHER_CTX_new();
-   EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, &key[0], iv.empty() ? nullptr : &iv[0]);
+   EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, key.data(), iv.empty() ? nullptr : iv.data());
 
    // provide any AAD data
-   if (!aad.empty())
+   if (aad_length > 0)
    {
-      if(!EVP_EncryptUpdate(ctx, NULL, &outlen, &aad[0], gsl::narrow_cast<int>(aad.size())))
+      if(!EVP_EncryptUpdate(ctx, NULL, &outlen, aad, gsl::narrow_cast<int>(aad_length)))
       {
          EVP_CIPHER_CTX_free(ctx);
+         out_bytesEncrypted = 0;
          return getLastCryptoError(ERROR_LOCATION);
       }
    }
 
    // perform the encryption
-   if(!EVP_EncryptUpdate(ctx, &out_encrypted[0], &outlen, &data[0], gsl::narrow_cast<int>(data.size())))
+   if(!EVP_EncryptUpdate(ctx, out_encrypted, &outlen, data, data_length))
    {
       EVP_CIPHER_CTX_free(ctx);
+      out_bytesEncrypted = 0;
       return getLastCryptoError(ERROR_LOCATION);
    }
-   bytesEncrypted += outlen;
+   out_bytesEncrypted += outlen;
 
    // perform final flush including left-over padding
    if(!EVP_EncryptFinal_ex(ctx, &out_encrypted[outlen], &outlen))
    {
       EVP_CIPHER_CTX_free(ctx);
+      out_bytesEncrypted = 0;
       return getLastCryptoError(ERROR_LOCATION);
    }
-   bytesEncrypted += outlen;
+   out_bytesEncrypted += outlen;
 
    // get the tag
-   if (EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, &out_mac[0]))
+   if (1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, MAC_SIZE_BYTES, out_mac.data()))
+   {
+      EVP_CIPHER_CTX_free(ctx);
+      out_bytesEncrypted = 0;
+      return getLastCryptoError(ERROR_LOCATION);
+   }
 
    EVP_CIPHER_CTX_free(ctx);
 
-   // resize the container to the amount of actual bytes encrypted (including padding)
-   out_encrypted.resize(bytesEncrypted);
-
    return Success();
+}
+
+Error aesDecrypt(
+    const std::vector<unsigned char>& in_v2_data,
+    const std::vector<unsigned char>& in_key,
+    const std::vector<unsigned char>& in_iv,
+    std::vector<unsigned char>& out_decrypted)
+{
+   // Key size mismatches can silently read more data than intended. Prevent that upfront.
+   if (in_key.size() < KEY_LENGTH_BYTES )
+      throw EncryptionVersionMismatchException();
+
+   // Index maths. v2 buffer structure is: [ version byte ][ v2 encrypted data ][ mac ]
+   // Use std::max to prevent index math going negative.
+   int dataLength = std::max(in_v2_data.size() - ENCRYPTION_VERSION_SIZE_BYTES - MAC_SIZE_BYTES, (size_t)0);
+   int macIndex = ENCRYPTION_VERSION_SIZE_BYTES + dataLength;
+
+   out_decrypted.resize(dataLength);
+
+   // For some reason the decryption doesn't like const data for the mac
+   // Copy it out of the original v2 buffer to remove the const
+   std::vector<unsigned char> mac(in_v2_data.begin() + macIndex, in_v2_data.end());
+
+   // Decrypt just the data
+   return aesDecrypt(&in_v2_data[ENCRYPTION_VERSION_SIZE_BYTES],
+                     gsl::narrow_cast<int>(dataLength),
+                     in_key,
+                     in_iv,
+                     &in_v2_data[VERSION_BYTE_INDEX],
+                     ENCRYPTION_VERSION_SIZE_BYTES,
+                     mac.data(),
+                     MAC_SIZE_BYTES,
+                     out_decrypted);
+}
+
+Error aesEncrypt(
+    const std::vector<unsigned char>& data,
+    const std::vector<unsigned char>& key,
+    const std::vector<unsigned char>& iv,
+    std::vector<unsigned char>& out_v2_encrypted)
+{
+   // Key size mismatches can silently read more data than intended. Prevent that upfront.
+   if (key.size() < KEY_LENGTH_BYTES )
+      throw EncryptionVersionMismatchException();
+
+   int bytesEncrypted = 0;
+
+   // allow enough space in output buffer for data and potential additional block
+   out_v2_encrypted.resize(data.size() + EVP_MAX_BLOCK_LENGTH + ENCRYPTION_VERSION_SIZE_BYTES + MAC_SIZE_BYTES);
+   std::vector <unsigned char> mac(MAC_SIZE_BYTES);
+
+   // Encrypt the data
+   Error result = aesEncrypt(
+       data.data(),
+       gsl::narrow_cast<int>(data.size()),
+       key,
+       iv,
+       &VERSION_BYTE,
+       ENCRYPTION_VERSION_SIZE_BYTES,
+       mac,
+       bytesEncrypted,
+       &out_v2_encrypted[ENCRYPTION_VERSION_SIZE_BYTES]);
+
+   // Add v2 metadata
+   if (result == Success())
+   {
+      // resize the container to the amount of actual bytes encrypted (including padding)
+      // plus any
+      out_v2_encrypted.resize(ENCRYPTION_VERSION_SIZE_BYTES + bytesEncrypted + MAC_SIZE_BYTES);
+
+      // Add the AAD
+      out_v2_encrypted[VERSION_BYTE_INDEX] = VERSION_BYTE;
+
+      // Add the MAC
+      std::copy(mac.begin(), mac.end(), out_v2_encrypted.begin() + ENCRYPTION_VERSION_SIZE_BYTES + bytesEncrypted);
+   }
+   else
+   {
+      out_v2_encrypted.resize(0);
+   }
+
+   return result;
+
 }
 
 } // namespace v2
--- src/cpp/shared_core/system/encryption/EncryptionVersionTests.cpp
+++ src/cpp/shared_core/system/encryption/EncryptionVersionTests.cpp
@@ -17,6 +17,7 @@
 
 #include <gsl/gsl>
 
+#include <shared_core/system/Crypto.hpp>
 #include <shared_core/system/encryption/EncryptionVersion.hpp>
 
 #include <tests/TestThat.hpp>
@@ -52,14 +53,20 @@ bool random(uint32_t in_length, std::vector<unsigned char>& out_randomData)
    return true;
 }
 
-bool generateKeys()
+bool generateKeys(unsigned char version)
 {
    // construct the data to encrypt in tests
    if (g_data.empty())
       std::copy(g_payload.begin(), g_payload.end(), std::back_inserter(g_data));
 
-   // generate a random 128-bit key and IV
-   return random(16, g_key) && random(16, g_iv);
+   if (version == crypto::v1::VERSION_BYTE || version == 0)
+      // generate a random 128-bit key and IV
+      return random(16, g_key) && random(16, g_iv);
+   else if (version == crypto::v2::VERSION_BYTE)
+      // generate a random 256-bit key and 128-bit IV
+      return random(32, g_key) && random(16, g_iv);
+
+   return false;
 }
 
 bool decryptedPayloadMatches(std::vector<unsigned char>& decryptedData)
@@ -74,7 +81,7 @@ test_context("EncryptionVersionTests")
    test_that("v0: Can AES encrypt/decrypt")
    {
       // setup
-      REQUIRE(generateKeys());
+      REQUIRE(generateKeys(crypto::v0::VERSION_BYTE));
 
       // encrypt the data
       std::vector<unsigned char> encryptedData;
@@ -93,7 +100,7 @@ test_context("EncryptionVersionTests")
    test_that("v1: Can AES encrypt/decrypt")
    {
       // setup
-      REQUIRE(generateKeys());
+      REQUIRE(generateKeys(crypto::v1::VERSION_BYTE));
 
       // encrypt the data
       std::vector<unsigned char> encryptedData;
@@ -109,48 +116,19 @@ test_context("EncryptionVersionTests")
       REQUIRE(decryptedPayloadMatches(decryptedData));
    }
 
-   test_that("Are v0/v1 AES encrypt/decrypt enterchangable")
-   {
-      // setup
-      REQUIRE(generateKeys());
-
-      // encrypt with v0, decrypt with v1
-      std::vector<unsigned char> encryptedData;
-      Error error = core::system::crypto::v0::aesEncrypt(g_data, g_key, g_iv, encryptedData);
-      REQUIRE_FALSE(error);
-      std::vector<unsigned char> decryptedData;
-      error = core::system::crypto::v1::aesDecrypt(encryptedData, g_key, g_iv, decryptedData);
-      REQUIRE_FALSE(error);
-      REQUIRE(decryptedPayloadMatches(decryptedData));
-
-      // re-setup
-      REQUIRE(generateKeys());
-      encryptedData = {};
-      decryptedData = {};
-
-      // encrypt with v1, decrypt with v0
-      error = core::system::crypto::v1::aesEncrypt(g_data, g_key, g_iv, encryptedData);
-      REQUIRE_FALSE(error);
-      error = core::system::crypto::v0::aesDecrypt(encryptedData, g_key, g_iv, decryptedData);
-      REQUIRE_FALSE(error);
-      REQUIRE(decryptedPayloadMatches(decryptedData));
-   }
-
    test_that("v2: Can AES encrypt/decrypt")
    {
       // setup
-      REQUIRE(generateKeys());
+      REQUIRE(generateKeys(crypto::v2::VERSION_BYTE));
 
       // encrypt the data
       std::vector<unsigned char> encryptedData;
-      std::vector<unsigned char> aad = {2};
-      std::vector<unsigned char> mac;
-      Error error = core::system::crypto::v2::aesEncrypt(g_data, g_key, g_iv, aad, mac, encryptedData);
+      Error error = core::system::crypto::v2::aesEncrypt(g_data, g_key, g_iv, encryptedData);
       REQUIRE_FALSE(error);
 
       // decrypt the encrypted data
       std::vector<unsigned char> decryptedData;
-      error = core::system::crypto::v2::aesDecrypt(encryptedData, g_key, g_iv, aad, mac, decryptedData);
+      error = core::system::crypto::v2::aesDecrypt(encryptedData, g_key, g_iv, decryptedData);
       REQUIRE_FALSE(error);
 
       // verify that the decryption gives us back the original data
@@ -160,39 +138,159 @@ test_context("EncryptionVersionTests")
    test_that("v2: Can AES encrypt/decrypt detect tampering/corruption")
    {
       // setup
-      REQUIRE(generateKeys());
+      REQUIRE(generateKeys(crypto::v2::VERSION_BYTE));
 
       // encrypt the data
       std::vector<unsigned char> encryptedData;
-      std::vector<unsigned char> aad = {2};
-      std::vector<unsigned char> mac;
-      Error error = core::system::crypto::v2::aesEncrypt(g_data, g_key, g_iv, aad, mac, encryptedData);
+      Error error = core::system::crypto::v2::aesEncrypt(g_data, g_key, g_iv, encryptedData);
       REQUIRE_FALSE(error);
 
       // decrypt the encrypted data with wrong AAD
       std::vector<unsigned char> decryptedData;
-      std::vector<unsigned char> bad_aad = {0};
-      error = core::system::crypto::v2::aesDecrypt(encryptedData, g_key, g_iv, bad_aad, mac, decryptedData);
+      std::vector<unsigned char> bad_aad = encryptedData;
+      bad_aad[crypto::VERSION_BYTE_INDEX] += 1;
+      error = core::system::crypto::v2::aesDecrypt(bad_aad, g_key, g_iv, decryptedData);
       REQUIRE(decryptedData.size() == 0);
       REQUIRE(error != Success());
 
       // decrypt the encrypted data with changed data
-
       std::vector<unsigned char> bad_encryptedData = encryptedData;
-      bad_encryptedData[0] += 1;
-      error = core::system::crypto::v2::aesDecrypt(bad_encryptedData, g_key, g_iv, aad, mac, decryptedData);
+      bad_encryptedData[crypto::ENCRYPTION_VERSION_SIZE_BYTES] += 1;
+      error = core::system::crypto::v2::aesDecrypt(bad_encryptedData, g_key, g_iv, decryptedData);
       REQUIRE(decryptedData.size() == 0);
       REQUIRE(error != Success());
 
       // decrypt the encrypted data with wrong MAC
-      std::vector<unsigned char> bad_mac(16);
-      error = core::system::crypto::v2::aesDecrypt(encryptedData, g_key, g_iv, aad, bad_mac, decryptedData);
+      std::vector<unsigned char> bad_mac = encryptedData;
+      bad_mac.back() += 1;
+      error = core::system::crypto::v2::aesDecrypt(bad_mac, g_key, g_iv, decryptedData);
       REQUIRE(decryptedData.size() == 0);
       REQUIRE(error != Success());
 
       // Finally, decrypt the encrypted data correctly
-      error = core::system::crypto::v2::aesDecrypt(encryptedData, g_key, g_iv, aad, mac, decryptedData);
+      error = core::system::crypto::v2::aesDecrypt(encryptedData, g_key, g_iv, decryptedData);
       REQUIRE(decryptedData.size() > 0);
+      REQUIRE_FALSE(error);
+
+      // verify that the decryption gives us back the original data
+      REQUIRE(decryptedPayloadMatches(decryptedData));
+   }
+
+   test_that("Key size mismatches aren't allowed")
+   {
+      std::vector<unsigned char> iv(16);
+      std::vector<unsigned char> encryptedData;
+
+      // Encrypting v0/v1 with too small key size throws
+      std::vector<unsigned char> key = {};
+      REQUIRE_THROWS(core::system::crypto::v0::aesEncrypt(g_data, key, iv, encryptedData));
+      REQUIRE_THROWS(core::system::crypto::v1::aesEncrypt(g_data, key, iv, encryptedData));
+
+      // Encrypting v2 with v1 key size throws
+      key.resize(crypto::v1::KEY_LENGTH_BYTES);
+      REQUIRE_THROWS(core::system::crypto::v2::aesEncrypt(g_data, key, iv, encryptedData));
+   }
+}
+
+test_context("Versioned Crypto Calls")
+{
+   test_that("Crypto can decrypt v0 data")
+   {
+      // setup
+      REQUIRE(generateKeys(crypto::v0::VERSION_BYTE));
+
+      // encrypt the data
+      std::vector<unsigned char> encryptedData;
+      Error error = core::system::crypto::v0::aesEncrypt(g_data, g_key, g_iv, encryptedData);
+      REQUIRE_FALSE(error);
+
+      // decrypt the encrypted data
+      std::vector<unsigned char> decryptedData;
+      error = core::system::crypto::aesDecrypt(encryptedData, g_key, g_iv, decryptedData);
+      REQUIRE_FALSE(error);
+
+      // verify that the decryption gives us back the original data
+      REQUIRE(decryptedPayloadMatches(decryptedData));
+   }
+
+   test_that("Crypto can decrypt v1 data")
+   {
+      // setup
+      REQUIRE(generateKeys(crypto::v1::VERSION_BYTE));
+
+      // encrypt the data
+      std::vector<unsigned char> encryptedData;
+      Error error = core::system::crypto::v1::aesEncrypt(g_data, g_key, g_iv, encryptedData);
+      REQUIRE_FALSE(error);
+
+      // decrypt the encrypted data
+      std::vector<unsigned char> decryptedData;
+      error = core::system::crypto::aesDecrypt(encryptedData, g_key, g_iv, decryptedData);
+      REQUIRE_FALSE(error);
+
+      // verify that the decryption gives us back the original data
+      REQUIRE(decryptedPayloadMatches(decryptedData));
+   }
+
+   test_that("Crypto can decrypt v2 data")
+   {
+      // setup
+      REQUIRE(generateKeys(crypto::v2::VERSION_BYTE));
+
+      // encrypt the data
+      std::vector<unsigned char> encryptedData;
+      Error error = core::system::crypto::v2::aesEncrypt(g_data, g_key, g_iv, encryptedData);
+      REQUIRE_FALSE(error);
+
+      // decrypt the encrypted data
+      std::vector<unsigned char> decryptedData;
+      error = core::system::crypto::aesDecrypt(encryptedData, g_key, g_iv, decryptedData);
+      REQUIRE_FALSE(error);
+
+      // verify that the decryption gives us back the original data
+      REQUIRE(decryptedPayloadMatches(decryptedData));
+   }
+
+   test_that("Crypto can decrypt v0 data with v1 version byte")
+   {
+      // setup
+      REQUIRE(generateKeys(crypto::v0::VERSION_BYTE));
+
+      // Use a key/iv combo that will generate a v0 encrypted buffer that starts with a v1 version byte
+      std::vector<unsigned char> encryptedData;
+      std::vector<unsigned char> key = {0x95, 0x34, 0xef, 0xab, 0x94, 0xbb, 0xb2, 0xf2, 0x49, 0x3a, 0xbc, 0xe3, 0x69, 0x71, 0x59, 0x06};
+      std::vector<unsigned char> iv = {0xbc, 0x8f, 0x17, 0x56, 0x50, 0xca, 0xeb, 0x4a, 0xcb, 0xcb, 0x63, 0x53, 0xff, 0xba, 0x58, 0x51};
+      Error error = core::system::crypto::v0::aesEncrypt(g_data, key, iv, encryptedData);
+      REQUIRE_FALSE(error);
+
+      REQUIRE(encryptedData[0] == crypto::v1::VERSION_BYTE);
+
+      // decrypt the encrypted data
+      std::vector<unsigned char> decryptedData;
+      error = core::system::crypto::aesDecrypt(encryptedData, key, iv, decryptedData);
+      REQUIRE_FALSE(error);
+
+      // verify that the decryption gives us back the original data
+      REQUIRE(decryptedPayloadMatches(decryptedData));
+   }
+
+   test_that("Crypto can decrypt v0 data with v2 version byte")
+   {
+      // setup
+      REQUIRE(generateKeys(crypto::v0::VERSION_BYTE));
+
+      // Use a key/iv combo that will generate a v0 encrypted buffer that starts with a v2 version byte
+      std::vector<unsigned char> encryptedData;
+      std::vector<unsigned char> key = {0x78, 0x84, 0x9b, 0x4c, 0x27, 0x6a, 0x07, 0x17, 0xb1, 0xbb, 0x1d, 0xd0, 0x9e, 0xc5, 0x39, 0x55};
+      std::vector<unsigned char> iv = {0x22, 0xaf, 0xca, 0x38, 0x2d, 0xeb, 0xf8, 0xaa, 0xa0, 0xfb, 0x97, 0x40, 0xaa, 0xbb, 0x97, 0x13};
+      Error error = core::system::crypto::v0::aesEncrypt(g_data, key, iv, encryptedData);
+      REQUIRE_FALSE(error);
+
+      REQUIRE(encryptedData[0] == crypto::v2::VERSION_BYTE);
+
+      // decrypt the encrypted data
+      std::vector<unsigned char> decryptedData;
+      error = core::system::crypto::aesDecrypt(encryptedData, key, iv, decryptedData);
       REQUIRE_FALSE(error);
 
       // verify that the decryption gives us back the original data
