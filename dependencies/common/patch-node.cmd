bf315f4ec2bd8f5d6de4729ecf460181ab2f0f98
Gary Ritchie <gary@rstudio.com>
2024-06-25 11:36:06 -0600

    more node.js cleanup

--- dependencies/common/patch-node.cmd
+++ dependencies/common/patch-node.cmd
@@ -4,27 +4,18 @@ setlocal
 
 call ..\tools\rstudio-tools.cmd
 
-if "%NODE_VERSION%" == "%RSTUDIO_INSTALLED_NODE_VERSION%" (
-	echo Applying patches to node %NODE_VERSION%
+echo Applying patches to node %NODE_VERSION%
 
-	:: remove npm from the node installation
-	rd /s /q "%NODE_SUBDIR%\node_modules\npm"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\nodewin\npm"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\nodewin\npm.cmd"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\nodewin\npm.ps1"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\nodewin\npx"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\nodewin\npx.cmd"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\nodewin\npx.ps1"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\npm"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\npm.cmd"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\npm.ps1"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\npx"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\npx.cmd"
-	del "%NODE_SUBDIR%\node_modules\corepack\shims\npx.ps1"
-	del "%NODE_SUBDIR%\npm"
-	del "%NODE_SUBDIR%\npm.cmd"
-	del "%NODE_SUBDIR%\npx"
-	del "%NODE_SUBDIR%\npx.cmd"
-)
+rd /s /q "%NODE_SUBDIR%\node_modules"
+del "%NODE_SUBDIR%\corepack"
+del "%NODE_SUBDIR%\corepack.cmd"
+del "%NODE_SUBDIR%\install_tools.bat"
+del "%NODE_SUBDIR%\npm"
+del "%NODE_SUBDIR%\npm.cmd"
+del "%NODE_SUBDIR%\npx"
+del "%NODE_SUBDIR%\npx.cmd"
+del "%NODE_SUBDIR%\CHANGELOG.md"
+del "%NODE_SUBDIR%\README.md"
+del "%NODE_SUBDIR%\nodevars.bat"
 
 exit /b 0
