66b049a4c5a46e3390bcdbd217391272230e7225
Kevin Ushey <kevinushey@gmail.com>
2024-09-18 16:37:16 -0700

    add automation

---
+++ src/cpp/tests/automation/testthat/test-automation-python.R
@@ -0,0 +1,16 @@
+
+library(testthat)
+
+self <- remote <- .rs.automation.newRemote()
+withr::defer(.rs.automation.deleteRemote())
+
+# https://github.com/rstudio/rstudio/issues/14560
+test_that("attributes like __foo__ are not quoted when inserted via completions", {
+   remote$consoleExecute("reticulate::repl_python()")
+   remote$consoleExecute("import sys")
+   remote$keyboardExecute("sys.__name", "<Tab>", "<Enter>")
+   output <- remote$consoleOutput()
+   expect_equal(tail(output, n = 2L), c(">>> sys.__name__", "'sys'"))
+   remote$consoleExecute("exit")
+   remote$consoleClear()
+})
