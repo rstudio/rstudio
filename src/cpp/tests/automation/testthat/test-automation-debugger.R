6546608f46c9894b8d5d5954f5ca9fb8ff2b5ae5
Kevin Ushey <kevinushey@gmail.com>
2024-09-26 11:06:51 -0700

    add automated test

--- src/cpp/tests/automation/testthat/test-automation-debugger.R
+++ src/cpp/tests/automation/testthat/test-automation-debugger.R
@@ -57,3 +57,109 @@ test_that("the debug position is correct in braced expressions", {
    remote$keyboardExecute("<Ctrl + L>")
    
 })
+
+# https://github.com/rstudio/rstudio/issues/15201
+test_that("package functions can be debugged after build and reload", {
+   
+   # Create an R package project.
+   projectPath <- tempfile("rstudio.automation.", tmpdir = dirname(tempdir()))
+   
+   remote$consoleExecuteExpr({
+      .rs.rpc.package_skeleton(
+         packageName = "rstudio.automation",
+         packageDirectory = !!projectPath,
+         sourceFiles = character(),
+         usingRcpp = FALSE
+      )
+   })
+   
+   # Open that project.
+   remote$consoleExecuteExpr(
+      .rs.api.openProject(!!projectPath)
+   )
+   
+   .rs.waitUntil("the new project is opened", function()
+   {
+      el <- remote$jsObjectViaSelector("#rstudio_project_menubutton_toolbar")
+      grepl("rstudio.automation", el$innerText, fixed = TRUE)
+   })
+   
+   # Add a source document.
+   remote$consoleExecuteExpr(
+      file.edit("R/example.R")
+   )
+   
+   code <- .rs.heredoc('
+      example <- function() {
+         1 + 1
+         2 + 2
+         3 + 3
+         4 + 4
+         5 + 5
+      }
+   ')
+   
+   editor <- remote$editorGetInstance()
+   editor$insert(code)
+   
+   # Save it, and build the package.
+   remote$commandExecute("saveSourceDoc")
+   remote$commandExecute("buildAll")
+   
+   .rs.waitUntil("build has completed", function()
+   {
+      output <- remote$consoleOutput()
+      any(output == "> library(rstudio.automation)")
+   })
+   
+   remote$consoleClear()
+   
+   # Try adding some breakpoints.
+   gutterEls <- remote$jsObjectsViaSelector(".ace_gutter-cell")
+   remote$domClickElement(objectId = gutterEls[[3]], horizontalOffset = -4L)
+   breakpointEls <- remote$jsObjectsViaSelector(".ace_breakpoint")
+   expect_equal(length(breakpointEls), 1L)
+   expect_equal(breakpointEls[[1]]$innerText, "3")
+   
+   remote$domClickElement(objectId = gutterEls[[4]], horizontalOffset = -4L)
+   breakpointEls <- remote$jsObjectsViaSelector(".ace_breakpoint")
+   expect_equal(length(breakpointEls), 2L)
+   expect_equal(breakpointEls[[2]]$innerText, "4")
+   
+   # Confirm that the object definition is in sync.
+   remote$consoleExecuteExpr({
+      .rs.isFunctionInSync("example", "R/example.R", "rstudio.automation")
+   })
+   
+   output <- remote$consoleOutput()
+   expect_contains(output, "[1] TRUE")
+   remote$consoleClear()
+   
+   # Try running the function, and checking the view.
+   remote$consoleExecuteExpr(example())
+   
+   activeLineEl <- remote$jsObjectViaSelector(".ace_executing-line")
+   expect_equal(activeLineEl$innerText, "3")
+   remote$commandExecute("activateConsole")
+   remote$keyboardExecute("c", "<Enter>")
+   
+   activeLineEl <- remote$jsObjectViaSelector(".ace_executing-line")
+   expect_equal(activeLineEl$innerText, "4")
+   remote$keyboardExecute("c", "<Enter>")
+   
+   # All done testing; close the project.
+   remote$commandExecute("closeProject")
+   
+   # If we received a modal, click "Don't Save".
+   hasDialog <- tryCatch(
+      { remote$domGetNodeId("#rstudio_dlg_no"); TRUE },
+      error = function(cnd) FALSE
+   )
+   
+   if (hasDialog)
+   {
+      noEl <- remote$jsObjectViaSelector("#rstudio_dlg_no")
+      remote$domClickElement(objectId = noEl)
+   }
+   
+})
