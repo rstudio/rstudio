1670f732afd5f9988eb9a4ffa444609acf1c94ad
Kevin Ushey <kevin@posit.co>
2024-09-27 12:09:47 -0700

    allow rename in scope to work across chunks (#15233)
    * allow rename in scope to work across chunks
    
    * add test

---
+++ src/cpp/tests/automation/testthat/test-automation-refactoring.R
@@ -0,0 +1,75 @@
+
+library(testthat)
+
+self <- remote <- .rs.automation.newRemote()
+withr::defer(.rs.automation.deleteRemote())
+
+# https://github.com/rstudio/rstudio/issues/4961
+test_that("rename in scope operates across chunks", {
+   
+   contents <- .rs.heredoc('
+      ---
+      title: Refactoring
+      ---
+
+      ```{r}
+      # These should all get selected.
+      variable <- 42
+      print(variable + variable)
+      ```
+
+      ```{r}
+      # These should also get selected.
+      print(variable + variable)
+      ```
+      
+      ```{r}
+      # These should be ignored.
+      example <- function(variable) {
+         variable
+      }
+      ```
+   ')
+   
+   remote$documentExecute(".Rmd", contents, function(editor) {
+      
+      editor$gotoLine(7, 0)
+      remote$commandExecute("renameInScope")
+      
+      allRanges <- as.vector(editor$selection$rangeList$ranges)
+      expect_length(allRanges, 5L)
+      
+      expectedRanges <- list(
+         list(
+            start = list(row = 6L, column = 0L),
+            end = list(row = 6L, column = 8L)
+         ),
+         list(
+            start = list(row = 7L, column = 6L),
+            end = list(row = 7L, column = 14L)
+         ),
+         list(
+            start = list(row = 7L, column = 17L),
+            end = list(row = 7L, column = 25L)
+         ),
+         list(
+            start = list(row = 12L, column = 6L),
+            end = list(row = 12L, column = 14L)
+         ),
+         list(
+            start = list(row = 12L, column = 17L),
+            end = list(row = 12L, column = 25L)
+         )
+      )
+      
+      for (i in seq_along(allRanges))
+      {
+         actual <- allRanges[[i]][c("start", "end")]
+         expected <- expectedRanges[[i]]
+         expect_equal(!!actual, !!expected)
+      }
+      
+      remote$keyboardExecute("<Enter>")
+      
+   })
+})
