95378e9fe9002ba3403f391560f8719e453f581f
Gary Ritchie <gary@posit.co>
2024-10-03 11:12:49 -0600

    add test for visual editor round trip of raw latex blocks

--- src/cpp/tests/automation/testthat/test-automation-quarto.R
+++ src/cpp/tests/automation/testthat/test-automation-quarto.R
@@ -392,4 +392,54 @@ test_that("raw html blocks are preserved by visual editor", {
       expect_equal(editor$session$getLine(4), "```{=html}")
    })
 })
+
+# https://github.com/rstudio/rstudio/issues/15253
+test_that("raw latex blocks are preserved by visual editor", {
    
+   contents <- .rs.heredoc('
+      ---
+      title: Raw LaTeX blocks
+      ---
+
+      ```{=latex}
+      \\LaTeX
+      ``` 
+   ')
+
+   remote$consoleExecute(".rs.writeUserState(\"visual_mode_confirmed\", TRUE)")
+   remote$consoleExecute(".rs.writeUserPref(\"visual_markdown_editing_is_default\", FALSE)")
+
+   remote$documentExecute(".qmd", contents, function(editor) {
+
+      sourceModeToggle <- remote$jsObjectsViaSelector(".rstudio_visual_md_off")[[1]]
+      visualModeToggle <- remote$jsObjectsViaSelector(".rstudio_visual_md_on")[[1]]
+      expect_equal(sourceModeToggle$ariaPressed, "true")
+
+      # verify starting state
+      editor <- remote$editorGetInstance()
+      expect_equal(editor$session$getLine(4), "```{=latex}")
+      
+      # switch to visual mode
+      remote$domClickElement(".rstudio_visual_md_on")
+      .rs.waitUntil("Visual Editor appears", function() {
+         tryCatch({
+            visualEditor <- remote$jsObjectViaSelector(".ProseMirror")
+            visualEditor$contentEditable
+         }, error = function(e) FALSE)
+      })
+
+      # back to source mode
+      remote$domClickElement(".rstudio_visual_md_off")
+      .rs.waitUntil("Source editor appears", function() {
+         tryCatch({
+            sourceEditor <- remote$jsObjectViaSelector("#rstudio_source_text_editor")
+            sourceEditor$checkVisibility()
+         }, error = function(e) FALSE)
+      })
+      
+      # verify ending state
+      editor <- remote$editorGetInstance()
+      expect_equal(editor$session$getLine(4), "```{=latex}")
+   })
+})
+ 
