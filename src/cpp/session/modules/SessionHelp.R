fbefc6eb91b8c2dfb39ff4b4f54166c3f3369789
Kevin Ushey <kevinushey@gmail.com>
2024-12-06 15:10:10 -0800

    move impl function into own piece

--- src/cpp/session/modules/SessionHelp.R
+++ src/cpp/session/modules/SessionHelp.R
@@ -278,6 +278,64 @@ options(help_type = "html")
    NULL
 })
 
+.rs.addFunction("getHelpRpcImpl", function(what, from, type, envir)
+{
+   # If we've encoded the package and function in 'what', pull it out
+   if (grepl("::.", what))
+   {
+      splat <- strsplit(what, "::", fixed = TRUE)[[1]]
+      from <- splat[[1]]
+      what <- splat[[2]]
+   }
+   
+   # Avoid install.packages hook
+   if (what == "install.packages" &&
+       type == .rs.acCompletionTypes$ARGUMENT &&
+       is.null(from))
+   {
+      return(.rs.getHelp("install.packages", "utils"))
+   }
+   
+   # Help for options
+   if (type == .rs.acCompletionTypes$OPTION)
+      return(.rs.getHelp("options", "base", subset = FALSE))
+   
+   if (type %in% c(.rs.acCompletionTypes$S4_GENERIC,
+                   .rs.acCompletionTypes$S4_METHOD))
+   {
+      # Try getting methods for the method from the associated package
+      if (!is.null(help <- .rs.getHelp(paste(what, "methods", sep = "-"), from)))
+         return(help)
+      
+      # Try getting help from anywhere
+      if (!is.null(help <- .rs.getHelp(what, from)))
+         return(help)
+      
+      # Give up
+      return()
+   }
+   
+   if (type %in% c(.rs.acCompletionTypes$FUNCTION,
+                   .rs.acCompletionTypes$S4_GENERIC,
+                   .rs.acCompletionTypes$S4_METHOD,
+                   .rs.acCompletionTypes$R5_METHOD))
+      return(.rs.getHelpFunction(what, from))
+   else if (type %in% c(.rs.acCompletionTypes$ARGUMENT, .rs.acCompletionTypes$SECUNDARY_ARGUMENT))
+      return(.rs.getHelpArgument(what, from, parent.frame()))
+   else if (type == .rs.acCompletionTypes$PACKAGE)
+      return(.rs.getHelpPackage(what))
+   else if (type == .rs.acCompletionTypes$DATATABLE_SPECIAL_SYMBOL)
+      return(.rs.getHelpDataTableSpecialSymbol(what))
+   else if (type == .rs.acCompletionTypes$COLUMN)
+      return(.rs.getHelpColumn(what, from, envir))
+   else if (type == .rs.acCompletionTypes$DATAFRAME)
+      return(.rs.getHelpDataFrame(what, from, envir))
+   else if (length(from) && length(what))
+      return(.rs.getHelp(what, from))
+   else
+      return()
+})
+
 .rs.addJsonRpcHandler("get_help", function(what, from, type)
 {
    # Protect against missing type
@@ -285,64 +343,7 @@ options(help_type = "html")
       return()
    
    envir <- .rs.getActiveFrame()
-
-   impl <- function()
-   {
-      # If we've encoded the package and function in 'what', pull it out
-      if (grepl("::.", what))
-      {
-         splat <- strsplit(what, "::", fixed = TRUE)[[1]]
-         from <- splat[[1]]
-         what <- splat[[2]]
-      }
-      
-      # Avoid install.packages hook
-      if (what == "install.packages" &&
-         type == .rs.acCompletionTypes$ARGUMENT &&
-         is.null(from))
-         return(.rs.getHelp("install.packages", "utils"))
-      
-      # Help for options
-      if (type == .rs.acCompletionTypes$OPTION)
-         return(.rs.getHelp("options", "base", subset = FALSE))
-      
-      if (type %in% c(.rs.acCompletionTypes$S4_GENERIC,
-                     .rs.acCompletionTypes$S4_METHOD))
-      {
-         # Try getting methods for the method from the associated package
-         if (!is.null(help <- .rs.getHelp(paste(what, "methods", sep = "-"), from)))
-            return(help)
-         
-         # Try getting help from anywhere
-         if (!is.null(help <- .rs.getHelp(what, from)))
-            return(help)
-         
-         # Give up
-         return()
-      }
-      
-      if (type %in% c(.rs.acCompletionTypes$FUNCTION,
-                     .rs.acCompletionTypes$S4_GENERIC,
-                     .rs.acCompletionTypes$S4_METHOD,
-                     .rs.acCompletionTypes$R5_METHOD))
-         return(.rs.getHelpFunction(what, from))
-      else if (type %in% c(.rs.acCompletionTypes$ARGUMENT, .rs.acCompletionTypes$SECUNDARY_ARGUMENT))
-         return(.rs.getHelpArgument(what, from, parent.frame()))
-      else if (type == .rs.acCompletionTypes$PACKAGE)
-         return(.rs.getHelpPackage(what))
-      else if (type == .rs.acCompletionTypes$DATATABLE_SPECIAL_SYMBOL)
-         return(.rs.getHelpDataTableSpecialSymbol(what))
-      else if (type == .rs.acCompletionTypes$COLUMN)
-         return(.rs.getHelpColumn(what, from, envir))
-      else if (type == .rs.acCompletionTypes$DATAFRAME)
-         return(.rs.getHelpDataFrame(what, from, envir))
-      else if (length(from) && length(what))
-         return(.rs.getHelp(what, from))
-      else
-         return()
-   }
-   
-   out <- impl()
+   out <- .rs.getHelpRpcImpl(what, from, type, envir)
    if (is.null(out))
       return()
    
