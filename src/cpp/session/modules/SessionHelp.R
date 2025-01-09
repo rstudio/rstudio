fc5f2c84583475ff65250c2c5a8aec0f62687d78
olivroy <52606734+olivroy@users.noreply.github.com>
2025-01-09 15:48:11 -0500

    Address comment

--- src/cpp/session/modules/SessionHelp.R
+++ src/cpp/session/modules/SessionHelp.R
@@ -886,28 +886,35 @@ options(help_type = "html")
 
 .rs.addJsonRpcHandler("show_help_topic", function(what, from, type)
 {
-   # strip off a 'package:' prefix if necessary
-   if (is.character(from) && nzchar(from))
-      from <- sub("^package:", "", from)
+  # strip off a 'package:' prefix if necessary
+  if (is.character(from) && nzchar(from))
+    from <- sub("^package:", "", from)
 
-   # handle dev topics and objects imported in NAMESPACE if F1 is pressed
-  if ("devtools_shims" %in% search() && requireNamespace("pkgload", quiety = TRUE)) {
-    dev_package <- tryCatch(pkgload:::dev_packages(), error = function(e) NULL)
+  # handle dev topics and objects imported in NAMESPACE if F1 is pressed
+  if ("devtools_shims" %in% search() && requireNamespace("pkgload", quietly = TRUE))
+  {
+    # Find packages loaded via load_all()
+    dev_packages <- .rs.tryCatch(pkgload:::dev_packages())
 
-    # Enter this loop if in devtools
-    for (i in seq_along(dev_package)) {
-      if (from == dev_package[i]) {
-        # Strip dev package name and search everywhere
-        # Will enable searching in all packages if topic is not found. or if imported
-        from <- NULL
-        # If topic in package, attempt to find dev help
-        dev_topics <- tryCatch(names(pkgload::dev_topic_index()), error = function(e) NULL)
-        if (what %in% dev_topics) {
-          return(print(pkgload::dev_help(what)))
-        }
-      }
+    packages <- .rs.tryCatch(pkgload:::dev_packages())
+    if (is.character(packages) && from %in% packages)
+    {
+      from <- NULL
+      dev_help <- tryCatch(pkgload::dev_help(what), error = function(e) NULL)
+      if (length(dev_help) > 0)
+        return(print(dev_help))
     }
   }
+
+  if (type == .rs.acCompletionTypes$FUNCTION)
+    .rs.showHelpTopicFunction(what, from)
+  else if (type == .rs.acCompletionTypes$ARGUMENT)
+    .rs.showHelpTopicArgument(from)
+  else if (type == .rs.acCompletionTypes$PACKAGE)
+    .rs.showHelpTopicPackage(what)
+  else
+    .rs.showHelpTopic(what, from)
+})
 
    if (type == .rs.acCompletionTypes$FUNCTION)
       .rs.showHelpTopicFunction(what, from)
