fdc8d31cb91453174045a9d2a7637fd20c1852e4
Zach Hannum <zach.hannum@posit.co>
2025-03-11 10:13:32 -0400

    Allow for more permissive no_proxy domain rules (#15756)
    * Pull in changes from rstudio/rstudio-pro#7265
    
    Changes were made on the pro side that didn't get pulled in
    to OS.
    
    * Add tld and subdomain support to no_proxy domain rule
    
    Uses a more permissive regex for the domain filter to support
    expressions like `.local` and `.sub.domain.local`
    
    * Fix missing NEWS entry for rstudio/rstudio-pro#7265
    
    * Add NEWS entry for rstudio/rstudio#15607
    
    * Fix formatting
    
    * Use simpler regex for no proxy subdomain rule
    
    * Simplify matching logic for submdomain no proxy rule
    
    * Update version/news/NEWS-2024.12.1-kousa-dogwood.md
    
    Co-authored-by: Skye Turriff <turriff.skye@gmail.com>
    
    ---------
    
    Co-authored-by: Skye Turriff <turriff.skye@gmail.com>

--- src/cpp/core/http/NoProxyRulesTests.cpp
+++ src/cpp/core/http/NoProxyRulesTests.cpp
@@ -35,7 +35,7 @@ test_context("NoProxyRulesTests")
 
    test_that("NoProxyRuleDomain matches domains and subdomains")
    {
-      NoProxyRuleDomain rule("example.com");
+      NoProxyRuleDomain rule(".example.com");
       REQUIRE(rule.match("example.com", "443"));
       REQUIRE(rule.match("sub.example.com", "443"));
       REQUIRE(rule.match("example.com", "80"));
@@ -43,6 +43,34 @@ test_context("NoProxyRulesTests")
       REQUIRE(rule.match("sub.sub.example.com", "443"));
       REQUIRE_FALSE(rule.match("example.org", "443"));
       REQUIRE_FALSE(rule.match("subexample.com", "443"));
+   }
+
+   test_that("NoProxyRuleDomain supports subdomains")
+   {
+      auto rule = createNoProxyRule(".sub.example.com");
+      REQUIRE(dynamic_cast<NoProxyRuleDomain*>(rule.get()) != nullptr);
+      REQUIRE(rule->match("sub.example.com", "443"));
+      REQUIRE(rule->match("sub.sub.example.com", "443"));
+      REQUIRE_FALSE(rule->match("example.com", "443"));
+      REQUIRE_FALSE(rule->match("subexample.com", "443"));
+
+      rule = createNoProxyRule(".sub.sub.example.com");
+      REQUIRE(dynamic_cast<NoProxyRuleDomain*>(rule.get()) != nullptr);
+      REQUIRE(rule->match("sub.sub.example.com", "443"));
+      REQUIRE(rule->match("sub.sub.sub.example.com", "443"));
+      REQUIRE_FALSE(rule->match("sub.example.com", "443"));
+      REQUIRE_FALSE(rule->match("example.com", "443"));
+      REQUIRE_FALSE(rule->match("subexample.com", "443"));
+   }
+
+   test_that("NoProxyRuleDomain supports top-level domains")
+   {
+      auto rule = createNoProxyRule(".com");
+      REQUIRE(dynamic_cast<NoProxyRuleDomain*>(rule.get()) != nullptr);
+      REQUIRE(rule->match("example.com", "443"));
+      REQUIRE(rule->match("sub.example.com", "443"));
+      REQUIRE(rule->match("subexample.com", "443"));
+      REQUIRE_FALSE(rule->match("example.org", "443"));
    }
 
    test_that("NoProxyRuleAddress matches addresses and ports")
@@ -70,10 +98,11 @@ test_context("NoProxyRulesTests")
    test_that("NoProxyRuleCidrBlock matches CIDR blocks")
    {
       const auto cidrBlock = "192.0.2.0/24";
-      const auto cidrBlockResult = NoProxyRuleCidrBlock::parseCidrBlock(cidrBlock);
+      const auto cidrBlockResult =
+          NoProxyRuleCidrBlock::parseCidrBlock(cidrBlock);
       REQUIRE(cidrBlockResult);
       NoProxyRuleCidrBlock rule(cidrBlockResult.value());
-      for(uint32_t i = 0; i < 256; ++i)
+      for (uint32_t i = 0; i < 256; ++i)
       {
          const auto address = "192.0.2." + std::to_string(i);
          REQUIRE(rule.match(address, "443"));
@@ -82,56 +111,66 @@ test_context("NoProxyRulesTests")
       REQUIRE_FALSE(rule.match("10.3.4.1", "443"));
 
       const auto cidrBlock2 = "172.0.0.0/8";
-      const auto cidrBlockResult2 = NoProxyRuleCidrBlock::parseCidrBlock(cidrBlock2);
+      const auto cidrBlockResult2 =
+          NoProxyRuleCidrBlock::parseCidrBlock(cidrBlock2);
       REQUIRE(cidrBlockResult2);
       NoProxyRuleCidrBlock rule2(cidrBlockResult2.value());
       REQUIRE(rule2.match("172.4.75.2", "443"));
       REQUIRE_FALSE(rule2.match("171.4.75.2", "443"));
 
       const auto invalidOctetsBlock = "1.1.1/24";
-      const auto invalidOctetsBlockResult = NoProxyRuleCidrBlock::parseCidrBlock(invalidOctetsBlock);
+      const auto invalidOctetsBlockResult =
+          NoProxyRuleCidrBlock::parseCidrBlock(invalidOctetsBlock);
       REQUIRE_FALSE(invalidOctetsBlockResult);
-      REQUIRE(invalidOctetsBlockResult.error() == systemError(boost::system::errc::invalid_argument,
-                                                             ERROR_LOCATION));
+      REQUIRE(
+          invalidOctetsBlockResult.error() ==
+          systemError(boost::system::errc::invalid_argument, ERROR_LOCATION));
 
       const auto invalidMaskBlock = "172.0.0.0/gh";
-      const auto invalidMaskBlockResult = NoProxyRuleCidrBlock::parseCidrBlock(invalidMaskBlock);
+      const auto invalidMaskBlockResult =
+          NoProxyRuleCidrBlock::parseCidrBlock(invalidMaskBlock);
       REQUIRE_FALSE(invalidMaskBlockResult);
-      REQUIRE(invalidMaskBlockResult.error() == systemError(boost::system::errc::invalid_argument,
-                                                             ERROR_LOCATION));
+      REQUIRE(
+          invalidMaskBlockResult.error() ==
+          systemError(boost::system::errc::invalid_argument, ERROR_LOCATION));
 
       const auto invalidMaskBlockAbove32 = "127.0.0.0/400";
-      const auto invalidMaskBlockAbove32Result = NoProxyRuleCidrBlock::parseCidrBlock(invalidMaskBlockAbove32);
+      const auto invalidMaskBlockAbove32Result =
+          NoProxyRuleCidrBlock::parseCidrBlock(invalidMaskBlockAbove32);
       REQUIRE_FALSE(invalidMaskBlockAbove32Result);
 
       const auto invalidMaskBlockNegative = "127.0.0.0/-32";
-      const auto invalidMaskBlockNegativeResult = NoProxyRuleCidrBlock::parseCidrBlock(invalidMaskBlockNegative);
+      const auto invalidMaskBlockNegativeResult =
+          NoProxyRuleCidrBlock::parseCidrBlock(invalidMaskBlockNegative);
       REQUIRE_FALSE(invalidMaskBlockNegativeResult);
-
    }
 
    test_that("createNoProxyBuilder creates the correct rule")
    {
       auto rule = createNoProxyRule("*");
       REQUIRE(dynamic_cast<NoProxyRuleWildcard*>(rule.get()) != nullptr);
+      REQUIRE(rule->match("anything", "anyport"));
 
       rule = createNoProxyRule(".example.com");
       REQUIRE(dynamic_cast<NoProxyRuleDomain*>(rule.get()) != nullptr);
+      REQUIRE(rule->match("sub.example.com", "443"));
 
       rule = createNoProxyRule("example.com");
       REQUIRE(dynamic_cast<NoProxyRuleAddress*>(rule.get()) != nullptr);
+      REQUIRE(rule->match("example.com", "80"));
 
       rule = createNoProxyRule("example.com:443");
       REQUIRE(dynamic_cast<NoProxyRuleAddress*>(rule.get()) != nullptr);
+      REQUIRE(rule->match("example.com", "443"));
 
       rule = createNoProxyRule("192.0.2.0/24");
       REQUIRE(dynamic_cast<NoProxyRuleCidrBlock*>(rule.get()) != nullptr);
+      REQUIRE(rule->match("192.0.2.23", "443"));
 
       // junk will always return an address rule
       rule = createNoProxyRule("junk");
       REQUIRE(dynamic_cast<NoProxyRuleAddress*>(rule.get()) != nullptr);
    }
-
 }
 
 } // end namespace tests
