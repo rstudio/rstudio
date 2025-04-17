26fdbdef080de59e8c59023fb96f638042ef2d8a
Gary Ritchie <gary@posit.co>
2025-04-17 13:50:38 -0600

    pull panmirror main branch during development

--- dependencies/windows/install-panmirror/clone-quarto-repo.cmd
+++ dependencies/windows/install-panmirror/clone-quarto-repo.cmd
@@ -12,8 +12,8 @@ pushd ..\..\..\src\gwt\lib
 ::            "panmirror check for changes" command to use the equivalent.
 
 if not exist quarto (
-  REM git clone https://github.com/quarto-dev/quarto.git ..\..\..\src\gwt\lib\quarto
-  git clone --branch release/rstudio-mariposa-orchid https://github.com/quarto-dev/quarto.git ..\..\..\src\gwt\lib\quarto
+  git clone https://github.com/quarto-dev/quarto.git ..\..\..\src\gwt\lib\quarto
+  REM git clone --branch release/rstudio-sandyland-bluebonnet https://github.com/quarto-dev/quarto.git ..\..\..\src\gwt\lib\quarto
   pushd ..\..\..\src\gwt\lib\quarto
   git rev-parse HEAD
   popd
@@ -22,8 +22,8 @@ if not exist quarto (
   git fetch
   git reset --hard
   git clean -dfx
-  REM git checkout main
-  git checkout release/rstudio-mariposa-orchid
+  git checkout main
+  REM git checkout release/rstudio-sandyland-bluebonnet
   git pull
   git rev-parse HEAD
   popd
