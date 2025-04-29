fede5c6daa7d29f0b277506fa2162d4937085cc4
Kevin Ushey <kevinushey@gmail.com>
2025-04-29 10:03:23 -0700

    add support for clean

--- docker/win-docker-build-image.cmd
+++ docker/win-docker-build-image.cmd
@@ -5,20 +5,28 @@ setlocal EnableDelayedExpansion
 :: Script to build docker image for building RStudio Desktop for Windows
 ::
 
+for %%A in (%*) do (
+    if /I "%%A" == "clean" (
+        set "NO_CACHE=--no-cache"
+    )
+)
+
 set IMAGE=windows
 
 REM move to the repo root (script's parent directory)
 cd %~dp0\..
 call dependencies\tools\rstudio-tools.cmd
 
-REM determine repo name
+REM Determine the repository name.
+REM Used to differentiate 'rstudio' from 'rstudio-pro' when tagging the image.
 for /F "delims=" %%i in ("%CD%") do set REPO=%%~nxi
 
-REM rebuild the image if necessary
+REM Build the image.
 %RUN% with-echo docker build ^
-    --tag %REPO%:%IMAGE% ^
     --file docker\jenkins\Dockerfile.%IMAGE% ^
+    --tag %REPO%:%IMAGE% ^
     --memory 16GB ^
+    %NO_CACHE% ^
     --build-arg JENKINS_URL=development-build ^
     --build-arg GITHUB_LOGIN=%DOCKER_GITHUB_LOGIN% ^
     .
