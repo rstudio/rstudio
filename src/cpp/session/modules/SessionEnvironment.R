290d3e7fe8a3d3ac08cd7489597ae188c915c5ce
Kevin Ushey <kevin@posit.co>
2025-06-18 09:49:25 -0700

    use lobstr when computing object sizes (#16152)
    * use lobstr when computing object sizes
    
    * update NOTICE
    
    * update NEWS
    
    * more tweaks
    
    * tweak
    
    * patches to conform with base R implementation
    
    * tweaks
    
    * whitespace
    
    * guard against immediate bindings
    
    * ignore duplicates in string

--- src/cpp/session/modules/SessionEnvironment.R
+++ src/cpp/session/modules/SessionEnvironment.R
@@ -670,7 +670,7 @@
    .Call("rs_hasExternalPointer", object, nullPtr, PACKAGE = "(embedding)")
 })
 
-.rs.addFunction("describeObject", function(env, objName, computeSize = TRUE)
+.rs.addFunction("describeObject", function(env, objName)
 {
    obj <- get(objName, env)
 
@@ -711,7 +711,7 @@
 
       # some objects (e.g. ALTREP) have compact representations that are forced to materialize if
       # an attempt is made to compute their metrics exactly; avoid computing the size for these
-      size <- if (computeSize) .rs.estimatedObjectSize(obj) else 0
+      size <- .rs.estimatedObjectSize(obj)
       len <- length(obj)
    }
 
@@ -769,19 +769,19 @@
          desc <- .rs.valueDescription(obj)
 
          # expandable object--supply contents
-         if (is.list(obj) ||  is.data.frame(obj) || isS4(obj) ||
+         if (is.list(obj) || is.data.frame(obj) || isS4(obj) ||
              inherits(obj, c("data.table", "ore.frame", "cast_df", "xts", "DataFrame")))
          {
-            if (computeSize)
-            {
-               # normal object
-               contents <- .rs.valueContents(obj)
-            }
-            else
+            if (.rs.hasAltrep(obj))
             {
                # don't prefetch content for altreps
                val <- "NO_VALUE"
                contents_deferred <- TRUE
+            }
+            else
+            {
+               # normal object
+               contents <- .rs.valueContents(obj)
             }
          }
       }
@@ -1018,12 +1018,22 @@
    TRUE
 })
 
-.rs.addFunction("objectSize", function(x)
+.rs.addFunction("objectSize", function(object)
 {
-   if ("lobstr" %in% loadedNamespaces())
-      lobstr::obj_size(x)
-   else
-      utils::object.size(x)
+   envir <- parent.frame()
+   result <- .Call("rs_objectSize", object, envir, PACKAGE = "(embedding)")
+   class(result) <- "object_size"
+   result
+})
+
+.rs.addFunction("computeNodeSize", function()
+{
+   as.integer(utils::object.size(quote(expr = )))
+})
+
+.rs.addFunction("computeVectorSize", function()
+{
+   as.integer(utils::object.size(logical()))
 })
 
 .rs.addFunction("estimatedObjectSize", function(x)
@@ -1041,4 +1051,3 @@
    
    .rs.objectSize(x)
 })
-
