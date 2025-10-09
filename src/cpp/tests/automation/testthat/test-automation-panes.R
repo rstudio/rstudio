8d31a1f1d1d6f2a6432a9a73600ad23a139e1523
Gary Ritchie <gary@posit.co>
2025-10-09 14:47:55 -0600

    test initial pane layout at startup and source column creation/deletion

---
+++ src/cpp/tests/automation/testthat/test-automation-panes.R
@@ -0,0 +1,99 @@
+# tests related to zooming panes and columns with and without sidebar visible
+
+library(testthat)
+
+self <- remote <- .rs.automation.newRemote()
+withr::defer(.rs.automation.deleteRemote())
+
+
+
+.rs.test("Default quadrants exist and have expected visibility", {
+   # Check that rstudio_TabSet1_pane exists and is visible
+   tabSet1Exists <- remote$dom.elementExists("#rstudio_TabSet1_pane")
+   expect_true(tabSet1Exists, "rstudio_TabSet1_pane element should exist")
+   
+   # Check that rstudio_TabSet2_pane exists and is visible
+   tabSet2Exists <- remote$dom.elementExists("#rstudio_TabSet2_pane")
+   expect_true(tabSet2Exists, "rstudio_TabSet2_pane element should exist")
+   
+   # Check that rstudio_Console_pane exists and is visible
+   consoleExists <- remote$dom.elementExists("#rstudio_Console_pane")
+   expect_true(consoleExists, "rstudio_Console_pane element should exist")
+   
+   # Check that rstudio_Source_pane exists but is NOT visible
+   sourceExists <- remote$dom.elementExists("#rstudio_Source_pane")
+   expect_true(sourceExists, "rstudio_Source_pane element should exist")
+   
+   # Check that rstudio_Sidebar_pane does NOT exist
+   sidebarExists <- remote$dom.elementExists("#rstudio_Sidebar_pane")
+   expect_false(sidebarExists, "rstudio_Sidebar_pane element should NOT exist")
+   
+   # Wait for elements to be visible and check their visibility
+   tabSet1NodeId <- remote$dom.waitForElement("#rstudio_TabSet1_pane")
+   tabSet2NodeId <- remote$dom.waitForElement("#rstudio_TabSet2_pane")
+   consoleNodeId <- remote$dom.waitForElement("#rstudio_Console_pane")
+   
+   # Check that all elements are actually visible (not just present in DOM)
+   tabSet1Element <- remote$js.querySelector("#rstudio_TabSet1_pane")
+   tabSet2Element <- remote$js.querySelector("#rstudio_TabSet2_pane")
+   consoleElement <- remote$js.querySelector("#rstudio_Console_pane")
+   sourceElement <- remote$js.querySelector("#rstudio_Source_pane")
+   
+   expect_true(tabSet1Element$offsetWidth > 0, "rstudio_TabSet1_pane should be visible (width > 0)")
+   expect_true(tabSet1Element$offsetHeight > 0, "rstudio_TabSet1_pane should be visible (height > 0)")
+   expect_true(tabSet2Element$offsetWidth > 0, "rstudio_TabSet2_pane should be visible (width > 0)")
+   expect_true(tabSet2Element$offsetHeight > 0, "rstudio_TabSet2_pane should be visible (height > 0)")
+   expect_true(consoleElement$offsetWidth > 0, "rstudio_Console_pane should be visible (width > 0)")
+   expect_true(consoleElement$offsetHeight > 0, "rstudio_Console_pane should be visible (height > 0)")
+   
+   # Check that rstudio_Source_pane is NOT visible (width or height should be 0)
+   expect_true(sourceElement$offsetWidth == 0 || sourceElement$offsetHeight == 0, 
+               "rstudio_Source_pane should NOT be visible (width or height should be 0)")
+})
+
+.rs.test("Source columns can be created and closed", {
+   # Create a new source column
+   remote$commands.execute("newSourceColumn")
+   
+   # Wait for the new source column to be created
+   .rs.waitUntil("source column created", function() {
+      remote$dom.elementExists("#rstudio_Source1_pane")
+   })
+   
+   # Verify the first additional source column exists
+   source1Exists <- remote$dom.elementExists("#rstudio_Source1_pane")
+   expect_true(source1Exists, "rstudio_Source1_pane should exist after creating source column")
+   
+   # Create another source column
+   remote$commands.execute("newSourceColumn")
+   
+   # Wait for the second source column to be created
+   .rs.waitUntil("second source column created", function() {
+      remote$dom.elementExists("#rstudio_Source2_pane")
+   })
+   
+   # Verify the second additional source column exists
+   source2Exists <- remote$dom.elementExists("#rstudio_Source2_pane")
+   expect_true(source2Exists, "rstudio_Source2_pane should exist after creating second source column")
+   
+   # Verify both source columns are visible
+   source1Element <- remote$js.querySelector("#rstudio_Source1_pane")
+   source2Element <- remote$js.querySelector("#rstudio_Source2_pane")
+   
+   expect_true(source1Element$offsetWidth > 0, "rstudio_Source1_pane should be visible")
+   expect_true(source1Element$offsetHeight > 0, "rstudio_Source1_pane should be visible")
+   expect_true(source2Element$offsetWidth > 0, "rstudio_Source2_pane should be visible")
+   expect_true(source2Element$offsetHeight > 0, "rstudio_Source2_pane should be visible")
+
+   # Close all source docs (which will close the source columns)
+   remote$commands.execute("closeAllSourceDocs")
+
+   # Wait a tad (scientific term) for the source columns to be closed
+   Sys.sleep(0.5)
+   
+   # Verify that the source columns are no longer visible
+   source1Exists <- remote$dom.elementExists("#rstudio_Source1_pane")
+   source2Exists <- remote$dom.elementExists("#rstudio_Source2_pane")
+   expect_false(source1Exists, "rstudio_Source1_pane should NOT exist after closing all source docs")
+   expect_false(source2Exists, "rstudio_Source2_pane should NOT exist after closing all source docs")
+})
