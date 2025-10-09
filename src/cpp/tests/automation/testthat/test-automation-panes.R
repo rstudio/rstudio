f336d238ff778dde0b4af749906b13652d72e6d5
Gary Ritchie <gary@posit.co>
2025-10-09 14:57:49 -0600

    test all three possible source columns

--- src/cpp/tests/automation/testthat/test-automation-panes.R
+++ src/cpp/tests/automation/testthat/test-automation-panes.R
@@ -1,10 +1,9 @@
-# tests related to zooming panes and columns with and without sidebar visible
+# tests related to pane and column management
 
 library(testthat)
 
 self <- remote <- .rs.automation.newRemote()
 withr::defer(.rs.automation.deleteRemote())
-
 
 
 .rs.test("Default quadrants exist and have expected visibility", {
@@ -27,7 +26,19 @@ withr::defer(.rs.automation.deleteRemote())
    # Check that rstudio_Sidebar_pane does NOT exist
    sidebarExists <- remote$dom.elementExists("#rstudio_Sidebar_pane")
    expect_false(sidebarExists, "rstudio_Sidebar_pane element should NOT exist")
-   
+
+   # Check that rstudio_Source1_pane does NOT exist
+   source1Exists <- remote$dom.elementExists("#rstudio_Source1_pane")
+   expect_false(source1Exists, "rstudio_Source1_pane element should NOT exist")
+
+   # Check that rstudio_Source2_pane does NOT exist
+   source2Exists <- remote$dom.elementExists("#rstudio_Source2_pane")
+   expect_false(source2Exists, "rstudio_Source2_pane element should NOT exist")
+
+   # Check that rstudio_Source3_pane does NOT exist
+   source3Exists <- remote$dom.elementExists("#rstudio_Source3_pane")
+   expect_false(source3Exists, "rstudio_Source3_pane element should NOT exist")
+
    # Wait for elements to be visible and check their visibility
    tabSet1NodeId <- remote$dom.waitForElement("#rstudio_TabSet1_pane")
    tabSet2NodeId <- remote$dom.waitForElement("#rstudio_TabSet2_pane")
@@ -60,11 +71,11 @@ withr::defer(.rs.automation.deleteRemote())
       remote$dom.elementExists("#rstudio_Source1_pane")
    })
    
-   # Verify the first additional source column exists
+   # Verify the first source column exists
    source1Exists <- remote$dom.elementExists("#rstudio_Source1_pane")
    expect_true(source1Exists, "rstudio_Source1_pane should exist after creating source column")
    
-   # Create another source column
+   # Create second source column
    remote$commands.execute("newSourceColumn")
    
    # Wait for the second source column to be created
@@ -72,18 +83,33 @@ withr::defer(.rs.automation.deleteRemote())
       remote$dom.elementExists("#rstudio_Source2_pane")
    })
    
-   # Verify the second additional source column exists
+   # Verify the second source column exists
    source2Exists <- remote$dom.elementExists("#rstudio_Source2_pane")
    expect_true(source2Exists, "rstudio_Source2_pane should exist after creating second source column")
    
-   # Verify both source columns are visible
+   # Create third source column
+   remote$commands.execute("newSourceColumn")
+   
+   # Wait for the third source column to be created
+   .rs.waitUntil("third source column created", function() {
+      remote$dom.elementExists("#rstudio_Source3_pane")
+   })
+   
+   # Verify the third source column exists
+   source3Exists <- remote$dom.elementExists("#rstudio_Source3_pane")
+   expect_true(source3Exists, "rstudio_Source3_pane should exist after creating third source column")
+
+   # Verify all source columns are visible
    source1Element <- remote$js.querySelector("#rstudio_Source1_pane")
    source2Element <- remote$js.querySelector("#rstudio_Source2_pane")
+   source3Element <- remote$js.querySelector("#rstudio_Source3_pane")
    
    expect_true(source1Element$offsetWidth > 0, "rstudio_Source1_pane should be visible")
    expect_true(source1Element$offsetHeight > 0, "rstudio_Source1_pane should be visible")
    expect_true(source2Element$offsetWidth > 0, "rstudio_Source2_pane should be visible")
    expect_true(source2Element$offsetHeight > 0, "rstudio_Source2_pane should be visible")
+   expect_true(source3Element$offsetWidth > 0, "rstudio_Source3_pane should be visible")
+   expect_true(source3Element$offsetHeight > 0, "rstudio_Source3_pane should be visible")
 
    # Close all source docs (which will close the source columns)
    remote$commands.execute("closeAllSourceDocs")
@@ -94,6 +120,8 @@ withr::defer(.rs.automation.deleteRemote())
    # Verify that the source columns are no longer visible
    source1Exists <- remote$dom.elementExists("#rstudio_Source1_pane")
    source2Exists <- remote$dom.elementExists("#rstudio_Source2_pane")
+   source3Exists <- remote$dom.elementExists("#rstudio_Source3_pane")
    expect_false(source1Exists, "rstudio_Source1_pane should NOT exist after closing all source docs")
    expect_false(source2Exists, "rstudio_Source2_pane should NOT exist after closing all source docs")
+   expect_false(source3Exists, "rstudio_Source3_pane should NOT exist after closing all source docs")
 })
