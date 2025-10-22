5b09e7ce9d31efd4b08989b300fb3b55354f6b92
Kevin Ushey <kevinushey@gmail.com>
2025-10-22 11:32:16 -0700

    add automation test

--- src/cpp/tests/automation/testthat/test-automation-files.R
+++ src/cpp/tests/automation/testthat/test-automation-files.R
@@ -43,3 +43,41 @@ withr::defer(.rs.automation.deleteRemote())
    })
    
 })
+
+.rs.test("the virtualized open file web dialog works with <500 files", {
+   
+   skip_if(.rs.isDesktop())
+   
+   # work in temporary directory
+   remote$console.executeExpr({
+      dir <- tempfile("rstudio-automation-")
+      dir.create(dir)
+      setwd(dir)
+   })
+   
+   # create a bunch of files
+   remote$console.executeExpr({
+      files <- sprintf("%03i.R", 0:450)
+      invisible(file.create(files))
+   })
+   
+   # show the Open File dialog
+   remote$commands.execute(.rs.appCommands$openSourceDoc)
+   
+   # wait until the modal dialog is shown
+   remote$dom.waitForElement(".rstudio_modal_dialog")
+   Sys.sleep(1)
+   
+   # try typing some keys to select a file
+   remote$keyboard.sendKeys("4", "5", "5", "<Enter>")
+   
+   # check that we opened the file we tried to
+   tabPanelEl <- remote$js.querySelector("div[aria-label=\"Documents\"]")
+   contents <- tabPanelEl$innerText
+   expect_equal(.rs.trimWhitespace(contents), "450.R")
+   
+   # clean up
+   remote$console.executeExpr({
+      unlink(files)
+   })
+})
