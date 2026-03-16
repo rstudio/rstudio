a86b432bd0a58553d6112ef615306da73f11d8e9
Gary Ritchie <gary@posit.co>
2026-03-16 15:57:26 -0600

    Fix chat restarting when Global Options dismissed without changes
    When the user opens Tools > Global Options and clicks OK without
    changing anything, the chat provider preference is re-written with
    the same value. Because the user layer may not contain the key
    (the backend strips values matching the default), PrefValue treats
    this as a change and fires a ValueChangeEvent, causing ChatPresenter
    to restart the chat backend unnecessarily.
    
    Add a guard in onChatProviderChanged() that tracks the last effective
    chat provider and skips re-initialization when the effective provider
    has not actually changed. This handles spurious events from any
    source (preference dialog, satellite sync, project options).

--- src/gwt/src/org/rstudio/studio/client/workbench/views/chat/ChatPresenter.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/chat/ChatPresenter.java
@@ -15,6 +15,7 @@ package org.rstudio.studio.client.workbench.views.chat;
 import org.rstudio.core.client.Debug;
 import org.rstudio.core.client.Point;
 import org.rstudio.core.client.Size;
+import org.rstudio.core.client.StringUtil;
 import org.rstudio.core.client.command.CommandBinder;
 import org.rstudio.core.client.command.Handler;
 import org.rstudio.core.client.dom.WindowEx;
@@ -156,6 +157,7 @@ public class ChatPresenter extends BasePresenter
       paiUtil_ = paiUtil;
       prefs_ = prefs;
       installManager_ = new PositAiInstallManager();
+      lastEffectiveChatProvider_ = paiUtil_.getConfiguredChatProvider();
       satelliteManager_ = satelliteManager;
       paneManager_ = paneManager;
 
@@ -645,6 +647,11 @@ public class ChatPresenter extends BasePresenter
     */
    private void onChatProviderChanged()
    {
+      String currentProvider = paiUtil_.getConfiguredChatProvider();
+      if (StringUtil.equals(currentProvider, lastEffectiveChatProvider_))
+         return;
+      lastEffectiveChatProvider_ = currentProvider;
+
       if (paiUtil_.isChatProviderPosit())
       {
          // Prevent concurrent initialization
@@ -1100,6 +1107,9 @@ public class ChatPresenter extends BasePresenter
 
    // Guard against concurrent initialization (multiple polling loops)
    private boolean initializing_ = false;
+
+   // Last effective chat provider, used to suppress spurious change events
+   private String lastEffectiveChatProvider_;
 
    // Satellite pop-out state
    private boolean windowsClosing_ = false;
