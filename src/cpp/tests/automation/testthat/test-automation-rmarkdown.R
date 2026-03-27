rstudio/rstudio

commit 9edcff42113bac641fc94ae04b39256d17dc67a3

provide knit/render on save option

src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/TextEditingTarget.java
@@ -370,7 +370,7 @@ public class TextEditingTarget implements
             setRMarkdownBehaviorEnabled(newFileType_.isRmd());
 
             events_.fireEvent(new FileTypeChangedEvent());
-            if (!fileType_.canSourceOnSave() && docUpdateSentinel_.sourceOnSave())
+            if (!isSourceOnSaveEnabled() && docUpdateSentinel_.sourceOnSave())
             {
                view_.getSourceOnSave().setValue(false, true);
             }
@@ -3397,8 +3397,9 @@ public class TextEditingTarget implements
          ((getPath() == null) && docDisplay_.getCode().isEmpty()) ||
 
          // source on save is active
-         (fileType_.canSourceOnSave() && docUpdateSentinel_.sourceOnSave());
+         (isSourceOnSaveEnabled() && docUpdateSentinel_.sourceOnSave());
    }
+
 
    @Override
    public void forceSaveCommandActive()
@@ -4083,6 +4084,12 @@ public class TextEditingTarget implements
               }
             }
       );
+   }
+   
+   
+   private boolean isSourceOnSaveEnabled()
+   {
+      return fileType_.canSourceOnSave() || StringUtil.equals(extendedType_, SourceDocument.XT_QUARTO_DOCUMENT);
    }
 
    private boolean checkSelectionAndAlert(String refactoringName,
@@ -7459,7 +7466,7 @@ public class TextEditingTarget implements
                                              docDisplay_.getCursorPosition()));
 
             // check for source on save
-            if (fileType_.canSourceOnSave() && docUpdateSentinel_.sourceOnSave())
+            if (isSourceOnSaveEnabled() && docUpdateSentinel_.sourceOnSave())
             {
                if (fileType_.isRd())
                {
@@ -7478,6 +7485,11 @@ public class TextEditingTarget implements
                else if (fileType_.canPreviewFromR())
                {
                   previewFromR();
+               }
+               else if (extendedType_ == SourceDocument.XT_RMARKDOWN_DOCUMENT ||
+                        extendedType_ == SourceDocument.XT_QUARTO_DOCUMENT)
+               {
+                  renderRmd();
                }
                else
                {

src/gwt/src/org/rstudio/studio/client/common/filetypes/FileTypeRegistry.java
@@ -101,7 +101,7 @@ public class FileTypeRegistry
 
    public static final RWebContentFileType RMARKDOWN =
          new RWebContentFileType("r_markdown", "R Markdown", EditorLanguage.LANG_RMARKDOWN,
-                              ".Rmd", new ImageResource2x(ICONS.iconRmarkdown2x()), true);
+                              ".Rmd", new ImageResource2x(ICONS.iconRmarkdown2x()), true, true, true);
 
    public static final RWebContentFileType RNOTEBOOK =
          new RWebContentFileType("r_notebook", "R Notebook", EditorLanguage.LANG_RMARKDOWN,

src/gwt/src/org/rstudio/studio/client/common/filetypes/QuartoFileType.java
@@ -28,6 +28,8 @@ public class QuartoFileType extends RWebContentFileType
             EditorLanguage.LANG_RMARKDOWN,
             ".qmd", 
             new ImageResource2x(FileIconResources.INSTANCE.iconQuarto2x()), 
+            true,
+            true,
             true);
    }
 }

src/gwt/src/org/rstudio/studio/client/common/filetypes/RWebContentFileType.java
@@ -36,12 +36,24 @@ public class RWebContentFileType extends TextFileType
    }
    
    RWebContentFileType(String id,
+         String label,
+         EditorLanguage editorLanguage,
+         String defaultExtension,
+         ImageResource icon,
+         boolean isMarkdown,
+         boolean previewIsKnit)
+   {
+      this(id, label, editorLanguage, defaultExtension, icon, isMarkdown, previewIsKnit, false);
+   }
+   
+   RWebContentFileType(String id,
                        String label,
                        EditorLanguage editorLanguage,
                        String defaultExtension,
                        ImageResource icon,
                        boolean isMarkdown,
-                       boolean previewIsKnit)
+                       boolean previewIsKnit,
+                       boolean sourceOnSave)
    {
       super(id, 
             label, 
@@ -49,7 +61,7 @@ public class RWebContentFileType extends TextFileType
             defaultExtension,
             icon,
             true,    // word-wrap
-            false, 
+            sourceOnSave, 
             true, 
             true, 
             false,

src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/TextEditingTargetWidget.java
@@ -823,7 +823,9 @@ public class TextEditingTargetWidget
       boolean canCompileNotebook = fileType.canCompileNotebook();
       boolean canSource = fileType.canSource();
       boolean canSourceWithEcho = fileType.canSourceWithEcho();
-      boolean canSourceOnSave = fileType.canSourceOnSave() &&
+      boolean isQuarto = extendedType_ != null && 
+            extendedType_.equals(SourceDocument.XT_QUARTO_DOCUMENT);
+      boolean canSourceOnSave = (fileType.canSourceOnSave() || isQuarto) &&
             !userPrefs_.autoSaveEnabled();
       if (canSourceOnSave && fileType.isJS())
          canSourceOnSave = (extendedType_.equals(SourceDocument.XT_JS_PREVIEWABLE));
@@ -834,8 +836,6 @@ public class TextEditingTargetWidget
       boolean isPlainMarkdown = fileType.isPlainMarkdown();
       boolean isCpp = fileType.isCpp();
       boolean isScript = fileType.isScript();
-      boolean isQuarto = extendedType_ != null && 
-             extendedType_.equals(SourceDocument.XT_QUARTO_DOCUMENT);
       boolean isRMarkdown2 = extendedType_ != null && 
                              (extendedType_.startsWith(SourceDocument.XT_RMARKDOWN_PREFIX) ||
                               isQuarto);
@@ -865,10 +865,8 @@ public class TextEditingTargetWidget
 
       sourceOnSave_.setVisible(canSourceOnSave);
       srcOnSaveLabel_.setVisible(canSourceOnSave);
-      if (fileType.isRd() || fileType.isJS() || canPreviewFromR || fileType.isSql())
-         srcOnSaveLabel_.setText(fileType.getPreviewButtonText() + " on Save");
-      else
-         srcOnSaveLabel_.setText("Source on Save");
+      String action = getSourceOnSaveAction();
+      srcOnSaveLabel_.setText(action + " on Save");
       codeTransform_.setVisible(
             (canExecuteCode && !isScript && !fileType.canAuthorContent()) ||
             fileType.isC() || fileType.isStan());
@@ -1046,6 +1044,8 @@ public class TextEditingTargetWidget
       if (width == 0)
          return;
 
+      
+      
       texToolbarButton_.setText(width >= 520, "Format");
       runButton_.setText(((width >= 480) && !isShinyFile()), "Run");
       compilePdfButton_.setText(width >= 450, "Compile PDF");
@@ -1053,19 +1053,34 @@ public class TextEditingTargetWidget
       knitDocumentButton_.setText(width >= 450, knitCommandText_);
       quartoRenderButton_.setText(width >= 450, quartoCommandText_);
 
-      if (editor_.getFileType().isRd() || editor_.getFileType().isJS() ||
-          editor_.getFileType().isSql() || editor_.getFileType().canPreviewFromR())
+      String action = getSourceOnSaveAction();
+      srcOnSaveLabel_.setText(width < 450 ? action : action + " on Save");
+      sourceButton_.setText(width >= 400, sourceCommandText_);
+      
+      goToNextButton_.setVisible(commands_.goToNextChunk().isVisible() && width >= 650);
+      goToPrevButton_.setVisible(commands_.goToPrevChunk().isVisible() && width >= 650);
+      toolbar_.invalidateSeparators();
+   }
+   
+   private String getSourceOnSaveAction()
+   {
+      TextFileType fileType = editor_.getFileType();
+      if (fileType.isRd() || fileType.isJS() || fileType.canPreviewFromR() || fileType.isSql())
       {
-         String preview = editor_.getFileType().getPreviewButtonText();
-         srcOnSaveLabel_.setText(width < 450 ? preview : preview + " on Save");
+         return fileType.getPreviewButtonText();
+      }
+      else if (extendedType_ != null && 
+            (extendedType_.startsWith(SourceDocument.XT_RMARKDOWN_PREFIX) || extendedType_.equals(SourceDocument.XT_QUARTO_DOCUMENT)) )
+      {
+         return  extendedType_.equals(SourceDocument.XT_QUARTO_DOCUMENT) ?
+            "Render" : "Knit";
       }
       else
       {
-         srcOnSaveLabel_.setText(width < 450 ? "Source" : "Source on Save");
+         return "Source";
       }
-
-      sourceButton_.setText(width >= 400, sourceCommandText_);
    }
+   
 
 
    private void showWarningImpl(final Command command)

commit 81f9493131e0a6799d4b6f416ab67f9cde453048

fix notebook "preview on save" calling source() instead of rendering
The source-on-save handler in TextEditingTarget didn't match the
XT_RMARKDOWN_NOTEBOOK extended type, causing notebooks to fall through
to executeRSourceCommand() which called source() on the .Rmd file.
This regressed in 9edcff42 (2021-06-23).

Also: NotebookHtmlRenderer now respects the "Preview on Save" checkbox,
and createNotebookFromCache short-circuits when the .nb.html is already
up to date, avoiding an unnecessary rmarkdown::render / pandoc
invocation that blocks the R session.

Addresses #15925, #17293.

src/cpp/session/modules/rmarkdown/NotebookCache.cpp
@@ -548,6 +548,21 @@ Error createNotebookFromCache(const json::JsonRpcRequest& request,
       return error;
    }
    
+   // if the .nb.html output already exists and is at least as new as the
+   // chunk definitions cache, there is nothing to do -- skip the expensive
+   // rmarkdown::render / pandoc invocation
+   FilePath rmdFile = module_context::resolveAliasedPath(rmdPath);
+   FilePath outputFile = module_context::resolveAliasedPath(outputPath);
+   FilePath chunkDefsFile = chunkDefinitionsPath(rmdFile, kSavedCtx);
+   if (outputFile.exists() && chunkDefsFile.exists() &&
+       outputFile.getLastWriteTime() >= chunkDefsFile.getLastWriteTime())
+   {
+      json::Object result;
+      result["succeeded"] = true;
+      pResponse->setResult(result);
+      return Success();
+   }
+
    SEXP resultSEXP = R_NilValue;
    r::sexp::Protect protect;
    r::exec::RFunction createNotebook(".rs.createNotebookFromCache");
@@ -563,9 +578,6 @@ Error createNotebookFromCache(const json::JsonRpcRequest& request,
    // bump the write time on our local chunk definition file so that it matches
    // the notebook file; this prevents us from thinking that the .nb.html file
    // we just wrote is ahead of the local cache.
-   FilePath outputFile = module_context::resolveAliasedPath(outputPath);
-   FilePath chunkDefsFile = chunkDefinitionsPath(
-         module_context::resolveAliasedPath(rmdPath), kSavedCtx);
    if (chunkDefsFile.exists() && 
        chunkDefsFile.getLastWriteTime() < outputFile.getLastWriteTime())
       chunkDefsFile.setLastWriteTime(outputFile.getLastWriteTime());

src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/TextEditingTarget.java
@@ -8598,9 +8598,14 @@ public class TextEditingTarget implements
                   previewFromR();
                }
                else if (extendedType_ == SourceDocument.XT_RMARKDOWN_DOCUMENT ||
+                        extendedType_ == SourceDocument.XT_RMARKDOWN_NOTEBOOK ||
                         extendedType_ == SourceDocument.XT_QUARTO_DOCUMENT)
                {
-                  renderRmd();
+                  // notebooks handle on-save rendering separately via
+                  // NotebookHtmlRenderer (which assembles the .nb.html from
+                  // the chunk cache rather than doing a full knit)
+                  if (!isRmdNotebook())
+                     renderRmd();
                }
                else
                {

src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/rmd/NotebookHtmlRenderer.java
@@ -129,6 +129,10 @@ public class NotebookHtmlRenderer
       if (!target_.hasRmdNotebook())
          return;
 
+      // bail if "Preview on Save" is not checked
+      if (!sentinel_.sourceOnSave())
+         return;
+
       final String rmdPath = sentinel_.getPath();
 
       // bail if unsaved doc (no point in generating notebooks for those)

src/cpp/tests/automation/testthat/test-automation-rmarkdown.R
@@ -670,5 +670,39 @@ withr::defer(.rs.automation.deleteRemote())
       Sys.sleep(1)
       expect_error(remote$dom.querySelector(".pagedtable"))
    })
-   
+
+})
+
+# https://github.com/rstudio/rstudio/issues/15925
+.rs.test("saving a notebook with preview on save does not call source()", {
+
+   contents <- .rs.heredoc('
+      ---
+      title: Notebook Save Test
+      output: html_notebook
+      ---
+
+      This is prose that would cause a source() error.
+
+      ```{r}
+      1 + 1
+      ```
+   ')
+
+   remote$editor.executeWithContents(".Rmd", contents, function(editor) {
+
+      # enable "Preview on Save"
+      remote$dom.setChecked("#rstudio_cb_source_on_save", checked = TRUE)
+
+      # clear the console, then save the document
+      remote$console.clear()
+      remote$commands.execute("saveSourceDoc")
+      Sys.sleep(2)
+
+      # verify no source() error appeared in the console
+      output <- remote$console.getOutput()
+      expect_false(any(grepl("unexpected symbol", output, fixed = TRUE)))
+      expect_false(any(grepl("Error in source", output, fixed = TRUE)))
+   })
+
 })