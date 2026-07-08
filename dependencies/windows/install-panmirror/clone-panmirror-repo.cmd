52dd606b2c8aeb1129124b75355a63ec8e72530a
Gary Ritchie <gary@posit.co>
2026-07-08 16:49:12 -0600

    Point commented panmirror pin at yellow-yarrow branch
    Update the now-inactive release-branch placeholder lines from
    release/rstudio-pacific-dogwood to release/rstudio-yellow-yarrow so
    they are ready to uncomment when 2026.08 pins panmirror near the end
    of development (tracked by #18163). Only the commented lines change;
    the active lines still pull from main.
    
    Addresses #18162

--- dependencies/windows/install-panmirror/clone-panmirror-repo.cmd
+++ dependencies/windows/install-panmirror/clone-panmirror-repo.cmd
@@ -13,7 +13,7 @@ pushd ..\..\..\src\gwt\lib
 
 if not exist quarto (
   git clone https://github.com/quarto-dev/quarto.git ..\..\..\src\gwt\lib\quarto
-  REM git clone --branch release/rstudio-pacific-dogwood https://github.com/quarto-dev/quarto.git ..\..\..\src\gwt\lib\quarto
+  REM git clone --branch release/rstudio-yellow-yarrow https://github.com/quarto-dev/quarto.git ..\..\..\src\gwt\lib\quarto
   pushd ..\..\..\src\gwt\lib\quarto
   git rev-parse HEAD
   popd
@@ -23,7 +23,7 @@ if not exist quarto (
   git reset --hard
   git clean -dfx
   git checkout main
-  REM git checkout release/rstudio-pacific-dogwood
+  REM git checkout release/rstudio-yellow-yarrow
   git pull
   git rev-parse HEAD
   popd
