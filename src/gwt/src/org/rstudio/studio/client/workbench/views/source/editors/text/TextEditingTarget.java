80aa931d845f91d0ed855c838c65a2bfaba615d2
Kevin Ushey <kevin@posit.co>
2026-09-12 11:13:03 -0700

    add select current statement command (#18773)
    * add select current statement command
    
    Code > Select Current Statement (Ctrl+Alt+Shift+S) selects the whole R
    statement containing the cursor, using the same range computation as
    Execute Current Statement (minus the comment lines that picks up above
    the statement), so a multi-line call can be grabbed from anywhere in it.
    
    Addresses #16102
    
    * preserve execution history when selecting statements

--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/TextEditingTarget.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/TextEditingTarget.java
@@ -6258,6 +6258,18 @@ public class TextEditingTarget implements
    }
 
    @Handler
+   void onSelectCurrentStatement()
+   {
+      withVisualModeSelection(() ->
+      {
+         DocDisplay display = activeDisplay();
+         Range range = EditingTargetCodeExecution.getCurrentStatementRange(display);
+         if (range != null)
+            display.setSelectionRange(range);
+      });
+   }
+
+   @Handler
    void onExecuteCurrentParagraph()
    {
       withVisualModeSelection(() ->
